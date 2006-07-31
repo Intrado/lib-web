4c10f2e435253379b7771e524ccc57fcfb08247e
Ben Hencke <bhencke@schoolmessenger.com>
2006-07-31 17:39:09 +0000

    change reserved name "trigger" to "triggertype"

--- obj/Schedule.obj.php
+++ obj/Schedule.obj.php
@@ -3,7 +3,7 @@
 class Schedule extends DBMappedObject {
 
 	var $userid;
-	var $trigger;
+	var $triggertype;
 	var $type;
 	var $time;
 	var	$nextrun;
@@ -11,26 +11,26 @@ class Schedule extends DBMappedObject {
 	function Schedule ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "schedule";
-		$this->_fieldlist = array("userid", "trigger", "type", "time", "nextrun");
+		$this->_fieldlist = array("userid", "triggertype", "type", "time", "nextrun");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
-	
+
 	function calcNextRun () {
 		$enableddows = QuickQueryList("select dow from scheduleday where scheduleid=$this->id");
-		
+
 		$nextrun = Date("Y-m-d") ." " . $this->time;
-		
-		for ($x = 0; $x < 8 ; $x++) {		
+
+		for ($x = 0; $x < 8 ; $x++) {
 			$query = "select convert('$nextrun', datetime) > now(), dayofweek('$nextrun')";
-			
+
 			list($islater, $nextrundow) = QuickQueryRow($query);
 			if ($islater && in_array($nextrundow, $enableddows))
 				return $nextrun;
-		
+
 			$nextrun = QuickQuery("select date_add('$nextrun',interval 1 day)");
 		}
-		
+
 		return NULL;
 	}
 }
