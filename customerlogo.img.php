6a96ea0c1a7b4352762e84cc6d5b663fbcf7d848
Ben Hencke <bhencke@schoolmessenger.com>
2006-07-31 17:40:27 +0000

    add support for external content servers

--- customerlogo.img.php
+++ customerlogo.img.php
@@ -1,16 +1,24 @@
 <?
 
 include_once("inc/common.inc.php");
+include_once("inc/content.inc.php");
 include_once("obj/Customer.obj.php");
 include_once("obj/Content.obj.php");
 
 $customer = new Customer($USER->customerid);
 if (isset($customer->logocontentid) && 0) {
-	$c = new Content($customer->logocontentid);
-	header('Content-type: ' . $c->contenttype);
-	$data = base64_decode($c->data);
-	$size = strlen($data);
-	echo $data;
+	if ($IS_COMMSUITE) {
+
+		$c = new Content($customer->logocontentid);
+		header('Content-type: ' . $c->contenttype);
+		$data = base64_decode($c->data);
+		$size = strlen($data);
+		echo $data;
+	} else {
+		list($contenttype,$data) = contentGet($customer->logocontentid);
+		header('Content-type: ' . $contenttype);
+		echo $data;
+	}
 } else {
 	header('Content-type: image/GIF');
 	readfile("img/spacer.gif");
