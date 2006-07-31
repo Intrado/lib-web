c81d802c6872b2de9cb072cab58a59b3c6a84161
Ben Hencke <bhencke@schoolmessenger.com>
2006-07-31 17:38:49 +0000

    removed unused code

--- obj/Content.obj.php
+++ obj/Content.obj.php
@@ -11,14 +11,6 @@ class Content extends DBMappedObject {
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
-
-	function newFromUpload($path) {
-		$content = new Content();
-		//TODO use mime magic to detect the mime type
-		$content->contenttype = "audio/wav";
-		$content->data = base64_encode(file_get_contents($path));
-		return $content;
-	}
 }
 
 ?>
