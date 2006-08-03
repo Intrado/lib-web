1242c854aa4893b02d332a9eb11c4d41d43f6a69
Ben Hencke <bhencke@schoolmessenger.com>
2006-08-03 13:52:35 +0000

    remove unsed stuff

--- audio.wav.php
+++ audio.wav.php
@@ -1,22 +1,6 @@
 <?
-/* Use this if audio doesn't play, sometimes external apps start new sessions
-*/
-
-/*
-require_once("inc/db.inc.php");
-require_once("inc/DBMappedObject.php");
-require_once("inc/DBRelationMap.php");
-
-require_once("inc/utils.inc.php");
-require_once("obj/User.obj.php");
-require_once("obj/Access.obj.php");
-require_once("obj/Permission.obj.php");
-require_once("obj/Rule.obj.php"); //for search and sec profile rules
-*/
 
 include_once("inc/common.inc.php");
-
-
 include_once('inc/securityhelper.inc.php');
 include_once('inc/content.inc.php');
 include_once('obj/AudioFile.obj.php');
