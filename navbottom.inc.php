181c599d535e1dd1e13c671b95d064bf06b86ed6
Ben Hencke <bhencke@schoolmessenger.com>
2006-09-27 21:57:49 +0000

    add TOS/Privacy, page time debug

--- navbottom.inc.php
+++ navbottom.inc.php
@@ -9,9 +9,13 @@
 		</div>
 	</div>
 </div>
+<div id="termsinfo">Use of this system is subject to the <a href="privacy.html" target="_blank">Privacy Policy</a> and <a href="terms.html" target="_blank">Terms of Service</a>
+<br>
+&copy; 2006 Reliance Communications, Inc.
 
+</div>
 <?
-print "<div id='logininfo' class='noprint' >Logged in as $USER->firstname $USER->lastname ($USER->login) - Current system time is " . date("F jS, Y h:i a (e)") . "</div>";
+print "<div id='logininfo' class='noprint' >Logged in as $USER->firstname $USER->lastname ($USER->login) <br> Current system time is " . date("F jS, Y h:i a (e)") . "</div>";
 
 if(isset($ERRORS) && is_array($ERRORS)) {
 	foreach($ERRORS as $key => $value) {
@@ -21,5 +25,6 @@ if(isset($ERRORS) && is_array($ERRORS)) {
 }
 ?>
 <img id="state" src="img/spacer.gif" width="1" height="1">
+<? if ($_GET['debug']) printf("<!-- %0.2f -->", microtime(true) - $PAGETIME) ?>
 </body>
 </html>
