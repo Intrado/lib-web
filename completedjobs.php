7ad51afb3b04098884bcb47534fcab71bf345f7b
Ben Hencke <bhencke@schoolmessenger.com>
2006-09-27 21:50:30 +0000

    optimized/cleaned up page

--- completedjobs.php
+++ completedjobs.php
@@ -23,8 +23,8 @@ if (!$USER->authorize('viewsystemcompleted')) {
 ////////////////////////////////////////////////////////////////////////////////
 // Data Handling
 ////////////////////////////////////////////////////////////////////////////////
-
-
+$start = 0 + $_GET['pagestart'];
+$limit = 100;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Display
@@ -37,114 +37,94 @@ include_once("nav.inc.php");
 
 startWindow('Completed Notification Jobs ' . help('System_CompletedJobs', NULL, 'blue'), 'padding: 3px;');
 
-$start = 0 + $_GET['pagestart'];
-$limit = 100;
 
-// jobworkitem columns are: id jobid type priority personid messageid status resultdata assignedto
-
-$query = "select SQL_CALC_FOUND_ROWS u.login, j.name, ADDTIME(j.startdate, j.starttime), j.status,
-				sum(wi.type='phone') as total_phone,
-				sum(wi.type='email') as total_email,
-				sum(wi.type='print') as total_print,
-				j.type LIKE '%phone%' AS has_phone,
-				j.type LIKE '%email%' AS has_email,
-				j.type LIKE '%print%' AS has_print,
-				100 * sum(wi.status='success' and wi.type='phone') / sum( (wi.status='success' or wi.status='fail' or (jt.numattempts > 0 and wi.status='queued' or wi.status='scheduled')) and wi.type='phone' ) as percent_phone,
-				100 * sum(wi.status='success' and wi.type='email') / sum( (wi.status='success' or wi.status='fail' or (jt.numattempts > 0 and wi.status='queued' or wi.status='scheduled')) and wi.type='email' ) as percent_email,
-				100 * sum(wi.status='success' and wi.type='print') / sum( (wi.status='success' or wi.status='fail' or (jt.numattempts > 0 and wi.status='queued' or wi.status='scheduled')) and wi.type='print' ) as percent_print,
-            ADDTIME(j.enddate, j.endtime), j.finishdate, j.id, j.status, j.deleted, jobowner.login
-
-            from job j
-
-            left join user jobowner
-            	on (jobowner.id = j.userid)
-            left join jobworkitem wi on
-            	(j.id = wi.jobid)
-        	left join jobtask jt on
-				(jt.jobworkitemid=wi.id),
-			user u
-
-            where u.customerid = $USER->customerid and j.userid = u.id and j.deleted = 0 and
+//get this page's worth of jobs
+$query = "select SQL_CALC_FOUND_ROWS j.id from job j, user u
+where j.userid=u.id and u.customerid = $USER->customerid and j.deleted = 0 and
             	(j.status = 'complete' or j.status = 'cancelled')
+order by finishdate desc limit $start, $limit
+";
 
-            group by j.id order by j.finishdate desc limit $start, $limit";
-$result = Query($query);
-
-while ($row = DBGetRow($result)) {
-	$data[] = $row;
-}
-
-
-function fmt_total ($row, $index) {
-	$data = array();
-
-	if ($row[$index+3])
-		$data[] = "Phone:&nbsp;" . $row[$index];
-	if ($row[$index+4])
-		$data[] = "Email:&nbsp;" . $row[$index+1];
-	if ($row[$index+5])
-		$data[] = "Print:&nbsp;" . $row[$index+2];
-
-	return implode("<br>",$data);
-}
-
-function fmt_complete_percent ($row, $index) {
-	$data = array();
-
-	if ($row[$index-3])
-		$data[] = "Phone:&nbsp;" . (isset($row[$index]) ? $row[$index] : "0.00") . "%";
-	if ($row[$index-2])
-		$data[] = "Email:&nbsp;" . (isset($row[$index+1]) ? $row[$index+1] : "0.00") . "%";
-	if ($row[$index-1])
-		$data[] = "Print:&nbsp;" . (isset($row[$index+2] ) ? $row[$index+2]  : "0.00"). "%";
-
-	return implode("<br>",$data);
-}
-
-$titles = array(
-				"0" => 'Submitted by',
-				"1" => 'Job Name',
-				"2" => 'Start Date',
-				"3" => 'Status',
-				"4" => 'Total',
-				"10" => 'Success Rate',
-				"13" => 'End Date',
-				"15" => 'Actions');
-$formatters = array(
-				"2" => 'fmt_date',
-				"3" => 'fmt_status_index',
-				"4" => 'fmt_total',
-				"10" => 'fmt_complete_percent',
-				"13" => 'fmt_job_date',
-				"15" => 'fmt_jobs_actions_customer');
-
+$jobids = QuickQueryList($query);
 $query = "select FOUND_ROWS()";
 $total = QuickQuery($query);
+
+if ($total == 0) {
+	$jobs = array();
+} else {
+
+	$jobidlist = implode(",",$jobids);
+
+	//just get the stats data for this page
+	$query = "select wi.jobid,
+					wi.type,
+					count(*) as total,
+					100 * sum(wi.status='success') / (sum(wi.status='success' or wi.status='fail' or (jt.numattempts>0 and wi.status = 'queued' or wi.status='scheduled')) +0.00) as success_rate
+	from jobworkitem wi left join jobtask jt on (jt.jobworkitemid=wi.id)
+	where wi.jobid in ($jobidlist)
+	group by wi.jobid, wi.type
+	";
+
+	$jobstats = array();
+	$result = Query($query);
+	while ($row = DBGetRow($result)) {
+		$jobstats[$row[0]][$row[1]] = array ("total" => $row[2], "rate" => $row[3]);
+	}
+
+	$jobs = DBFindMany("Job", "from job where id in ($jobidlist) order by finishdate desc");
+}
+
+function fmt_job_owner ($obj, $name) {
+	static $users = array();
+	if (isset($users[$obj->userid])) {
+		return $users[$obj->userid];
+	} else {
+		$user = new User($obj->userid);
+		$users[$obj->userid] = $user->login;
+		return $user->login;
+	}
+}
+
+function fmt_total ($obj, $name) {
+	global $jobstats;
+
+	$total = "";
+	foreach (explode(",",$obj->type) as $type)
+		$total .= ucfirst($type) . ": " . $jobstats[$obj->id][$type]['total'] . "<br>";
+	return $total;
+}
+function fmt_rate ($obj, $name) {
+	global $jobstats;
+
+	$rate = "";
+	foreach (explode(",",$obj->type) as $type)
+		$rate .= ucfirst($type) . ": " . sprintf("%0.2f",$jobstats[$obj->id][$type]['rate']) . "%<br>";
+	return $rate;
+}
+
+$titles = array ("Owner" => "Submitted by",
+				"name" => 'Job Name',
+				"status" => 'Status',
+				"Total" => 'Total',
+				"Rate" => 'Success Rate',
+				"startdate" => 'Start Date',
+				"enddate" => 'End Date',
+				"Actions" => 'Actions');
+$formatters = array(
+				"Owner" => 'fmt_job_owner',
+				"startdate" => 'fmt_job_startdate',
+				"status" => "fmt_ucfirst",
+				"Total" => 'fmt_total',
+				"Rate" => 'fmt_rate',
+				"enddate" => 'fmt_job_enddate',
+				"Actions" => 'fmt_jobs_actions_customer');
+
+
 showPageMenu($total, $start, $limit);
-echo "\n";
-echo '<table width="100%" cellpadding="3" cellspacing="1" class="list">';
-showTable($data, $titles,$formatters);
-echo "\n</table>";
+showObjects($jobs, $titles, $formatters, false, false);
 showPageMenu($total, $start, $limit);
 endWindow();
 
 include_once("navbottom.inc.php");
-
-
-function fmt_job_date ($row,$index) {
-	if (isset($row[$index + 1])) { // Check job.finishdate field first
-		$time = strtotime($row[$index + 1]);
-		if ($time !== -1 && $time !== false) {
-			return date("M j, g:i a", $time);
-		}
-	} else {
-		$time = strtotime($row[$index]);
-		if ($time !== -1 && $time !== false) {
-			return date("M j, g:i a", $time);
-		}
-	}
-
-	return "&nbsp;";
-}
 
 ?>
