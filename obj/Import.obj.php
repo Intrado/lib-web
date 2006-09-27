bbb0d7cfb7329476176bb3bf22067eca4e6873e5
Ben Hencke <bhencke@schoolmessenger.com>
2006-09-27 21:46:16 +0000

    add uplaod key

--- obj/Import.obj.php
+++ obj/Import.obj.php
@@ -2,6 +2,7 @@
 
 class Import extends DBMappedObject {
 
+	var $uploadkey;
 	var $customerid;
 	var $userid;
 	var	$listid;
@@ -18,7 +19,7 @@ class Import extends DBMappedObject {
 	function Import ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "import";
-		$this->_fieldlist = array("customerid", "userid", "listid", "name", "description", "status", "type", "path", "scheduleid", "ownertype", "updatemethod", "lastrun");
+		$this->_fieldlist = array("uploadkey", "customerid", "userid", "listid", "name", "description", "status", "type", "path", "scheduleid", "ownertype", "updatemethod", "lastrun");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
