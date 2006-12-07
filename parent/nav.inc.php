4d3460af2c270a2d0d9d26b41d24dc3d5d29c41d
Josh Lai <jlai@schoolmessenger.com>
2006-12-07 17:16:08 +0000

    Top nav for parent portal

---
+++ parent/nav.inc.php
@@ -0,0 +1,9 @@
+<html>
+<body>
+<table border = 1>
+	<tr>
+		<td><a href="addstudent.php">Add a student to your account</a></td>
+		<td><a href="./?logout=1">Log&nbsp;Out</a></td>
+
+	</tr>
+</table>
