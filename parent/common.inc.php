0f42f564d99fec680d10f8f0f89bb70b5762c5fc
Josh Lai <jlai@schoolmessenger.com>
2006-12-07 17:15:25 +0000

    Common file for parent portal. Validates if url path matches session url.

---
+++ parent/common.inc.php
@@ -0,0 +1,36 @@
+<?
+
+$SETTINGS = parse_ini_file("../inc/settings.ini.php",true);
+$IS_COMMSUITE = $SETTINGS['feature']['is_commsuite'];
+
+require_once("../inc/db.inc.php");
+require_once("../inc/DBMappedObject.php");
+require_once("../inc/DBRelationMap.php");
+require_once("../inc/utils.inc.php");
+
+require_once("ParentUser.obj.php");
+
+
+if (!$IS_COMMSUITE) {
+	$CUSTOMERURL = substr($_SERVER["SCRIPT_NAME"],1);
+	$CUSTOMERURL = strtolower(substr($CUSTOMERURL,0,strpos($CUSTOMERURL,"/")));
+} else {
+	$CUSTOMERURL = "default";
+}
+
+session_name($CUSTOMERURL . "_session");
+session_start();
+
+if(!isset($isparentlogin) || !$isparentlogin){
+	if(!isset($_SESSION["parentloginid"])) {
+		redirect("index.php?logout=1");
+	} else {
+		$PARENTUSER = $_SESSION['parentloginid'];
+		$PARENTUSER->refresh();
+		$CHILDLIST = $_SESSION['childlist'];
+	}
+}
+
+
+
+?>
