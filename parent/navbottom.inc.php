8db2cb8fef7b33363f410cadb5503ca4e736ea7d
Josh Lai <jlai@schoolmessenger.com>
2006-12-15 22:55:21 +0000

    Moved error checks to here.

--- parent/navbottom.inc.php
+++ parent/navbottom.inc.php
@@ -1,4 +1,11 @@
 <?
+if(isset($ERRORS) && is_array($ERRORS)) {
+	foreach($ERRORS as $key => $value) {
+		$ERRORS[$key] = addslashes($value);
+	}
+	print '<script language="javascript">window.alert(\'' . implode('.\n', $ERRORS) . '.\');</script>';
+}
+
 	print "<div > Current system time is " . date("F jS, Y h:i a (e)") . "</div>";
 ?>
 </body>
