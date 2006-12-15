6b20399db18705043b3a878ef06b647122eb15a3
Josh Lai <jlai@schoolmessenger.com>
2006-12-15 22:54:43 +0000

    Added link to parentportal.php

--- parent/nav.inc.php
+++ parent/nav.inc.php
@@ -2,6 +2,7 @@
 <body>
 <table border = 1>
 	<tr>
+		<td><a href="parentportal.php">Main</a></td>
 		<td><a href="addstudent.php">Add a student to your account</a></td>
 		<td><a href="./?logout=1">Log&nbsp;Out</a></td>
 
