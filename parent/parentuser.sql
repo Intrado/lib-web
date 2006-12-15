93678a76c61178a3efdf72e30c3b300c8ecb0427
Josh Lai <jlai@schoolmessenger.com>
2006-12-15 23:09:35 +0000

    Changed Login max length to 255. Changed customerid to int.

--- parent/parentuser.sql
+++ parent/parentuser.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 -- 
 -- Host: localhost
--- Generation Time: Dec 05, 2006 at 05:10 PM
+-- Generation Time: Dec 15, 2006 at 03:04 PM
 -- Server version: 5.0.24
 -- PHP Version: 5.1.6
 -- 
@@ -18,14 +18,10 @@
 
 CREATE TABLE `parentuser` (
   `id` int(11) NOT NULL auto_increment,
-  `firstname` varchar(50) collate utf8_bin NOT NULL,
-  `lastname` varchar(50) collate utf8_bin NOT NULL,
-  `login` varchar(50) collate utf8_bin NOT NULL,
-  `password` varchar(255) collate utf8_bin NOT NULL,
-  `customerid` varchar(255) collate utf8_bin NOT NULL,
+  `firstname` varchar(50) NOT NULL,
+  `lastname` varchar(50) NOT NULL,
+  `login` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `customerid` int(11) NOT NULL,
   PRIMARY KEY  (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1;
-
--- 
--- Dumping data for table `parentuser`
--- 
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
