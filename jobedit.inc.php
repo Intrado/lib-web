cfe4d3a4d0c24e32e8493b1757cc2cfdd9af528f
Josh Lai <jlai@schoolmessenger.com>
2007-01-05 01:04:03 +0000

    Added check to see if job end date is before current date, if so, display error. Added check to see if  job end date is today and job end time is before current time, if so, display error.

--- jobedit.inc.php
+++ jobedit.inc.php
@@ -68,7 +68,7 @@ if(CheckFormSubmit($f,$s) || CheckFormSubmit($f,'phone') || CheckFormSubmit($f,'
 	else
 	{
 		MergeSectionFormData($f, $s);
-
+		
 		//do check
 		if( CheckFormSection($f, $s) ) {
 			error('There was a problem trying to save your changes', 'Please verify that all required field information has been entered properly');
@@ -80,6 +80,10 @@ if(CheckFormSubmit($f,$s) || CheckFormSubmit($f,'phone') || CheckFormSubmit($f,'
 			error('The end time is invalid');
 		} else if (strtotime(GetFormData($f,$s,"endtime")) < strtotime(GetFormData($f,$s,"starttime")) ) {
 			error('The end time cannot be before the start time');
+		} else if (strtotime(GetFormData($f,$s,"startdate"))+GetFormData($f,$s,"numdays")*86400 < strtotime("today")) {
+			error('The end date has already passed. Please correct this problem before proceeding.');
+		} else if ( (strtotime(GetFormData($f,$s,"startdate"))+GetFormData($f,$s,"numdays")*86400 == strtotime("today")) && (strtotime(GetFormData($f,$s,"endtime")) < strtotime("now")) ) {
+			error('The end time has already passed. Please correct this problem before proceeding.');
 		} else if (QuickQuery("select id from job where deleted = 0 and name = '" . DBsafe(GetFormData($f,$s,"name")) . "' and userid = $USER->id and status in ('new','active','repeating') and id != " . ( 0+ $_SESSION['jobid']))) {
 			error('A job named \'' . GetFormData($f,$s,"name") . '\' already exists');
 		} else if (GetFormData($f,$s,"callerid") != "" && strlen(Phone::parse(GetFormData($f,$s,"callerid"))) != 10) {
