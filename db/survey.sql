b9709e9602862d8aea34fa1600189fc8ad5a6d71
Ben Hencke <bhencke@schoolmessenger.com>
2007-01-25 00:15:37 +0000

    fix bug w/ job type

--- db/survey.sql
+++ db/survey.sql
@@ -80,3 +80,12 @@ ALTER TABLE `surveyemailcode` ADD `resultdata` TEXT NOT NULL AFTER `loggedip` ;
 
 ALTER TABLE `surveyemailcode` DROP INDEX `jobworkitemid` ,
 ADD UNIQUE `jobworkitemid` ( `jobworkitemid` ) ;
+
+
+ALTER TABLE `job` CHANGE `type` `type` SET( 'phone', 'email', 'print', 'survey' ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT 'phone';
+
+
+
+
+
+
