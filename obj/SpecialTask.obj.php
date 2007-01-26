a6df7a2c3e16049bfb835d3cb03b4a2adbd85cca
Ben Hencke <bhencke@schoolmessenger.com>
2007-01-26 19:51:30 +0000

    updated for schema change

--- obj/SpecialTask.obj.php
+++ obj/SpecialTask.obj.php
@@ -2,30 +2,32 @@
 
 class SpecialTask extends DBMappedObject {
 
+	var $customerid;
+	var $status;
 	var $type;
-	var $assignedto;
 	var $data = "";
 	var $lastcheckin;
 
 	function SpecialTask ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "specialtask";
-		$this->_fieldlist = array("type", "assignedto", "data","lastcheckin");
+		$this->_fieldlist = array("customerid","status","type", "data","lastcheckin");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
-	
+
 	function getData($field) {
+		$data = array();
 		parse_str($this->data, $data);
 		return $data[$field];
 	}
-	
+
 	function setData($field, $value) {
 		$data = array();
 		parse_str($this->data, $data);
-		
+
 		$data[$field] = $value;
-		
+
 		$pairs = array();
 		foreach($data as $key => $value)
 			$pairs[] = urlencode($key) . '=' . urlencode($value);
