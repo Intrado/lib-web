f90abef25824aeb9c1ced869c00e61e9978685b4
Ben Hencke <bhencke@schoolmessenger.com>
2007-02-01 19:13:25 +0000

    dblog.txt now optional, controlled by ini setting. also added another log for db errors that logs the mysql_error

--- inc/db.inc.php
+++ inc/db.inc.php
@@ -27,16 +27,39 @@ unset($dbs);
 
 
 function DBDebug($query) {
+	global $SETTINGS;
 	static $initdblog = false;
 	static $logfp;
 
+	if (!$SETTINGS['feature']['log_db_queries'])
+		return;
+
 	if (!$initdblog) {
 		list($usec,$sec) = explode(" ", microtime());
-		$logfp = fopen("dblog.txt","a");
-		fwrite($logfp,"\n=================== " . date("Y-m-d H:i:") . sprintf("%.3f",$usec + ($sec %60)) . ", ===================\n");
+		$logfp = fopen($SETTINGS['feature']['log_dir'] . "dblog.txt","a");
+		fwrite($logfp,"\n=== t:" . mysql_thread_id() . " ================ " . date("Y-m-d H:i:") . sprintf("%.3f",$usec + ($sec %60)) . ", ===================\n");
 		$initdblog = true;
 	}
-	fwrite($logfp,"------------------------------\n$query\n");
+	fwrite($logfp,"--- t:" . mysql_thread_id() . " ---------------------------\n$query\n");
+}
+
+//wrap mysql_query, and optionally log query errors
+function DBQueryWrapper($query, $dbcon) {
+	global $SETTINGS;
+
+	static $initdblog = false;
+	static $logfp;
+
+	$res = mysql_query($query, $dbcon);
+	if (!$res && $SETTINGS['feature']['log_db_errors']) {
+		if (!$initdblog) {
+			$logfp = fopen($SETTINGS['feature']['log_dir'] . "dberrors.txt","a");
+			$initdblog = true;
+		}
+		$errorstr = date("Y-m-d H:i:") . sprintf("%.3f",$usec + ($sec %60)) . " t:" . mysql_thread_id() . " e:" . mysql_error() . " q:" . preg_replace('/\s\s+/', ' ',$query);
+		fwrite($logfp, $errorstr);
+	}
+	return $res;
 }
 
 function DBClose () {
@@ -52,14 +75,14 @@ function DBSafe ($string) {
 function Query ($query) {
 	DBDebug($query);
 	global $_dbcon;
-	return mysql_query($query,$_dbcon);
+	return DBQueryWrapper($query,$_dbcon);
 }
 
 function QuickQuery ($query) {
 	DBDebug($query);
 	global $_dbcon;
 	$val = false;
-	if ($result = mysql_query($query,$_dbcon)) {
+	if ($result = DBQueryWrapper($query,$_dbcon)) {
 		if ($row = mysql_fetch_row($result)) {
 			$val = $row[0];
 		}
@@ -72,7 +95,7 @@ function QuickQuery ($query) {
 function QuickUpdate ($query) {
 	DBDebug($query);
 	global $_dbcon;
-	if (mysql_query($query,$_dbcon)) {
+	if (DBQueryWrapper($query,$_dbcon)) {
 		return mysql_affected_rows();
 	}
 	return false;
@@ -82,7 +105,7 @@ function QuickQueryRow ($query, $assoc = false) {
 	DBDebug($query);
 	global $_dbcon;
 	$row = false;
-	if ($result = mysql_query($query,$_dbcon)) {
+	if ($result = DBQueryWrapper($query,$_dbcon)) {
 		if ($assoc)
 			$row = mysql_fetch_assoc($result);
 		else
@@ -99,7 +122,7 @@ function QuickQueryList ($query, $pair = false) {
 	global $_dbcon;
 	$list = array();
 
-	if ($result = mysql_query($query,$_dbcon)) {
+	if ($result = DBQueryWrapper($query,$_dbcon)) {
 		while ($row = mysql_fetch_row($result)) {
 			if($pair)
 				$list[$row[0]] = $row[1];
