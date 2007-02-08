6cf7c5784e22e2f88dbdb705f0e2992d1d8c2b09
Josh Lai <jlai@schoolmessenger.com>
2007-02-08 18:59:15 +0000

    Updated easycall with multilingual option

--- easycallstart.php
+++ easycallstart.php
@@ -1,7 +1,4 @@
 <?
-//asks for user input (phone, name)
-//posts to callme2
-
 
 include_once('inc/common.inc.php');
 include_once('obj/SpecialTask.obj.php');
@@ -12,9 +9,12 @@ include_once('obj/Phone.obj.php');
 include_once("obj/JobType.obj.php");
 include_once("obj/PeopleList.obj.php");
 include_once("obj/Job.obj.php");
+include_once("obj/JobLanguage.obj.php");
+include_once("obj/Language.obj.php");
 include_once('inc/html.inc.php');
 include_once("inc/form.inc.php");
 include_once('inc/table.inc.php');
+include_once('inc/utils.inc.php');
 
 // AUTHORIZATION //////////////////////////////////////////////////
 if (!$USER->authorize("starteasy")) {
@@ -24,12 +24,19 @@ if (!$USER->authorize("starteasy")) {
 // DATA SECTION
 $VALIDJOBTYPES = JobType::getUserJobTypes();
 
+$languages = DBFindMany("Language","from language where customerid=" . $USER->customerid);
+if(isset($_REQUEST['id'])) {
+	$_SESSION['easycallid'] = null;
+	redirect();
+}
+
+
 // FORM HANDLING
 $f = "easycall";
 $s = "main";
 $reloadform = 0;
 
-if(CheckFormSubmit($f,$s))
+if(CheckFormSubmit($f,$s) || CheckFormSubmit($f, 'lang'))
 {
 	//check to see if formdata is valid
 	if(CheckFormInvalid($f))
@@ -53,50 +60,77 @@ if(CheckFormSubmit($f,$s))
 				error('The phone number must be exactly 10 digits long (including area code)','You do not need to include a 1 for long distance');
 		} else if (GetFormData($f,$s,"listid") <=0 ) {
 			error('Please choose a list');
+			
+		} else if(QuickQuery("select * from job where userid = '$USER->id' and deleted = 0 
+					and name = '" . DBSafe(GetFormData($f, $s, 'name')) ."'")) {
+			error('This job name is already in use, please make another');
+		
 		} else {
-
-			$task = new SpecialTask();
-			$task->type = 'EasyCall';
-			$task->setData('phonenumber', $phone);
-
-			$testname = GetFormData($f, $s, 'name');
-			if (QuickQuery("select * from audiofile where userid = {$USER->id} and deleted = 0 and name = '" .
-				  DBSafe($testname) . "' and id != '" . $audio->id. "'")) {
-				error('This audio file name is already in use, a unique one was generated');
-
-				$testname = GetFormData($f, $s, 'name') . ' ' . date("F jS, Y h:i a");
-				PutFormData($f, $s, 'name', $testname, 'text', 1, 50, true); // Repopulate the form/session data with the generated name
+			if($_SESSION['easycallid'] == null){
+				$task = new SpecialTask();
+			} else {
+				$task = new SpecialTask($_SESSION['easycallid']);
 			}
-
-			$task->setData('name', $testname);
-			$task->setData('origin', "start");
-			$task->setData('userid', $USER->id);
-			$task->setData('listid', GetFormData($f,$s,"listid"));
+			$task->type = 'EasyCall';
+			$task->setData('phonenumber', $phone);	
+			$task->setData('callerid', getSystemSetting('callerid'));				
+			$task->setData('name', GetFormData($f, $s, 'name'));	
+			$task->setData('origin', "start");			
+			$task->setData('userid', $USER->id);				
+			$task->setData('listid', GetFormData($f,$s,"listid"));			
 			$task->setData('jobtypeid', GetFormData($f,$s,"jobtypeid"));
+			$task->setData('progress', "Creating Call");		
 			$task->lastcheckin = date("Y-m-d H:i:s");
-			$task->create();
-
-			redirect('easycallrecord.php?taskid=' . $task->id);
+			if(CheckFormSubmit($f, 'lang'))
+				$task->status = "new";
+			else			
+				$task->status = "queued";
+			$task->customerid=$USER->customerid;
+			if($USER->authorize('sendmulti')) {
+				$langlist = $task->getData('languagelist');
+				if($langlist == null) {
+					$languagearray = array();
+					$languagearray[] = "Default";
+				} else {
+					$languagearray = explode("|", $langlist);
+				}
+				$selectedlangs = getFormData($f, $s, "newlang");
+				if($selectedlangs){
+					$languagearray[]=$selectedlangs;
+				}
+				$languagelist = implode("|",$languagearray);
+				$task->setData('languagelist', $languagelist);
+			} else {
+				$task->setData('languagelist', "Default");
+			}
+			if (isset($_GET['retry']))
+				$task->setData("error", "0");
+			if($task->id){
+				$task->update();
+			} else {
+				$task->create();
+			}		
+			$_SESSION['easycallid'] = $task->id;
+			if(!CheckFormSubmit($f, 'lang'))
+				redirect('easycallrecord.php?taskid=' . $task->id);
+			
 		}
 	}
 } else {
+	$reloadform = 1;
+}
+
+if($reloadform == 1) {
 	ClearFormData($f);
 
-	if (isset($_GET['retry']))
+	if (isset($_GET['retry'])){
 		$specialtask = new SpecialTask(DBSafe($_GET['retry']));
-	else
-		$specialtask = false;
-
-	PutFormData($f,$s,"name",$specialtask ? $specialtask->getData('name') : "" ,"text",1,50);
-
-
-
-	$testname = DBSafe(GetFormData($f, $s, 'name'));
-	if (QuickQuery("select * from audiofile where userid = {$USER->id} and deleted = 0 and name = '" .
-		  $testname . "' and id != '" . $audio->id. "'")) {
-		error('This audio file name is already in use, a unique one was generated');
-		$testname = DBSafe(GetFormData($f, $s, 'name')) . ' ' . date("F jS, Y h:i a");
-		PutFormData($f, $s, 'name', $testname, 'text', 1, 50, true); // Repopulate the form/session data with the generated name
+		$languagearray = explode("|", $specialtask->getData('languagelist'));
+	}else{
+		if($_SESSION['easycallid'] != null)
+			$specialtask = new SpecialTask($_SESSION['easycallid']);
+		else 
+			$specialtask = false;
 	}
 
 	PutFormData($f,$s,"listid",$specialtask ? $specialtask->getData('listid') : 0);
@@ -104,14 +138,44 @@ if(CheckFormSubmit($f,$s))
 
 	if ($specialtask) {
 		$phone = Phone::format($specialtask->getData('phonenumber'));
+		PutFormData($f,$s,"name",$specialtask->getData('name'),"text",1,20);
 	} else {
+		PutFormData($f, $s, 'name', GetFormData($f, $s, 'name'), 'text', 1, 50, true);
 		if ($USER->phone)
 			$phone = Phone::format($USER->phone);
 		else
 			$phone = "";
 	}
 	PutFormData($f,$s,"phone",$phone,"text","2","20"); // 20 is the max to accomodate formatting chars
+	
 }
+
+
+//////////////
+//FUNCTIONS
+
+function language_select($form, $section, $name) {
+	global $languages, $languagearray;
+
+	NewFormItem($form, $section, $name, 'selectstart', NULL, NULL, "");
+	NewFormItem($form, $section, $name, 'selectoption',"- Select a Language -","");
+	foreach ($languages as $language) {
+		$used = false;
+		if(isset($languagearray)) {
+			foreach ($languagearray as $lang) {
+				if ($lang == $language->name) {
+					$used = true;
+					break;
+				}
+			}
+		}
+		if ($used)
+		continue;
+		NewFormItem($form, $section, $name, 'selectoption', $language->name, $language->name);
+	}
+	NewFormItem($form, $section, $name, 'selectend');
+}
+
 
 
 $TITLE = 'EasyCall';
@@ -127,7 +191,7 @@ startWindow("EasyCall");
 	<table border="0" cellpadding="3" cellspacing="0" width="400">
 		<tr>
 			<th align="right" class="windowRowHeader bottomBorder" style="width: 100px;">Job&nbsp;Name:&nbsp;<?= help("EasyCall_Name", null, 'small'); ?></td>
-			<td class="bottomBorder"><? NewFormItem($f,$s,"name","text",30); ?></td>
+			<td class="bottomBorder"><? NewFormItem($f,$s,"name","text",30,30); ?></td>
 		</tr>
 		<tr>
 			<th align="right" class="windowRowHeader bottomBorder" style="width: 100px;">Priority:&nbsp;<?= help('EasyCall_Priority', NULL, "small"); ?></td>
@@ -164,6 +228,27 @@ startWindow("EasyCall");
 			<td class="bottomBorder"><? NewFormItem($f,$s,"phone","text",20); ?></td>
 		</tr>
 <?
+		if($USER->authorize('sendmulti')) {
+			?>
+			<tr>
+				<th align="right" class="windowRowHeader bottomBorder" style="width: 100px;">Multilingual Language Option:</td>
+				<td>
+				<table>
+					<?
+					if(isset($languagearray)) {
+						foreach($languagearray as $language) {
+							if($language == "Default") continue;
+							?><tr><td><?=$language?></td></tr><?
+						}
+					}
+					?>
+					<tr><td><?language_select($f,$s,"newlang"); NewFormItem($f, 'lang', 'Add', 'submit', NULL, NULL, ""); ?></td></tr>
+				</table>
+				</td>
+			</tr>
+			<?
+		}
+
 		if($IS_COMMSUITE){
 			?> <tr><td colspan=3 style="padding: 10px;">If dialing an outside line, please include the area code.</td><tr> <?
 		} else {
