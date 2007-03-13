2922a27517046725dce54d24d95ddc34ef07389b
Josh Lai <jlai@schoolmessenger.com>
2007-03-13 01:28:37 +0000

    Added nav to active jobs

--- manager/nav.inc.php
+++ manager/nav.inc.php
@@ -4,8 +4,8 @@
 	<tr>
 		<td><a href="customers.php">Customer&nbsp;List</a></td>
 		<td><a href="customerimports.php">Customer&nbsp;Imports</a></td>
-		<td><a href="newcustomer.php">New&nbsp;Customer</a></td>
+		<td><a href="customeractivejobs.php">Active&nbsp;Jobs</a></td>
+		<td><a href="newcustomer.php">New&nbsp;Customer</a></td>		
 		<td><a href="./?logout=1">Log&nbsp;Out</a></td>
-
 	</tr>
 </table>
