3160d57ac0943923429672085c40d3c79d7e7bd0
Josh Lai <jlai@schoolmessenger.com>
2007-03-14 18:23:52 +0000

    Added function to translate file time from pacific to customer based added column for update method added column for timezone

--- manager/customerimports.php
+++ manager/customerimports.php
@@ -4,6 +4,32 @@ include_once("../inc/ftpfile.inc.php");
 include_once("../inc/formatters.inc.php");
 include_once("../inc/form.inc.php");
 
+
+////////////////////////////
+function translatetime($datetime, $zone){
+	$offsets = array( "US/Samoa" => -3,
+					"US/Aleutian" => -2,
+					"US/Hawaii" => -2,
+					"US/Alaska" => -1,
+					"US/Pacific" => 0,
+					"US/Arizona" => 1,
+					"US/Mountain" => 1,
+					"US/Central" => 2,
+					"US/Indiana-Starke" => 2,
+					"US/East-Indiana" => 3,
+					"US/Eastern" => 3,
+					"US/Michigan" => 3
+					);
+	if(strtotime("now") > strtotime("March 11") && strtotime("now") < strtotime("November 4")){
+		$offsets["US/Arizona"] = 0;
+		$offsets["US/Hawaii"] = -3;
+	}
+	return $datetime + ($offsets[$zone] * 3600);
+}
+////////////////////////////
+
+
+
 $f = "form";
 $s = "imports";
 $reloadform = 0;
@@ -75,6 +101,8 @@ EndForm();
 		<td>Import Name</td>
 		<td>Status</td>
 		<td>Type </td>
+		<td>Upd. Method</td>
+		<td>TimeZone</td>
 		<td>Last Run</td>
 		<td>File Date</td>
 		<td>File Size in Bytes</td>
@@ -82,7 +110,7 @@ EndForm();
 <?
 
 $query = "SELECT  import.customerID, customer.name, customer.hostname, import.id, 
-			import.name, import.status, import.type, import.lastrun
+			import.name, import.status, import.type, import.updatemethod, import.lastrun, customer.timezone
 			FROM import, customer
 			WHERE import.customerID=customer.id
 			$queryextra
@@ -94,31 +122,35 @@ while($row = DBGetRow($list)){
 	$importfile = getImportFileURL($row[0],$row[3]);
 
 	if (is_readable($importfile) && is_file($importfile)) {
-		$row[8]=date("M j, g:i a",filemtime($importfile));
-		$row[9]=filesize($importfile);
+		$row[10]=date("M j, g:i a ",translatetime(filemtime($importfile), $row[9]));
+		$row[11]=filesize($importfile);
 	} else {
-		$row[8]="Not Found";
-		$row[9]="-";
+		$row[10]="Not Found";
+		$row[11]="-";
 	}
-	$row[7] = fmt_date($row, 7);
+	$row[8] = fmt_date($row, 8);
 ?>
 	<tr>
 		<td><?=$row[0]?></td>
 		<td><?=$row[1]?></td>
-		<td><a href="https://asp.schoolmessenger.com/<?=$row[2]?>"><?=$row[2]?></a></td>
+		<td><a href="https://asp.schoolmessenger.com/<?=$row[2]?>" target="_blank"><?=$row[2]?></a></td>
 		<td><?=$row[3]?></td>
 		<td><?=$row[4]?></td>
 		<td><?=$row[5]?></td>
 		<td><?=$row[6]?></td>
 		<td><?=$row[7]?></td>
-		<td><?=$row[8]?></td>
 		<td><?=$row[9]?></td>
+		<td><?=$row[8]?></td>
+		<td><?=$row[10]?></td>
+		<td><?=$row[11]?></td>
 	</tr>
 <?
 }
+date_default_timezone_set("US/Pacific");
 ?>
 </table>
 <br> Automatic jobs have the "Import when uploaded" checkbox checked, manual jobs do not.  Both are from imports page.
+<div style="color:FF0000"> All time stamps are in customer time. </div>
 
 <?
 include("navbottom.inc.php");
