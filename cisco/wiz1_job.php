d1479239cbf2bab4e1358ea24a54e7d37e38db81
Josh Lai <jlai@schoolmessenger.com>
2007-03-26 17:25:36 +0000

    Updated retries to 4

--- cisco/wiz1_job.php
+++ cisco/wiz1_job.php
@@ -12,7 +12,7 @@ if (!isset($_GET['edit'])) {
 	$_SESSION['newjob']['name'] = "";
 	$_SESSION['newjob']['desc'] = "";
 	$_SESSION['newjob']['numdays'] = min($ACCESS->getValue('maxjobdays'), $USER->getSetting("maxjobdays","2"));
-	$_SESSION['newjob']['retries'] = min(3,$ACCESS->getValue('callmax'));
+	$_SESSION['newjob']['retries'] = min(4,$ACCESS->getValue('callmax'));
 	$_SESSION['newjob']['easycall'] = false;
 }
 
