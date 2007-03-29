5965fde06e3b2111922897a678bfbf84441724fa
Josh Lai <jlai@schoolmessenger.com>
2007-03-29 23:20:48 +0000

    new obj for voice replies

---
+++ obj/VoiceReply.obj.php
@@ -0,0 +1,30 @@
+<?
+class VoiceReply extends DBMappedObject{
+
+	var $jobtaskid = "";
+	var $jobworkitemid = "";
+	var $personid = "";
+	var $jobid = "";
+	var $userid = "";
+	var $customerid = "";
+	var $contentid = "";
+	var $replytime = "";
+	var $listened = 0;
+
+
+	function AspAdminUser($id = NULL){
+		$this->_allownulls = false;
+		$this->_tablename = "voicereply";
+		$this->_fieldlist = array("jobtaskid", "jobworkitemid", "personid",
+									"jobid", "userid", "customerid", "contentid", "replytime", "listened");
+
+		//call super's constructor
+		DBMappedObject::DBMappedObject($id);
+
+	}
+
+	/**static functions**/
+
+}
+
+?>
