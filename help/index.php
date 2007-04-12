b5c54fbc135342ff45fb0cc13d3d808fb209394c
Ben Hencke <bhencke@schoolmessenger.com>
2007-04-12 16:09:32 +0000

    removed excess code

--- help/index.php
+++ help/index.php
@@ -1,6 +1,23 @@
 <?
-include_once("common.inc.php");
+$SETTINGS = parse_ini_file("../inc/settings.ini.php",true);
+$IS_COMMSUITE = $SETTINGS['feature']['is_commsuite'];
 
-header("Location: schoolmessenger_help.htm");
+
+//get the customer URL
+
+if ($IS_COMMSUITE) {
+	$CUSTOMERURL = "default";
+} /*CSDELETEMARKER_START*/ else {
+	$CUSTOMERURL = substr($_SERVER["SCRIPT_NAME"],1);
+	$CUSTOMERURL = strtolower(substr($CUSTOMERURL,0,strpos($CUSTOMERURL,"/")));
+} /*CSDELETEMARKER_END*/
+
+session_name($CUSTOMERURL . "_session");
+session_start();
+
+if (!isset($_SESSION['user']))
+	header("Location: ..");
+else
+	header("Location: schoolmessenger_help.htm");
 
 ?>
