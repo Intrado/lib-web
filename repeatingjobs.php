299cd43e47ba139157786421ebccd09694e3bfdf
Josh Lai <jlai@schoolmessenger.com>
2007-04-27 17:25:45 +0000

    removed customer id checks

--- repeatingjobs.php
+++ repeatingjobs.php
@@ -40,13 +40,13 @@ startWindow('Repeating Notification Jobs ' . help('System_RepeatingJobs', NULL, 
 $start = 0 + (isset($_GET['pagestart']) ? $_GET['pagestart'] : 0);
 $limit = 100;
 
-// jobworkitem columns are: id jobid type priority personid messageid status resultdata assignedto
+
 $query = "select SQL_CALC_FOUND_ROWS u.login, j.name, schedule.nextrun, j.id, j.status, j.deleted, jobowner.login, jobowner.id, name+0 as foo, j.type, j.finishdate
 			from job j
 			left join user jobowner
 				on j.userid = jobowner.id,
 			user u, schedule
-			where u.customerid = $USER->customerid and j.userid = u.id and j.status = 'repeating' and j.scheduleid = schedule.id
+			where j.userid = u.id and j.status = 'repeating' and j.scheduleid = schedule.id
 			group by j.id order by u.login,foo,name limit $start, $limit
 ";
 
