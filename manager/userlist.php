9d5c124cae65ad8d49dd97b4851dfeaf7b4a620f
Josh Lai <jlai@schoolmessenger.com>
2007-05-03 22:57:58 +0000

    updated code to use new shard design

--- manager/userlist.php
+++ manager/userlist.php
@@ -4,13 +4,11 @@ include_once("../obj/User.obj.php");
 include_once("../obj/Access.obj.php");
 include_once("../obj/Permission.obj.php");
 
-$customerID = $_GET["customer"] + 0;
+$customerid = $_GET["customer"] + 0;
 
+$custquery = Query("select dbhost, dbusername, dbpassword, hostname from customer where id = '$customerid'");
+$cust = mysql_fetch_row($custquery);
 
-//Finds how many users in customer.
-$users = DBFindMany("User","from user where customerid='$customerID' AND enabled=1 AND deleted=0");
-$customerresult = Query("select id, name, hostname from customer where id = '$customerID'");
-$customerinfo = DBGetRow($customerresult);
 include_once("nav.inc.php");
 
 ?>
@@ -33,40 +31,49 @@ include_once("nav.inc.php");
 </tr>
 
 <?
-//iterates through each user and finds needed information.
-foreach ($users as $user) {
-	$lastlogin = strtotime($user->lastlogin);
-	if ($lastlogin !== -1 && $lastlogin !== false && $user->lastlogin != "0000-00-00 00:00:00")
-		$lastlogin = date("M j, g:i a",$lastlogin);
-	else
-		$lastlogin = "- Never -";
-
-
-	$jobcount = QuickQuery("SELECT COUNT(*) FROM job WHERE job.userid = $user->id
-						AND job.status = 'active'");
-
-	$accessid=new Access($user->accessid);
-
+if($custdb = DBConnect($cust[0], $cust[1], $cust[2], "c_$customerid")){
+	$displayname = QuickQuery("select value from setting where name = 'displayname'", $custdb);
+	$result = Query("select * from user where enabled=1 AND deleted=0", $custdb);
+	$users = array();
+	while($row = DBGetRow($result, true)){
+		$users[] = $row;
+	}
+	//iterates through each user and find needed information.
+	foreach ($users as $user) {
+		$lastlogin = strtotime($user['lastlogin']);
+		if ($lastlogin !== -1 && $lastlogin !== false && $lastlogin != "0000-00-00 00:00:00")
+			$lastlogin = date("M j, g:i a",$lastlogin);
+		else
+			$lastlogin = "- Never -";
+	
+	
+		$jobcount = QuickQuery("SELECT COUNT(*) FROM job WHERE job.userid = '" . $user['id'] . "'
+							AND job.status = 'active'", $custdb);
+	
+		//$accessid=new Access($user->accessid);
+		$access = QuickQueryRow("select * from access where id = '" . $user['accessid'] ."'", true, $custdb);
 ?>
-	<tr>
-		<td><?= $customerinfo[0] ?></td>
-		<td><?= $customerinfo[1] ?></td>
-		<td><a href="https://asp.schoolmessenger.com/<?=$customerinfo[2]?>" target="_blank"><?=$customerinfo[2]?></a></td>
-		<td><?= $user->id ?></td>
-		<td><?= $user->login ?></td>
-		<td><?= $user->lastname ?></td>
-		<td><?= $user->firstname ?></td>
-		<td><?= $lastlogin ?></td>
-		<td><a href="customeractivejobs.php?user=<?=$user->id?>"><?= $jobcount ?></a></td>
-		<td><?= $accessid->name ?></td>
-		<td><?= $user->phone ?></td>
-		<td><?= $user->email ?></td>
-
-	</tr>
+		<tr>
+			<td><?= $customerid ?></td>
+			<td><?= $displayname ?></td>
+			<td><a href="https://asp.schoolmessenger.com/<?=$cust[3]?>" target="_blank"><?=$cust[3]?></a></td>
+			<td><?= $user['id'] ?></td>
+			<td><?= $user['login'] ?></td>
+			<td><?= $user['lastname'] ?></td>
+			<td><?= $user['firstname'] ?></td>
+			<td><?= $lastlogin ?></td>
+			<td><a href="customeractivejobs.php?user=<?=$user['id']?>"><?= $jobcount ?></a></td>
+			<td><?= $access['name'] ?></td>
+			<td><?= $user['phone'] ?></td>
+			<td><?= $user['email'] ?></td>
+	
+		</tr>
 <?
+	}
 }
 ?>
 </table>
 <?
 include_once("navbottom.inc.php");
+
 ?>
