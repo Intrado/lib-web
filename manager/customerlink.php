fa67994552d1f6fea8367c0e231431b721ac797a
Josh Lai <jlai@schoolmessenger.com>
2007-05-04 16:43:47 +0000

    new auth login for managers

---
+++ manager/customerlink.php
@@ -0,0 +1,51 @@
+<?
+include_once("common.inc.php");
+include_once("AspAdminUser.obj.php");
+include_once("../inc/html.inc.php");
+
+
+if(isset($_GET['id'])){
+	$custid = $_GET['id'] +0;
+}
+
+$manager = new AspAdminUser($_SESSION['aspadminuserid']);
+
+if($_REQUEST['submit']){
+	$password = get_magic_quotes_gpc() ? stripslashes($_POST['password']) : $_POST['password'];
+	if($manager->runCheck($password)){
+		$string = md5(microtime());
+		QuickQuery("update customer set asptoken = '$string', aspexpiration = now() where id = '$custid'");
+		$customerurl = QuickQuery("select hostname from customer where id = '$custid'");
+	} else {
+		error("That was an invalid password");
+	}
+}
+
+include_once("nav.inc.php");
+?>
+<br>
+Link: 
+<?
+	if(isset($string)){
+?>
+		<a href="http://localhost/<?=$customerurl?>/?asptoken=<?=$string?>" target="_blank"><?=$customerurl?></a>
+<? 
+	} 
+?>
+<br>
+<br>
+<form method="POST" action="customerlogin.php?id=<?=$custid?>">
+Enter Manager Password: <input type="password" name="password" />
+<input type="submit" name="submit" />
+</form>
+
+<?
+if(isset($ERRORS) && is_array($ERRORS)) {
+	foreach($ERRORS as $key => $value) {
+		$ERRORS[$key] = addslashes($value);
+	}
+	print '<script language="javascript">window.alert(\'' . implode('.\n', $ERRORS) . '.\');</script>';
+}
+
+include_once("navbottom.inc.php");
+?>
