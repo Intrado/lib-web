1940c854da1b3847a4fe2d802e5637a771b43af7
Josh Lai <jlai@schoolmessenger.com>
2007-05-07 23:23:56 +0000

    fixed random token generation

--- manager/customerlink.php
+++ manager/customerlink.php
@@ -4,6 +4,25 @@ include_once("AspAdminUser.obj.php");
 include_once("../inc/html.inc.php");
 
 
+////////////////////////////////////////////////////////////////////////////////
+// Functions
+////////////////////////////////////////////////////////////////////////////////
+
+function genpassword() {
+	$digits = 32;
+	$passwd = "";
+	$chars = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	while ($digits--) {
+		$passwd .= $chars[mt_rand(0,strlen($chars)-1)];
+	}
+	return $passwd;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+
 if(isset($_GET['id'])){
 	$custid = $_GET['id'] +0;
 }
@@ -13,10 +32,10 @@ $manager = new AspAdminUser($_SESSION['aspadminuserid']);
 if($_REQUEST['submit']){
 	$password = get_magic_quotes_gpc() ? stripslashes($_POST['password']) : $_POST['password'];
 	if($manager->runCheck($password)){
-		$string = md5(microtime());
+		$customerurl = QuickQuery("select hostname from customer where id = '$custid'");
+		$string = md5(genpassword() . $manager->login . microtime() . $customerurl);
 		// TODO we may want to set the expiration interval in the properties file
 		QuickQuery("update customer set asptoken = '$string', aspexpiration = now() + interval 10 minute where id = '$custid'");
-		$customerurl = QuickQuery("select hostname from customer where id = '$custid'");
 	} else {
 		error("That was an invalid password");
 	}
@@ -29,7 +48,7 @@ Link:
 <?
 	if(isset($string)){
 ?>
-		<a href="http://asp.schoolmessenger.com/<?=$customerurl?>/?asptoken=<?=$string?>" target="_blank"><?=$customerurl?></a>
+		<a href="https://localhost/<?=$customerurl?>/?asptoken=<?=$string?>" target="_blank"><?=$customerurl?></a>
 <?
 	}
 ?>
