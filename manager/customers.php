42d21768ee93c9e843fc9fd2a1ec1b39cf765b06
Josh Lai <jlai@schoolmessenger.com>
2007-05-10 00:27:22 +0000

    active users count only displays users other than schoolmessenger user red added for accounts with more user accounts than max users added field for manager notes added field for customer status, for now just dis. repeating jobs

--- manager/customers.php
+++ manager/customers.php
@@ -19,8 +19,11 @@ include_once("nav.inc.php");
 <td>Customer URL</td>
 <td>Toll Free Number</td>
 <td>Timezone</td>
+<td>Status</td>
 <td>Active Users</td>
 <td>Active Jobs</td>
+<td>Actions</td>
+<td>NOTES:&nbsp;</td>
 </tr>
 
 <?
@@ -33,18 +36,31 @@ foreach($customers as $cust) {
 		$hostname = $cust[4];
 		$inboundnumber = QuickQuery("select value from setting where name = 'inboundnumber'", $custdb);
 		$timezone = QuickQuery("select value from setting where name = 'timezone'", $custdb);
-		$usercount = QuickQuery("SELECT COUNT(*) FROM user where enabled = '1'", $custdb);
+		$maxusers = QuickQuery("select value from setting where name = '_maxusers'", $custdb);
+		$notes = QuickQuery("select value from setting where name = '_managernote'", $custdb);
+		$status = QuickQuery("select value from setting where name = 'disablerepeat'", $custdb);
+
+		$maxusers = $maxusers ? $maxusers : 1;
+		$usercount = QuickQuery("SELECT COUNT(*) FROM user where enabled = '1' and login != 'schoolmessenger'", $custdb);
 		$jobcount = QuickQuery("SELECT COUNT(*) FROM job INNER JOIN user ON(job.userid = user.id)
 								WHERE job.status = 'active'", $custdb);
+		$userstyle = "";
+		if($usercount > $maxusers){
+			$userstyle = 'style="background-color: #ff0000;"';
+		} else if($usercount == 0){
+			$userstyle = 'style="background-color: #ffcccc;"';
+		}
 	?>
 		<td><?= $cust[0] ?></td>
 		<td><?= $custname ?></td>
 		<td><a href="customerlink.php?id=<?=$cust[0] ?>"><?=$hostname?></a></td>
-		<td><?= $inboundnumber ?></td>
+		<td><?= $inboundnumber ? $inboundnumber : "&nbsp;" ?></td>
 		<td><?= $timezone ?></td>
-		<td <?= $usercount == 1 ? 'style="background-color: #ffcccc;"' : "" ?>><?= $usercount ?></td>
+		<td><?= $status ? "Repeating Jobs Disabled" : "Repeating Jobs Enabled" ?></td>
+		<td <?=$userstyle?>><?= $usercount ?></td>
 		<td <?= $jobcount > 0 ? 'style="background-color: #ccffcc;"' : "" ?>><?= $jobcount ?></td>
 		<td><a href="customeredit.php?id=<?=$cust[0] ?>">Edit</a>&nbsp;|&nbsp;<a href="userlist.php?customer=<?= $cust[0] ?>">Show&nbsp;Users</a>&nbsp;|&nbsp;<a href="customerimports.php?customer=<?=$cust[0]?>">Customer&nbsp;Imports</a>&nbsp;|&nbsp;<a href="customeractivejobs.php?customer=<?=$cust[0]?>">Active&nbsp;Jobs</a>&nbsp;|&nbsp;<a href="customerpriorities.php?id=<?=$cust[0]?>">Customer&nbsp;Priorities</a></td>
+		<td><?=$notes ? $notes : "&nbsp" ?></td>
 		</tr>
 	
 	<?
@@ -53,7 +69,8 @@ foreach($customers as $cust) {
 ?>
 </table>
 
-<div>Red cells indicate that only the system user account has been created</div>
+<div>Pink cells indicate that only the system user account has been created</div>
+<div>Red cells indicate that the customer has more users than they should</div>
 <div>Green cells indicate customers with active jobs</div>
 <?
 include_once("navbottom.inc.php");
