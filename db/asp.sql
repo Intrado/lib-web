b4df88c9fd1d8d332fc6d6a7adda8f2f8cef597f
Josh Lai <jlai@schoolmessenger.com>
2007-05-31 22:07:22 +0000

    added sms tables data is now null'able

--- db/asp.sql
+++ db/asp.sql
@@ -1583,7 +1583,7 @@ where wi.type = 'print';
 
 drop table persondata;
 
-ALTER TABLE `import` ADD `data` LONGBLOB NOT NULL ;
+ALTER TABLE `import` ADD `data` LONGBLOB Default NULL ;
 
 CREATE TABLE `surveyweb` (
 `code` CHAR( 22 ) CHARACTER SET ascii COLLATE ascii_bin NOT NULL ,
@@ -1603,8 +1603,41 @@ wi.personid, wi.customerid,
 sec.isused+1, sec.dateused, 
 sec.loggedip, sec.resultdata
 from surveyemailcode sec
-inner join jobworkitem wi on (wi.id = sec.jobworkitemid)
+inner join jobworkitem wi on (wi.id = sec.jobworkitemid);
 
 
+
+CREATE TABLE `smsjob` (
+  `id` int(11) NOT NULL auto_increment,
+  `userid` int(11) NOT NULL,
+  `listid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `txt` varchar(160) NOT NULL,
+  `sendoptout` tinyint(4) NOT NULL,
+  `sentdate` datetime NOT NULL,
+  `status` enum('new','queued','sent','error') NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `smsmsg` (
+  `id` int(11) NOT NULL auto_increment,
+  `smsjobid` int(11) NOT NULL,
+  `personid` int(11) NOT NULL,
+  `sequence` tinyint(4) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `phone` ADD `smsenabled` TINYINT NOT NULL DEFAULT '0';
+
+ALTER TABLE `blockednumber` ADD `type` ENUM( 'call', 'sms', 'both' ) NOT NULL DEFAULT 'both';
+
+
+ALTER TABLE `smsjob` ADD `deleted` TINYINT NOT NULL DEFAULT '0';
 
 
