3b2252b9a3037c3adb6a21375c9f8a7da70272db
Josh Lai <jlai@schoolmessenger.com>
2007-06-19 23:29:01 +0000

    new job report proxy page

---
+++ reportjob.php
@@ -0,0 +1,29 @@
+<?
+
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+require_once("inc/common.inc.php");
+include_once("inc/securityhelper.inc.php");
+require_once("obj/Job.obj.php");
+require_once("obj/Phone.obj.php");
+require_once("inc/html.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/utils.inc.php");
+require_once("inc/formatters.inc.php");
+require_once("obj/FieldMap.obj.php");
+require_once("obj/SurveyQuestionnaire.obj.php");
+require_once("obj/SurveyQuestion.obj.php");
+require_once("obj/ReportInstance.obj.php");
+require_once("obj/ReportGenerator.obj.php");
+require_once("inc/form.inc.php");
+require_once("obj/UserSetting.obj.php");
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+if (!$USER->authorize('createreport') && !$USER->authorize('viewsystemreports')) {
+	redirect('unauthorized.php');
+}
+
+$_SESSION['reporttype'] = "jobreport";
+include("reportjobsurvey.php");
