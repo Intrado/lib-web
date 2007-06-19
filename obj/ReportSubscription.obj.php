778a01b04ec856c6b29f6dcdd3daffdd2181e29b
Josh Lai <jlai@schoolmessenger.com>
2007-06-19 22:30:03 +0000

    updated report subscriptions merged report schedule in here

--- obj/ReportSubscription.obj.php
+++ obj/ReportSubscription.obj.php
@@ -1,29 +1,14 @@
 <?
 
-/*
-
-#
-# Table structure for table `reportsubscription`
-#
-
-
-CREATE TABLE `reportsubscription` (
-  `ID` int(11) NOT NULL auto_increment,
-  `UserID` int(11) NOT NULL default '0',
-  `Name` varchar(20) NOT NULL default '',
-  `ReportInstanceID` int(11) NOT NULL default '0',
-  `ReportScheduleID` int(11) NOT NULL default '0',
-  PRIMARY KEY  (`ID`),
-  KEY `subscription` (`UserID`,`ReportInstanceID`)
-) TYPE=MyISAM
-
-*/
-
 class ReportSubscription extends DBMappedObject {
 	var $userid;
 	var $name = "";
 	var $reportinstanceid;
-	var $reportscheduleid;
+	var $dow;
+	var $dom;
+	var $date;
+	var $nextrun;
+	var $time;
 
 	//var $reportinstance; // doesnt make sence in this context, subscriptions are children of reportinstance
 	var $reportschedule;
@@ -31,9 +16,7 @@ class ReportSubscription extends DBMappedObject {
 	//new constructor
 	function ReportSubscription ($id = NULL) {
 		$this->_tablename = "reportsubscription";
-		$this->_fieldlist = array("userid", "name","reportinstanceid","reportscheduleid");
-		$this->_childobjects = array("reportschedule");
-		$this->_childclasses = array("ReportSchedule");
+		$this->_fieldlist = array("userid", "name","reportinstanceid","dow", "dom", "date", "nextrun", "time");
 
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
