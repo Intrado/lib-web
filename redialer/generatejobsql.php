d591438f5bf1556f78a9c35b5a77919536fe673e
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2007-06-25 19:24:35 +0000

    move to redialer subfolder - not used by www

---
+++ redialer/generatejobsql.php
@@ -0,0 +1,66 @@
+<?
+// redialer.schedulemanager executes this to populate job.thesql field
+
+
+require_once("../inc/db.inc.php");
+require_once("../inc/DBMappedObject.php");
+require_once("../inc/DBRelationMap.php");
+require_once("../inc/utils.inc.php");
+require_once("../inc/date.inc.php");
+require_once("../obj/User.obj.php");
+require_once("../obj/Access.obj.php");
+require_once("../obj/Permission.obj.php");
+require_once("../obj/Job.obj.php");
+require_once("../obj/JobLanguage.obj.php");
+require_once("../obj/JobType.obj.php");
+require_once("../obj/Rule.obj.php");
+require_once("../obj/FieldMap.obj.php");
+require_once("../obj/SurveyQuestionnaire.obj.php");
+
+
+$custid = $argv[1] +0;
+if ($custid == 0) {
+	echo "missing customer id, Usage: generatejobsql customerid jobid dbhost:port dbuser dbpass dbname \n";
+	exit(-1);
+}
+
+$jobid = $argv[2] +0;
+if ($jobid == 0) {
+	echo "missing job id, Usage: generatejobsql customerid jobid dbhost:port dbuser dbpass dbname \n";
+	exit(-1);
+}
+
+echo "generatejobsql for customerid=".$custid." jobid=".$jobid."\n";
+
+// gather database connection info
+$db['host'] = $argv[3]; //"localhost:3306";
+$db['db'] = $argv[4]; //"c_7";
+$db['user'] = $argv[5]; //"root";
+$db['pass'] = $argv[6]; //""; // password is last argument in case it is blank
+
+
+
+// 	now connect to the customer database
+global $_dbcon;
+$_dbcon = mysql_connect($db['host'], $db['user'], $db['pass']);
+if (!$_dbcon) {
+	echo("Problem connecting to MySQL server at " . $db['host'] . " error:" . mysql_error() . "\n");
+	exit(-1);
+}
+if (!mysql_select_db($db['db'])) {
+	echo("Problem selecting database for " . $db['host'] . " error:" . mysql_error() . "\n");
+	exit(-1);
+}
+
+if (!QuickQuery("select count(*) from job where id=".$jobid)) {
+	echo("Error: job not found\n");
+	exit(-1);
+}
+
+$job = new Job($jobid);
+$job->generateSql();
+
+echo "new sql is: " . $job->thesql . "\n";
+
+$job->update();
+?>
