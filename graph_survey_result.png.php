ed67c4a3a6a32f492426f670b61aa15aa92b4ed2
Josh Lai <jlai@schoolmessenger.com>
2007-06-25 22:32:07 +0000

    new graph for survey

---
+++ graph_survey_result.png.php
@@ -0,0 +1,122 @@
+<?php
+
+
+include_once("inc/common.inc.php");
+include_once("inc/securityhelper.inc.php");
+include_once("obj/Job.obj.php");
+
+
+include ("jpgraph/jpgraph.php");
+include ("jpgraph/jpgraph_pie.php");
+include ("jpgraph/jpgraph_pie3d.php");
+include ("jpgraph/jpgraph_canvas.php");
+
+session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
+
+$jobid = $_GET['jobid'] + 0;
+//check userowns or customerowns and viewsystemreports
+if (!userOwns("job",$jobid) && !($USER->authorize('viewsystemreports') && customerOwns("job",$jobid))) {
+	redirect('unauthorized.php');
+}
+
+$jobstats = $_SESSION['jobstats'][$jobid];
+if ($_GET['valid'] != $jobstats['validstamp'])
+	redirect('unauthorized.php');
+
+$question = $_REQUEST['question']+0;
+
+//figure out how much space for the pie charts
+$piesperrow	= 2;
+$piesize 	= 50;
+$xoffset 	= 120;
+
+
+$yoffset	= 100;
+
+
+$startwidth	= 140;
+$widthdelta	= 95;
+
+$startheight= 120;
+$heightdelta= 95;
+
+$cols = min($piesperrow,count($jobstats['survey']['questions']));
+$rows = ceil(count($jobstats['survey']['questions'])/(float)$piesperrow);
+
+
+$width = $startwidth + $widthdelta;
+$height = $startheight + $heightdelta;
+
+
+//echo "c:$cols r:$rows w:$width h:$height<br>";
+//exit();
+
+
+$legend = range(1,9);
+$colorset = array("blue","red","green","yellow","orange","purple","cyan","lightslateblue","forestgreen");
+
+
+$graph = new PieGraph($width,$height,"auto");
+//$graph->SetShadow();
+$graph->SetFrame(false);
+$graph->SetAntiAliasing();
+
+//$graph->title->Set("Participants of People Contacted");
+//$graph->title->SetFont(FF_FONT1,FS_BOLD);
+
+//$graph->legend->Pos(0.0,0.05,"right","top");
+
+$i = 0;
+$question=$jobstats['survey']['questions'][$question];
+
+	$questiondata = $question['answers'];
+//	$questiondata = array (1=> 1,2,3,4,5,6,7,8,9);
+
+	$labels = array();
+	$data = array();
+	$colors = array();
+	foreach ($questiondata as $index => $value) {
+		if ($value) {
+			$data[] = $value;
+			$labels[] = "#" . ($index) . ": %.1f%%";
+			$colors[] = $colorset[$index-1];
+		}
+	}
+
+//var_dump(($data));
+//exit();
+
+	$p1 = new PiePlot(($data));
+
+//	$p1->SetLabelType(PIE_VALUE_ABS);
+
+	$p1->SetLabelType(PIE_VALUE_PER);
+
+	$p1->SetLabels($labels);
+	$p1->SetLabelPos(1);
+
+
+
+	$p1->title->Set($question['label']);
+	$p1->title->SetBox(array(255,255,255,0.6),false,false,0,1);
+	$p1->title->SetMargin(5);
+
+	$p1->SetSize($piesize);
+	$p1->SetCenter($xoffset, $yoffset);
+	$p1->SetGuideLines(true,true,false);
+	$p1->SetGuideLinesAdjust(1.2);
+
+//	$p1->value->SetFormat('');
+	$p1->value->Show();
+	$p1->value->SetFont(FF_FONT0);
+
+//	if ($i+1 == $cols)//only set on top right pie chart
+//		$p1->SetLegends(($legend));
+	$p1->SetSliceColors(array_reverse($colors));
+
+
+	$graph->Add($p1);
+
+$graph->Stroke();
+
+?>
