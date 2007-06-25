c9a57a13b7e13018e4698a87b4c3df7601db1118
Josh Lai <jlai@schoolmessenger.com>
2007-06-25 21:56:39 +0000

    changed functionality to be standalone instead of included

--- reportjobsurvey.php
+++ reportjobsurvey.php
@@ -1,5 +1,32 @@
 <?
 ////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+require_once("inc/common.inc.php");
+include_once("inc/securityhelper.inc.php");
+require_once("obj/Job.obj.php");
+require_once("obj/Phone.obj.php");
+require_once("inc/html.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/utils.inc.php");
+require_once("inc/formatters.inc.php");
+require_once("obj/FieldMap.obj.php");
+require_once("obj/SurveyQuestionnaire.obj.php");
+require_once("obj/SurveyQuestion.obj.php");
+require_once("obj/ReportInstance.obj.php");
+require_once("obj/ReportGenerator.obj.php");
+require_once("inc/form.inc.php");
+require_once("obj/UserSetting.obj.php");
+require_once("obj/SurveyReport.obj.php");
+require_once("obj/JobReport.obj.php");
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+if (!$USER->authorize('createreport') && !$USER->authorize('viewsystemreports')) {
+	redirect('unauthorized.php');
+}
+
+////////////////////////////////////////////////////////////////////////////////
 // Formatters
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -10,7 +37,12 @@ function fmt_survey_graph($row, $index){
 }
 
 function fmt_question($row, $index){
-	return "<div style='font-weight:bold'>$row[$index]</div><br><div>$row[2]</div>";	
+	return "<div style='font-weight:bold; text-decoration: underline'>$row[$index]</div><br><div>$row[2]</div>";	
+}
+
+function fmt_answer($row, $index){
+	$offset = $index+12;
+	return "<div style='font-weight:bold; text-decoration: underline'>" . (isset($row[$offset]) ? $row[$offset] : "") . "</div><br><div>$row[$index]</div>";	
 }
 
 
@@ -27,11 +59,17 @@ $orders = array("order1", "order2", "order3");
 
 if(isset($_REQUEST['reportid'])){
 	$reportinstance = new ReportInstance($_REQUEST['reportid']);
-	$reportgenerator = new ReportGenerator();
+	$options = $reportinstance->getParameters();
+	if($options['reporttype'] == "surveyreport"){
+		$reportgenerator = new SurveyReport();
+	} else {
+		$reportgenerator = new JobReport();
+	}
 	$reportgenerator->reportinstance = $reportinstance;
 	$reportgenerator->format = "html";
-	$params = $reportinstance->getParameters();
-	$jobid = $params['jobid'];
+	$reportgenerator->userid = $USER->id;
+
+	$jobid = $options['jobid'];
 	
 	$activefields = $reportinstance->getActiveFields();
 	foreach($fields as $field){
@@ -41,51 +79,42 @@ if(isset($_REQUEST['reportid'])){
 			$_SESSION['fields'][$field->fieldnum] = false;
 		}
 	}
-	$_SESSION['reporttype'] = $params['reporttype'];
 	$job = new Job($jobid);
-	$reportinstance->setParameters($params);
 	$_SESSION['saved_report'] = true;
-} else if (isset($_GET['jobid'])) {
-	$jobid = $_GET['jobid'] + 0;
-	//check userowns or customerowns and viewsystemreports
-	if (!userOwns("job",$jobid) && !($USER->authorize('viewsystemreports') && customerOwns("job",$jobid))) {
-		redirect('unauthorized.php');
-	}
-	if ($jobid) {
-	
-		$options = array("jobid" => $jobid);
+} else {
+	if (isset($_GET['jobid'])) {
+		$jobid = $_GET['jobid'] + 0;
+		//check userowns or customerowns and viewsystemreports
+		if (!userOwns("job",$jobid) && !($USER->authorize('viewsystemreports') && customerOwns("job",$jobid))) {
+			redirect('unauthorized.php');
+		}
+		if ($jobid) {
 		
-		$activefields = array();
-		$fieldlist = array();
-		foreach($fields as $field){
-			// used in html
-			$fieldlist[$field->fieldnum] = $field->name;
+			$options = array("jobid" => $jobid);
+			unset($_SESSION['jobstats'][$jobid]);
+			$job = new Job($jobid);	
 			
-			// used in pdf
-			if(isset($_SESSION['fields']['$field->fieldnum']) && $_SESSION['fields']['$field->fieldnum']){
-				$activefields[] = $field->fieldnum; 
-			}
-		}
-	
-		foreach($orders as $order){
-			$options[$order] = isset($_SESSION[$order]) ? $_SESSION[$order] : "";
-		}
-		
-		unset($_SESSION['jobstats'][$jobid]);
-		$job = new Job($jobid);	
-		
-		$options["reporttype"] = $_SESSION['reporttype'];
-		$reportinstance = new ReportInstance();
-		$reportinstance->setParameters($options);
-		$reportinstance->setFields($fieldlist);
-		$reportinstance->setActiveFields($activefields);
-		$reportgenerator = new ReportGenerator();
-		$reportgenerator->reportinstance = $reportinstance;
-		$reportgenerator->format = "html";
-	}
-	$_SESSION['saved_report'] = false;
-}
+			$options["reporttype"] = $_SESSION['reporttype'];
 
+		}
+		$_SESSION['saved_report'] = false;
+		$options=array("jobid" => $jobid);
+		$_SESSION['report']['options'] = $options;
+	} else {
+		$options = $_SESSION['report']['options'];
+	}
+	$reportinstance = new ReportInstance();
+	$reportinstance->setParameters($options);
+	if($options['reporttype'] == "surveyreport"){
+		$reportgenerator = new SurveyReport();
+	} else {
+		$reportgenerator = new JobReport();
+	}
+	$reportgenerator->reportinstance = $reportinstance;
+	$reportgenerator->format = "html";
+	$reportgenerator->userid = $USER->id;
+}
+$_SESSION['reporttype'] = $options['reporttype'];
 
 ////////////////////////////////////////////////////////////////////////////////
 // Display
@@ -101,55 +130,9 @@ include_once("nav.inc.php");
 
 //TODO buttons for notification log: download csv, view call details
 if (isset($jobid) && $jobid)
-	echo buttons(button('refresh', 'window.location.reload()'), button('done', 'window.history.go(-1)'));
+	echo buttons(button('refresh', 'window.location.reload()'), button('done', 'location.href=\'reports.php\''));
 else
 	buttons();
-
-
-//--------------- Select window ---------------
-startWindow("Select", NULL, false);
-?>
-<table border="0" cellpadding="3" cellspacing="0" width="100%">
-<tr>
-	<th align="right" class="windowRowHeader" valign="top" style="padding-top: 6px;">Job:</th>
-	<td width="1%">
-	<select name="jobid" id="jobid" onchange="location.href='?jobid=' + this.value">
-			<option value='0'>-- Select a Job --</option>
-<?
-if($_SESSION['reporttype'] == "jobreport"){
-	$jobs = DBFindMany("Job","from job where userid=$USER->id and deleted = 0 and status in ('active','complete','cancelled','cancelling') order by id desc");
-} else if($_SESSION['reporttype'] == "surveyreport"){
-	$jobs = DBFindMany("Job","from job where userid=$USER->id and deleted = 0 and status in ('active','complete','cancelled','cancelling') and questionnaireid is not null order by id desc");
-}
-
-foreach ($jobs as $job) {
-	echo '<option value="' . $job->id . '">' . htmlentities($job->name) . '</option>';
-}
-?>
-	</select>
-	<select id="jobid_archived" style="display: none" onchange="location.href='?jobid=' + this.value">
-			<option value='0'>-- Select a Job --</option>
-<?
-if($_SESSION['reporttype'] == "jobreport"){
-	$jobs = DBFindMany("Job","from job where userid=$USER->id and deleted = 2 and status!='repeating' order by id desc");
-} else if($_SESSION['reporttype'] == "surveyreport"){
-	$jobs = DBFindMany("Job","from job where userid=$USER->id and deleted = 2 and status in ('active','complete','cancelled','cancelling') and questionnaireid is not null order by id desc");
-}
-foreach ($jobs as $job) {
-	echo '<option value="' . $job->id . '">' . htmlentities($job->name) . '</option>';
-}
-?>
-	</select></td>
-	<td aligh="left"><input id="check_archived" type="checkbox" name="check_archived" value="true" onclick = "setHiddenIfChecked(this, 'jobid'); setVisibleIfChecked(this, 'jobid_archived'); ">
-	Show archived jobs</td>
-	</tr>
-	</table>
-
-<?
-endWindow();
-?>
-<br>
-<?
 
 if(isset($reportgenerator)){
 	$reportgenerator->generate();
