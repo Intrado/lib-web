8ce86b8655534f8d8c69ee2e91491f35717e4faf
Josh Lai <jlai@schoolmessenger.com>
2007-06-26 00:20:35 +0000

    updated to use manger settings

--- manager/common.inc.php
+++ manager/common.inc.php
@@ -1,6 +1,6 @@
 <?
-$SETTINGS = parse_ini_file("../inc/settings.ini.php",true);
-$IS_COMMSUITE = $SETTINGS['feature']['is_commsuite'];
+$SETTINGS = parse_ini_file("managersettings.ini.php",true);
+$IS_COMMSUITE = false;
 
 $_dbcon = mysql_connect($SETTINGS['db']['host'], $SETTINGS['db']['user'], $SETTINGS['db']['pass']);
 mysql_select_db($SETTINGS['db']['db'], $_dbcon);
