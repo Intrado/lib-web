580f1ee7f25a134d357fdb59476cc155a0626ac1
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2007-07-03 23:29:00 +0000

    add customer_url_prefix

--- manager/managersettings.ini.php
+++ manager/managersettings.ini.php
@@ -3,17 +3,18 @@
 ;authdb for manager use
 [db]
 persistent=true
-host="localhost"
+host="localhost:3306"
 user="root"
 pass=""
 db="authserver"
 
 
-
 [feature]
-log_dir="/commsuite/logs/"
+log_dir="/usr/commsuite/logs/"
 log_db_errors=true
 log_db_queries=true
 
+; server to redirect customer link to
+customer_url_prefix=https://asp.schoolmessenger.com
 
 ;*/?>
