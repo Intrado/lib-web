7671eecec4761cb5c19cbccdd61e5b68f102a896
Josh Lai <jlai@schoolmessenger.com>
2007-07-05 22:26:06 +0000

    added param for turning off user setting altering

--- checkboxfield.png.php
+++ checkboxfield.png.php
@@ -6,21 +6,32 @@ header ("Content-type: image/png");
 
 $toggle = $_REQUEST['toggle'];
 $field = $_REQUEST['field'];
-$usersetting = DBFind("UserSetting", "from usersetting where name ='" . DBSafe($field) . "' and userid = '$USER->id'");
-if($usersetting == null){
-	$usersetting = new UserSetting();
-	$usersetting->name = DBSafe($field);
-	$usersetting->userid = $USER->id;
-}
-if($toggle == "true"){
-	$_SESSION['fields'][$field] = true;
-	$usersetting->value = "true";
-	$usersetting->update();
-	readfile("img/checkbox-check.png");
-} else if ($toggle == "false"){
-	$_SESSION['fields'][$field] = false;
-	$usersetting->value="false";
-	$usersetting->update();
-	readfile("img/checkbox-clear.png");
+
+if(!$_REQUEST['saved']){
+	$usersetting = DBFind("UserSetting", "from usersetting where name ='" . DBSafe($field) . "' and userid = '$USER->id'");
+	if($usersetting == null){
+		$usersetting = new UserSetting();
+		$usersetting->name = DBSafe($field);
+		$usersetting->userid = $USER->id;
+	}
+	if($toggle == "true"){
+		$_SESSION['fields'][$field] = true;
+		$usersetting->value = "true";
+		$usersetting->update();
+		readfile("img/checkbox-check.png");
+	} else if ($toggle == "false"){
+		$_SESSION['fields'][$field] = false;
+		$usersetting->value="false";
+		$usersetting->update();
+		readfile("img/checkbox-clear.png");
+	}
+} else {
+	if($toggle == "true"){
+		$_SESSION['fields'][$field] = true;
+		readfile("img/checkbox-check.png");
+	} else if ($toggle == "false"){
+		$_SESSION['fields'][$field] = false;
+		readfile("img/checkbox-clear.png");
+	}
 }
 ?>
