6a3ed62e801e7117e9a37e43fbbb6d315a24bc52
Josh Lai <jlai@schoolmessenger.com>
2007-07-09 14:31:06 +0000

    moved sms changes to proper spot

--- db/asp.sql
+++ db/asp.sql
@@ -1255,6 +1255,45 @@ ALTER TABLE `voicereply` ADD INDEX ( `replytime` ) ;
 
 ------------------------------------------------------------
 
+
+
+CREATE TABLE `smsjob` (
+  `id` int(11) NOT NULL auto_increment,
+  `userid` int(11) NOT NULL,
+  `listid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `txt` varchar(160) NOT NULL,
+  `sendoptout` tinyint(4) NOT NULL,
+  `sentdate` datetime NOT NULL,
+  `status` enum('new','queued','sent','error') NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `status` (`status`)
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
+
+
+
+CREATE TABLE `smsmsg` (
+  `id` int(11) NOT NULL auto_increment,
+  `smsjobid` int(11) NOT NULL,
+  `personid` int(11) NOT NULL,
+  `sequence` tinyint(4) NOT NULL,
+  `phone` varchar(10) NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+
+ALTER TABLE `phone` ADD `smsenabled` TINYINT NOT NULL DEFAULT '0';
+
+ALTER TABLE `blockednumber` ADD `type` ENUM( 'call', 'sms', 'both' ) NOT NULL DEFAULT 'both';
+
+
+ALTER TABLE `smsjob` ADD `deleted` TINYINT NOT NULL DEFAULT '0';
+
+------------------------------
+
+
+
 drop table reportcompleted;
 
 
@@ -1605,39 +1644,5 @@ sec.loggedip, sec.resultdata
 from surveyemailcode sec
 inner join jobworkitem wi on (wi.id = sec.jobworkitemid);
 
-
-
-CREATE TABLE `smsjob` (
-  `id` int(11) NOT NULL auto_increment,
-  `userid` int(11) NOT NULL,
-  `listid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `description` varchar(50) NOT NULL,
-  `txt` varchar(160) NOT NULL,
-  `sendoptout` tinyint(4) NOT NULL,
-  `sentdate` datetime NOT NULL,
-  `status` enum('new','queued','sent','error') NOT NULL,
-  PRIMARY KEY  (`id`),
-  KEY `status` (`status`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1;
-
-
-
-CREATE TABLE `smsmsg` (
-  `id` int(11) NOT NULL auto_increment,
-  `smsjobid` int(11) NOT NULL,
-  `personid` int(11) NOT NULL,
-  `sequence` tinyint(4) NOT NULL,
-  `phone` varchar(10) NOT NULL,
-  PRIMARY KEY  (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
-
-ALTER TABLE `phone` ADD `smsenabled` TINYINT NOT NULL DEFAULT '0';
-
-ALTER TABLE `blockednumber` ADD `type` ENUM( 'call', 'sms', 'both' ) NOT NULL DEFAULT 'both';
-
-
-ALTER TABLE `smsjob` ADD `deleted` TINYINT NOT NULL DEFAULT '0';
 
 
