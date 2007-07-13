c5378d83d1223dc0d29beadc921aca500cc9d399
Josh Lai <jlai@schoolmessenger.com>
2007-07-13 17:49:05 +0000

    removed unnecessary code changed query method changed method of parsing/displaying data

--- obj/DrillDownReport.obj.php
+++ obj/DrillDownReport.obj.php
@@ -6,108 +6,118 @@ class DrillDownReport extends ReportGenerator{
 		$instance = $this->reportinstance;
 		$params = $this->params = $instance->getParameters();
 		$this->reporttype = $params['reporttype'];
-		$orders = array("order1", "order2", "order3");
-		$orderquery = "";
-		foreach($orders as $order){
-			if(!isset($params[$order]))
-				continue;
-			$orderby = $params[$order];
-			if($orderby == "") continue;
-			if($orderquery == ""){
-				$orderquery = " order by ";
-			} else {
-				$orderquery .= ", ";
-			}
-			$orderquery .= $orderby;
-		}
-		$rulesql = "";
 		$fieldquery = generateFields("rp");
 		$id = $params['personid'];
 		$jobid = $params['jobid'];
 		$firstnamefield = FieldMap::getFirstNameField();
 		$lastnamefield = FieldMap::getLastNameField();
 		$this->query = "Select
-				j.name as jobname,
 				rp.pkey as pkey, 
 				rp.$firstnamefield as firstname, 
-				rp.$lastnamefield as lastname, 
-				coalesce(rc.phone, rc.email) as destination,
-				rp.type as jobtype, 
-				rc.attemptdata as result,
-				rc.numattempts as numattempts,
-				rc.sequence as sequence,
-				coalesce(rc.result, rp.status) as finalresult 
+				rp.$lastnamefield as lastname,
+				j.name
 				$fieldquery
 				 from reportperson rp
-				left join reportcontact rc on (rp.jobid = rc.jobid and rp.personid = rc.personid and rp.type = rc.type)
 				inner join job j on (j.id = rp.jobid)
 				where rp.personid = '$id'
-				and j.id = '$jobid'";
+				and rp.jobid = '$jobid'
+				group by rp.personid";
 	}
 	
 	function runHtml($params = null){
+		
+		$id = $this->params['personid'];
+		$jobid = $this->params['jobid'];
 		$fieldlist = $this->reportinstance->getFields();	
-		$result = Query($this->query);
-		$info = array();
-		while($row = DBGetRow($result)){
-			$info[] = $row;
-		}
+		$info = QuickQueryRow($this->query);
 		
-		$firstname = $info[0][2];
-		$lastname = $info[0][3];
-		$jobname = $info[0][0];
-		$pkey = $info[0][1];
+		$firstname = $info[1];
+		$lastname = $info[2];
+		$pkey = $info[0];
 		$field = array();
-		for($i = 10; $i<10+count($fieldlist);$i++){
-			$field[] = $info[0][$i];
+		for($i = 3; $i<3+count($fieldlist);$i++){
+			$field[] = $info[$i];
 		}
-		
-		$totalattempts = 0;
-		$attempts = array();
-		foreach($info as $index => $dest){
-			$tmp = explode(",", $dest[6]);
-			$count=0;
-			foreach($tmp as $attempt){
-				if($attempt == "" || $attempt == null){
-					$attempt = array(($dest[5] == "email" && $dest[4]=="") ? "No Email Address" : $dest[4], $dest[8], "", $dest[9]);
-				} else {
-					list($time, $result) = explode(":", $attempt);
-					if($count>0){
-						$attempt = array("","",$time, $result);
-					} else {
-						$attempt = array($dest[4], $dest[8], $time, $result);
-						$count++;
-					}
-				}
-				$totalattempts++;
-				$attempts[] = $attempt;
+		$phonequery = "Select
+				rc.phone as destination,
+				rc.attemptdata as result,
+				rc.result as finalresult 
+				from reportcontact rc
+				where rc.personid = '$id'
+				and rc.jobid = '$jobid'
+				and rc.type = 'phone'";
+		$emailquery = "Select
+				rc.email as destination,
+				rc.starttime as result,
+				rc.result as finalresult 
+				from reportcontact rc
+				where rc.personid = '$id'
+				and rc.jobid = '$jobid'
+				and rc.type = 'email'";
 				
+		$phoneres = Query($phonequery);
+		$phonedata = array();
+		while($row = DBGetRow($phoneres)){
+			$phonedata[] = $row;
+		}
+		$emailres = Query($emailquery);
+		$emaildata = array();
+		while($row = DBGetRow($emailres)){
+			$emaildata[] = $row;
+		}
+		$totalattempts = 0;
+		$tempattempts = array();
+		$phoneattempts = array();
+		$order = array();
+		$count=0;
+		foreach($phonedata as $dest){
+			if($dest[0] == null) continue;
+			$tmp = explode(",", $dest[1]);
+			
+			foreach($tmp as $attempt){
+				$time = 0;
+				$result = 0;
+				if($attempt != "" || $attempt != null){
+					list($time, $result) = explode(":", $attempt);
+					$attempt = array($dest[0], $time, $result);
+					$totalattempts++;
+				} else if($dest[2] != null){
+					$attempt = array($dest[0], "", $dest[2]);
+					$totalattempts++;
+				} else {
+					continue;
+				}
+				$tempattempts[$count] = $attempt;
+				$order[$count] = $time;
+				$count++;
 			}
 		}
-	
+		if(asort($order, SORT_NUMERIC)){
+			foreach($order as $count => $time){
+				$phoneattempts[] = $tempattempts[$count];
+			}
+		} else { 
+			$phoneattempts = $tempattempts;
+		}
+		
+		foreach($emaildata as $index => $dest){
+			if($dest[0] == null)
+				unset($emaildata[$index]);
+		}
+		
 		startWindow("Information", "padding: 3px;"); 
 		?>
-			<table width="100%" cellpadding="3" cellspacing="1" class="list" id="fields">
-				<tr class="listheader">
-					<td>ID#:</td>
-					<td>First Name:</td>
-					<td>Last Name:</td>
-					<td>Job Name:</td>
-<?
-					foreach($fieldlist as $index => $fieldname){
-						?><td><?=$fieldname?></td><?
-					}
-?>
-				</tr>
+			<table width="100%" cellpadding="3" cellspacing="1">
+				<tr><th align="right" class="windowRowHeader bottomBorder">ID#:</th><td  class="bottomBorder"><?=$pkey?></td></tr>
+				<tr><th align="right" class="windowRowHeader bottomBorder">First Name:</th><td class="bottomBorder"><?=$firstname?></td></tr>
+				<tr><th align="right" class="windowRowHeader bottomBorder">Last Name:</th><td class="bottomBorder"><?=$lastname?></td></tr>
 				<tr>
-					<td><?=$pkey?></td>
-					<td><?=$firstname?></td>
-					<td><?=$lastname?></td>
-					<td><?=$jobname?></td>
 <?
-					for($i=0; $i<count($fieldlist); $i++){
-						?><td><?=$field[$i]?></td><?
-					}
+				$i = 0;
+				foreach($fieldlist as $index => $fieldname){
+					?><tr><th align="right" class="windowRowHeader bottomBorder"><?=$fieldname?></th><td class="bottomBorder"><?=$field[$i]?></td></tr><?
+					$i++;
+				}
 ?>
 				</tr>
 			</table>
@@ -117,24 +127,40 @@ class DrillDownReport extends ReportGenerator{
 		<br>
 		<?
 		 
-		startWindow("Call Attempts");
+		startWindow("Phone Attempts");
 		
-		$titles = array("0" => "Destination",
-						"1" => "Sequence",
-						"2" => "Date/Time",
-						"3" => "Result");
+		$titles = array("0" => "Phone#",
+						"1" => "Date/Time",
+						"2" => "Result");
 		$formatters = array("0" => "fmt_destination",
-							"1" => "fmt_sequence",
-							"2" => "fmt_ms_timestamp",
-							"3" => "job_status");
+							"1" => "fmt_ms_timestamp",
+							"2" => "job_status");
 		?>
 		<table width="100%" cellpadding="3" cellspacing="1" class="list" >
 		<?
-			showTable($attempts, $titles, $formatters);
+			showTable($phoneattempts, $titles, $formatters);
 		?>
 		</table>
 		<?
 		endWindow();
+		
+		?><br><?
+		
+		startWindow("Email Attempts");
+		
+		$titles = array("0" => "Email",
+						"1" => "Date/Time");
+		$formatters = array("1" => "fmt_ms_timestamp");
+		?>
+		<table width="100%" cellpadding="3" cellspacing="1" class="list" >
+		<?
+			showTable($emaildata, $titles, $formatters);
+		?>
+		</table>
+		<?
+		endWindow();
+		
+		
 	}
 	
 	function setReportFile(){
