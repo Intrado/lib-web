28bc72ec9aa26c598ed9005404da5f7b5f31e200
Josh Lai <jlai@schoolmessenger.com>
2007-07-13 17:52:21 +0000

    removed saved report case added reportutil to includes

--- contactresult.php
+++ contactresult.php
@@ -8,6 +8,7 @@ require_once("inc/table.inc.php");
 require_once("inc/html.inc.php");
 require_once("inc/form.inc.php");
 require_once("inc/utils.inc.php");
+require_once("inc/reportutils.inc.php");
 require_once("inc/formatters.inc.php");
 require_once("obj/FieldMap.obj.php");
 require_once("obj/Phone.obj.php");
@@ -64,39 +65,23 @@ $lastname = DBFind("FieldMap", "from fieldmap where options like '%lastname%'");
 $orders = array("order1", "order2", "order3");
 
 
-if(isset($_REQUEST['reportid'])){
-	$reportinstance = new ReportInstance($_REQUEST['reportid']+0);
-	$activefields = $reportinstance->getActiveFields();
-	foreach($fields as $field){
-		if(in_array($field->fieldnum, $activefields)){
-			$_SESSION['fields'][$field->fieldnum] = true;
-		} else {
-			$_SESSION['fields'][$field->fieldnum] = false;
-		}
-	}
-	$options = $reportinstance->getParameters();
+$options = isset($_SESSION['contacts']['options']) ? $_SESSION['contacts']['options'] : array();
+$activefields = array();
+$fieldlist = array();
+foreach($fields as $field){
+	// used in html
+	$fieldlist[$field->fieldnum] = $field->name;
 	
-	$_SESSION['saved_report'] = true;
-	$_SESSION['contacts']['options'] = $options;
-} else {
-	$options = isset($_SESSION['contacts']['options']) ? $_SESSION['contacts']['options'] : array();
-	$activefields = array();
-	$fieldlist = array();
-	foreach($fields as $field){
-		// used in html
-		$fieldlist[$field->fieldnum] = $field->name;
-		
-		// used in pdf
-		if(isset($_SESSION['fields']['$field->fieldnum']) && $_SESSION['fields']['$field->fieldnum']){
-			$activefields[] = $field->fieldnum; 
-		}
+	// used in pdf
+	if(isset($_SESSION['fields'][$field->fieldnum]) && $_SESSION['fields'][$field->fieldnum]){
+		$activefields[] = $field->fieldnum; 
 	}
-	$reportinstance = new ReportInstance();
-	
-	$reportinstance->setFields($fieldlist);
-	$reportinstance->setActiveFields($activefields);
-	$_SESSION['saved_report']=false;
 }
+$reportinstance = new ReportInstance();
+
+$reportinstance->setFields($fieldlist);
+$reportinstance->setActiveFields($activefields);
+
 $pagestart = 0;
 if(isset($_REQUEST['pagestart'])){
 	$pagestart = $_REQUEST['pagestart'];
@@ -161,7 +146,7 @@ $TITLE = "Contact Search";
 include_once("nav.inc.php");
 NewForm($f);
 
-buttons(button("back", "window.history.go(-1)"));
+buttons(button("back", "location.href='contactsearch.php'"),submit($f, $s, "refresh", "refresh"));
 startWindow("Display Options", "padding: 3px;");
 ?>
 <table border="0" cellpadding="3" cellspacing="0" width="100%">
@@ -196,7 +181,6 @@ startWindow("Display Options", "padding: 3px;");
 <?
 			}
 ?>
-				<td><? echo submit($f, $s, "refresh", "refresh");?></td>
 				</tr>
 				
 			</table>
