5b8fdb1fc68a5315ae816f4f07392ca9c916a75a
Josh Lai <jlai@schoolmessenger.com>
2007-07-13 22:38:10 +0000

    seperated email and phone participation graphs removed unnecessary code

--- obj/SurveyReport.obj.php
+++ obj/SurveyReport.obj.php
@@ -46,164 +46,48 @@ class SurveyReport extends ReportGenerator{
 		//////////////////////////////////////
 		
 		$jobid = $this->params['jobid'];
-		if ($jobid) {
-			$job = new Job($jobid);
-			
-			//TODO check if there are new workitems, then display a pie chart with % queued
-			if (QuickQuery("select personid from reportperson where status='new' and jobid='$jobid' limit 1")) {
-				$isprocessing = true;
-			} else {
-				$isprocessing = false;
-		
-				$validstamp = time();
-				$jobstats = array ("validstamp" => $validstamp);
-				$jobtypes = explode(",",$job->type);
-		
-				//--------------- SURVEY ---------------
-				if(in_array("survey",$jobtypes) !== false) {
-		
-					$questionnaire = new SurveyQuestionnaire($job->questionnaireid);
-					if ($questionnaire->hasphone)
-						$jobtypes[] = "phone";
-					if ($questionnaire->hasweb)
-						$jobtypes[] = "email";
-		
-				}
-				
-				//--------------- PHONE ---------------
-				if(in_array("phone",$jobtypes) !== false) {
-					//people, dupes, contacted, notcontacted, %complete (actually from phone)
-		
-					$query = "select count(*) as cnt, rp.status
-								from reportperson rp
-								where rp.jobid='$jobid' and rp.type='phone' group by rp.status";
-					//then need to stitch the results back together by summing them.
-		
-					$totalpeople = 0;
-					$duplicates = 0;
-					$contacted = 0;
-					$notcontacted = 0;
-					$result = Query($query);
-					while ($row = DBGetRow($result)) {
-						$totalpeople += $row[0];
-		
-						if ($row[1] == "success")
-							$contacted += $row[0];
-						else if ($row[1] == "duplicate")
-							$duplicates += $row[0];
-						else
-							$notcontacted += $row[0];
-					}
-		
-					//phones by cp
-					$maxcallattempts = QuickQuery("select value from jobsetting where name='maxcallattempts' and jobid = '$jobid'");
-					$query = "select count(*) as cnt, rc.result, sum(rp.status not in ('success','fail') and rc.numattempts < $maxcallattempts) as remaining
-								from reportperson rp 
-								left join reportcontact rc on (rc.jobid = rp.jobid and rc.type = rp.type and rc.personid = rp.personid)
-					where rp.jobid = '$jobid'
-					and rp.status != 'duplicate' and rp.type='phone'
-					group by rc.result";
-					//may need to clean up, null means not called yet
-					//do math for the % completed
-					$cpstats = array (
-						"C" => 0,
-						"A" => 0,
-						"M" => 0,
-						"N" => 0,
-						"B" => 0,
-						"X" => 0,
-						"F" => 0,
-						"nullcp" => 0
-					);
-					$remainingcalls = 0;
-					$totalcalls = 0;
-					$result = Query($query);
-					while ($row = DBGetRow($result)) {
-						$totalcalls += $row[0];
-						$index = $row[1] !== NULL ? $row[1] : "nullcp";
-						$cpstats[$index] += $row[0];
-						if ($row[1] != "A" && $row[1] != "M") {
-							$remainingcalls += $row[2];
-						}
-					}
-		
-					$jobstats["phone"] = $cpstats; //start with the cp codes
-					//add people stats
-					$jobstats["phone"]["totalpeople"] = $totalpeople;
-					$jobstats["phone"]["duplicates"] = $duplicates;
-					$jobstats["phone"]["contacted"] = $contacted;
-					$jobstats["phone"]["notcontacted"] = $notcontacted;
-		
-					$jobstats["phone"]["remainingcalls"] = $remainingcalls;
-					$jobstats["phone"]["totalcalls"] = $totalcalls;
-					$jobstats["phone"]["percentcomplete"] = $totalcalls ? ($totalcalls - $remainingcalls)/$totalcalls : 0;
-		
-				}
-				//-------------------------------------
-		
-				//--------------- EMAIL ---------------
-				if(in_array("email",$jobtypes) !== false) {
-					//email people, emails, % sent
-					$query = "select count(*)
-								from reportperson rp
-								where rp.jobid='$jobid' and rp.type='email'";
-		
-					$emailpeople = QuickQuery($query);
-		
-					$query = "select count(*) totalemails, sum(rc.numattempts>0) as sent
-								from reportperson rp 
-								left join reportcontact rc on (rc.jobid = rp.jobid and rc.type = rp.type and rc.personid = rp.personid)
-								where rp.jobid='$jobid' and rp.type='email'";
-					list($totalemails, $sentemails) = QuickQueryRow($query);
-		
-					$jobstats["email"] = array();
-					$jobstats["email"]["emailpeople"] = $emailpeople;
-					$jobstats["email"]["totalemails"] = $totalemails;
-					$jobstats["email"]["sentemails"] = $sentemails;
-					$jobstats["email"]["percentsent"] = $sentemails/$totalemails;
-		
-		
-				}
-				//-------------------------------------
-		
-				//--------------- PRINT ---------------
-				if(in_array("print",$jobtypes) !== false) {
-					//print people %sent
-					$query = "select count(*) as totoal, sum(rp.status='success') as printed
-								from reportperson rp
-								where rp.jobid='$jobid' and rp.type='print'";
-					list($totalprint, $printed) = QuickQueryRow($query);
-		
-					$jobstats["print"] = array();
-					$jobstats["print"]["totalprint"] = $totalprint;
-					$jobstats["print"]["printed"] = $printed;
-				}
-				//-------------------------------------
-		
-				//save all these stats to the session with a jobid and timestamp so we can use them in the pie charts
-				$_SESSION['jobstats'][$jobid] = $jobstats;
-				$urloptions = "jobid=$jobid&valid=$validstamp";
-			}
+		if(!$jobid){
+			error_log("Ran survey report without a jobid");
+			exit();
 		}
-
-		$jobstats = $_SESSION['jobstats'][$jobid];
-		$validstamp = $jobstats['validstamp']; 
+		$job = new Job($jobid);
+		$questionnaire = new SurveyQuestionnaire($job->questionnaireid);
+		$validstamp = time();
+		$jobstats = array ("validstamp" => $validstamp);
+		$jobstats['survey'] = array();
+		$jobstats['phone'] = array();
+		$jobstats['email'] = array();
+		
+		$query = "select sum(rp.status='success' and rc.result='A' and rc.participated=1), count(*) as cnt
+			from reportperson rp 
+			left join reportcontact rc on (rc.jobid = rp.jobid and rc.type = rp.type and rc.personid = rp.personid)
+			where rp.jobid='$jobid'";
+			
+		$result = QuickQueryRow($query);
+		$phoneparticipants = 0;
+		$contacted = 0;
+		if($result[0] != 0){
+			$phoneparticipants = $result[0];
+			$contacted = $result[1];
+		}
+		$jobstats["survey"]["phoneparticipants"] = $phoneparticipants;
+		$jobstats["phone"]["contacted"] = $contacted;
+		
 		$urloptions = "jobid=$jobid&valid=$validstamp";
-		//% people participated (subset of contacted)
-		$query = "select count(*) as cnt
-					from reportperson rp 
-					left join reportcontact rc on (rc.jobid = rp.jobid and rc.type = rp.type and rc.personid = rp.personid)
-					where rp.jobid='$jobid'
-					and rp.status='success' and rc.result='A' and rc.participated=1";
-		$phoneparticipants = QuickQuery($query);
-	
-		//TODO: fix jobworkitem related code
+
 		$query = "select count(*) from surveyweb sw
 				inner join reportperson rp on (rp.personid = sw.personid and rp.jobid = sw.jobid)
 				where sw.status='web' and sw.jobid=$jobid";
 	
-		$emailparticipants = QuickQuery($query);
-	
+		$result = QuickQueryRow($query);
+		$sentemails=0;
+		$emailparticipants = 0;
+		if($result[0] != 0){
+			$emailparticipants = $result[0];
+			$sentemails = $result[1];
+		}
+		$jobstats["survey"]["emailparticipants"] = $emailparticipants;
+		$jobstats["email"]["sentemails"] = $sentemails;
 		$query = $this->query;
 		
 		$res = Query($query);
@@ -217,7 +101,7 @@ class SurveyReport extends ReportGenerator{
 			$questiontext[$row[0]] = $row[4] != NULL ? $row[4] : "";
 		}
 	
-		$jobstats['survey'] = array();
+		
 		$jobstats['survey']['phoneparticipants'] = $phoneparticipants;
 		$jobstats['survey']['emailparticipants'] = $emailparticipants;
 		$jobstats['survey']['questions'] = $questions;
@@ -239,11 +123,27 @@ class SurveyReport extends ReportGenerator{
 						<table border="0" cellpadding="3" cellspacing="0" width="100%">
 							<tr>
 								<td>
-					<? if ($jobstats['survey']['phoneparticipants'] || $jobstats['survey']['emailparticipants']) { ?>
-									<img src="graph_survey_participation.png.php?<?= $urloptions ?>">
-					<? } else { ?>
-									No one has yet participated in this survey.
-					<? } ?>
+					<? 
+						$noparticipants=true;
+						if ($jobstats['survey']['phoneparticipants'] ){ 
+							$noparticipants=false;
+					?>
+									<div><img src="graph_survey_phone_participation.png.php?<?= $urloptions ?>"></div>
+					<? 
+						}
+						
+						if ($jobstats['survey']['emailparticipants'] ){ 
+							$noparticipants=false;
+					?>
+									<div style="float; left"><img src="graph_survey_email_participation.png.php?<?= $urloptions ?>"></div>
+					<? 
+						}
+						if($noparticipants){
+							?>No one has yet participated in this survey.<?
+						}
+					?>
+					
+					
 								</td>
 								<td>
 									<table width="100%" cellpadding="3" cellspacing="1" class="list">
