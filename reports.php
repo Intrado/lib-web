6439450467b367fe798232b9635c6b2dee76aa09
Josh Lai <jlai@schoolmessenger.com>
2007-07-13 18:05:37 +0000

    added date formatters for timestamps changed naming of actions

--- reports.php
+++ reports.php
@@ -25,15 +25,19 @@ if (!$USER->authorize('createreport') && !$USER->authorize('viewsystemreports'))
 ////////////////////////////////////////////////////////////////////////////////
 
 function fmt_report_actions($obj){
-	return "<a href='reportedit.php?reportid=$obj->id' >Edit</a>&nbsp;|&nbsp;<a href='reportsavedoptions.php?reportid=$obj->id&runreport=true' >Run Report</a>&nbsp;|&nbsp;<a href='reportsavedoptions.php?reportid=$obj->id' >Options</a>&nbsp;|&nbsp;<a href='reports.php?delete=$obj->id' onclick='return confirm(\"Are you sure you want to delete this?\")';>Delete</a>";
+	return "<a href='reportedit.php?reportid=$obj->id' >Edit Schedule</a>&nbsp;|&nbsp;<a href='reportsavedoptions.php?reportid=$obj->id' >Edit Options</a>&nbsp;|&nbsp;<a href='reportsavedoptions.php?reportid=$obj->id&runreport=true' >View</a>&nbsp;|&nbsp;<a href='reports.php?delete=$obj->id' onclick='return confirm(\"Are you sure you want to delete this?\")';>Delete</a>";
 }
 
 function fmt_next_run($obj){
-	return $obj->nextrun;
+	if($obj->nextrun != null)
+		return date("M j, Y g:i a", strtotime($obj->nextrun));
+	return "";
 }
 
 function fmt_last_run($obj){
-	return $obj->lastrun;
+	if($obj->lastrun != null)
+		return date("M j, Y g:i a", strtotime($obj->lastrun));
+	return "";
 }
 
 function fmt_report_type($obj){
@@ -64,18 +68,18 @@ $TITLE= "Reports";
 
 include("nav.inc.php");
 
-startWindow("Custom Reports");
+startWindow("Standard Reports");
 ?>
 	<table width="100%" cellpadding="3" cellspacing="2" class="list" >
 		<tr>
 			<td><a href='reportjob.php?clear=1'/>Jobs</a></td>
-			<td><a href='reportcallssearch.php?clear=1&callsreport=1'/>Individual Calls Report</a></td>
-			<td><a href='reportcallssearch.php?clear=1&attendance=1'/>Attendance Calls</a></td>
+			<td><a href='reportcallssearch.php?clear=1&type=callsreport'/>Individual's Report</a></td>
+			<td><a href='reportcallssearch.php?clear=1&type=attendance'/>Attendance</a></td>
 		</tr>
 		<tr>
 			<td><a href='reportsurvey.php?clear=1'/>Surveys</a></td>
-			<td><a href='reportcallssearch.php?clear=1&undelivered=1'/>Undelivered Calls</a></td>
-			<td><a href='reportcallssearch.php?clear=1&emergency=1'/>Emergency Calls</a></td>
+			<td><a href='reportcallssearch.php?clear=1&type=undelivered'/>Undelivered</a></td>
+			<td><a href='reportcallssearch.php?clear=1&type=emergency'/>Emergency</a></td>
 		</tr>
 	</table>
 <?
@@ -88,18 +92,17 @@ $data = DBFindMany("ReportSubscription", "from reportsubscription where userid =
 $titles = array("name" => "Name",
 				"description" => "Description",
 				"Type"	=> "Type",
-				"Next Run" => "Next Run",
+				"Next Scheduled Run" => "Next Scheduled Run",
 				"Last Run" => "Last Run",
 				"Actions" => "Actions");
 $formatters = array("Actions" => "fmt_report_actions",
-					"Next Run" => "fmt_next_run",
+					"Next Scheduled Run" => "fmt_next_run",
 					"Type" => "fmt_report_type",
 					"Last Run" => "fmt_last_run");
 $scroll = false;
 
 
-startWindow("Saved Reports");
-button_bar(button("newreport", null, "reportedit.php"));
+startWindow("My Reports");
 showObjects($data, $titles, $formatters, $scroll, true);
 EndWindow();
 include("navbottom.inc.php");
