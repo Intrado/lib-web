6a945a8ecc3775795218367be5918144b12ece73
Josh Lai <jlai@schoolmessenger.com>
2007-07-30 23:12:18 +0000

    new sql applier for all customer databases. be very careful with this as it will execute sql to all customers in the auth server. sql must be $$$ delimited

---
+++ build/apply_customer_sql.php
@@ -0,0 +1,37 @@
+<?
+if($argc < 2)
+	exit ("Please specify the file with sql you would like to apply.");
+
+$sqlqueries = explode("$$$",file_get_contents($argv[1]));
+
+$authhost="localhost:3306";
+$authuser="root";
+$authpass="";
+$authdb="authserver";
+
+$auth = mysql_connect($authhost, $authuser, $authpass)
+			or die("Could not connect to auth: " . mysql_error($authdb));
+mysql_select_db($authdb, $auth);
+
+$query = "select c.id, s.dbhost, s.dbusername, s.dbpassword from shard s inner join customer c on (c.shardid = s.id)";
+$res = mysql_query($query, $auth);
+$data = array();
+while($row = mysql_fetch_row($res)){
+	$data[] = $row;
+}
+
+foreach($data as $customer){
+	$custdb = mysql_connect($customer[1], $customer[2], $customer[3])
+				or die("Could not connect to customer: " . mysql_error($custdb));
+	mysql_select_db("c_$customer[0]", $custdb)
+				or die("Could not select customer db: " . mysql_error($custdb));
+
+	foreach ($sqlqueries as $sqlquery) {
+		if (trim($sqlquery)){
+			mysql_query($sqlquery,$custdb)
+				or die ("Failed to execute sql: " . mysql_error($custdb));
+		}
+	}
+}
+
+?>
