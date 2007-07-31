ac6cfd0e4995dec79d9c82a6bb14f6f008d847c1
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2007-07-31 23:59:13 +0000

    export from test db, no longer append the changes in this release.

--- db/shard.sql
+++ db/shard.sql
@@ -1,83 +1,12 @@
--- --------------------------------------------------------
-
-CREATE TABLE qjob (
-  customerid int(11) NOT NULL,
-  id int(11) NOT NULL,
-  userid int(11) NOT NULL default '0',
-  scheduleid int(11) default NULL,
-  listid int(11) NOT NULL default '0',
-  phonemessageid int(11) default NULL,
-  emailmessageid int(11) default NULL,
-  printmessageid int(11) default NULL,
-  questionnaireid int(11) default NULL,
-  `timezone` varchar(50) NOT NULL,
-  startdate date NOT NULL default '0000-00-00',
-  enddate date NOT NULL default '0000-00-00',
-  starttime time NOT NULL default '00:00:00',
-  endtime time NOT NULL default '00:00:00',
-  `status` enum('new','processing','active','cancelling','repeating') NOT NULL default 'new',
-  `phonetaskcount` int(11) NOT NULL default '0',
-  systempriority tinyint(4) NOT NULL default '3',
-  timeslices smallint(6) NOT NULL default '0',
-  `thesql` text,
-  PRIMARY KEY  (customerid,id),
-  KEY `status` (`status`,id),
-  KEY startdate (startdate),
-  KEY enddate (enddate),
-  KEY starttime (starttime),
-  KEY endtime (endtime),
-  KEY startdate_2 (startdate,enddate,starttime,endtime,id),
-  KEY scheduleid (scheduleid)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--
+-- Database: `aspshard`
+--
 
 -- --------------------------------------------------------
 
-CREATE TABLE `qjobsetting` (
-  `customerid` int(11) NOT NULL,
-  `jobid` int(11) NOT NULL,
-  `name` varchar(50) NOT NULL,
-  `value` varchar(255) NOT NULL default '',
-  PRIMARY KEY  (`customerid`,`jobid`,`name`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
-CREATE TABLE qjobtask (
-  customerid int(11) NOT NULL default '0',
-  jobid int(11) NOT NULL default '0',
-  `type` enum('phone','email','print','sms') NOT NULL,
-  personid int(11) NOT NULL default '0',
-  sequence tinyint(4) NOT NULL default '0',
-  `status` enum('active','pending','assigned','progress','waiting','throttled') NOT NULL,
-  attempts tinyint(4) NOT NULL default '0',
-  renderedmessage text,
-  lastresult enum('A','M','N','B','X','F','sent','unsent','printed','notprinted') default NULL,
-  lastresultdata text,
-  lastduration float default NULL,
-  lastattempttime bigint(20) default NULL,
-  nextattempttime bigint(20) default NULL,
-  leasetime bigint(20) default NULL,
-  phone varchar(20) default NULL,
-  uuid varchar(255) NOT NULL,
-  PRIMARY KEY  (customerid,jobid,`type`,personid,sequence),
-  KEY dispatch (jobid,`status`,attempts,sequence),
-  UNIQUE uuid (uuid)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- --------------------------------------------------------
-
-CREATE TABLE qschedule (
-  `customerid` int(11) NOT NULL,
-  `id` int(11) NOT NULL,
-  `daysofweek` varchar(20) NOT NULL,
-  `time` time NOT NULL default '00:00:00',
-  nextrun datetime default NULL,
-  PRIMARY KEY  (customerid,id),
-  KEY nextrun (nextrun)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+--
+-- Table structure for table `importqueue`
+--
 
 CREATE TABLE `importqueue` (
   `id` int(11) NOT NULL auto_increment,
@@ -85,10 +14,13 @@ CREATE TABLE `importqueue` (
   `localimportid` int(11) NOT NULL,
   PRIMARY KEY  (`id`),
   UNIQUE KEY `import` (`customerid`,`localimportid`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
+
+--
+-- Table structure for table `jobstatdata`
+--
 
 CREATE TABLE `jobstatdata` (
   `jobid` int(11) NOT NULL,
@@ -103,60 +35,155 @@ CREATE TABLE `jobstatdata` (
 
 -- --------------------------------------------------------
 
-CREATE TABLE `qreportsubscription` (
-  customerid int(11) NOT NULL,
-  `id` int(11) NOT NULL,
-  `userid` int(11) NOT NULL default '0',
-  `type` enum('notscheduled','once','weekly','monthly') NOT NULL default 'notscheduled',
-  daysofweek varchar(20) default NULL,
-  dayofmonth tinyint(4) default NULL,
-  `timezone` varchar(50) NOT NULL,
-  `nextrun` datetime default NULL,
-  `time` TIME default NULL,
-  email text NOT NULL,
-  PRIMARY KEY  (customerid, `id`),
-  KEY nextrun (nextrun)
-) TYPE=InnoDB;
-
--- --------------------------------------------------------
-
-CREATE TABLE specialtaskqueue (
-  id int(11) NOT NULL auto_increment,
-  customerid int(11) NOT NULL,
-  localspecialtaskid int(11) NOT NULL,
-  uuid varchar(255) default NULL,
-  `status` enum('new','assigned') NOT NULL default 'new',
-  `type` varchar(50) default NULL,
-  leasetime bigint(20) default NULL,
-  PRIMARY KEY  (id),
-  UNIQUE KEY specialtask (customerid,localspecialtaskid),
-  UNIQUE KEY uuid (uuid)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
-
--- -------------------------------------------------------------------------------------------------------------
--- ---- July 13, 2007
--- -------------------------------------------------------------------------------------------------------------
-
-ALTER TABLE `qjobtask` ADD INDEX `waiting` ( `status` , `nextattempttime` ) ;
-ALTER TABLE `qjobtask` ADD INDEX `progresshandler` ( `status` , `lastattempttime` ) ;
-ALTER TABLE `qjobtask` ADD INDEX `emailer` ( `type` , `nextattempttime` ) ;
-ALTER TABLE `qjobtask` ADD INDEX `expired` ( `status` , `leasetime` ) ;
-
-ALTER TABLE `qjobtask` ADD `contactsequence` TINYINT( 4 ) NOT NULL DEFAULT '0' AFTER `sequence` ;
-
-ALTER TABLE `qjobtask` DROP INDEX `dispatch`,
-ADD INDEX `dispatch` ( `status`, `customerid`, `jobid`, `type`, `attempts`, `sequence`);
-
-ALTER TABLE `qjobtask` DROP INDEX `emailer` ,
-ADD INDEX `emailer` ( `type` , `status` , `nextattempttime` );
+--
+-- Table structure for table `leasetask`
+--
 
 CREATE TABLE `leasetask` (
   `taskuuid` varchar(255) NOT NULL,
   `leasetime` bigint(20) NOT NULL,
-  PRIMARY KEY (`taskuuid`)
+  PRIMARY KEY  (`taskuuid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-ALTER TABLE `qschedule` ADD `timezone` VARCHAR( 50 ) NOT NULL ;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qjob`
+--
+
+CREATE TABLE `qjob` (
+  `customerid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL default '0',
+  `scheduleid` int(11) default NULL,
+  `listid` int(11) NOT NULL default '0',
+  `phonemessageid` int(11) default NULL,
+  `emailmessageid` int(11) default NULL,
+  `printmessageid` int(11) default NULL,
+  `questionnaireid` int(11) default NULL,
+  `timezone` varchar(50) NOT NULL,
+  `startdate` date NOT NULL default '0000-00-00',
+  `enddate` date NOT NULL default '0000-00-00',
+  `starttime` time NOT NULL default '00:00:00',
+  `endtime` time NOT NULL default '00:00:00',
+  `status` enum('new','processing','active','cancelling','repeating') NOT NULL default 'new',
+  `phonetaskcount` int(11) NOT NULL default '0',
+  `systempriority` tinyint(4) NOT NULL default '3',
+  `timeslices` smallint(6) NOT NULL default '0',
+  `thesql` text,
+  PRIMARY KEY  (`customerid`,`id`),
+  KEY `status` (`status`,`id`),
+  KEY `startdate` (`startdate`),
+  KEY `enddate` (`enddate`),
+  KEY `starttime` (`starttime`),
+  KEY `endtime` (`endtime`),
+  KEY `startdate_2` (`startdate`,`enddate`,`starttime`,`endtime`,`id`),
+  KEY `scheduleid` (`scheduleid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qjobsetting`
+--
+
+CREATE TABLE `qjobsetting` (
+  `customerid` int(11) NOT NULL,
+  `jobid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `value` varchar(255) NOT NULL default '',
+  PRIMARY KEY  (`customerid`,`jobid`,`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qjobtask`
+--
+
+CREATE TABLE `qjobtask` (
+  `customerid` int(11) NOT NULL default '0',
+  `jobid` int(11) NOT NULL default '0',
+  `type` enum('phone','email','print','sms') NOT NULL,
+  `personid` int(11) NOT NULL default '0',
+  `sequence` tinyint(4) NOT NULL default '0',
+  `contactsequence` tinyint(4) NOT NULL default '0',
+  `status` enum('active','pending','assigned','progress','waiting','throttled') NOT NULL,
+  `attempts` tinyint(4) NOT NULL default '0',
+  `renderedmessage` text,
+  `lastresult` enum('A','M','N','B','X','F','sent','unsent','printed','notprinted') default NULL,
+  `lastresultdata` text,
+  `lastduration` float default NULL,
+  `lastattempttime` bigint(20) default NULL,
+  `nextattempttime` bigint(20) default NULL,
+  `leasetime` bigint(20) default NULL,
+  `phone` varchar(20) default NULL,
+  `uuid` varchar(255) NOT NULL,
+  PRIMARY KEY  (`customerid`,`jobid`,`type`,`personid`,`sequence`),
+  UNIQUE KEY `uuid` (`uuid`),
+  KEY `waiting` (`status`,`nextattempttime`),
+  KEY `progresshandler` (`status`,`lastattempttime`),
+  KEY `dispatch` (`status`,`customerid`,`jobid`,`type`,`attempts`,`sequence`),
+  KEY `emailer` (`type`,`status`,`nextattempttime`),
+  KEY `personid` (`personid`),
+  KEY `expired` (`status`,`leasetime`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qreportsubscription`
+--
+
+CREATE TABLE `qreportsubscription` (
+  `customerid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `userid` int(11) NOT NULL default '0',
+  `type` enum('notscheduled','once','weekly','monthly') NOT NULL default 'notscheduled',
+  `daysofweek` varchar(20) default NULL,
+  `dayofmonth` tinyint(4) default NULL,
+  `timezone` varchar(50) NOT NULL,
+  `nextrun` datetime default NULL,
+  `time` time default NULL,
+  `email` text NOT NULL,
+  PRIMARY KEY  (`customerid`,`id`),
+  KEY `nextrun` (`nextrun`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `qschedule`
+--
+
+CREATE TABLE `qschedule` (
+  `customerid` int(11) NOT NULL,
+  `id` int(11) NOT NULL,
+  `daysofweek` varchar(20) NOT NULL,
+  `time` time NOT NULL default '00:00:00',
+  `nextrun` datetime default NULL,
+  `timezone` varchar(50) NOT NULL,
+  PRIMARY KEY  (`customerid`,`id`),
+  KEY `nextrun` (`nextrun`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `specialtaskqueue`
+--
+
+CREATE TABLE `specialtaskqueue` (
+  `id` int(11) NOT NULL auto_increment,
+  `customerid` int(11) NOT NULL,
+  `localspecialtaskid` int(11) NOT NULL,
+  `uuid` varchar(255) default NULL,
+  `status` enum('new','assigned') NOT NULL default 'new',
+  `type` varchar(50) default NULL,
+  `leasetime` bigint(20) NOT NULL,
+  PRIMARY KEY  (`id`),
+  UNIQUE KEY `specialtask` (`customerid`,`localspecialtaskid`),
+  UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 
