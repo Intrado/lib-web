2981f25d1bbabc25647e8466e5729d329566e844
Josh Lai <jlai@schoolmessenger.com>
2007-10-02 20:33:06 +0000

    settings page for parent portal

---
+++ parentportal/parentportalsettings.ini.php
@@ -0,0 +1,15 @@
+;<?/* this prevents browsers from seeing anything
+
+;authentication server connection information
+[authserver]
+host="localhost:8088"
+path="/xmlrpc"
+
+[feature]
+is_commsuite=false
+log_dir="/commsuite/logs/"
+log_db_errors=true
+log_db_queries=true
+tmp_dir="/tmp"
+
+;*/?>
