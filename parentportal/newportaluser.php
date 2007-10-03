98c931a8e733154e2565ff9e985664d0de9e396e
Josh Lai <jlai@schoolmessenger.com>
2007-10-03 20:19:05 +0000

    handle magic quotes gpc moved success text inpage

--- parentportal/newportaluser.php
+++ parentportal/newportaluser.php
@@ -8,12 +8,15 @@ $login = "";
 $firstname = "";
 $lastname = "";
 $zipcode = "";
+$success = false;
 
-if(isset($_POST['login'])){
-	$login = $_POST['login'];
-	$firstname = $_POST['firstname'];
-	$lastname = $_POST['lastname'];
-	$zipcode = $_POST['zipcode'];
+if ((strtolower($_SERVER['REQUEST_METHOD']) == 'post') ) {
+	$login = get_magic_quotes_gpc() ? stripslashes($_POST['login']) : $_POST['login'];
+	$firstname = get_magic_quotes_gpc() ? stripslashes($_POST['firstname']) : $_POST['firstname'];
+	$lastname = get_magic_quotes_gpc() ? stripslashes($_POST['lastname']) : $_POST['lastname'];
+	$zipcode = get_magic_quotes_gpc() ? stripslashes($_POST['zipcode']) : $_POST['zipcode'];
+	$password1 = get_magic_quotes_gpc() ? stripslashes($_POST['password1']) : $_POST['password1'];
+	$password2 = get_magic_quotes_gpc() ? stripslashes($_POST['password2']) : $_POST['password2'];
 	if(!preg_match("/^[\w-\.]{1,}\@([\da-zA-Z-]{1,}\.){1,}[\da-zA-Z-]{2,}$/", $login)){
 		error("That is not a valid email format");
 	} else if(!ereg("^[0-9]*$",$zipcode)){
@@ -22,48 +25,55 @@ if(isset($_POST['login'])){
 		error("Zip code must be a 5 digit number");
 	} else if($_POST['password1'] == ""){
 		error("You must enter a password");
-	} else if($_POST['password1'] != $_POST['password2']){
+	} else if($password1 != $password2){
 		error("Your passwords don't match");
-	} else if(portalCreateAccount($login, $_POST['password1'], $firstname, $lastname, $zipcode)){
-		redirect("accountcreated.php");
+	} else if(portalCreateAccount($login, $password1, $firstname, $lastname, $zipcode)){
+		$success = true;
+		?>
+		<br>Thank you, Your account has been created.
+		<br>Please check your email to activate your account.
+		<br>You will be redirected to the login page in 5 seconds.
+		<meta http-equiv="refresh" content="5;url=index.php">
+		<?
 	} else {
 		error("Your account was not created, please check your information");
 	}
 }
+
+if(!$success){
 ?>
-
-
-
-<form method="POST" action="newportaluser.php">
-	<table>
-		<tr>
-			<td>Email(this will be your login name):</td>
-			<td><input type="text" name="login" value="<?=$login?>" /> </td>
-		</tr>
-		<tr>
-			<td>Password: </td>
-			<td><input type="password" name="password1" /> </td>
-			<td>Confirm Password: </td>
-			<td><input type="password" name="password2" /> </td>
-		</tr>
-		<tr>
-			<td>First Name:</td>
-			<td><input type="text" name="firstname" value="<?=$firstname?>" /></td>
-		</tr>
-		<tr>
-			<td>Last Name:</td>
-			<td><input type="text" name="lastname" value="<?=$lastname?>" /></td>
-		</tr>
-		<tr>
-			<td>Zip Code:</td>
-			<td><input type="text" name="zipcode" value="<?=$zipcode?>" /></td>
-		</tr>
-	</table>
-	<input type="submit" value="Create Account" name="submit" />
-</form>
-<br><a href="index.php">Return to Parent Portal Login</a>
-
+	<form method="POST" action="newportaluser.php">
+		<table>
+			<tr>
+				<td>Email(this will be your login name):</td>
+				<td><input type="text" name="login" value="<?=$login?>" /> </td>
+			</tr>
+			<tr>
+				<td>Password: </td>
+				<td><input type="password" name="password1" /> </td>
+				<td>Confirm Password: </td>
+				<td><input type="password" name="password2" /> </td>
+			</tr>
+			<tr>
+				<td>First Name:</td>
+				<td><input type="text" name="firstname" value="<?=$firstname?>" /></td>
+			</tr>
+			<tr>
+				<td>Last Name:</td>
+				<td><input type="text" name="lastname" value="<?=$lastname?>" /></td>
+			</tr>
+			<tr>
+				<td>Zip Code:</td>
+				<td><input type="text" name="zipcode" value="<?=$zipcode?>" /></td>
+			</tr>
+		</table>
+		<input type="submit" value="Create Account" name="submit" />
+	</form>
+	<br><a href="index.php">Return to Parent Portal Login</a>
 <?
+}
+
+
 if(isset($ERRORS) && is_array($ERRORS)) {
 	foreach($ERRORS as $key => $value) {
 		$ERRORS[$key] = addslashes($value);
