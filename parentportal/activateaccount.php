47df265a4961cc238fb03cb4be713cc082f44fac
Josh Lai <jlai@schoolmessenger.com>
2007-10-03 16:43:51 +0000

    automatically log in with both activation and forgot password cases

--- parentportal/activateaccount.php
+++ parentportal/activateaccount.php
@@ -4,11 +4,13 @@ require_once("common.inc.php");
 
 if(isset($_GET['token'])){
 	$id = portalActivateAccount($_GET['token']);
-	if($id && isset($_GET['forgot'])) {
+	if($id){
 		doStartSession();
 		$_SESSION['portaluserid'] = $id;
 		$_SESSION['custname'] = "";
 		$_SESSION['customerid'] = 0;
+	}
+	if($id && isset($_GET['forgot'])) {
 		?>
 		<br>Thank you, you will be redirected in 3 seconds to reset your password.
 		<meta http-equiv="refresh" content="3;url=account.php">
@@ -16,7 +18,7 @@ if(isset($_GET['token'])){
 	} else if($id) {
 		?>
 		<br>Thank you, your account has been activated.
-		<br>You will be redirected to the login page in 10 seconds.
+		<br>You will be redirected to the welcome page in 10 seconds.
 		<meta http-equiv="refresh" content="10;url=index.php">
 		<?
 	} else {
