e8147f204c5a1cb5c3e5883d5d53caa102b06637
Josh Lai <jlai@schoolmessenger.com>
2007-10-03 20:17:39 +0000

    moved inpage of newportaluser

--- parentportal/accountcreated.php
+++
@@ -1,5 +0,0 @@
-
-<br>Thank you, Your account has been created.
-<br>Please check your email to activate your account.
-<br>You will be redirected to the login page in 10 seconds.
-<meta http-equiv="refresh" content="10;url=index.php">
