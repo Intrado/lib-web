6209b99692f9f120c847506dab55312648a184b9
Josh Lai <jlai@schoolmessenger.com>
2007-10-03 16:40:22 +0000

    added case for forgotten passwords

--- parentportal/activateaccount.php
+++ parentportal/activateaccount.php
@@ -3,7 +3,17 @@ $ppNotLoggedIn = 1;
 require_once("common.inc.php");
 
 if(isset($_GET['token'])){
-	if($id = portalActivateAccount($_GET['token'])){
+	$id = portalActivateAccount($_GET['token']);
+	if($id && isset($_GET['forgot'])) {
+		doStartSession();
+		$_SESSION['portaluserid'] = $id;
+		$_SESSION['custname'] = "";
+		$_SESSION['customerid'] = 0;
+		?>
+		<br>Thank you, you will be redirected in 3 seconds to reset your password.
+		<meta http-equiv="refresh" content="3;url=account.php">
+		<?
+	} else if($id) {
 		?>
 		<br>Thank you, your account has been activated.
 		<br>You will be redirected to the login page in 10 seconds.
