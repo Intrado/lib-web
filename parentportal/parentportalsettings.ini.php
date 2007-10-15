0214223311d237c3591ec522bdc43c721e28137d
Josh Lai <jlai@schoolmessenger.com>
2007-10-15 19:37:34 +0000

    added content settings added ssl

--- parentportal/parentportalsettings.ini.php
+++ parentportal/parentportalsettings.ini.php
@@ -7,9 +7,15 @@ path="/xmlrpc"
 
 [feature]
 is_commsuite=false
+has_ssl=true
+force_ssl=true
 log_dir="/commsuite/logs/"
 log_db_errors=true
 log_db_queries=true
 tmp_dir="/tmp"
 
+
+[content]
+;tts="10.25.25.17,8080,/tts/ttsloquendo"
+tts="localhost,8080,/phone/Tts"
 ;*/?>
