90848d8b5b1e22964f274b38ea452d44e5bf6d73
Josh Lai <jlai@schoolmessenger.com>
2007-10-15 17:32:48 +0000

    updated jobtype object

--- obj/JobType.obj.php
+++ obj/JobType.obj.php
@@ -5,11 +5,13 @@ class JobType extends DBMappedObject {
 	var $priority;
 	var $systempriority;
 	var $timeslices;
+	var $infoforparents;
+	var $issurvey;
 	var $deleted;
 
 	function JobType($id = NULL) {
 		$this->_tablename = "jobtype";
-		$this->_fieldlist = array("name", "priority","systempriority","timeslices","deleted");
+		$this->_fieldlist = array("name", "priority","systempriority","timeslices","infoforparents","issurvey","deleted");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
