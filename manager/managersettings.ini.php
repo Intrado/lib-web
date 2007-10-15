076333c3922920d287b137573e727d2ba75a77f6
Josh Lai <jlai@schoolmessenger.com>
2007-10-15 21:08:36 +0000

    added ssl and content settings

--- manager/managersettings.ini.php
+++ manager/managersettings.ini.php
@@ -13,8 +13,14 @@ db="authserver"
 log_dir="/usr/commsuite/logs/"
 log_db_errors=true
 log_db_queries=true
+tmp_dir="/tmp"
+has_ssl=false
+force_ssl=false
 
 ; server to redirect customer link to
 customer_url_prefix=https://asp.schoolmessenger.com
+
+[content]
+tts="localhost,8080,/phone/Tts"
 
 ;*/?>
