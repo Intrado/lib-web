52ebd28972a6d9e1847743933d64b6922a4bb851
Josh Lai <jlai@schoolmessenger.com>
2007-10-15 19:36:12 +0000

    order people

--- parentportal/parentportalutils.inc.php
+++ parentportal/parentportalutils.inc.php
@@ -1,13 +1,12 @@
 <?
 
 function getContactIDs($portaluserid){
-	return QuickQueryList("select personid from personportal where portaluserid = '$portaluserid'");
+	return QuickQueryList("select personid from portalperson where portaluserid = '$portaluserid'");
 }
 
 function getContacts($portaluserid) {
 	$contactList = getContactIDs($portaluserid);
-	return DBFindMany("Person", "from person where id in ('" . implode("','", $contactList) . "') and not deleted");
+	return DBFindMany("Person", "from person where id in ('" . implode("','", $contactList) . "') and not deleted order by id");
 }
-
 
 ?>
