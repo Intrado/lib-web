d6a8724525483979df8b165f063941b46c2ea205
Josh Lai <jlai@schoolmessenger.com>
2007-10-22 20:34:15 +0000

    added gui added forgot password flag

--- parentportal/choosecustomer.php
+++ parentportal/choosecustomer.php
@@ -2,6 +2,7 @@
 require_once("common.inc.php");
 require_once("../inc/form.inc.php");
 require_once("../inc/html.inc.php");
+require_once("../inc/table.inc.php");
 
 $error = 0;
 $result = portalGetCustomerAssociations(session_id());
@@ -11,6 +12,12 @@ if($result['result'] == ""){
 } else {
 	$customeridlist = array();
 }
+
+$forgot = false;
+if(isset($_GET['forgot'])){
+	$forgot = true;
+}
+
 if(isset($customeridlist) && !(count($customeridlist) > 1)){
 	if(count($customeridlist) == 1){
 		$_SESSION['customerid'] = $customeridlist[0];
@@ -21,12 +28,19 @@ if(isset($customeridlist) && !(count($customeridlist) > 1)){
 			$error = 1;
 		}
 	} else {
-		$_SESSION['customerid'] = 0;
 		$_SESSION['custname'] = "";
 	}
-	if(!$error)
-	redirect("start.php");
+	if(!$error){
+		if($forgot)
+			redirect("account.php");
+		else
+			redirect("start.php");
+	}
 }
+
+//redirect to "my account" if user forgot password.  Have them choose the customer after they've fixed their password.
+if($forgot)
+	redirect("account.php");
 
 if(isset($_GET['customerid']) && $_GET['customerid']){
 	$_SESSION['customerid'] = $_GET['customerid']+0;
@@ -45,12 +59,23 @@ if(isset($_GET['logoutcustomer'])){
 	redirect();
 }
 
+
+$PAGE = ":";
+$TITLE = "Parent Portal Login";
+$hidenav = 1;
+include_once("nav.inc.php");
+startWindow("Choose District/School");
 ?>
 <br>You have students in multiple districts/schools
 <br>Please choose one of the districts/schools you are associated with:
-<br>
+	<table cellpadding="3" cellspacing="1">
 <?
-foreach($customerlist as $index => $customername){
-	?><a href="choosecustomer.php?customerid=<?=$index?>"/><?=$customername?></a><br><?
-}
+		foreach($customerlist as $index => $customername){
+			?><tr><td><a href="choosecustomer.php?customerid=<?=$index?>"/><?=$customername?></a></td></tr><br><?
+		}
+?>
+	</table>
+<?
+endWindow();
+include_once("navbottom.inc.php");
 ?>
