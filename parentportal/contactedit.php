b58301f453849c533554c851e02a53c054f8e4ea
Josh Lai <jlai@schoolmessenger.com>
2007-10-24 22:14:18 +0000

    removed change button moved checkboxes into table

--- parentportal/contactedit.php
+++ parentportal/contactedit.php
@@ -4,18 +4,22 @@ if($PERSONID){
 
 	NewForm($f);
 ?>
-	<table border="1px" cellpadding="3" cellspacing="1">
-		<tr>
-			<th colspan="2">Contact Detail</th>
-			<th width="50%">Contact Settings</th>
+	<table cellpadding="3" cellspacing="1">
+		<tr class="listheader" >
+			<th colspan="2">Contact Details</th>
+<?
+			foreach($jobtypes as $jobtype){
+				?><th><?=jobtype_info($jobtype)?></th><?
+			}
+?>
 		<tr>
 <?
 			$type = "phone";
 			foreach($phones as $phone){
 ?>
 				<tr>
-					<td>Phone <?=$phone->sequence+1?>:</td>
-					<td>
+					<th align="right" class="windowRowHeader bottomBorder" valign="top" style="padding-top: 6px;">Phone <?=$phone->sequence+1?>:</th>
+					<td class="bottomBorder" >
 					<? 
 						if(!$lockedphones[$phone->sequence]){ 
 							NewFormItem($f, $s, "phone" . $phone->sequence, "text", 14, null);
@@ -23,27 +27,10 @@ if($PERSONID){
 							echo Phone::format($phone->phone);
 						}
 					?>
-					</td>
-					<td>
-						<table id="phonetable<?=$phone->sequence?>" style="display:none">
-							<tr>
-								<td>
 <?
-									displayJobtypeForm($f, $s, "phone", $phone->sequence, $jobtypes);
+						displayJobtypeForm($f, $s, "phone", $phone->sequence, $jobtypes);
 ?>									
-								</td>
-							</tr>
-						</table>
-						<div id="<?=$type?><?=$phone->sequence?>enabledjobtypes" ><?=displayEnabledJobtypes($contactprefs, $defaultcontactprefs, $type, $phone->sequence, $jobtypes)?></div>
-						<div style="float:right">
-<?
-							echo button("Change", "show('" . $type . "table" . $phone->sequence . "');
-													hide('" . $type . "edit" . $phone->sequence ."');
-													hide('" . $type . $phone->sequence . "enabledjobtypes');", 
-										null, "id='" . $type . "edit" . $phone->sequence . "'");
-?>
-						</div>
-					</td>
+
 				</tr>
 <?
 			}
@@ -51,28 +38,15 @@ if($PERSONID){
 			foreach($emails as $email){
 ?>
 				<tr>
-					<td>Email <?=$email->sequence+1?>: </td>
-					<td><? NewFormItem($f, $s, "email" . $email->sequence, "text", 40, 100, 'id="email' . $email->sequence . '"'); ?>
-						<?=button("Copy Email", "var email=new getObj('email" . $email->sequence . "').obj; email.value='" . $_SESSION['portaluser']['portaluser.username'] . "'") ?></td>
-					<td>
-						<table id="emailtable<?=$email->sequence?>" style="display:none">
-							<tr>
-								<td>
+					<th align="right" class="windowRowHeader bottomBorder" valign="top" style="padding-top: 6px;">Email <?=$email->sequence+1?>: </th>
+					<td class="bottomBorder" >
+						<div style="float:left"><? NewFormItem($f, $s, "email" . $email->sequence, "text", 40, 100, 'id="email' . $email->sequence . '"'); ?></div>
+						<?=button("Copy Email", "var email=new getObj('email" . $email->sequence . "').obj; email.value='" . $_SESSION['portaluser']['portaluser.username'] . "'") ?>
+						
+					</td>
 <?
-									displayJobtypeForm($f, $s, "email", $email->sequence, $jobtypes);
+					displayJobtypeForm($f, $s, "email", $email->sequence, $jobtypes);
 ?>									
-								</td>
-							</tr>
-						</table>
-						<div id="<?=$type?><?=$email->sequence?>enabledjobtypes" ><?=displayEnabledJobtypes($contactprefs, $defaultcontactprefs, $type, $email->sequence, $jobtypes)?></div>
-						<div style="float:right">
-<?
-							echo button("Change", "show('" . $type . "table" . $email->sequence . "'); 
-													hide('" . $type . "edit" . $email->sequence ."');
-													hide('" . $type . $email->sequence . "enabledjobtypes');",
-										null, "id='" . $type . "edit" . $email->sequence . "'");
-?>
-						</div>
 					</td>
 				</tr>
 <?
@@ -82,28 +56,11 @@ if($PERSONID){
 				foreach($smses as $sms){
 ?>
 				<tr>
-					<td>SMS <?=$sms->sequence+1?>:</td>
-					<td><? NewFormItem($f, $s, "sms" . $sms->sequence, "text", 40, 100); ?></td>
-					<td>
-						<table id="smstable<?=$sms->sequence?>" style="display:none">
-							<tr>
-								<td>
+					<th align="right" class="windowRowHeader bottomBorder" valign="top" style="padding-top: 6px;">SMS <?=$sms->sequence+1?>:</th>
+					<td class="bottomBorder" ><? NewFormItem($f, $s, "sms" . $sms->sequence, "text", 14); ?></td>
 <?
-									displayJobtypeForm($f, $s, "sms", $sms->sequence, $jobtypes);
+					displayJobtypeForm($f, $s, "sms", $sms->sequence, $jobtypes);
 ?>									
-								</td>
-							</tr>
-						</table>
-						<div id="<?=$type?><?=$sms->sequence?>enabledjobtypes" ><?=displayEnabledJobtypes($contactprefs, $defaultcontactprefs, $type, $sms->sequence, $jobtypes)?></div>
-						<div style="float:right">
-<?
-							echo button("Change", "show('" . $type . "table" . $sms->sequence . "'); 
-													hide('" . $type . "edit" . $sms->sequence ."');
-													hide('" . $type . $sms->sequence . "enabledjobtypes');",
-										null, "id='" . $type . "edit" . $sms->sequence . "'");
-?>
-						</div>
-					</td>
 				</tr>
 <?
 				}
