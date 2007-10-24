c901599b495e0c56e06f956591cf13a2de1f6abd
Josh Lai <jlai@schoolmessenger.com>
2007-10-24 18:48:06 +0000

    message editor for sms

---
+++ messagesms.php
@@ -0,0 +1,31 @@
+<?
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+include_once("inc/common.inc.php");
+include_once("inc/securityhelper.inc.php");
+include_once("obj/Message.obj.php");
+include_once("inc/table.inc.php");
+include_once("inc/html.inc.php");
+include_once("inc/utils.inc.php");
+include_once("inc/form.inc.php");
+include_once("inc/text.inc.php");
+include_once("obj/Message.obj.php");
+include_once("obj/MessagePart.obj.php");
+include_once("obj/AudioFile.obj.php");
+include_once("obj/Voice.obj.php");
+include_once("obj/FieldMap.obj.php");
+
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+if ($USER->authorize("sendsms") === false) {
+	redirect('./');
+}
+
+$MESSAGETYPE = "sms";
+
+
+
+
+include("messageedit.inc.php");
