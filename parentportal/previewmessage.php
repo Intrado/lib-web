ddb78a8dbed65498d4785318b5d00e8697e38a62
Josh Lai <jlai@schoolmessenger.com>
2007-10-29 22:02:14 +0000

    merged preview message and email

--- parentportal/previewmessage.php
+++ parentportal/previewmessage.php
@@ -16,23 +16,38 @@ require_once("../inc/securityhelper.inc.php");
 require_once("../inc/formatters.inc.php");
 require_once("../inc/reportutils.inc.php");
 require_once("../obj/Phone.obj.php");
+require_once("../obj/Message.obj.php");
+require_once("../obj/AudioFile.obj.php");
+require_once("../obj/MessagePart.obj.php");
 
 
 ////////////////////////////////////////////////////////////////////////////////
 // Data Handling
 ////////////////////////////////////////////////////////////////////////////////
 
+if(isset($_GET['type'])){
+	$_SESSION['type'] = $_GET['type'];
+}
+
 if(isset($_GET['jobid']) && isset($_GET['personid'])){
 	$_SESSION['previewmessage_jobid'] = $_GET['jobid']+0;
 	$_SESSION['previewmessage_personid'] = $_GET['personid']+0;
-	$_SESSION['previewmessageid'] = QuickQuery("select messageid from reportperson where jobid = '" . $_SESSION['previewmessage_jobid'] . "'
-												 and personid = '" . $_SESSION['previewmessage_personid'] . "'");
+	$query = "select messageid from reportperson where jobid = '" . $_SESSION['previewmessage_jobid'] . "'
+												 and personid = '" . $_SESSION['previewmessage_personid'] . "'";
+	if(isset($_SESSION['type']) && $_SESSION['type'] == "phone"){
+		$query .= " and type = 'phone'";
+	} else if(isset($_SESSION['type']) && $_SESSION['type'] == "email"){
+		$query .= " and type = 'email'";
+	}
+	$_SESSION['previewmessageid'] = QuickQuery($query);
 	redirect();
 }
 
 $jobid = isset($_SESSION['previewmessage_jobid']) ? $_SESSION['previewmessage_jobid'] : 0;
 $personid = isset($_SESSION['previewmessage_personid']) ? $_SESSION['previewmessage_personid'] : 0;
 $messageid = isset($_SESSION['previewmessageid']) ? $_SESSION['previewmessageid'] : 0;
+$phone = isset($_SESSION['type']) && $_SESSION['type'] == "phone" ? true : false;
+$email = isset($_SESSION['type']) && $_SESSION['type'] == "email" ? true : false;
 
 //find all unique fields used in this message
 
@@ -48,27 +63,32 @@ if($jobid != 0 && $personid != 0){
 			rp." . FieldMap::GetLastNameField()
 			. generateFields("rp")
 			. ",
-			rc.phone as destination, 
 			j.name as jobname, 
-			j.startdate as startdate,
-			m.name as messagename
+			j.startdate as startdate
 			from reportperson rp
 			left join reportcontact rc on (rc.jobid = rp.jobid and rc.personid = rp.personid and rc.type = rp.type)
 			left join job j on (j.id = rp.jobid)
-			left join message m on (m.id = rp.messageid)
 			where j.id = '$jobid'
 			and rp.personid = '$personid'";
+	if(isset($_SESSION['type']) && $_SESSION['type'] == "phone"){
+		$query .= " and rp.type = 'phone'";
+	} else if(isset($_SESSION['type']) && $_SESSION['type'] == "email"){
+		$query .= " and rp.type = 'email'";
+	}
 	$historicdata = QuickQueryRow($query, true);
 }
-
-if (count($messagefields) > 0) {
-	$previewdata = "";
-	foreach ($messagefields as $fieldmap) {
-		$fields[$fieldmap->fieldnum] = $fieldmap;
-		$previewdata .= "&$fieldmap->fieldnum=" . urlencode($historicdata[$fieldmap->fieldnum]);
+if($phone){
+	if (count($messagefields) > 0) {
+		$previewdata = "";
+		foreach ($messagefields as $fieldmap) {
+			$fields[$fieldmap->fieldnum] = $fieldmap;
+			$previewdata .= "&$fieldmap->fieldnum=" . urlencode($historicdata[$fieldmap->fieldnum]);
+		}
+	} else {
+		$previewdata = "&qt=";
 	}
-} else {
-	$previewdata = "&qt=";
+} else if($email){
+	$message = formatEmail($messageid, $historicdata);
 }
 
 
@@ -77,19 +97,90 @@ if (count($messagefields) > 0) {
 // Display Functions
 ////////////////////////////////////////////////////////////////////////////////
 
+function formatEmail($messageid, $historicdata) {
+	$messageparts = DBFindMany("MessagePart", "from messagepart where messageid = '" . $messageid . "' order by sequence");
+	$data = Message::format($messageparts);
+	$message = "";
+	
+	if (!isset($errors))
+		$errors = array();
+	
+	$txtpart = "";
+	while (true) {
+		//get dist to next field and type of field
+		$pos_f = strpos($data,"<<");
+	
+		if ($pos_f !== false) {
+			$pos = $pos_f;
+		} else {
+			break;
+		}
+	
+		//make a text part up to the pos of the field
+		$txt = substr($data,0,$pos);
+		if (strlen($txt) > 0) {
+			$message .= $txt;
+		}
+	
+		$pos += 2; // pass over the begintoken
+	
+		$endtoken = ">>";
+		$length = @strpos($data,$endtoken,$pos+1); // assume at least one char for audio/field name
+	
+		if ($length === false) {
+			$errors[] = "Can't find end of field, was expecting '$endtoken'";
+			$length = 0;
+		} else {
+			$length -= $pos;
+	
+			$token  = substr($data,$pos,$length);
+	
+	
+			if (strpos($token,":") !== false) {
+				list($fieldname,$defvalue) = explode(":",$token);
+			} else {
+				$fieldname = $token;
+				$defvalue = "";
+				$maxlen = null;
+			}
+			$fieldname = DBSafe($fieldname);
+			$query = "select fieldnum from fieldmap where name='$fieldname'";
+	
+			$fieldnum = QuickQuery($query);
+	
+			if ($fieldnum !== false) {
+				$message .= " " . $historicdata[$fieldnum] . " ";
+			} else {
+				$errors[] = "Can't find field named '$fieldname'";
+			}
+		}
+		//skip the end if we found it
+		if ($length)
+			$skip = $pos + $length +2;
+		else
+			$skip = $pos + $length ;
+	
+		$data = substr($data,$skip );
+	}
+	if(count($errors)){
+		return $errors;
+	} else {
+		return $message;
+	}
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Display
 ////////////////////////////////////////////////////////////////////////////////
 
-$TITLE = "Message Preview";
+$TITLE = "Message";
 
 include_once("popup.inc.php");
 
 buttons(button('Done', 'window.close()'));
 
 
-startWindow('Message Information', 'padding: 3px;');
+startWindow('Details', 'padding: 3px;');
 ?>
 	<table width="100%" border="0" cellpadding="3" cellspacing="0">
 		<tr>
@@ -104,39 +195,40 @@ startWindow('Message Information', 'padding: 3px;');
 			<th align="right" class="windowRowHeader bottomBorder">Date:</td>
 			<td class="bottomBorder"><?=date("M d, Y", strtotime($historicdata['startdate']))?></td>
 		</tr>
-		<tr>
-			<th align="right" class="windowRowHeader bottomBorder">Message Name:</td>
-			<td class="bottomBorder"><?=$historicdata['messagename']?></td>
-		</tr>
-		<tr>
-			<th align="right" class="windowRowHeader bottomBorder">Phone:</td>
-			<td class="bottomBorder"><?=Phone::format($historicdata['destination'])?></td>
-		</tr>
 	</table>
 <?
 endWindow();
 
-
-startWindow('Message Preview', 'padding: 3px;');
-?>
-
-	<div align="center">
-
-	<OBJECT ID="MediaPlayer" WIDTH=320 HEIGHT=42
-	CLASSID="CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95"
-	STANDBY="Loading Windows Media Player components..."
-	TYPE="application/x-oleobject">
-
-	<PARAM NAME="FileName" VALUE="preview.wav.php/mediaplayer_preview.wav?id=<?= $messageid ?><?= $previewdata ?>">
-	<param name="controller" value="true">
-	<EMBED SRC="preview.wav.php/embed_preview.wav?id=<?= $messageid ?><?= $previewdata ?>" AUTOSTART="TRUE"></EMBED>
-	</OBJECT>
-
-
-	<br><a href="preview.wav.php/download_preview.wav?id=<?= $messageid ?>&download=true<?= $previewdata ?>">Click here to download</a>
-	</div>
-<?
-endWindow();
+if($phone){
+	startWindow('Play', 'padding: 3px;');
+	?>
+	
+		<div align="center">
+	
+		<OBJECT ID="MediaPlayer" WIDTH=320 HEIGHT=42
+		CLASSID="CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95"
+		STANDBY="Loading Windows Media Player components..."
+		TYPE="application/x-oleobject">
+	
+		<PARAM NAME="FileName" VALUE="preview.wav.php/mediaplayer_preview.wav?id=<?= $messageid ?><?= $previewdata ?>">
+		<param name="controller" value="true">
+		<EMBED SRC="preview.wav.php/embed_preview.wav?id=<?= $messageid ?><?= $previewdata ?>" AUTOSTART="TRUE"></EMBED>
+		</OBJECT>
+	
+	
+		<br><a href="preview.wav.php/download_preview.wav?id=<?= $messageid ?>&download=true<?= $previewdata ?>">Click here to download</a>
+		</div>
+	<?
+	endWindow();
+} else if ($email){
+	startWindow('Read', 'padding: 3px;');
+	if(is_array($message)){
+		error($message);
+	} else {
+		?><div><?=$message?></div><?
+	}
+	endWindow();
+}
 
 include_once("popupbottom.inc.php");
 
