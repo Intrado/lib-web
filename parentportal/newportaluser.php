14c35540954e45c7b00a19d3f3ef6756786bc04c
Josh Lai <jlai@schoolmessenger.com>
2007-10-30 16:20:15 +0000

    added some gui changed redirect to go to activate instead of index

--- parentportal/newportaluser.php
+++ parentportal/newportaluser.php
@@ -2,6 +2,7 @@
 $ppNotLoggedIn = 1;
 require_once("common.inc.php");
 require_once("../inc/html.inc.php");
+require_once("../inc/table.inc.php");
 
 
 $login = "";
@@ -46,12 +47,13 @@ $hidenav = 1;
 $TITLE = "Create a New User";
 $PAGE = ":";
 include_once("nav.inc.php");
+startWindow("Enter Your Information");
 if(!$success){
 ?>
 	<form method="POST" action="newportaluser.php">
 		<table>
 			<tr>
-				<td>Email(this will be your login name):</td>
+				<td>Email&nbsp;(this will be your login name):</td>
 				<td><input type="text" name="login" value="<?=$login?>" size="50" maxlength="100"/> </td>
 			</tr>
 			<tr>
@@ -74,19 +76,24 @@ if(!$success){
 				<td>Zip Code:</td>
 				<td><input type="text" name="zipcode" value="<?=$zipcode?>" /></td>
 			</tr>
+			<tr>
+				<td>&nbsp;</td>
+				<td><?=submit("newaccount", "main", "Create Account")?></td>
+			<tr>
+				<td>&nbsp;</td>
+				<td><a href="index.php">Return to Contact Manager Login</a></td>
+			</tr>
 		</table>
-		<input type="submit" value="Create Account" name="submit" />
 	</form>
-	<br><a href="index.php">Return to Parent Portal Login</a>
 <?
 } else {
 ?>
 	<br>Thank you, Your account has been created.
 	<br>Please check your email to activate your account.
-	<br>You will be redirected to the login page in 5 seconds.
-	<meta http-equiv="refresh" content="5;url=index.php">
+	<br>You will be redirected to the activate page in 5 seconds.
+	<meta http-equiv="refresh" content="5;url=activate.php">
 <?
 }
-
+endWindow();
 include_once("navbottom.inc.php");
 ?>
