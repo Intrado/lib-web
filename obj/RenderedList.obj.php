929d24ec599ff2aeeeb4491612f6c4f195289d09
Josh Lai <jlai@schoolmessenger.com>
2007-11-06 16:46:44 +0000

    in search, conditionally display sms based on hassms

--- obj/RenderedList.obj.php
+++ obj/RenderedList.obj.php
@@ -226,12 +226,19 @@ class RenderedList {
 
 		$pfields = "p.id";
 		$contactfields = "";
+		$sms = "";
+		$smsquery = "";
+		if(getSystemSetting("_hassms", false)){
+			$sms = "s.sms,";
+			$smsquery = " left join sms s on (s.personid = p.id and s.sequence=0) ";
+		}
 		if ($getdata) {
 			$pfields .= ", p.pkey";
 			$pfields .= ", p.$this->firstname,p.$this->lastname, p.$this->language";
 
 			$contactfields = ",ph.phone,
 								e.email,
+								$sms
 								concat(
 									coalesce(a.addr1,''), ' ',
 									coalesce(a.addr2,''), ' ',
@@ -267,6 +274,7 @@ class RenderedList {
 								(ph.personid=p.id and ph.sequence=0)
 			left join	email e on
 								(e.personid=p.id  and e.sequence=0)
+			$smsquery
 			left join	address a on
 								(a.personid=p.id)
 			";
