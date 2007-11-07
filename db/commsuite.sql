3ab4aae18aaa8cc87d5434b7e8a828bb30dfdcd8
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2007-11-07 22:22:49 +0000

    include dmapi

--- db/commsuite.sql
+++ db/commsuite.sql
@@ -1442,3 +1442,95 @@ CREATE TABLE `specialtaskqueue` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8   $$$
 
 
+-- ------------------------------------------------------------------------------------------------------------------------
+-- dmapi
+-- ------------------------------------------------------------------------------------------------------------------------
+
+-- phpMyAdmin SQL Dump
+-- version 2.9.1.1
+-- http://www.phpmyadmin.net
+-- 
+-- Host: localhost
+-- Generation Time: Nov 05, 2007 at 05:03 PM
+-- Server version: 5.0.27
+-- PHP Version: 5.2.0
+-- 
+-- Database: `dmapi`
+-- 
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `jobtaskactive`
+-- 
+
+CREATE TABLE `jobtaskactive` (
+  `id` bigint(20) NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `shardid` tinyint(4) NOT NULL,
+  `tasktime` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `renderedmessage` text character set utf8 NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `status` (`tasktime`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin   $$$
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `jobtaskcomplete`
+-- 
+
+CREATE TABLE `jobtaskcomplete` (
+  `id` bigint(20) NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `shardid` tinyint(4) NOT NULL,
+  `starttime` bigint(20) NOT NULL default '0',
+  `duration` float default NULL,
+  `result` enum('A','M','N','B','X','F','sent','unsent','printed','notprinted') collate utf8_bin NOT NULL,
+  `resultdata` text character set utf8 NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin   $$$
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `sessiondata`
+-- 
+
+CREATE TABLE `sessiondata` (
+  `id` char(255) NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `lastused` timestamp NOT NULL default CURRENT_TIMESTAMP on update CURRENT_TIMESTAMP,
+  `data` text NOT NULL,
+  PRIMARY KEY  (`id`),
+  KEY `lastused` (`lastused`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8   $$$
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `specialtaskactive`
+-- 
+
+CREATE TABLE `specialtaskactive` (
+  `id` varchar(50) collate utf8_bin NOT NULL,
+  `customerid` int(11) NOT NULL,
+  `specialtaskid` int(11) NOT NULL,
+  `shardid` tinyint(4) NOT NULL,
+  `type` varchar(50) collate utf8_bin NOT NULL,
+  PRIMARY KEY  (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin   $$$
+
+-- --------------------------------------------------------
+
+-- 
+-- Table structure for table `tasksyncdata`
+-- 
+
+CREATE TABLE `tasksyncdata` (
+  `name` varchar(50) NOT NULL,
+  `value` varchar(50) NOT NULL,
+  PRIMARY KEY  (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8   $$$
+
+
