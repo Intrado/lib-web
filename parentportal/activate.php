53cebd8cccd068bd593883f449f7c31ef0a71ff0
Josh Lai <jlai@schoolmessenger.com>
2007-11-12 23:36:04 +0000

    changed handling of forgot password

--- parentportal/activate.php
+++ parentportal/activate.php
@@ -30,29 +30,36 @@ if ((strtolower($_SERVER['REQUEST_METHOD']) == 'post') ) {
 	$token = get_magic_quotes_gpc() ? stripslashes($_POST['token']) : $_POST['token'];
 	
 	if(isset($_POST['password1']) && isset($_POST['password2'])){
-		$password1 = get_magic_quotes_gpc() ? stripslashes($_POST['password1']) : $_POST['password1'];
-		$password2 = get_magic_quotes_gpc() ? stripslashes($_POST['password2']) : $_POST['password2'];
-		if($password1 !== $password2){
-			error("The passwords do not match");
-		} else if(strlen($password1) < 5){
-			error("Passwords must be at least 5 characters long");
-		} else {
-			$result = portalActivateAccount($token, $password1);
-			if($result['result'] == ""){
-				if(!$forgot && $result['functionCode'] != '3'){
-					error("An unknown error occurred");
-					$error = true;
-				} else {
-					$form = false;
-					$forgotsuccess = true;
-					doStartSession();
-					$_SESSION['portaluserid'] = $result['userID'];
-				}
+		$password1 = get_magic_quotes_gpc() ? trim(stripslashes($_POST['password1'])) : trim($_POST['password1']);
+		$password2 = get_magic_quotes_gpc() ? trim(stripslashes($_POST['password2'])) : trim($_POST['password2']);
+		$result = portalPreactivateForgottenPassword($token);
+		if($result['result'] == ""){
+			$user = $result['portaluser'];
+			if($password1 !== $password2){
+				error("The passwords do not match");
+			} else if(strlen($password1) < 5){
+				error("Passwords must be at least 5 characters long");
+			} else if($password1 && $passworderror = isSameUserPass($user['portaluser.username'], $password1, $user['portal.firstname'], $user['portal.lastname'])){
+				error($passworderror);
 			} else {
-				$error = true;
+				$result = portalActivateAccount($token, $password1);
+				if($result['result'] == ""){
+					if(!$forgot && $result['functionCode'] != '3'){
+						error("An unknown error occurred");
+						$error = true;
+					} else {
+						$form = false;
+						$forgotsuccess = true;
+						doStartSession();
+						$_SESSION['portaluserid'] = $result['userID'];
+					}
+				} else {
+					$error = true;
+				}
 			}
+		} else {
+			$error = true;
 		}
-		
 	} else if(isset($_POST['password'])){
 		$password = get_magic_quotes_gpc() ? stripslashes($_POST['password']) : $_POST['password'];
 		$result = portalActivateAccount($token, $password);
@@ -82,15 +89,21 @@ if ((strtolower($_SERVER['REQUEST_METHOD']) == 'post') ) {
 
 
 $PAGE = ":";
-$TITLE = "Activate Account";
+if($forgot){
+	$TITLE = "Forgot Password";
+} else if($changeuser){
+	$TITLE = "Change User Name";
+} else {
+	$TITLE = "Activate Account";
+}
 $hidenav = 1;
 include_once("nav.inc.php");
 
 if($forgot){
-	startWindow("Activate" . help("Activateforgotpassword"));
+	startWindow("Forgot Password" . help("Activateforgotpassword"));
 	$action = "?forgot=1";
 } else if($changeuser){
-	startWindow("Activate" . help("Activatenewemail"));
+	startWindow("Change User Name" . help("Activatenewemail"));
 	$action = "?changeuser=1";
 } else {
 	startWindow("Activate" . help("Activateaccount"));
@@ -98,20 +111,26 @@ if($forgot){
 }
 if($forgotsuccess){
 	?>
-	<br>Thank you, your password has been updated.
-	<br>You will be redirected to the welcome page in 5 seconds.
+	<div style="margin:5px">
+		Thank you, your password has been updated.
+		<br>You will be redirected to the main page in 5 seconds.
+	</div>
 	<meta http-equiv="refresh" content="5;url=choosecustomer.php">
 	<?
 } else if($success){
 	?>
-	<br>Thank you, your account has been activated.
-	<br>You will be redirected to the welcome page in 5 seconds.
+	<div style="margin:5px">
+		Thank you, your account has been activated.
+		<br>You will be redirected to the main page in 5 seconds.
+	</div>
 	<meta http-equiv="refresh" content="5;url=index.php">
 	<?
 } else if($newusersuccess){
 	?>
-	<br>Thank you, your email address has been changed.
-	<br>You will be redirected to the welcome page in 5 seconds.
+	<div style="margin:5px">
+		Thank you, your email address has been changed.
+		<br>You will be redirected to the main page in 5 seconds.
+	</div>
 	<meta http-equiv="refresh" content="5;url=index.php">
 	<?
 } else if ($error && $forgot){
