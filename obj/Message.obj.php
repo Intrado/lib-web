542668dfa1873b3dafdfaa61b26773c2999593f6
Josh Lai <jlai@schoolmessenger.com>
2007-11-15 18:08:21 +0000

    moved playaudio here from utils

--- obj/Message.obj.php
+++ obj/Message.obj.php
@@ -196,6 +196,104 @@ class Message extends DBMappedObject {
 
 		return $data;
 	}
+	
+	static function playAudio($id, $fields){
+
+	$message = new Message($id);
+	$parts = DBFindMany("MessagePart", "from messagepart where messageid=$message->id order by sequence");
+
+	$voices = DBFindMany("Voice","from ttsvoice");
+
+	// -- digest the message --
+	$renderedparts = array();
+	$curpart = 0;
+
+	$lastVoice = null;
+
+	foreach ($parts as $part) {
+		switch ($part->type) {
+		case "A":
+			//invalidate the tts joining (audio breaks tts)
+			$lastVoice = null;
+
+			$af = new AudioFile($part->audiofileid);
+			$renderedparts[++$curpart] = array("a",$af->contentid);
+			break;
+		case "T":
+			//see if we should combine, or make a new one
+			if ($lastVoice == $part->voiceid) {
+				//just append to the last one
+				$renderedparts[$curpart][1] .= " " . $part->txt;
+			} else {
+				$renderedparts[++$curpart] = array("t",$part->txt,$part->voiceid);
+				$lastVoice = $part->voiceid;
+			}
+			break;
+		case "V":
+			if (!($value = $fields[$part->fieldnum])) {
+				$value = $part->defaultvalue;
+			}
+			//see if we should combine, or make a new one
+			if ($lastVoice == $part->voiceid) {
+				//just append to the last one
+				$renderedparts[$curpart][1] .= " " . $value;
+			} else {
+				$renderedparts[++$curpart] = array("t",$value,$part->voiceid);
+				$lastVoice = $part->voiceid;
+			}
+			break;
+		}
+	}
+
+	// -- get the wav files --
+	$wavfiles = array();
+
+	foreach ($renderedparts as $part) {
+		if ($part[0] == "a") {
+			list($contenttype,$data) = contentGet($part[1]);
+			$wavfiles[] = writeWav($data);
+		} else if ($part[0] == "t") {
+			$voice = $voices[$part[2]];
+			list($contenttype,$data) = renderTts($part[1],$voice->language,$voice->gender);
+			$wavfiles[] = writeWav($data);
+		}
+	}
+
+	//finally, merge the wav files
+	$outname = secure_tmpname("preview",".wav");
+	$cmd = 'sox "' . implode('" "',$wavfiles) . '" "' . $outname . '"';
+
+	$result = exec($cmd, $res1,$res2);
+
+
+	foreach ($wavfiles as $file)
+		@unlink($file);
+
+	if(!$res2 && file_exists($outname)) {
+
+
+		$data = file_get_contents ($outname); //readfile seems to cause problems
+
+		header("HTTP/1.0 200 OK");
+		if (isset($_GET['download']))
+			header('Content-type: application/x-octet-stream');
+		else
+			header("Content-Type: audio/wav");
+
+
+		header('Pragma: private');
+		header('Cache-control: private, must-revalidate');
+		header("Content-Length: " . strlen($data));
+		header("Connection: close");
+
+		echo $data;
+
+	} else {
+		echo "oops";
+	}
+
+	@unlink($outname);
+}
 }
 
 
