801df26e985751992b7af9dac62292b99680f59f
Josh Lai <jlai@schoolmessenger.com>
2007-11-28 23:38:52 +0000

    apply call window restrictions on both repeating and normal jobs

--- jobedit.inc.php
+++ jobedit.inc.php
@@ -86,9 +86,9 @@ if(CheckFormSubmit($f,$s) || CheckFormSubmit($f,'phone') || CheckFormSubmit($f,'
 			error('The start time is invalid');
 		} else if ($JOBTYPE=='normal' && (strtotime(GetFormData($f,$s,"endtime")) === -1 || strtotime(GetFormData($f,$s,"endtime")) === false)) {
 			error('The end time is invalid');
-		} else if ($JOBTYPE=='normal' && (strtotime(GetFormData($f,$s,"endtime")) <= strtotime(GetFormData($f,$s,"starttime")) ) ) {
+		} else if (strtotime(GetFormData($f,$s,"endtime")) <= strtotime(GetFormData($f,$s,"starttime")) ) {
 			error('The end time cannot be before or the same as the start time');
-		} else if ($JOBTYPE=='normal' && (strtotime(GetFormData($f, $s,"endtime"))-(30*60) < strtotime(GetFormData($f,$s,"starttime")))){
+		} else if (strtotime(GetFormData($f, $s,"endtime"))-(30*60) < strtotime(GetFormData($f,$s,"starttime"))){
 			error('The end time must be at least 30 minutes after the start time');
 		} else if ($JOBTYPE == "normal" &&  (strtotime(GetFormData($f,$s,"startdate"))+((GetFormData($f,$s,"numdays")-1)*86400) < strtotime("today")) && !$completedmode){
 			error('The end date has already passed. Please correct this problem before proceeding');
