7cdbd7286f7ccea07b45f625f2f6643ddbe9c286
Josh Lai <jlai@schoolmessenger.com>
2007-12-05 18:38:30 +0000

    new top for not logged in pages

---
+++ parentportal/cmlogintop.inc.php
@@ -0,0 +1,22 @@
+
+<html>
+<head>
+	<title>SchoolMessenger Login</title>
+	<script src='script/utils.js'></script>
+	<script src='script/sorttable.js'></script>
+	<link href='css/style_print.css' type='text/css' rel='stylesheet' media='print'>
+	<link href='css/style.css' type='text/css' rel='stylesheet' media='screen'>
+</head>
+<body style='font-family: "Lucida Grande", verdana, arial, helvetica, sans-serif; margin: 0px; background-color: #365F8D;'>
+
+<table border=0 cellpadding=0 cellspacing=0 width="100%">
+<tr style="background-color: #365F8D;">
+	<td width="389"><img src="img/school_messenger_large.gif" /></td>
+	<td width="100%">&nbsp;</td>
+</tr>
+<tr style="background-color: #666666;">
+	<td colspan="2">&nbsp;</td>
+</tr>
+<tr>
+	<td valign="top" style="background-color: #D4DDE2; color: #365F8D;"><img src="img/contactmanager_splash.jpg"></td>
+	<td style="background-color: #D4DDE2; color: #365F8D;">
