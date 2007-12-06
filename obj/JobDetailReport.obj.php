8993badf1a3d35c03c77ae921aa0a79dd13b89e2
Josh Lai <jlai@schoolmessenger.com>
2007-12-06 21:07:12 +0000

    changed job to job name

--- obj/JobDetailReport.obj.php
+++ obj/JobDetailReport.obj.php
@@ -210,7 +210,7 @@ class JobDetailReport extends ReportGenerator{
 
 		showPageMenu($total,$pagestart,500);
 		echo '<table width="100%" cellpadding="3" cellspacing="1" class="list" id="reportdetails">';
-		$titles = array(0 => "Job",
+		$titles = array(0 => "Job Name",
 						1 => "Submitted By",
 						2 => "ID#",
 						3 => "First Name",
