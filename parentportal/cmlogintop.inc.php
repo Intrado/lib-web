ba350919f141efc7786ab797833891969e4942b8
Josh Lai <jlai@schoolmessenger.com>
2007-12-11 22:56:28 +0000

    changed window title to Contact Manager, using $TITLE as well

--- parentportal/cmlogintop.inc.php
+++ parentportal/cmlogintop.inc.php
@@ -1,7 +1,7 @@
 
 <html>
 <head>
-	<title>SchoolMessenger Login</title>
+	<title>Contact Manager<?=isset($TITLE) ? ": " . $TITLE : ""?></title>
 	<script src='script/utils.js'></script>
 	<link href='css/style_print.css' type='text/css' rel='stylesheet' media='print'>
 	<link href='css/style.css' type='text/css' rel='stylesheet' media='screen'>
