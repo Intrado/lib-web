539a3b4a17dd0cc0661921ce9d17a31bbe04ef50
Josh Lai <jlai@schoolmessenger.com>
2007-12-12 22:40:25 +0000

    fixed undefined index

--- parentportal/newportaluser.php
+++ parentportal/newportaluser.php
@@ -14,6 +14,7 @@ require_once("../inc/table.inc.php");
 ////////////////////////////////////////////////////////////////////////////////
 
 $login = "";
+$confirmlogin="";
 $firstname = "";
 $lastname = "";
 $zipcode = "";
