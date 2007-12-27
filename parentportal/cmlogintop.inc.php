69700c35686656cdb403013ad6cf9480edef6a57
Josh Lai <jlai@schoolmessenger.com>
2007-12-27 00:02:08 +0000

    removed logo, line banner and stock image

--- parentportal/cmlogintop.inc.php
+++ parentportal/cmlogintop.inc.php
@@ -6,15 +6,7 @@
 	<link href='css/style_print.css' type='text/css' rel='stylesheet' media='print'>
 	<link href='css/style.css' type='text/css' rel='stylesheet' media='screen'>
 </head>
-<body style='font-family: "Lucida Grande", verdana, arial, helvetica, sans-serif; margin: 0px; background-color: #365F8D;'>
-
-<table border=0 cellpadding=0 cellspacing=0 width="100%">
-<tr style="background-color: #365F8D;">
-	<td colspan="2"><img src="img/school_messenger_large.gif" /></td>
-</tr>
-<tr style="background-color: #666666;">
-	<td colspan="2">&nbsp;</td>
-</tr>
-<tr>
-	<td width="320" valign="top" style="background-color: #D4DDE2; color: #365F8D;"><img src="img/contactmanager_splash.jpg"></td>
-	<td style="background-color: #D4DDE2; color: #365F8D;">
+<body style='font-family: "Lucida Grande", verdana, arial, helvetica, sans-serif; margin: 0px;'>
+<br>
+<br>
+<br>
