efb8083d981a1e0559473896b5fc9c02b37f4848
Josh Lai <jlai@schoolmessenger.com>
2008-02-05 16:26:34 +0000

    fixed verisign logo link

--- parentportal/cmlogintop.inc.php
+++ parentportal/cmlogintop.inc.php
@@ -17,11 +17,19 @@ if((isset($_COOKIE['embeddedpage']) && $_COOKIE['embeddedpage']) || isset($_GET[
 				<table>
 					<tr>
 						<td><img src="img/sm_white.gif" /></td>
-						<td>	
-							<script src=https://seal.verisign.com/getseal?host_name=asp.schoolmessenger.com&size=S&use_flash=NO&use_transparent=NO&lang=en></script><br />
-							<a href="http://www.verisign.com/ssl-certificate/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;">About SSL Certificates</a>
+						<td>
+						<? /*CSDELETEMARKER_START*/ if (!$IS_COMMSUITE) { ?>
+									<div style="float:right">
+										<table width="135" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose VeriSign SSL for secure e-commerce and confidential communications.">
+											<tr>
+											<td width="135" align="center" valign="top"><script src=https://seal.verisign.com/getseal?host_name=contactme.schoolmessengerr.com&size=M&use_flash=YES&use_transparent=YES&lang=en></script><br />
+											<a href="http://www.verisign.com/ssl-certificate/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;">ABOUT SSL CERTIFICATES</a></td>
+											</tr>
+										</table>
+									</div>
+						<? } /*CSDELETEMARKER_END*/ ?>
 						</td>
-					</tr>
+						</tr>
 				</table>
 			</td>
 		</tr>
