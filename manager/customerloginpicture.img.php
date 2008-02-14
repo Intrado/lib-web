3f6918c7edff39d88c248c05316d0cd3b2cccd6f
Josh Lai <jlai@schoolmessenger.com>
2008-02-14 16:42:56 +0000

    preview picture for the login picture

---
+++ manager/customerloginpicture.img.php
@@ -0,0 +1,28 @@
+<?
+include_once('common.inc.php');
+
+if(isset($_GET['id'])) {
+	$currentid = $_GET['id']+0;
+	$custquery = Query("select s.dbhost, c.dbusername, c.dbpassword, c.urlcomponent, c.enabled from customer c inner join shard s on (c.shardid = s.id) where c.id = '$currentid'");
+	$custinfo = mysql_fetch_row($custquery);
+	$custdb = DBConnect($custinfo[0], $custinfo[1], $custinfo[2], "c_$currentid");
+	if(!$custdb) {
+		exit("Connection failed for customer: $custinfo[0], db: c_$currentid");
+	}
+} else {
+	exit();
+}
+
+
+$query = "select c.contenttype, c.data from content c inner join setting s on (s.value = c.id) where s.name = '_loginpicturecontentid'";
+$row = QuickQueryRow($query, false, $custdb);
+if($row){
+	$data = base64_decode($row[1]);
+
+	$ext = substr($row[0], strpos($row[0], "/")+1);
+	header("Content-disposition: filename=logo." . $ext);
+	header ("Content-type: " . $row[0]);
+	echo $data;
+} else {
+	return false;
+}
