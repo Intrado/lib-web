68f1c2de05f606fec0ee68304e531224adb2fce0
Josh Lai <jlai@schoolmessenger.com>
2008-02-14 16:42:36 +0000

    file that contains an array for themes

---
+++ inc/themes.inc.php
@@ -0,0 +1,47 @@
+<?
+//Color schemes.inc.php
+
+$COLORSCHEMES =  array("orangetheme" => array("_brandprimary" => "FF8C00",
+											"_brandtheme1" => "B8860B",
+											"_brandtheme2" => "DAA520",
+											"_brandratio" => ".3"),
+					"dodgerblue" => array("_brandprimary" => "1E90FF",
+											"_brandtheme1" => "87CEEB",
+											"_brandtheme2" => "1E90FF",
+											"_brandratio" => ".3"),
+					"redtheme" =>	array("_brandprimary" => "DC143C",
+											"_brandtheme1" => "800000",
+											"_brandtheme2" => "B22222",
+											"_brandratio" => ".3"),
+					"greentheme" => array("_brandprimary" =>"228B22",
+											"_brandtheme1" => "006400",
+											"_brandtheme2" => "32CD32",
+											"_brandratio" => ".3"),
+					"sm" => array("_brandprimary" => "346799",
+											"_brandtheme1" => "999999",
+											"_brandtheme2" => "CCCCCC",
+											"_brandratio" => "0"),
+					"clouds" => array("_brandprimary" => "1E90FF",
+											"_brandtheme1" => "1E90FF",
+											"_brandtheme2" => "1E90FF",
+											"_brandratio" => ".3"),
+					);
+
+
+$OPTIONALCOLORS = array("FF8C00",
+						"B8860B",
+						"DAA520",
+						"1E90FF",
+						"87CEEB",
+						"7FFFD4",
+						"DC143C",
+						"800000",
+						"B22222",
+						"228B22",
+						"006400",
+						"32CD32",
+						"346799",
+						"999999",
+						"CCCCCC"
+						);
+?>
