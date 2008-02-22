aacf7f2855a438f3fc2d17b04c764bf877aada1b
Josh Lai <jlai@schoolmessenger.com>
2008-02-22 18:15:51 +0000

    updated for branding

--- popup.inc.php
+++ popup.inc.php
@@ -1,27 +1,53 @@
 <?
 $pagetitle = preg_replace('/\\<.+>/','',(isset($TITLE) ? $TITLE : ""));
 ?>
+
+<script>
+	var _brandtheme = "<?=getBrandTheme();?>";
+</script>
+
+
 <html>
 <head>
-<title>SchoolMessenger: <?= $pagetitle ?></title>
+<title><?= getBrand();?>: <?= $PAGETITLE ?></title>
 	<script src='script/utils.js'></script>
 	<script src='script/nav.js'></script>
 	<script src='script/sorttable.js'></script>
 	<link href='css/style_print.css' type='text/css' rel='stylesheet' media='print'>
-	<link href='css/style.css' type='text/css' rel='stylesheet' media='screen'>
+	<link href='css.php' type='text/css' rel='stylesheet' media='screen'>
 </head>
+
+
 <body onload="try {UpdateTimer();} catch (e) {}" style="margin: 0px; background-color: white;">
 	<IFRAME src="blank.html" id="blocker" style="DISPLAY: none; LEFT: 0px; POSITION: absolute; TOP: 0px" frameBorder="0" scrolling="no"></IFRAME>
 
 
 
+<!-- ********************************************************************* -->
+
+<?
+	if($clickurl = getSystemSetting('_logoclickurl'))
+		$logo =  '<a href="' . $clickurl . '" target="_blank"><img src="logo.img.php"></a>';
+	else
+		$logo = '<img src="logo.img.php">';
+?>
+
+
 <div>
-	<table width="100%" border=0 cellpadding=0 cellspacing=0 background="img/header_bg.gif">
+	<table width="100%" border=0 cellpadding=0 cellspacing=0 background="img/themes/<?=getBrandTheme()?>/header_bg.gif" >
+		<tr><td style="font-size:8px;">&nbsp;</td></tr>
+	</table>
+</div>
+
+<div>
+	<table width="100%" border=0 cellpadding=0 cellspacing=0>
 	<tr>
-	<td><img src="img/logo.gif"></td>
+<?	// LOGO ?>
+		<td><div style="padding-left:10px; padding-bottom:10px;"><?=$logo?></div></td>
 	</tr>
 	</table>
 </div>
+
 
 <div class="pagetitle"><? if(isset($ICON)) print '<img src="img/icon_' . $ICON . '" align="absmiddle">'; ?> <?= (isset($TITLE) ? $TITLE : "") ?></div>
 <div class="pagetitlesubtext"><?= (isset($DESCRIPTION) ? $DESCRIPTION : "") ?></div>
