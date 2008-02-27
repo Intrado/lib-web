3c122e7b7e4d141d114b17633792d87c78ae8d74
Josh Lai <jlai@schoolmessenger.com>
2008-02-27 18:21:10 +0000

    remove default for support contacts

--- manager/customeredit.php
+++ manager/customeredit.php
@@ -291,8 +291,8 @@ if( $reloadform ) {
 	PutFormData($f, $s, "_brandprimary", getCustomerSystemSetting('_brandprimary', "346799", true, $custdb), "text", true);
 	PutFormData($f, $s, "_logoclickurl", getCustomerSystemSetting('_logoclickurl', "http://", true, $custdb), "text");
 
-	PutFormData($f, $s, "_supportemail", getCustomerSystemSetting('_supportemail', "support@schoolmessenger.com", true, $custdb), "email", "nomin", "nomax", true);
-	PutFormData($f, $s, "_supportphone", Phone::format(getCustomerSystemSetting('_supportphone', "8009203897", true, $custdb)), "phone", 10, 10, true);
+	PutFormData($f, $s, "_supportemail", getCustomerSystemSetting('_supportemail', "", true, $custdb), "email", "nomin", "nomax", true);
+	PutFormData($f, $s, "_supportphone", Phone::format(getCustomerSystemSetting('_supportphone', "", true, $custdb)), "phone", 10, 10, true);
 
 }
 
