aae9686f3abfb708816340801c28ff97791c9f83
Ben Hencke <bhencke@schoolmessenger.com>
2008-02-28 21:28:01 +0000

    *** empty log message ***

--- db/aspupgradecustomer.sql
+++ db/aspupgradecustomer.sql
@@ -337,5 +337,6 @@ $$$
 insert into setting (name, value) values ('_productname', 'SchoolMessenger'), ('_brandtheme', 'sm'), ('_brandtheme1', '999999'), ('_brandtheme2', 'CCCCCC'), ('_brandprimary', '346799'), ('_brandratio', '0'), ('_supportphone', '8009203897'), ('_supportemail', 'support@schoolmessenger.com')
 $$$
 
+-- THIS SETTING IS NOT FOR COMMSUITE!!!
 delete from usersetting where name='maxjobdays';
 $$$
