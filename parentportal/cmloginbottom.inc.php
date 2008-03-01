e74497e8f0eda64a5ee9351f91b605553e4a7806
Josh Lai <jlai@schoolmessenger.com>
2008-03-01 06:45:17 +0000

    fixed verisign logo typo

--- parentportal/cmloginbottom.inc.php
+++ parentportal/cmloginbottom.inc.php
@@ -22,7 +22,7 @@ if(isset($_COOKIE['embeddedpage']) && $_COOKIE['embeddedpage']=="1"){
 			<div style="float:right">
 				<table width="135" border="0" cellpadding="2" cellspacing="0" title="Click to Verify - This site chose VeriSign SSL for secure e-commerce and confidential communications.">
 					<tr>
-					<td width="135" align="center" valign="top"><script src=https://seal.verisign.com/getseal?host_name=contactme.schoolmessengerr.com&size=M&use_flash=YES&use_transparent=YES&lang=en></script><br />
+					<td width="135" align="center" valign="top"><script src=https://seal.verisign.com/getseal?host_name=contactme.schoolmessenger.com&size=M&use_flash=YES&use_transparent=YES&lang=en></script><br />
 					<a href="http://www.verisign.com/ssl-certificate/" target="_blank"  style="color:#000000; text-decoration:none; font:bold 7px verdana,sans-serif; letter-spacing:.5px; text-align:center; margin:0px; padding:0px;">ABOUT SSL CERTIFICATES</a></td>
 					</tr>
 				</table>
