ae383a86a044ba57cb56e2ac7584fb7a98f258d5
Josh Lai <jlai@schoolmessenger.com>
2008-03-12 20:40:27 +0000

    added more fields

--- obj/API_Job.obj.php
+++ obj/API_Job.obj.php
@@ -4,7 +4,20 @@ class API_Job{
 	var $id;
 	var $name;
 	var $description;
-
+	var $phonetotal;
+	var $emailtotal;
+	var $printtotal;
+	var $smstotal;
+	var $hasphone;
+	var $hasemail;
+	var $hasprint;
+	var $hassms;
+	var $phoneremaining;
+	var $emailremaining;
+	var $printremaining;
+	var $smsremaining;
+	var $startdate;
+	var $status;
 
 }
 
