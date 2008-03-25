df9e395d67e06a24f629f22efb5df70f66b57e9d
Josh Lai <jlai@schoolmessenger.com>
2008-03-25 20:08:04 +0000

    removed status code

--- obj/API_Job.obj.php
+++ obj/API_Job.obj.php
@@ -4,21 +4,6 @@ class API_Job{
 	var $id;
 	var $name;
 	var $description;
-	var $phonetotal;
-	var $emailtotal;
-	var $printtotal;
-	var $smstotal;
-	var $hasphone;
-	var $hasemail;
-	var $hasprint;
-	var $hassms;
-	var $phoneremaining;
-	var $emailremaining;
-	var $printremaining;
-	var $smsremaining;
-	var $startdate;
-	var $status;
-
 }
 
 ?>
