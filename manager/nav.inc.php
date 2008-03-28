727fa72078b1f19fd661116d0bb7dfd3064dafa2
Josh Lai <jlai@schoolmessenger.com>
2008-03-28 19:41:41 +0000

    added locked user to nav

--- manager/nav.inc.php
+++ manager/nav.inc.php
@@ -28,7 +28,8 @@
 		<td><a href="customers.php">Customer&nbsp;List</a></td>
 		<td><a href="customerimports.php">Customer&nbsp;Imports</a></td>
 		<td><a href="customeractivejobs.php">Active&nbsp;Jobs</a></td>
-		<td><a href="newcustomer.php">New&nbsp;Customer</a></td>		
+		<td><a href="newcustomer.php">New&nbsp;Customer</a></td>
+		<td><a href="lockedusers.php">Locked&nbsp;Users</a></td>
 		<td><a href="./?logout=1">Log&nbsp;Out</a></td>
 	</tr>
 </table>
