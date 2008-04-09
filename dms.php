b4c0870307c1f95d1de52cb43060460523fa9bed
Josh Lai <jlai@schoolmessenger.com>
2008-04-09 21:23:47 +0000

    added reset button to page

--- dms.php
+++ dms.php
@@ -8,6 +8,15 @@ if (!$USER->authorize('managesystem')) {
 	redirect('unauthorized.php');
 }
 
+if(isset($_GET['resetdm'])){
+	$resetid = $_GET['resetdm']+0;
+	if(auth_resetDM($resetid)){
+		redirect();
+	} else {
+		error("Something happened when trying to reset a DM", "Please try again later");
+	}
+}
+
 $result = Query("select dmid, name from custdm where enablestate = 'active' order by dmid");
 $data = array();
 while($row = DBGetRow($result)){
@@ -16,15 +25,15 @@ while($row = DBGetRow($result)){
 
 
 // index 0 is dmid
-function editDMRoute($row, $index){
-	$url = '<a href="dmsettings.php?dmid=' . $row[0] . '">Edit Route Plan</a>';
+function fmt_editDMRoute($row, $index){
+	$url = '<a href="dmsettings.php?dmid=' . $row[0] . '">Edit Route Plan</a>&nbsp;|&nbsp;<a href="dms.php?resetdm=' . $row[0] . '">Reset</a>';
 	return $url;
 }
 
 $titles = array(1 => "Name",
 				"actions" => "Actions");
 
-$formatters = array("actions" => "editDMRoute");
+$formatters = array("actions" => "fmt_editDMRoute");
 
 $PAGE="admin:settings";
 $TITLE="DM's";
