c30b706d9ef28867af025036afa13bfcc50a2009
Josh Lai <jlai@schoolmessenger.com>
2008-04-23 16:37:34 +0000

    new upload routes preview page

---
+++ uploadroutespreview.php
@@ -0,0 +1,127 @@
+<?
+include_once("inc/common.inc.php");
+include_once("inc/table.inc.php");
+include_once("inc/html.inc.php");
+include_once("inc/form.inc.php");
+include_once("obj/DMRoute.obj.php");
+
+if (!$USER->authorize('managesystem')) {
+	redirect('unauthorized.php');
+}
+
+$errormsg = false;
+$dmname = "";
+$curfilename = "";
+
+if(isset($_SESSION['dmid'], $_SESSION['routeuploadfiles'][$_SESSION['dmid']]) ){
+	$dmid = $_SESSION['dmid'];
+	$dmname = QuickQuery("select name from custdm where dmid = " . $dmid);
+	$curfilename = $_SESSION['routeuploadfiles'][$_SESSION['dmid']];
+} else {
+	$errormsg = "Please upload a file";
+}
+
+$dmroutes = DBFindMany("DMRoute", "from dmroute where dmid = " . $dmid);
+$dmroutes = resequence($dmroutes, "match");
+$routes = array();
+
+$f="uploadroutepreview";
+$s="main";
+$reloadform = 0;
+
+if(!CheckFormSubmit($f, "save") && $curfilename && !$errormsg){
+	if($fp = @fopen($curfilename, "r")){
+		while($row = fgetcsv($fp)){
+			if(count($row) == 4){
+				$routes[$row[0]] = $row;
+			}
+		}
+	} else {
+		$errormsg = "Unable to open the file";
+	}
+}
+
+if(CheckFormSubmit($f, "save")  && !$errormsg){
+	if($fp = @fopen($curfilename, "r")){
+		// CSV format is match, strip, prefix, suffix
+		while($row = fgetcsv($fp)){
+			if(count($row) == 4){
+				//validate each item in the row.
+				//match, prefix, and suffix should be numbers or empty string
+				//strip must be a number
+				if($row[0] && $row[0] != $row[0]+0){
+					$continue;
+				}
+				if($row[1] != $row[1]+0){
+					$continue;
+				}
+				if($row[2] && $row[2] != $row[2]+0){
+					$continue;
+				}
+				if($row[3] && $row[3] != $row[3]+0){
+					$continue;
+				}
+				if(isset($dmroutes[$row[0]])){
+					if($dmroutes[$row[0]]->strip != $row[1]
+						|| $dmroutes[$row[0]]->prefix != $row[2]
+						|| $dmroutes[$row[0]]->suffix != $row[3]){
+							QuickUpdate("update custdm set routechange=1 where dmid = " . $dmid);
+						}
+					$dmroutes[$row[0]]->strip = $row[1];
+					$dmroutes[$row[0]]->prefix = $row[2];
+					$dmroutes[$row[0]]->suffix = $row[3];
+					$dmroutes[$row[0]]->update();
+				} else {
+					$route = new DMRoute();
+					$route->dmid = $dmid;
+					$route->match = $row[0];
+					$route->strip = $row[1];
+					$route->prefix = $row[2];
+					$route->suffix = $row[3];
+					$route->create();
+					$update = true;
+				}
+			}
+		}
+		redirect("dmsettings.php");
+	} else {
+		$errormsg = "Unable to open the file";
+	}
+} else {
+	$reloadform=1;
+}
+
+
+if( $reloadform )
+{
+	ClearFormData($f);
+}
+
+if ($errormsg)
+	error($errormsg);
+
+
+$titles = array(0 => "Match",
+				1 => "Strip",
+				2 => "Prefix",
+				3 => "Suffix");
+
+
+NewForm($f);
+$PAGE="admin:settings";
+$TITLE="Upload Telco Settings Preview: " . $dmname;
+include("nav.inc.php");
+buttons(submit($f, "save", "Save"), button("Select Different File", null, "uploadroutes.php"), button("Cancel", null, "dmsettings.php"));
+startWindow("Routes Preview");
+?>
+	<table cellpadding="3" cellspacing="1" class="list" width="100%">
+<?
+		showTable($routes, $titles, array());
+?>
+	</table>
+<?
+endWindow();
+buttons();
+EndForm();
+include("navbottom.inc.php");
+?>
