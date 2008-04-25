aa9b8b09ebe2ff57b71e9697eec027538932d214
Josh Lai <jlai@schoolmessenger.com>
2008-04-25 17:16:39 +0000

    added dow, time, and email fields

--- manager/importalerts.php
+++ manager/importalerts.php
@@ -34,6 +34,8 @@ if(!isset($customerid, $importid)){
 	exit();
 }
 
+$dow = array(1 => "su", 2=>"m", 3=>"tu", 4=>"w", 5=>"th", 6=>"f", 7=>"s");
+
 // DB Connection
 $custinfo = QuickQueryRow("select s.dbhost, s.dbusername, s.dbpassword from shard s left join customer c on (s.id = c.shardid) where c.id = " . $customerid);
 $custdb = DBConnect($custinfo[0], $custinfo[1], $custinfo[2], "c_" . $customerid);
@@ -61,12 +63,24 @@ if(CheckFormSubmit($f, $s)){
 		} else {
 			$minsize = ereg_replace("[^0-9]*","",GetFormData($f, $s, "minsize"));
 			$maxsize = ereg_replace("[^0-9]*","",GetFormData($f, $s, "maxsize"));
+			$emaillist = GetFormData($f, $s, "emails");
 			if($maxsize !== "" && $maxsize < $minsize){
 				error("Max size must be greater than min size", "If you don't want a max size, set it to blank");
+			} elseif($bademaillist = checkemails($emaillist)) {
+				error("These emails are invalid", $bademaillist);
 			} else {
 				$importalert['minsize'] = $minsize;
 				$importalert['maxsize'] = $maxsize;
 				$importalert['daysold'] = GetFormData($f, $s, "daysold");
+				$newdows = array();
+				foreach($dow as $index => $day){
+					if(GetFormData($f, $s, $day)){
+						$newdows[] = $index;
+					}
+				}
+				$importalert['dow'] = implode(",", $newdows);
+				$importalert['time'] = date("H:i", strtotime(GetFormData($f, $s, "time")));
+				$importalert['emails'] = DBSafe($emaillist);
 				$importalerturl = http_build_query($importalert, false, "&");
 				QuickUpdate("update import set alertoptions = '" . DBSafe($importalerturl) . "' where id = " . $importid, $custdb);
 				redirect("customerimports.php");
@@ -84,6 +98,17 @@ if($reloadform){
 	PutFormData($f, $s, "daysold", isset($importalert['daysold']) ? $importalert['daysold'] : "", "text");
 	PutFormData($f, $s, "managerpassword", "", "text");
 	PutFormData($f, $s, "Save", "");
+
+	if(isset($importalert['dow'])){
+		$storeddow = array_flip(explode(",", $importalert['dow']));
+	} else {
+		$storeddow = array();
+	}
+	foreach($dow as $index => $day){
+		PutFormData($f, $s, $day, isset($storeddow[$index]) ? 1 : 0, "bool", 0, 1);
+	}
+	PutFormData($f, $s, "time", isset($importalert['time']) ? date("g:i a", strtotime($importalert['time'])) : "", "text");
+	PutFormData($f, $s, "emails", isset($importalert['emails']) ? $importalert['emails'] : "", "text");
 }
 
 
@@ -95,6 +120,34 @@ NewForm($f,"onSubmit='if(new getObj(\"managerpassword\").obj.value == \"\"){ win
 	<tr><td>Min Size:</td><td><? NewFormItem($f, $s, "minsize", "text", 10, 20)?></td></tr>
 	<tr><td>Max Size:</td><td><? NewFormItem($f, $s, "maxsize", "text", 10, 20)?></td></tr>
 	<tr><td>Days Old:</td><td><? NewFormItem($f, $s, "daysold", "text", 10, 20)?></td></tr>
+	<tr>
+		<td>Schedule:</td>
+		<td>
+			<table border="1px" margin="1px">
+				<tr>
+<?
+				foreach($dow as $day){
+					?><th><?=ucfirst($day)?></th><?
+				}
+?>
+					<th>Time</th>
+				</tr>
+				<tr>
+<?
+				foreach($dow as $day){
+					?><td><? NewFormItem($f, $s, $day, "checkbox"); ?></td><?
+				}
+?>
+					<td><? time_select($f, $s, "time") ?></td>
+				</tr>
+
+			</table>
+		</td>
+	</tr>
+	<tr>
+		<td>Emails:</td>
+		<td><? NewFormItem($f, $s, "emails", "text", 50, 255);?></td>
+	</tr>
 </table>
 <div><? NewFormItem($f, $s, "Save", 'submit'); ?></div>
 <?
