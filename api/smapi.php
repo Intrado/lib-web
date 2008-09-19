614e030673525c8baa150c0a270278cd46f86449
Martin Arnberg <marnberg@schoolmessenger.com>
2008-09-19 20:52:43 +0000

    Label API Class import

--- api/smapi.php
+++ api/smapi.php
@@ -1290,6 +1290,7 @@ require_once("API_Job.obj.php");
 require_once("API_JobStatus.obj.php");
 require_once("API_Contact.obj.php");
 require_once("API_ContactPreference.obj.php");
+require_once("API_Label.obj.php");
 
 ini_set("soap.wsdl_cache_enabled", "0"); // disabling WSDL cache
 $server=new SoapServer("smapi.wsdl");
