36c3e14e20ba71e13cf60794a0d744832350cd0b
Martin Arnberg <marnberg@schoolmessenger.com>
2008-10-02 19:06:52 +0000

    Adding multiple reply detection. The code still search keywords on multiple reply but will not send sms if text does not match keyword.
    txtreply  will use a txtsource.log file for the source numbers recieved during the day. The first message the next day will flush previous day records.

--- txtreply.php
+++ txtreply.php
@@ -1,13 +1,22 @@
 <?
+
 $username = "schoolmessenger";
 $password = "h3qfh221";
 $shortcode = "45305";
 $logfile = "/usr/commsuite/logs/txt.log";
+$sourcelog = "/usr/commsuite/logs/txtsourceday.log";
+
 $javadir = "/usr/commsuite/java/j2sdk/bin/java";
 $emailjar = "/usr/commsuite/server/simpleemail/simpleemail.jar";
 
+$keywords = array("end","stop","quit","cancel","unsubscribe");
+
 $body="";
 $line = date("Y-m-d H:i:s,");
+$sourceaddress = trim($_GET["SourceAddr"]);
+$message = strtolower($_GET["MessageText"]);
+
+
 foreach ($_GET as $k => $v) {
 	$line .= "$k=$v&";
 	$body .= "$k=$v\n";
@@ -19,32 +28,54 @@ if ($fp) {
 	fclose($fp);
 }
 else {
-	error_log("Unable to log SMS message from $replyto in $logfile");
+	error_log("Unable to log SMS message from $sourceaddress in $logfile");
 }
 
 
-
-
-$message = $_GET["MessageText"];
-$replyto = $_GET["SourceAddr"];
-$message = strtolower($message);
-
-$keywords = array("end","stop","quit","cancel","unsubscribe");
-
 // Check if message start with help
 if (stripos($message,"help") === 0) {
-	//echo "Help: do nothing";// For testing
+	//echo "Help: do nothing. ";// For testing
 	exit(); // Nothing else to do, response handled by 3CI
 }
 
 // Send a reply message if not in out keyword list
 if (!in_array($message,$keywords)) {
-	$client = new SoapClient(null,array("location" => "http://api.cmsmobilesuite.com:8080/axis2-1.3/services/NmApi", "uri" => "http://nmapi.cmsmobilesuite.com"));
+	// Check file mod time
+	if (date('y m d', filemtime($sourcelog)) === date('y m d')) {
+		//echo "Same day check and append. ";
+		$numbers = fopen($sourcelog, 'r+');
+		if ($numbers) {
+			while(!feof($numbers) && $foundrecord != true ) {
+				$phonenumber = fgets($numbers, 20);
+				if(trim($phonenumber) === $sourceaddress){
+					//echo "Source found, exit. \n";// For testing
+					fclose ($numbers);
+					exit();
+				}
+			}
+			fseek($numbers,0,SEEK_END);
+			fwrite($numbers,"$sourceaddress\n");
+			fclose ($numbers);
+		} else {
+			error_log("Unable to search previous reply sources from $sourcelog. May send reply to same address more than once.");
+		}
+	} else {
+		//echo "New day write new file. ";// For testing
+		$numbers = fopen($sourcelog, 'w');
+		if ($numbers) {
+			fwrite($numbers,"$sourceaddress\n");
+			fclose ($numbers);
+		} else {
+			error_log("Unable to delete phone records and add phonenumer to $sourcelog. May send reply to same address more than once.");
+		}
 
+	}
+
+	$client = new SoapClient(null,array("location" => "http://api.cmsmobilesuite.com:8080/axis2-1.3/services/NmApi", "uri" => "http://nmapi.cmsmobilesuite.com"));
 	if (client) {
 		try {
-			$response = $client->SubmitSMS($username,$password,$shortcode,$replyto,"This is the SchoolMessenger automated notification system. For more information, reply HELP. Send STOP to opt out. Std rates/other chgs may apply.");
-			//echo "Sending SMS";// For testing
+			$response = $client->SubmitSMS($username,$password,$shortcode,$sourceaddress,"This is the SchoolMessenger automated notification system. For more information, reply HELP. Send STOP to opt out. Std rates/other chgs may apply.");
+			//echo "Sending SMS. ";// For testing
 		} catch (SoapFault $fault) {
 			error_log("SOAP Fault: (faultcode: {$fault->faultcode}, faultstring: {$fault->faultstring})");
 		}
@@ -53,7 +84,7 @@ if (!in_array($message,$keywords)) {
 	}
 
 } else { // Send a email to support
-	//echo "Emailing";// For testing
+	//echo "Emailing. ";// For testing
 
 	$cmd = "$javadir -jar $emailjar";
 	$cmd .= " -s \"New SMS Message\"";
@@ -64,7 +95,7 @@ if (!in_array($message,$keywords)) {
 		fwrite($process, $body);
 		fclose($process);
 	} else {
-		error_log("Unable to email SMS message from $replyto");
+		error_log("Unable to email SMS message from $sourceaddress");
 	}
 }
 
