e1b2eaf757e6f360fe68a0fa12cb0a68578fb1f1
Ben Hencke <bhencke@schoolmessenger.com>
2008-10-03 17:23:03 +0000

    added ini file for txtreply settings

---
+++ inc/txtreply.ini.php
@@ -0,0 +1,17 @@
+;<?/* 
+;/*CSDELETEMARKER_START*/
+
+username=
+password=
+shortcode=
+logfile="/usr/commsuite/logs/txt.log"
+throttlefile="/tmp/txtsourceday.dat"
+
+javadir="/usr/commsuite/java/j2sdk/bin/java"
+emailjar="/usr/commsuite/server/simpleemail/simpleemail.jar"
+
+email[]=email1@example.com
+email[]=email2@example.com
+
+;/*CSDELETEMARKER_END*/
+;*/?>
