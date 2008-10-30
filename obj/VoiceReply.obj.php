fd866d5365adca1600aa809f7d154b7097bb8a57
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2008-10-30 15:32:38 +0000

    add field sequence

--- obj/VoiceReply.obj.php
+++ obj/VoiceReply.obj.php
@@ -2,18 +2,19 @@
 class VoiceReply extends DBMappedObject{
 
 	var $personid;
+	var $sequence;
 	var $jobid;
 	var $userid;
 	var $contentid;
 	var $replytime;
 	var $listened = 0;
-	
+
 
 
 	function VoiceReply($id = NULL){
 		$this->_allownulls = false;
 		$this->_tablename = "voicereply";
-		$this->_fieldlist = array("personid",
+		$this->_fieldlist = array("personid", "sequence",
 									"jobid", "userid", "contentid", "replytime", "listened");
 
 		//call super's constructor
