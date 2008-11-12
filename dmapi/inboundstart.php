e8df20a44b211b6799973c09e605d7b45975610c
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2008-11-12 18:38:00 +0000

    bugfix 2415 - repeat prompt for option 1 or 2, only 3 times then hangup.

--- dmapi/inboundstart.php
+++ dmapi/inboundstart.php
@@ -3,44 +3,55 @@
 
 global $BFXML_VARS;
 
-
+function invalidgoodbye() {
+?>
+<voice>
+	<message name="welcome">
+    	<tts gender="female" language="english">I did not understand your response.  Goodbye.</tts>
+		<hangup />
+	</message>
+</voice>
+<?
+}
 
 function welcomemessage() {
-?>	
+?>
 <voice>
 	<message name="welcome">
     	<tts gender="female" language="english">Welcome to the Schoolmessenger Phone Service.</tts>
 		<goto message="choose" />
 	</message>
 	<message name="choose">
-			<field name="redirect" type="menu" timeout="5000" sticky="true">
+			<field name="redirect" type="menu" timeout="5000">
 			<prompt repeat="2">
 	    	    <tts gender="female" language="english">To retrieve messages sent to you, press 1. If you are a user and want to log in, press 2</tts>
 			</prompt>
 			<choice digits="1" />
-			<choice digits="2" />				
+			<choice digits="2" />
 			<default>
-	        	<tts gender="female" language="english">Sorry. That was not a valud option.</tts>
+	        	<tts gender="female" language="english">Sorry. That was not a valid option.</tts>
 			</default>
 			<timeout>
 				<tts gender="female" language="english">I was not able to understand your response. Goodbye.</tts>
 				<hangup />
-			</timeout>			
-		</field>	
+			</timeout>
+		</field>
 	</message>
 </voice>
 <?
 }
 
-if($REQUEST_TYPE == "new") {
+if ($REQUEST_TYPE == "new") {
 	welcomemessage();
-} else if($REQUEST_TYPE == "continue") {
-	if($BFXML_VARS['redirect'] == 2) {
+} else if ($REQUEST_TYPE == "continue") {
+	if ($BFXML_VARS['redirect'] == 2) {
 		forwardToPage("inboundlogin.php");
+	} else if ($BFXML_VARS['redirect'] == 1) {
+		forwardToPage("msgcallbackconfirmphone.php");
 	} else {
-		forwardToPage("msgcallbackconfirmphone.php");	
+		invalidgoodbye();
 	}
-  }
+}
 
 
 ?>
