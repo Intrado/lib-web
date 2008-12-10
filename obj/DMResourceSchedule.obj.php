bb618c0f90911a9b1455e55a4f1e3f5f60c24d38
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2008-12-10 18:48:38 +0000

    set defaults

--- obj/DMResourceSchedule.obj.php
+++ obj/DMResourceSchedule.obj.php
@@ -3,9 +3,9 @@
 class DMResourceSchedule extends DBMappedObject {
 
 	var $dmid;
-	var $daysofweek;
-	var $starttime;
-	var $endtime;
+	var $daysofweek = "2,3,4,5,6";
+	var $starttime = "08:00:00";
+	var $endtime = "17:00:00";
 	var $resourcepercentage = 1;
 
 	function DMResourceSchedule ($id = NULL) {
