84fbc90be7c54cc1d4198f2ea242d4cbc75e2b8d
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2008-12-12 14:45:28 +0000

    fix if isset($job) on new job save, trying to load all messages for copied deleted view

--- jobedit.inc.php
+++ jobedit.inc.php
@@ -729,32 +729,34 @@ if ($submittedmode || $completedmode) {
 	$messages['print'] = DBFindMany("Message","from message where userid=" . $USER->id ." and deleted=0 and type='print' order by name");
 	$messages['sms'] = DBFindMany("Message","from message where userid=" . $USER->id ." and deleted=0 and type='sms' order by name");
 	// find if this was a copied job, with deleted messages
-	$copiedmessages = DBFindMany("Message","from message where id='$job->phonemessageid' or id in (select messageid from joblanguage where type='phone' and jobid=$job->id)");
-	foreach ($copiedmessages as $m) {
-		if ($m->deleted == "1") {
-			$m->name = "(copy) ".$m->name;
-			$messages['phone'][] = $m;
+	if (isset($job)) {
+		$copiedmessages = DBFindMany("Message","from message where id='$job->phonemessageid' or id in (select messageid from joblanguage where type='phone' and jobid=$job->id)");
+		foreach ($copiedmessages as $m) {
+			if ($m->deleted == "1") {
+				$m->name = "(copy) ".$m->name;
+				$messages['phone'][] = $m;
+			}
 		}
-	}
-	$copiedmessages = DBFindMany("Message","from message where id='$job->emailmessageid' or id in (select messageid from joblanguage where type='email' and jobid=$job->id)");
-	foreach ($copiedmessages as $m) {
-		if ($m->deleted == "1") {
-			$m->name = "(copy) ".$m->name;
-			$messages['email'][] = $m;
+		$copiedmessages = DBFindMany("Message","from message where id='$job->emailmessageid' or id in (select messageid from joblanguage where type='email' and jobid=$job->id)");
+		foreach ($copiedmessages as $m) {
+			if ($m->deleted == "1") {
+				$m->name = "(copy) ".$m->name;
+				$messages['email'][] = $m;
+			}
 		}
-	}
-	$copiedmessages = DBFindMany("Message","from message where id='$job->printmessageid' or id in (select messageid from joblanguage where type='print' and jobid=$job->id)");
-	foreach ($copiedmessages as $m) {
-		if ($m->deleted == "1") {
-			$m->name = "(copy) ".$m->name;
-			$messages['print'][] = $m;
+		$copiedmessages = DBFindMany("Message","from message where id='$job->printmessageid' or id in (select messageid from joblanguage where type='print' and jobid=$job->id)");
+		foreach ($copiedmessages as $m) {
+			if ($m->deleted == "1") {
+				$m->name = "(copy) ".$m->name;
+				$messages['print'][] = $m;
+			}
 		}
-	}
-	$copiedmessages = DBFindMany("Message","from message where id='$job->smsmessageid' or id in (select messageid from joblanguage where type='sms' and jobid=$job->id)");
-	foreach ($copiedmessages as $m) {
-		if ($m->deleted == "1") {
-			$m->name = "(copy) ".$m->name;
-			$messages['sms'][] = $m;
+		$copiedmessages = DBFindMany("Message","from message where id='$job->smsmessageid' or id in (select messageid from joblanguage where type='sms' and jobid=$job->id)");
+		foreach ($copiedmessages as $m) {
+			if ($m->deleted == "1") {
+				$m->name = "(copy) ".$m->name;
+				$messages['sms'][] = $m;
+			}
 		}
 	}
 }
