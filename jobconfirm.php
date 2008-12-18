00e4d4cbf520e59fe0fc7b7ce1bcf5a8084f766c
Martin Arnberg <marnberg@schoolmessenger.com>
2008-12-18 18:56:24 +0000

    Show google branding only if it has languages

--- jobconfirm.php
+++ jobconfirm.php
@@ -140,7 +140,7 @@ foreach($joblangs[$type] as $joblang) {
 }
 ?>
 	</table>
-<?		if ($job && $job->getSetting('translationmessage')) { ?>
+<?		if (count($joblangs[$type]) != 0 && $job && $job->getSetting('translationmessage')) { ?>
 			<div id='branding'>
 				<div style="color: rgb(103, 103, 103);float: left;" class="gBranding"><span style="vertical-align: middle; font-family: arial,sans-serif; font-size: 11px;" class="gBrandingText">Translation powered by<img style="padding-left: 1px; vertical-align: middle;" alt="Google" src="http://www.google.com/uds/css/small-logo.png"></span></div>
 			</div>
