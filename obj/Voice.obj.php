16d4b7cbb7ca75fc560ec8860439ea88f12c36c6
Martin Arnberg <marnberg@schoolmessenger.com>
2008-12-29 18:31:57 +0000

    Order tts languages by id instead of language

--- obj/Voice.obj.php
+++ obj/Voice.obj.php
@@ -16,10 +16,10 @@ class Voice extends DBMappedObject {
 	// return array of Voice objects (indexed "language:gender") based on customer language table
 	static function getTTSVoices() {
 		if (getSystemSetting("_dmmethod", "asp") == "asp") {
-			$voices = DBFindMany("Voice","from ttsvoice t join language l where l.name = t.language order by t.language, t.gender desc", "t");
+			$voices = DBFindMany("Voice","from ttsvoice t join language l where l.name = t.language order by t.id", "t");
 		} else {
 			// if flex customer, limit to English and Spanish
-			$voices = DBFindMany("Voice","from ttsvoice t join language l where l.name in ('English', 'Spanish') and l.name = t.language order by t.language, t.gender desc", "t");
+			$voices = DBFindMany("Voice","from ttsvoice t join language l where l.name in ('English', 'Spanish') and l.name = t.language order by t.id", "t");
 		}
 		$retval = array();
 		foreach ($voices as $voice) {
@@ -31,10 +31,10 @@ class Voice extends DBMappedObject {
 	// return array of strings, all supported languages for tts, based on customer language table
 	static function getTTSLanguages() {
 		if (getSystemSetting("_dmmethod", "asp") == "asp") {
-			return QuickQueryList("select distinct t.language from ttsvoice t join language l where l.name = t.language order by t.language");
+			return QuickQueryList("select distinct t.language from ttsvoice t join language l where l.name = t.language order by t.id");
 		} else {
 			// if flex customer, limit to English and Spanish
-			return QuickQueryList("select distinct t.language from ttsvoice t join language l where l.name in ('English', 'Spanish') and l.name = t.language order by t.language");
+			return QuickQueryList("select distinct t.language from ttsvoice t join language l where l.name in ('English', 'Spanish') and l.name = t.language order by t.id");
 		}
 	}
 
