d6ceacfc9608e62afb8c5922fd6be8c7a33bc1a8
Nickolas Heckman <nheckman@schoolmessenger.com>
2008-12-31 19:06:21 +0000

    Changed an include file to required.

--- reportcallsperson.php
+++ reportcallsperson.php
@@ -3,7 +3,7 @@
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
 require_once("inc/common.inc.php");
-include_once("inc/securityhelper.inc.php");
+require_once("inc/securityhelper.inc.php");
 require_once("inc/table.inc.php");
 require_once("inc/html.inc.php");
 require_once("inc/form.inc.php");
