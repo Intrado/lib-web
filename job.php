7a99fa0ee232568a7ec2e12a479cb96c829936c2
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-01-07 12:56:55 +0000

    Changed included files to required.

--- job.php
+++ job.php
@@ -2,26 +2,26 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
-include_once("inc/common.inc.php");
-include_once("inc/securityhelper.inc.php");
-include_once("inc/html.inc.php");
-include_once("inc/utils.inc.php");
-include_once("inc/table.inc.php");
-include_once("inc/form.inc.php");
-include_once("obj/Job.obj.php");
-include_once("obj/JobLanguage.obj.php");
-include_once("obj/JobType.obj.php");
-include_once("obj/PeopleList.obj.php");
-include_once("obj/Message.obj.php");
-include_once("obj/MessagePart.obj.php");
-include_once("obj/AudioFile.obj.php");
-include_once("obj/FieldMap.obj.php");
-include_once("obj/Language.obj.php");
-include_once("obj/Schedule.obj.php");
-include_once("inc/formatters.inc.php");
-include_once("obj/Phone.obj.php");
-include_once("obj/Sms.obj.php");
-include_once("obj/Voice.obj.php");
+require_once("inc/common.inc.php");
+require_once("inc/securityhelper.inc.php");
+require_once("inc/html.inc.php");
+require_once("inc/utils.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/form.inc.php");
+require_once("obj/Job.obj.php");
+require_once("obj/JobLanguage.obj.php");
+require_once("obj/JobType.obj.php");
+require_once("obj/PeopleList.obj.php");
+require_once("obj/Message.obj.php");
+require_once("obj/MessagePart.obj.php");
+require_once("obj/AudioFile.obj.php");
+require_once("obj/FieldMap.obj.php");
+require_once("obj/Language.obj.php");
+require_once("obj/Schedule.obj.php");
+require_once("inc/formatters.inc.php");
+require_once("obj/Phone.obj.php");
+require_once("obj/Sms.obj.php");
+require_once("obj/Voice.obj.php");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Authorization
