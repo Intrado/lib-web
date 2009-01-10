22c287860158cd095d995b06eb6735547f39cee1
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-01-10 04:16:27 +0000

    More image location updates.

--- manager/managerutils.inc.php
+++ manager/managerutils.inc.php
@@ -107,12 +107,12 @@ function show_column_selector($tablename=null, $fields, $lockedFields=array()){
 					?><td><div align="center">
 					<?
 						if ($display) {
-							$result = "<img src=\"../img/checkbox-rule.png\" " .
+							$result = "<img src=\"img/checkbox-rule.png\" " .
 									"onclick=\"var x = new getObj('hiddenfield$fieldnum'); " .
 									"if(x.obj.checked){this.src='img/checkbox-clear.png'}else{this.src='img/checkbox-rule.png'};";
 							$checked = "checked>";
 						} else {
-							$result = "<img src=\"../img/checkbox-clear.png\" " .
+							$result = "<img src=\"img/checkbox-clear.png\" " .
 									"onclick=\"var x = new getObj('hiddenfield$fieldnum'); " .
 									"if(x.obj.checked){this.src='img/checkbox-clear.png'}else{this.src='img/checkbox-rule.png'};";
 							$checked = ">";
