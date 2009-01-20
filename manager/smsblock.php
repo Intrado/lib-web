8fb8d9d0e6adf2583c7b3dd04c13b0777ef37d15
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-01-20 17:00:47 +0000

    Make included files required.

--- manager/smsblock.php
+++ manager/smsblock.php
@@ -1,11 +1,11 @@
 <?
-include_once("common.inc.php");
-include_once("../obj/Phone.obj.php");
-include_once("../obj/Customer.obj.php");
-include_once("../inc/table.inc.php");
-include_once("../inc/form.inc.php");
-include_once("../inc/formatters.inc.php");
-include_once("../inc/html.inc.php");
+require_once("common.inc.php");
+require_once("../obj/Phone.obj.php");
+require_once("../obj/Customer.obj.php");
+require_once("../inc/table.inc.php");
+require_once("../inc/form.inc.php");
+require_once("../inc/formatters.inc.php");
+require_once("../inc/html.inc.php");
 require_once("XML/RPC.php");
 
 if (!$MANAGERUSER->authorized("smsblock"))
