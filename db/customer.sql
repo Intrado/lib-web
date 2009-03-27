0478cd740d78c7f617eb50a5b93fb9f0321244a2
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2009-03-27 13:12:26 +0000

    create table subscriber

--- db/customer.sql
+++ db/customer.sql
@@ -1353,3 +1353,16 @@ $$$
 ALTER TABLE `custdm` ADD `poststatus` TEXT NOT NULL default ''
 $$$
 
+
+CREATE TABLE `subscriber` (
+`id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+`username` VARCHAR( 255 ) NOT NULL ,
+`password` VARCHAR( 50 ) NOT NULL ,
+`personid` INT NULL ,
+`lastlogin` DATETIME NULL ,
+`enabled` TINYINT NOT NULL
+) ENGINE = InnoDB CHARACTER SET utf8 COLLATE utf8_bin
+$$$
+
+ALTER TABLE `persondatavalues` ADD `editlock` TINYINT NOT NULL DEFAULT '0'
+$$$
