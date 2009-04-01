cf00f0b8327eef1f4dfbd8ff5f78d734a30a213a
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2009-04-01 12:35:23 +0000

    shared css

--- css.php
+++ css.php
@@ -2,5 +2,5 @@
 include_once("inc/common.inc.php");
 session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
 
-include_once("inc/css.inc.php");
+include_once("css/css.inc.php");
 ?>
