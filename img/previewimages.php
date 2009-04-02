1aba3b0ae259fcce1d33b8ef33e4b3612d19d723
Ben Hencke <bhencke@schoolmessenger.com>
2009-04-02 13:09:14 +0000

    scans certain directories and puts every gif/png/jpg on a page. useful for picking an icon, and also comparing themes, etc

---
+++ img/previewimages.php
@@ -0,0 +1,122 @@
+<html>
+<head>
+<style>
+
+img {
+	border: 0px;
+	padding: 3px;
+}
+
+.clear {
+	clear: both;
+	height: 1px;
+	overflow: hidden;
+}
+
+h1 {
+	clear: both;
+}
+
+</style>
+</head>
+<body>
+
+
+background color:
+<select onchange="body.style.backgroundColor=this.value;">
+<?
+
+for ($x = 255; $x >= 0; $x -= 8) {
+	echo '<option style="background-color: rgb('.$x.','.$x.','.$x.');" value="rgb('.$x.','.$x.','.$x.')">'.$x.'</option>';
+}
+?>
+</select>
+
+<?
+
+
+$folders = array(
+	"icons",
+	"icons/fugue",
+	"icons/diagona/16",
+	"icons/diagona/10",
+	".",
+	"themes/3dblue",
+	"themes/cherrybubblegum",
+	"themes/chrome",
+	"themes/chromeblue",
+	"themes/chromeorange",
+	"themes/chromepurple",
+	"themes/CVS",
+	"themes/easy_invaders",
+	"themes/gold",
+	"themes/grass",
+	"themes/grass2",
+	"themes/greentheme",
+	"themes/lemon",
+	"themes/marble",
+	"themes/min",
+	"themes/mintgreen",
+	"themes/nature",
+	"themes/ocean",
+	"themes/redmainblueback",
+	"themes/redtheme",
+	"themes/rose",
+	"themes/rouge",
+	"themes/sm",
+	"themes/tangerine",
+);
+
+?>
+<h1>Contents:</h1>
+<ul>
+<?
+foreach ($folders as $folder) {
+	echo '<li><a href="#'.$folder.'">'.$folder.'</a></li>';
+}
+?>
+</ul>
+<hr></hr>
+<?
+foreach ($folders as $folder) {
+	
+	echo '<a name="'.$folder.'"><h1>'.$folder.DIRECTORY_SEPARATOR.'</h1></a><div>';
+	
+	
+	$icons = array();
+	$dir = dir($folder);
+	while (false !== ($entry = $dir->read())) {
+		if (strpos($entry,".") === 0)
+			continue;
+		
+		$ext = strtolower(substr($entry,strlen($entry) - 4,4));
+		
+		if (!in_array($ext,array(".gif",".png",".jpg")))
+			continue;
+
+		if (is_dir($folder.DIRECTORY_SEPARATOR.$entry))
+			continue;
+			
+		$icons[] = $entry;
+	}
+	
+	sort($icons);
+	
+	$count = 0;
+	foreach ($icons as $entry) {
+		
+		echo '<img Title="'.$entry.'" src="'.$folder.DIRECTORY_SEPARATOR.$entry.'">';
+		
+		if (++$count % 20 == 0)
+			echo '<div class="clear"></div>';
+	}
+	
+	echo '</div>';
+}
+
+
+
+
+?>
+</body>
+</html>
