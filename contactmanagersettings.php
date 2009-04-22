60dcd13386f6d3d1ce5704fa1eb06244ac01906d
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2009-04-22 17:37:09 +0000

    new form gui

--- contactmanagersettings.php
+++ contactmanagersettings.php
@@ -12,6 +12,10 @@ include_once("inc/text.inc.php");
 include_once("obj/Setting.obj.php");
 include_once("obj/Phone.obj.php");
 
+require_once("obj/Validator.obj.php");
+require_once("obj/Form.obj.php");
+require_once("obj/FormItem.obj.php");
+
 ////////////////////////////////////////////////////////////////////////////////
 // Authorization
 ////////////////////////////////////////////////////////////////////////////////
@@ -26,194 +30,135 @@ if (!$USER->authorize('managesystem') && !getSystemSetting("_hasportal", false) 
 $maxphones = getSystemSetting("maxphones", 3);
 $maxemails = getSystemSetting("maxemails", 2);
 $maxsms = getSystemSetting("maxsms", 2);
-$maxcolumns = max($maxphones, $maxemails, $maxsms);
 
-/****************** main message section ******************/
-
-$f = "contactmanagersettings";
-$s = "main";
-$reloadform = 0;
-
-if(CheckFormSubmit($f,$s) || CheckFormSubmit($f,'addtype'))
-{
-	//check to see if formdata is valid
-	if(CheckFormInvalid($f))
-	{
-		error('Form was edited in another window, reloading data');
-		$reloadform = 1;
+$formdata = array();
+$formdata["tokenlife"] = array(
+        "label" => _L("Activation Code Lifetime (1-365 days)"),
+        "value" => getSystemSetting('tokenlife', 30),
+        "validators" => array(
+            array("ValLength","min" => 1,"max" => 3),
+            array("ValNumeric","min"=>1,"max"=>365)
+        ),
+        "control" => array("TextField","maxlength" => 3),
+        "helpstep" => 1
+);
+$checked = "false";
+if (getSystemSetting('portalphoneactivation', 0))
+	$checked = "true";
+$formdata["portalphoneactivation"] = array(
+		"label" => _L("Allow Activation via Phone"),
+        "value" => $checked,
+        "validators" => array(
+        ),
+        "control" => array("Checkbox"),
+        "helpstep" => 1
+);
+$checked = "false";
+if (getSystemSetting('priorityenforcement', 0))
+	$checked = "true";
+$formdata["priorityenforcement"] = array(
+		"label" => _L("Require Phone Numbers for Emergency and High Priority Job Types"),
+        "value" => $checked,
+        "validators" => array(
+        ),
+        "control" => array("CheckBox"),
+        "helpstep" => 1
+);
+$restrictedfields = array(); // all fields
+$selectedvalues = array(); // selected fields
+for ($i=0; $i<$maxphones; $i++) {
+	$restrictedfields["lockedphone".$i] = "Phone ".($i+1); // TODO add phone label
+	if (getSystemSetting("lockedphone".$i, 0)) $selectedvalues[] = "lockedphone".$i;
+}
+for ($i=0; $i<$maxemails; $i++) {
+	$restrictedfields["lockedemail".$i] = "Email ".($i+1); // TODO add phone label
+	if (getSystemSetting("lockedemail".$i, 0)) $selectedvalues[] = "lockedemail".$i;
+}
+if (getSystemSetting("_hassms", false)) {
+	for ($i=0; $i<$maxsms; $i++) {
+		$restrictedfields["lockedsms".$i] = "SMS ".($i+1); // TODO add phone label
+		if (getSystemSetting("lockedsms".$i, 0)) $selectedvalues[] = "lockedsms".$i;
 	}
-	else
-	{
-		MergeSectionFormData($f, $s);
-		TrimFormData($f, $s, 'tokenlife');
-		//do check
-		if( CheckFormSection($f, $s) )
-		{
-			error('There was a problem trying to save your changes', 'Please verify that all required field information has been entered properly');
-		} else {
-			//check the parsing
+}
+$formdata["multicheckbox"] = array(
+        "label" => _L("Restricted Destination Fields"),
+        "value" => $selectedvalues,
+        "validators" => array(
+            array("ValRequired")
+        ),
+        "control" => array("MultiCheckbox", "height" => "100px", "values" => $restrictedfields),
+        "helpstep" => 1
+);
 
-			if (isset($errors) && count($errors) > 0) {
-				error('There was an error parsing the setting', implode("",$errors));
-			} else {
-				//submit changes
 
-				for($i = 0; $i < $maxphones; $i++){
-					setSystemSetting('lockedphone' . $i, GetFormData($f, $s, 'lockedphone' . $i));
-				}
-				for($i = 0; $i < $maxemails; $i++){
-					setSystemSetting('lockedemail' . $i, GetFormData($f, $s, 'lockedemail' . $i));
-				}
-				for($i = 0; $i < $maxsms; $i++){
-					setSystemSetting('lockedsms' . $i, GetFormData($f, $s, 'lockedsms' . $i));
-				}
-				setSystemSetting('tokenlife', TrimFormData($f, $s, 'tokenlife'));
-				setSystemSetting('priorityenforcement', GetFormData($f, $s, 'priorityenforcement'));
-				setSystemSetting('portalphoneactivation', GetFormData($f, $s, 'allowphoneactivation'));
+$helpsteps = array (
+	_L("Contact Manager Settings."),
+	_L("yes you can")
+);
 
-				redirect("settings.php");
-			}
+$buttons = array(submit_button(_L("Done"),"submit","accept"),
+				icon_button(_L("Cancel"),"cross",null,"settings.php"));
+
+$form = new Form("account", $formdata, $helpsteps, $buttons);
+$form->ajaxsubmit = true;
+
+//check and handle an ajax request (will exit early)
+//or merge in related post data
+$form->handleRequest();
+
+
+//check for form submission
+if ($button = $form->getSubmit()) { //checks for submit and merges in post data
+    if (($errors = $form->validate()) === false) { //checks all of the items in this form
+        $postdata = $form->getData(); //gets assoc array of all values {name:value,...}
+        $ajax = $form->isAjaxSubmit(); //whether or not this requires an ajax response        
+        
+        //save data here
+        
+        setSystemSetting('tokenlife', $postdata['tokenlife']);
+
+        if ($postdata['portalphoneactivation'])
+        	setSystemSetting('portalphoneactivation', '1');
+        else
+        	setSystemSetting('portalphoneactivation', '0');
+        
+        if ($postdata['priorityenforcement'])
+        	setSystemSetting('priorityenforcement', '1');
+        else
+        	setSystemSetting('priorityenforcement', '0');
+        
+        $fields = $postdata['multicheckbox'];
+		// unset them all
+		for ($i = 0; $i < $maxphones; $i++) {
+			setSystemSetting('lockedphone' . $i, '0');
 		}
-	}
-} else {
-	$reloadform = 1;
+		for ($i = 0; $i < $maxemails; $i++) {
+			setSystemSetting('lockedemail' . $i, '0');
+		}
+		for ($i = 0; $i < $maxsms; $i++) {
+			setSystemSetting('lockedsms' . $i, '0');
+		}
+		// now set the ones selected
+        foreach ($fields as $k => $v)
+			setSystemSetting($v, '1');
+		
+        	
+        if ($ajax)
+            $form->sendTo("settings.php");
+        else
+            redirect("settings.php");
+    }
 }
 
-if( $reloadform )
-{
-	ClearFormData($f);
 
-	//check for new setting name/desc from settings.php
-
-	for($i=0; $i < $maxphones; $i++){
-		PutFormData($f, $s, "lockedphone" . $i, getSystemSetting('lockedphone' . $i, 0), "bool", 0, 1);
-	}
-	for($i=0; $i < $maxemails; $i++){
-		PutFormData($f, $s, "lockedemail" . $i, getSystemSetting('lockedemail' . $i, 0), "bool", 0, 1);
-	}
-	for($i=0; $i < $maxsms; $i++){
-		PutFormData($f, $s, "lockedsms" . $i, getSystemSetting('lockedsms' . $i, 0), "bool", 0, 1);
-	}
-	PutFormData($f, $s, "tokenlife", getSystemSetting('tokenlife', 30), 'number', 1, 365, true);
-	PutFormData($f, $s, 'priorityenforcement', getSystemSetting('priorityenforcement', 0), "bool", 0, 1);
-	PutFormData($f, $s, 'allowphoneactivation', getSystemSetting('portalphoneactivation', 0), "bool", 0, 1);
-}
 ////////////////////////////////////////////////////////////////////////////////
 // Display
 ////////////////////////////////////////////////////////////////////////////////
 
 $PAGE = "admin:settings";
-$TITLE = 'Contact Manager Settings';
+$TITLE = _L('Contact Manager Settings');
 
 include_once("nav.inc.php");
-
-NewForm($f);
-buttons(submit($f, $s, 'Save'));
-startWindow('Contact Manager Settings');
-		?>
-			<table border="0" cellpadding="3" cellspacing="0" width="100%">
-				<tr>
-					<th align="right" class="windowRowHeader bottomBorder" valign="top" style="padding-top: 6px;">Contact Manager:</th>
-					<td class="bottomBorder">
-						<table border="0" cellpadding="2" cellspacing="0" width="100%">
-								<tr>
-									<td width="30%">Activation Code Lifetime<?=help("Settings_ActCodeLifetime", NULL, "small")?></td>
-									<td><? NewFormItem($f, $s, "tokenlife", "text", 3); ?> 1 - 365 days</td>
-								</tr>
-
-								<tr>
-									<td width="30%">Allow activation via phone <?=help("Settings_PortalPhoneActivation", NULL, "small")?></td>
-									<td><? NewFormItem($f, $s, "allowphoneactivation", "checkbox"); ?></td>
-								</tr>
-
-								<tr>
-									<td width="30%">Require phone numbers for Emergency and High Priority Job Types<?=help("Settings_RequirePhone", NULL, "small")?></td>
-									<td><? NewFormItem($f, $s, "priorityenforcement", "checkbox"); ?></td>
-								</tr>
-
-								<tr>
-									<td width="30%">Restricted Destination Fields<?=help("Settings_RestrictedDest", NULL, "small")?></td>
-									<td>
-										<table border="0" cellpadding="3" cellspacing="1">
-											<tr class="listheader">
-												<th>Contact Type</th>
-												<?
-												for($i=1; $i<= $maxcolumns;$i++){
-													?><th><?=$i?></th><?
-												}
-												?>
-											</tr>
-
-											<tr>
-												<td align="left" class="bottomBorder"><?=destination_label_popup_paragraph("phone")?></td>
-<?
-												for($i=0; $i< $maxcolumns; $i++){
-?>
-													<td align="center" class="bottomBorder">
-<?
-													if($i< $maxphones){
-														destination_label_popup("phone", $i, $f, $s, "lockedphone" . $i);
-													} else {
-														echo "&nbsp;";
-													}
-?>
-													</td>
-<?
-												}
-?>
-											</tr>
-											<tr>
-												<td align="left" class="bottomBorder"><?=destination_label_popup_paragraph("email")?></td>
-<?
-												for($i=0; $i< $maxcolumns; $i++){
-?>
-													<td align="center" class="bottomBorder">
-<?
-													if($i< $maxemails){
-														destination_label_popup("email", $i, $f, $s, "lockedemail" . $i);
-													} else {
-														echo "&nbsp;";
-													}
-?>
-													</td>
-<?
-												}
-?>
-											</tr>
-<?
-											if(getSystemSetting("_hassms", false)){
-?>
-											<tr>
-												<td align="left" class="bottomBorder"><?=destination_label_popup_paragraph("sms")?></td>
-<?
-												for($i=0; $i< $maxcolumns; $i++){
-?>
-													<td align="center" class="bottomBorder">
-<?
-													if($i< $maxsms){
-														destination_label_popup("sms", $i, $f, $s, "lockedsms" . $i);
-													} else {
-														echo "&nbsp;";
-													}
-?>
-													</td>
-<?
-												}
-?>
-											</tr>
-<?
-											}
-?>
-										</table>
-									</td>
-								</tr>
-						</table>
-					</td>
-				</tr>
-			</table>
-			<?
-endWindow();
-buttons();
-EndForm();
+echo $form->render();
 include_once("navbottom.inc.php");
 ?>
