1fadf9e389b69e42864e12a0549f4c8bfa0c2088
Ben Hencke <bhencke@schoolmessenger.com>
2009-04-22 14:39:28 +0000

    updated to proper default settings (log queries off, use ssl, etc) and add readonly example config

--- manager/managersettings.ini.php
+++ manager/managersettings.ini.php
@@ -13,20 +13,24 @@ user="root"
 pass=""
 db="authserver"
 
+;override's the information in the shard table in order to read from slaves
+;remember on asp shard2 is swapped IDs with shard1 (shard2 is index1, and shard1 is index2)
+;readonly[]=localhost
+;readonly[]=localhost
+;readonly[]=localhost
 
 [feature]
-log_dir="/commsuite/logs/"
+log_dir="/usr/commsuite/logs/"
 log_db_errors=true
-log_db_queries=true
+log_db_queries=false
 tmp_dir="/tmp"
-has_ssl=false
-force_ssl=false
+has_ssl=true
+force_ssl=true
 
 ; server to redirect customer link to
-customer_url_prefix=http://localhost
+customer_url_prefix=https://localhost
 
 [content]
-tts="devbox4,8080,/phone/Tts"
+tts="localhost,8080,/phone/Tts"
 
-externalcontent=false;
 ;*/?>
