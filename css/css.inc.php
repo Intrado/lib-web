4bdbc45dd4e33b38e0218c9546f74a3d89a8fa63
Ben Hencke <bhencke@schoolmessenger.com>
2009-05-08 11:52:24 +0000

    includes should NEVER set headers. also changed the way it checks to see if there is no colorscheme set to use defaults

--- css/css.inc.php
+++ css/css.inc.php
@@ -1,11 +1,7 @@
 <?
 
-header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire in 1 hour, but if theme changes so will hash pointing to this file
-header("Content-Type: text/css");
-header("Cache-Control: private");
-
 // hack for pages not logged in (no session)
-if (!isset($_SESSION)) {
+if (!isset($_SESSION['colorscheme'])) {
 	$_SESSION['colorscheme']['_brandtheme']   = "3dblue";
 	$_SESSION['colorscheme']['_brandtheme1']  = "89A3CE";
 	$_SESSION['colorscheme']['_brandtheme2']  = "89A3CE";
