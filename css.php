5e34618dbfe16fea8315a82bad7b2184cd7023a1
Ben Hencke <bhencke@schoolmessenger.com>
2009-05-08 11:54:46 +0000

    includes should NEVER set headers, so we set them here

--- css.php
+++ css.php
@@ -1,4 +1,9 @@
 <?
+header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire in 1 hour, but if theme changes so will hash pointing to this file
+header("Content-Type: text/css");
+header("Cache-Control: private");
+
+
 include_once("inc/common.inc.php");
 session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
 
