a62bbb3a26f236f3780509aba0b28fa526cc5616
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-05-14 17:09:11 +0000

    Message ajax request can also return parts and attachments if they are in the get request

--- ajax.php
+++ ajax.php
@@ -6,26 +6,32 @@ require_once("inc/common.inc.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessagePart.obj.php");
 require_once("obj/MessageAttachment.obj.php");
+require_once("obj/AudioFile.obj.php");
 
 if (isset($_GET['ajax']) && isset($_GET['type'])) {
 	$type = $_GET['type'];
-	$return = "";
+	$return = false;
 	switch ($type) {
 		case 'message':
-			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messagetype']))
-				exit();
-			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['id'])."' and type='".dbsafe($_GET['messagetype'])."' order by name");
-			$return = $message;
-			break;
-	
-		case 'messagepart':
 			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messageid']))
-				exit();
-			$return = DBFind("MessagePart","from message where userid=" . $USER->id ." and deleted=0 and messageid='".dbsafe($_GET['messageid'])."' order by name");
+				break;
+			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['messageid'])."' order by name");
+			if ($message && isset($_GET['parts']))
+				$parts = DBFindMany("MessagePart","from messagepart where messageid='".dbsafe($_GET['messageid'])."' order by sequence");
+			
+			if ($message && isset($_GET['attachments']))
+				$attachments = DBFindMany("messageattachment","from messageattachment where not deleted and messageid='" . DBSafe($_GET['messageid']) ."'");
+			
+			if ($message) {
+				$message->readHeaders();
+				$return = array('message'=>$message, 'parts'=>count(isset($parts)?$parts:array())?$parts:false, 'attachments'=>count(isset($attachments)?$attachments:array())?$attachments:false);
+			} else {
+				return false;
+			}
 			break;
 			
 		default;
-			exit();
+			break;
 	}
 	
 	header("Content-Type: application/json");
