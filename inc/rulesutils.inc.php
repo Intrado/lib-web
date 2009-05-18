fe5c14f8807f93f18f1fdb1660c540509aa24e5b
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-05-18 13:55:03 +0000

    Refactored getRuleFromForm(), extracted into Rule::initFrom()

--- inc/rulesutils.inc.php
+++ inc/rulesutils.inc.php
@@ -20,43 +20,8 @@ function getRuleFromForm($f, $s){
 		$value4 = TrimFormData($f,$s,"newrulevalue4_" . $fieldnum);
 		$value5 = TrimFormData($f,$s,"newrulevalue5_" . $fieldnum);
 
-		if (count($value) > 0) {
-			$rule = new Rule();
-			$rule->logical = $logic;
-			$rule->op = $op;
-			
-			if ($op == "num_range") //if its a range, we need to get the other value too
-				$rule->val = (ereg_replace("[^0-9\.-]*","",$value) + 0.0) . "|" . (ereg_replace("[^0-9\.-]*","",$value2) + 0.0);
-			else if ($op == "date_range") { //if its a range, we need to get the other value too
-				$t1 = strtotime($value2 == "" ? "today" : $value2);
-				$t2 = strtotime($value3 == "" ? "today" : $value3);
-				if ($t1 > $t2) { //ensure between order
-					$tmp = $t1;
-					$t1 = $t2;
-					$t2 = $tmp;
-				}
-				$rule->val = date('m/d/Y', $t1) . "|" . date('m/d/Y', $t2);
-			} else if ($type == "reldate" && $op == "eq")
-				$rule->val = date('m/d/Y',strtotime($value2 == "" ? "today" : $value2));
-			else if ($type == "reldate" && $op == "date_offset")
-				$rule->val = (int)ereg_replace("[^0-9\.-]*","",$value4);
-			else if ($type == "reldate" && $op == "reldate_range") {
-				$value4 = (int)ereg_replace("[^0-9\.-]*","",$value4);
-				$value5 = (int)ereg_replace("[^0-9\.-]*","",$value5);
-				if ($value4 > $value5) { //ensure between order
-					$tmp = $value4;
-					$value4 = $value5;
-					$value5 = $tmp;
-				}
-				$rule->val = "$value4|$value5";
-			} else if (strpos($op,"num_") === 0)
-				$rule->val = ereg_replace("[^0-9\.-]*","",$value) + 0.0;
-			else if ($type == 'multisearch' && is_array($value))
-				$rule->val = implode("|",$value);
-			else
-				$rule->val = $value;
-			$rule->fieldnum = $fieldnum;
-		}
+		if (count($value) > 0)
+			$rule = Rule::initFrom($fieldnum, $type, $logic, $op, array($value, $value2, $value3, $value4, $value5));
 	}
 	return $rule;
 }
