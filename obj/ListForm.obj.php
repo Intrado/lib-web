f5e94585098d94e593b996c0bd5cb32e22535304
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-05-21 19:23:03 +0000

    General refactoring. * removed handleRequest() override. * started working on the overall ui * can save to session, load from session.
    Issues:
    * F,G,C fields sorted oddly as C,F,G due to FieldMap::getAllAuthorizedFieldMaps()'s sorting by fieldnum.
    * Previous and Next buttons in jobwizard do not take me to the appropriate page, just stays on the ListForm.
    
    TODO:
    * flesh out the validator code.
    * think about appropriate messages/placement for helper/errors.

--- obj/ListForm.obj.php
+++ obj/ListForm.obj.php
@@ -1,223 +1,245 @@
 <?php
 
-require_once("obj/Message.obj.php");
-require_once("obj/MessagePart.obj.php");
-require_once("obj/MessageAttachment.obj.php");
-require_once("obj/AudioFile.obj.php");
-require_once("obj/Voice.obj.php");
-require_once("obj/FieldMap.obj.php");
-require_once("obj/PeopleList.obj.php");
-require_once("obj/Rule.obj.php");
-require_once("obj/ListEntry.obj.php");
-require_once("inc/date.inc.php");
-require_once("inc/securityhelper.inc.php");
-
 /* use the 3 column fieldset layout, each form item on a line by itself*/
 class ListForm extends Form {
 	function ListForm ($name) {
-		$this->name = $name;
-		$this->serialnum = md5(serialize($name)); // TODO: What to do with this? it used to be serialize($formdata).
+		$this->formdata['listids'] = array(
+			'value' => '',
+			'validators' => array(
+				array('ValRequired'),
+				array('ValLists')
+			)
+		);
+
+		parent::Form($name, $this->formdata, null);
 	}
-	
-	function handleRequest() {
-		global $USER;
-		global $RULE_OPERATORS;
-		
-		error_log('handling request');
-		if (!isset($_REQUEST['form']) || $_REQUEST['form'] != $this->name)
-			return false; //nothing to do
-		
-		$ajaxReturn = false;
-		
-		// Wizard clicked Next.
-		if ($USER->authorize('createlist') && isset($_POST['submit'])) {
-			// TODO: submit.
-			error_log('wizard click next');
-		// Remove List.
-		} else if ($USER->authorize('createlist') && isset($_POST['removelist'])) {
-			// TODO: remove list.
-			error_log('remove list');
-		// Add Existing List.
-		} else if ($USER->authorize('createlist') && isset($_POST['addlist'])) {
-			// TODO: add list.
-			error_log('add existing list');
-		// Save Rules
-		} else if ($USER->authorize('createlist') && isset($_POST['ruledata'])) {
-			error_log('save rules');
-			
-			$ruledata = json_decode($_POST['ruledata']);
-			$summary = array();
-			$rules = array();
-			foreach ($ruledata as $data) {
-				if (!$rule = Rule::initFrom($data->fieldnum, $data->type, $data->logical, $data->op, $data->val))
-					continue; // TODO: Should this add as many as possible, or break after the first error?
-					
-				$rules[] = $rule;
-				
-				// SUMMARY
-				$fieldname = FieldMap::getName($rule->fieldnum);
-				$opname = $RULE_OPERATORS[$data->type][$rule->op];
-				if ($rule->op == 'in') {
-					$opname = 'is';
-					if ($rule->logical == 'and not')
-						$opname = 'is NOT';
-					$val = str_replace('|', ', ', $rule->val);
-				} else {
-					$val = str_replace('|', ' and ', $rule->val);
-				}
-				
-				$line = "$fieldname $opname $val";
-				if (strlen($line) > 30)
-					$line = substr($line, 0, 28) . '..';
-				$summary[] = $line;
-			}
-			
-			$summary = implode(', ', $summary);
-			
-			error_log("SUMMARY FINAL: $summary");
-			// If summary is empty, then there's no rule to add so don't create the list.
-			if (!empty($summary)) {
-				$list = new PeopleList(null);
-				$list->name = $summary;
-				$list->description = 'JobWizard List ' . date('Y M d, H:i:s', time());
-				$list->userid = $USER->id;
-				$list->deleted = 0; // TODO: Set to deleted=1.
-				$list->update();
-				
-				if ($list->id) {
-					// TODO: use a transaction, QuickUpdate('Begin..')    QuickUpdate('Commit'); read about it in mysql docs.
-					foreach ($rules as $rule) {
-						$rule->create();
-						
-						$le = new ListEntry();
-						$le->listid = $list->id;
-						$le->type = "R";
-						$le->ruleid = $rule->id;
-						$le->create();
-					}
-					
-					$ajaxReturn = true;
-					// TODO: Add this list id to session data.
-				}
-			} else {
-				error_log("EMPTY RULES, CANCLED");
-			}
-		}
-		
-		header('Content-Type: application/json');
-		echo json_encode($ajaxReturn);
-		exit;
-	}
-	
+
 	function render () {
-		$theme = getBrandTheme();
-		$lasthelpstep = false;
-		
 		$posturl = $_SERVER['REQUEST_URI'];
 		$posturl .= mb_strpos($posturl,"?") !== false ? "&" : "?";
 		$posturl .= "form=". $this->name;
 		
-		$str = '
-		<div class="newform_container">
-		<form class="newform" id="'.$this->name.'" name="'.$this->name.'" method="POST" action="'.$posturl.'" style="width: 100%; /* TODO fix main css */">
-		<input name="formsnum_' . $this->name . '" type="hidden" value="' . $this->serialnum . '">
-		<table width="100%" cellspacing="0" table-layout="fixed"><tr><td valign=top> <!-- FORM CONTENT -->';
-
+		$listidsName = $this->name . '_listids';
 		// ListForm Stuff
 		$str.= "
-<style type='text/css'>
-td {
-	vertical-align: top;
-}
-select {
-	min-width: 70px;
-}
-</style>
-<table>
-	<tr>
-		<td style='width:400px'>
-			<h2>Build Your List
-				<div>
-					<button onclick='save_rules(); return false;' type='button'>Save Rules</button>
-				</div>
-			</h2>
-			<div id='Rules'></div>
-		</td>
-		
-		<td>
-			<h2>Select a List</h2>
-			<div id='Lists'></div>
-		</td>
-	</tr>
-</table>
-		";
-		
-		$str .= '
-				<!-- END FORM CONTENT -->
-				</td>
-				<!-- No Helper -->
-			</tr>
-		</table>
-		</form>
-		
-		
-		
-		</div>
-		<div style="clear: both;"></div>
-		';
+			<style type='text/css'>
+			td {
+				vertical-align: top;
+			}
+			select {
+				min-width: 70px;
+			}
+			</style>
+			<table>
+				<tr>
+					<td style='width:400px'>
+						<div id='helperWindow'>
+							<button id='buildRulesButton' type='button'>Build a list using rules</button> or <button id='chooseListButton' type='button'>Choose an existing list</button>
+						</div>
+						
+						<div id='buildRulesWindow'>
+							<h3>Build Rules</h3>
+							<div id='rulesDiv'></div>
+							<div><button id='buildRulesCancelButton' type='button'>Cancel</button><button id='buildRulesDoneButton' type='button'>Done</button></div>
+						</div>
+						
+						<div id='chooseListWindow'>
+							<h3>Choose a List</h3>
+							<div id='listSelectDiv'></div>
+							<div><button id='chooseListCancelButton' type='button'>Cancel</button><button id='chooseListDoneButton' type='button'>Done</button></div>
+						</div>
+					</td>
+					
+					<td style='width:400px'>
+						<h3>Final Lists</h3>
+						<table><tbody id='finalListsTable'>
+						</tbody></table>
+					</td>
+				</tr>
+			</table>";
 
+		// Taken from Form.obj.php, adapted to fit needs of ListForm.
 		$str .= "
-<script type='text/javascript' src='script/calendar.js'></script>
-<script type='text/javascript' src='script/RuleWidget.js'></script>
-<script type='text/javascript'>
-function show_lists() {
-	new Ajax.Request('ajax.php?ajax&type=lists', {
-		onSuccess: function(transport) {
-			var data = transport.responseJSON;
-			if (!data) {
-				alert('you are not logged in');
-				return;
-			}
-			
-			listSelect = new Element('select');
-			$('Lists').update(listSelect);
-			listSelect.insert(new Element('option', {'value':''}).insert('-- Select a List --'));
-			//listSelect.observe('change', ajaxthing);
-	
-			for (var i in data) {
-				if (data[i]['id'] === undefined){
-					alert('sir goodbye'); 
-					break;
+			<div class='newform_container'>
+				<form class='newform' id='{$this->name}' name='{$this->name}' method='POST' action='{$posturl}' style='width: 100%; /* TODO fix main css */'>";
+		//submit buttons
+		foreach ($this->buttons as $code) {
+			$str .= $code;
+		}
+		$str .="
+					<input name='formsnum_{$this->name}' type='hidden' value='{$this->serialnum}'/>
+					<input id='$listidsName' name='$listidsName' type='hidden' value='{$this->formdata['listids']['value']}'/>
+				</form>
+			</div>
+			<div style='clear: both;'></div>";
+
+		// ListForm javascript
+		$str .= "
+			<script type='text/javascript' src='script/calendar.js'></script>
+			<script type='text/javascript' src='script/RuleWidget.js'></script>
+			<script type='text/javascript'>
+				reset_windows();
+				var ruleWidget = new RuleWidget($('rulesDiv'));
+				var listSelectbox = null;
+				var premadeLists = null;
+				new Ajax.Request('ajax.php?type=lists', {
+					onSuccess: function(transport) {
+						premadeLists = transport.responseJSON;
+						if (!premadeLists) {
+							alert('you are not logged in');
+							return;
+						}
+						
+						// Load From Session Data.
+						if($('$listidsName').value) {
+							get_liststats($('$listidsName').value);
+						}
+					}
+				});
+				
+				// Build Rules Buttons
+				$('buildRulesButton').observe('click', function(event) {
+					event.stop();
+					$('buildRulesWindow').show();
+					$('chooseListWindow').hide();
+					$('helperWindow').hide();
+				});
+				$('buildRulesCancelButton').observe('click', function(event) {
+					event.stop();
+					reset_windows();
+				});
+				$('buildRulesDoneButton').observe('click', function(event) {
+					event.stop();
+					new Ajax.Request('ajaxlistform.php?type=saverules', { 'method':'post',
+						'postBody': 'ruledata='+ruleWidget.toJSON(),
+						onSuccess: function(transport) {
+							var id = transport.responseJSON;
+							if (!id) {
+								alert('sorry could not save these rules!');
+								return;
+							}
+							add_list(id);
+							ruleWidget.clear_rules();
+						}
+					});
+				});
+				
+				// Choose List Buttons
+				$('chooseListButton').observe('click', function(event) {
+					event.stop();
+					$('buildRulesWindow').hide();
+					$('chooseListWindow').show();
+					$('helperWindow').hide();
+				});
+				$('chooseListCancelButton').observe('click', function(event) {
+					event.stop();
+					reset_windows();
+				});
+				$('chooseListDoneButton').observe('click', function(event) {
+					event.stop();
+					var id = listSelectbox.getValue();
+					add_list(id);
+				});
+				
+				function reset_windows() {
+					$('buildRulesWindow').hide();
+					$('chooseListWindow').hide();
+					$('helperWindow').show();
 				}
-				listSelect.insert(new Element('option', {'value':data[i]['id']}).update(data[i]['name']));
-			}
-		}
-	});
-}
-
-function save_rules() {
-	var json = ruleWidget.toJSON();
-	
-	new Ajax.Request('jobwizard.php?ajax&form=$this->name', {'method':'post',
-		'postBody': 'ruledata='+json,
-		onSuccess: function(transport) {
-			var data = transport.responseText;
-			ruleWidget.clear_rules();
-		}
-	});
-}
-</script>
-
-<script type='text/javascript'>
-	var listSelect;
-	//show_lists();
-	
-	var ruleWidget = new RuleWidget($('Rules'));
-</script>
-		";
+				
+				function refresh_listSelectbox() {
+					listSelectbox = new Element('select');
+					listSelectbox.insert(new Element('option', {'value':''}).insert('-- Select a List --'));
+					$('listSelectDiv').update(listSelectbox);
+					if (!premadeLists)
+						return;
+					for (var id in premadeLists) {
+						if (!premadeLists[id]['added'])
+							listSelectbox.insert(new Element('option', {'value':id}).update(premadeLists[id]['name']));
+					}
+				}
+				
+				function add_list(id) {
+					var listids = $('$listidsName').value;
+					if (listids) listids = listids.evalJSON();
+					if (!listids.length) listids = [];
+					listids.push(id);
+					$('$listidsName').value = listids.toJSON();
+					alert($('$listidsName').value);
+					reset_windows();
+					get_liststats([id].toJSON());
+				}
+				
+				// @param json, json-encoded ARRAY of listids.
+				function get_liststats(json) {
+					var listids = json.evalJSON();
+					if (!listids.length)
+						return;
+						
+					new Ajax.Request('ajax.php?type=liststats&listids='+json, {
+						onSuccess: function(transport) {
+							var stats = transport.responseJSON;
+							if (!stats) {
+								alert('No data available for this list');
+								return;
+							}
+							
+							for (var i = 0; i < stats.length; i++) {
+								var data = stats[i];
+								
+								// Keep a hidden input field to keep track of id for this table row.
+								var nameTD = new Element('td').update(new Element('input',{'type':'hidden','value':data['id']})).insert(data['name']);
+								var totalTD = new Element('td').update(data['total']);
+								var actionsTD = new Element('td');
+								
+								var removeButton = new Element('button', {'type':'button'}).update('Remove');
+								removeButton.observe('click', function(event) {
+									var tr = Event.element(event).up('tr');
+									var id = tr.down('input[type=\"hidden\"]').getValue();
+									tr.remove();
+									if (premadeLists && premadeLists[id]) {
+										premadeLists[id]['added'] = false;
+										refresh_listSelectbox();
+									}
+									var listids = $('$listidsName').value;
+									if (listids) listids = listids.evalJSON();
+									if (listids.length) {
+										listids = listids.without(id);
+										$('$listidsName').value = listids.toJSON();
+									}
+								});
+								actionsTD.update(removeButton);
+								
+								$('finalListsTable').insert(new Element('tr').insert(nameTD).insert(totalTD).insert(actionsTD));
+							}
+						}
+					});
+					
+					// Mark any premade lists that were inserted.
+					if (premadeLists) {
+						for (var i = 0; i < listids.length; i++) {
+							var id = listids[i];
+							if (!premadeLists[id])
+								continue;
+							premadeLists[id]['added'] = true;
+						}
+					}
+					
+					refresh_listSelectbox();
+				}
+			</script>";
 		return $str;
 	}
 }
 
-
+class ValLists extends Validator {
+	var $onlyserverside = true;
+	
+	function validate ($value, $args) {
+		global $USER;
+		error_log('ValLists =========== $value=' . print_r($value,true));
+		return true;
+		return 'Testing testing testing testing testing, validate if any list has count == 0';
+	}
+}
 ?>
