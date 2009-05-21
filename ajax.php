ca893cbeaa86a7c76981234dd526f7d47f68b277
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-05-21 12:08:42 +0000

    * General refactoring. ** added handleRequest() to streamline switch/cases ** refactored 'lists', 'persondatavalues', 'fieldmapsdata', 'lists' to check for empty $_GET parameters and switch to using parameter binding for database queries. ** cases divided into 3 sections: Simple Objects, Complex Objects, and RPC. Case blocks  in each section are ordered alphabetically.

--- ajax.php
+++ ajax.php
@@ -16,123 +16,96 @@ require_once("obj/RenderedList.obj.php");
 require_once("inc/date.inc.php");
 require_once("inc/securityhelper.inc.php");
 
-if (isset($_GET['ajax']) && isset($_GET['type'])) {
-	$type = $_GET['type'];
-	$return = false;
-	switch ($type) {
-		case 'message':
-			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messageid']))
-				break;
-			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['messageid'])."' order by name");
-			$message->readHeaders();
-			if ($message)
-				$return = $message;
-			break;
-		
-		case 'wholemessage':
-			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messageid']))
-				break;
-			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['messageid'])."' order by name");
-			if ($message) {
-				$message->readHeaders();
-				$parts = DBFindMany("MessagePart","from messagepart where messageid='".dbsafe($_GET['messageid'])."' order by sequence");
-				$attachments = DBFindMany("messageattachment","from messageattachment where not deleted and messageid='" . DBSafe($_GET['messageid']) ."'");
-				if ($parts)
-					$body = $message->format($parts);
-				else
-					$body = "";
-				
-				$return = array(
-					"lastused"=>$message->lastused,
-					"description"=>$message->description,
-					"fromname"=>$message->fromname,
-					"fromemail"=>$message->fromemail,
-					"subject"=>$message->subject,
-					"attachment"=>count($attachments)?$attachments:false,
-					"body"=>$body,);
-			}
-			break;
-			
-		case 'hasmessage':
-			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || (!isset($_GET['messagetype']) || !isset($_GET['messageid'])))
-				break;
-			if (isset($_GET['messagetype']))
-				$return = QuickQuery("select count(id) from message where userid=" . $USER->id ." and not deleted and type='".dbsafe($_GET['messagetype'])."'");
-			if (isset($_GET['messageid']))
-				$return = QuickQuery("select count(id) from message where userid=" . $USER->id ." and not deleted and messageid='".dbsafe($_GET['messageid'])."'");				
-			break;
-			
+function handleRequest($type) {
+	global $USER;
+	global $RULE_OPERATORS;
+	global $RELDATE_OPTIONS;
+	
+	switch($type) {
+		//--------------------------- SIMPLE OBJECTS -------------------------------
 		case 'fieldmap':
 			if (!isset($_GET['fieldnum']))
-				break;
-			$return = FieldMap::getAuthorizedMapNames();
-			break;
+				return false;
+			return FieldMap::getAuthorizedMapNames();
 		
-		// NOTE: Should this be broken up into cases for Operators, ReldateOptions, and Fieldmaps? For now, it's clumped together for convenience 
-		// USED IN: RuleWidget.js
-		case 'fieldmapsdata':
-			if (!$USER->authorize('createlist'))
-				break;
-
-			$fFields = FieldMap::getAuthorizedFieldMapsLike("f%");
-			$gFields = FieldMap::getAuthorizedFieldMapsLike("g%");
-			$cFields = FieldMap::getAuthorizedFieldMapsLike("c%");
-
-			$return = array("operators" => $RULE_OPERATORS, "reldateOptions" => $RELDATE_OPTIONS, "fieldmaps" => $fFields + $gFields + $cFields);
-			
-			break;
-			
-		// USED IN: RuleWidget.js
-		case 'persondatavalues':
-			if (!$USER->authorize('createlist') || !isset($_GET['fieldnum']))
-				break;
-			
-			// Adapted from ruleeditform.inc.php
-			$fieldnum = dbsafe($_GET['fieldnum']);
-			
-			$limit = DBFind('Rule', "from rule inner join userrule on rule.id = userrule.ruleid where userid = $USER->id and fieldnum = '$fieldnum'");
-			$limitsql = $limit ? $limit->toSQL(false, "value", false, true) : "";
-			$return = QuickQueryList("select value from persondatavalues where fieldnum='$fieldnum' $limitsql order by value");
-			if (empty($return)) {
-				$return = false;
-				break;
-			}
-
-			// Clean values, needs to be utf8 or will show up blank when json-encoded.
-			foreach ($return as &$value) {
-				// NOTE: Be careful of htmlentities() when displaying in web browser.
-				// TODO: Decide whether to add htmlentities() here or in javascript.
-				$value = utf8_encode($value);
-			}
-			break;
-			
-		// USED IN: ListForm.php
 		case 'lists':
 			if (!$USER->authorize('createlist'))
-				break;
+				return false;
+			return DBFindMany('PeopleList', ', (name +0) as lettersfirst from list where userid=? and deleted=0 order by lettersfirst,name', false, array($USER->id));
 			
-			$deleted = isset($_GET['deleted']) ? '1' : '0';
-			$return = DBFindMany("PeopleList", "from list where userid='" . $USER->id . "' and deleted=$deleted order by name");
-			break;
+		case 'message':
+			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messageid']))
+				return false;
+			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['messageid'])."' order by name");
+			$message->readHeaders();
+			return $message;
 			
-		// USED IN: ListForm.php
-		case 'liststats':
-			if (!$USER->authorize('createlist') || !isset($_GET['listid']))
-				break;
-				
+		case 'persondatavalues':
+			if (!$USER->authorize('createlist') || empty(trim($_GET['fieldnum'])))
+				return false;
+			$limit = DBFind('Rule', 'from rule inner join userrule on rule.id = userrule.ruleid where userid=? and fieldnum=?', false, array($USER->id, $_GET['fieldnum']));
+			$limitsql = $limit ? $limit->toSQL(false, 'value', false, true) : '';
+			return QuickQueryList("select value from persondatavalues where fieldnum=? $limitsql order by value", false, false, array($_GET['fieldnum']));
+			
+		//--------------------------- COMPLEX OBJECTS -------------------------------
+		case 'fieldmapsdata': // USED IN: RuleWidget.js
+			if (!$USER->authorize('createlist'))
+				return false;
+			return array(
+				'operators' => $RULE_OPERATORS,
+				'reldateOptions' => $RELDATE_OPTIONS,
+				'fieldmaps' => FieldMap::getAuthorizedFieldMapsLike('%'));
+
+		case 'liststats': // USED IN: ListForm.php
+			if (!$USER->authorize('createlist') || empty(trim($_GET['listid'])))
+				return false;
 			$list = new PeopleList($_GET['listid']);
 			$renderedlist = new RenderedList($list);
 			$renderedlist->calcStats();
+			return array(
+				'id'=>$list->id,
+				'name'=>$list->name,
+				'total'=>$renderedlist->total);
+				
+		case 'wholemessage':
+			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || !isset($_GET['messageid']))
+				return false;
+			$message = DBFind("Message","from message where userid=" . $USER->id ." and deleted=0 and id='".dbsafe($_GET['messageid'])."' order by name");
+			if (!$message)
+				return false;
+
+			$message->readHeaders();
+			$parts = DBFindMany("MessagePart","from messagepart where messageid='".dbsafe($_GET['messageid'])."' order by sequence");
+			$attachments = DBFindMany("messageattachment","from messageattachment where not deleted and messageid='" . DBSafe($_GET['messageid']) ."'");
+			if ($parts)
+				$body = $message->format($parts);
+			else
+				$body = "";
 			
-			$return = array('id'=>$list->id, 'name'=>$list->name, 'total'=>$renderedlist->total);
-			break;
+			return array(
+				"lastused"=>$message->lastused,
+				"description"=>$message->description,
+				"fromname"=>$message->fromname,
+				"fromemail"=>$message->fromemail,
+				"subject"=>$message->subject,
+				"attachment"=>count($attachments)?$attachments:false,
+				"body"=>$body);
+
+		//--------------------------- RPC -------------------------------
+		case 'hasmessage':
+			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || (!isset($_GET['messagetype']) || !isset($_GET['messageid'])))
+				return false;
+			if (isset($_GET['messagetype']))
+				return QuickQuery("select count(id) from message where userid=" . $USER->id ." and not deleted and type='".dbsafe($_GET['messagetype'])."'");
+			if (isset($_GET['messageid']))
+				return QuickQuery("select count(id) from message where userid=" . $USER->id ." and not deleted and messageid='".dbsafe($_GET['messageid'])."'");
 			
-		default;
-			break;
+		default:
+			return false;
 	}
-	
-	header("Content-Type: application/json");
-	echo json_encode($return);
-	exit();	
 }
+
+header('Content-Type: application/json');
+$data = handleRequest($_GET['type']);
+echo json_encode(!empty($data) ? $data : false);
 ?>
