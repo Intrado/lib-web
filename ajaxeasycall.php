fe3560ed040a977a1f95fc3b5b2aad3c61c86110
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-05-21 14:11:17 +0000

    Use language names instead of IDs

--- ajaxeasycall.php
+++ ajaxeasycall.php
@@ -17,29 +17,19 @@ function getTaskLangs($reqlangs) {
 	if ($task->id == "new") {
 		$langdata = array();
 	} else {
-		$data = sane_parsestr($task->data);
-		$langmap = json_decode($data['langmap'], true);
 		$langdata = array();
-		foreach($data as $key => $value) {
-			if (substr($key,0,7) == "message") {
-				$msgindex = substr($key,7);
-				if (isset($reqlangs[$langmap["language$msgindex"]])) {
-					if (isset($langdata[$langmap["language$msgindex"]]))
-						$langdata[$langmap["language$msgindex"]]["message"] = $value;
-					else
-						$langdata[$langmap["language$msgindex"]] = array("message" => $value, "language"=>$reqlangs[$langmap["language$msgindex"]]);
-				}
-				$task->delData($key);
-			}
+		for($x=0; $x<$task->getData('totalamount'); $x++) {
+			if ($task->getData("message$x") && in_array($task->getData("language$x"), $reqlangs))
+				$langdata[$task->getData("language$x")] = $task->getData("message$x");
 			
-			if (substr($key,0,8) == "language") 
-				$task->delData($key);
+			$task->delData("language$x");
+			$task->delData("message$x");
 		}
 	}
 	
-	foreach ($reqlangs as $langid => $language) {
-		if (!isset($langdata[$langid]))
-			$langdata[$langid] = array("language"=>$language, "message"=>"");
+	foreach ($reqlangs as $language) {
+		if (!isset($langdata[$language]))
+			$langdata[$language] = "";
 	}
 	return $langdata;
 }
@@ -61,7 +51,6 @@ if ($USER->authorize("starteasy") && isset($_GET['id'])) {
 		$task->setData('userid', $USER->id);
 		$task->setData('listid', 0);
 		$task->setData('jobtypeid', 0);
-		$currlang = "Default";
 	}
 	if (isset($_GET['language'])) {
 		$reqlangs = json_decode($_GET['language'],true);
@@ -70,24 +59,23 @@ if ($USER->authorize("starteasy") && isset($_GET['id'])) {
 		$totalamount = count($tasklangs);
 		$count = 0;
 		$langindex = 0;
-		if (!isset($currlang))
-			$currlang = "";
 		
-		foreach ($tasklangs as $langid => $values) {
-			$langs["language$langindex"] = $langid;
-			$task->delData("language$langindex");
-			$task->delData("message$langindex");
-			$task->setData("language$langindex", $values['language']);
-			if ($values['message']) {
-				$task->setData("message$langindex", $values['message']);
+		foreach ($tasklangs as $language => $message) {
+			$task->setData("language$langindex", $language);
+			if ($message) {
+				$task->setData("message$langindex", $message);
 				$count = $langindex + 1;
-				$currlang = $values['language'];
 			}
 			$langindex++;
 		}
+		
+		if ($task->getData("language$count")) {
+			$currlang = $task->getData("language$count");
+		} else {
+			$currlang = "";
+		}
 		$task->setData('count', $count);
 		$task->setData('totalamount', $totalamount);
-		$task->setData('langmap',json_encode($langs));
 		$task->setData('currlang', $currlang);
 		if ($task->id !== "new")
 			$task->update();
@@ -98,10 +86,14 @@ if ($USER->authorize("starteasy") && isset($_GET['id'])) {
 	}
 		
 	if (isset($_GET['phonenumber'])) {
+		$task->delData('error');		
 		$task->setData('phonenumber', dbsafe($_GET['phonenumber']));
+		$task->update();
 	}
 	
-	if (($task->status == "done" || $task->status == "new") && isset($_GET['start'])) {
+	if (($task->status == "done" || $task->status == "new") 
+		&& isset($_GET['start']) 
+		&& (($task->getData('count') + 0) < ($task->getData('totalamount') + 0))) {
 		$task->delData('error');
 		$task->setData('progress', _L("Creating Call"));
 		$task->status = "queued";
@@ -111,29 +103,21 @@ if ($USER->authorize("starteasy") && isset($_GET['id'])) {
 
 	// Parse the task data
 	$langdata = array();
-	$langnametoid = array();
-	$data = sane_parsestr($task->data);
-	$langmap = json_decode($data['langmap'], true);
-	foreach($data as $key => $value) {
-		if (substr($key,0,7) == "message") {
-			$msgindex = substr($key,7);
-			$langdata[$langmap["language$msgindex"]] = $value;
-		}
-		if (substr($key,0,8) == "language") {
-			$msgindex = substr($key,8);
-			$langnametoid[$value] = $langmap["language$msgindex"];
-		}
-	}
+	for($x=0; $x<$task->getData('totalamount'); $x++)
+		if ($task->getData("message$x"))
+			$langdata[$task->getData("language$x")] = $task->getData("message$x");
 	
 	// Return the task info
 	$return = array(
 		"id"=>$task->id,
-		"language"=>$langdata,
-		"progress"=>$data['progress'],
-		"currlang"=>isset($langnametoid[$data['currlang']])?$langnametoid[$data['currlang']]:"",
-		"error"=>isset($data['error'])?$data['error']:"",
-		"count"=>$data['count'],
-		"totalamount"=>$data['totalamount']);
+		"status"=>$task->status,
+		"language"=>$langdata?$langdata:false,
+		"progress"=>$task->getData('progress'),
+		"currlang"=>$task->getData('currlang'),
+		"error"=>$task->getData('error'),
+		"count"=>$task->getData('count'),
+		"totalamount"=>$task->getData('totalamount')
+	);
 }
 
 header("Content-Type: application/json");
