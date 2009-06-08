bfb88255ae4f9f369a43db635a3e2591f3c06407
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-06-08 14:22:18 +0000

    Update brandtheme form item with javascript items integrated.

--- obj/FormItem.obj.php
+++ obj/FormItem.obj.php
@@ -177,23 +177,114 @@ class BrandTheme extends FormItem {
 		$n = $this->form->name."_".$this->name;
 		if (!$value)
 			$value = "{}";
+		$phpvalue = json_decode($value);
 		// Hidden input item to store values in
-		$str = '<input id="'.$n.'" name="'.$n.'" type="hidden" value="'.escapehtml(json_encode($value)).'" />';
+		$str = '<input id="'.$n.'" name="'.$n.'" type="hidden" value="'.escapehtml($value).'" />';
 		$str .= '<table class="msgdetails"><tr><td class="msglabel">'._L("Theme").':</td>';
-		$str .= '<td><select id="'.$n.'theme" name="'.$n.'theme" onchange="'.$n.'brandtheme.loadTheme();" >';
+		$str .= '<td><select id="'.$n.'theme" name="'.$n.'theme" onchange="loadTheme();" >';
 		foreach ($this->args['values'] as $selectvalue => $selectname)
-			$str .= '<option value="'.escapehtml($selectvalue).'" '.(($value['theme'] == $selectvalue)? "selected" : "").' )>'.escapehtml($selectname['displayname']).'</option>';
+			$str .= '<option value="'.escapehtml($selectvalue).'" '.(($phpvalue->theme == $selectvalue)? "selected" : "").' )>'.escapehtml($selectname['displayname']).'</option>';
 		$str .= '</select></td></tr>';
 		$str .= '<tr><td class="msglabel">'._L("Primary Color").':</td>';
-		$str .= '<td><input id="'.$n.'color" name="'.$n.'color" type="text" value="'.$value['color'].'" onchange="'.$n.'brandtheme.store();" />';
+		$str .= '<td><input id="'.$n.'color" name="'.$n.'color" type="text" value="'.$phpvalue->color.'" onchange="storeTheme();" />';
 		$str .= '<img src="img/sel.gif" type="image" onclick="TCP.popup($(\''.$n.'color\')); "/></td></tr>';
 		$str .= '<tr><td class="msglabel">'._L("Shader Ratio").':</td>';
-		$str .= '<td><input id="'.$n.'ratio" name="'.$n.'ratio" type="text" value="'.$value['ratio'].'" onchange="'.$n.'brandtheme.store();" /></td></tr>';
+		$str .= '<td><input id="'.$n.'ratio" name="'.$n.'ratio" type="text" value="'.$phpvalue->ratio.'" onchange="storeTheme();" /></td></tr>';
 		$str .= '</table>';
-		$str .= '<script type="text/javascript" src="script/brandtheme.js"></script>
-			<script type="text/javascript" src="script/picker.js"></script>
+		$str .= '<script type="text/javascript" src="script/picker.js"></script>
 			<script type="text/javascript">
-				var '.$n.'brandtheme = new BrandTheme("'.$n.'",'.json_encode($this->args['values']).');
+			
+			themeformname = "'.$this->form->name.'";
+			themeformitem = "'.$n.'";
+			themecolorschemes = '.json_encode($this->args['values']).';
+			
+			// When a new theme is selected. Update the color and ratio fields with default values
+			function loadTheme() {
+				theme = $(themeformitem+"theme").value;
+				$(themeformitem+"color").value = themecolorschemes[theme]._brandprimary;
+				$(themeformitem+"ratio").value = themecolorschemes[theme]._brandratio;
+				storeTheme();
+			}
+			
+			// Save changes in the hidden field
+			function storeTheme() {
+				$(themeformitem).value = Object.toJSON({
+					"theme": $(themeformitem+"theme").value,
+					"color": $(themeformitem+"color").value,
+					"ratio": $(themeformitem+"ratio").value
+				});
+				form_do_validation($(themeformname), $(themeformitem));
+			}
+			
+			// Title: Tigra Color Picker
+			// URL: http://www.softcomplex.com/products/tigra_color_picker/
+			// Version: 1.1
+			// Date: 06/26/2003 (mm/dd/yyyy)
+			// Note: Permission given to use this script in ANY kind of applications if
+			//    header lines are left unchanged.
+			// Note: Script consists of two files: picker.js and picker.html
+
+			// Modified by Joshua Lai jlai@schoolmessenger.com
+			// Modified by Nickolas Heckman nheckman@schoolmessenger.com
+
+			var TCP = new TColorPicker();
+
+			function TCPopup(field) {
+				this.field = field;
+				this.initPalette = 0;
+				var w = 600, h = 250,
+				move = screen ? 
+					",left=" + ((screen.width - w) >> 1) + ",top=" + ((screen.height - h) >> 1) : "", 
+				o_colWindow = window.open("picker.php", null, "help=no,status=no,scrollbars=no,resizable=no" + move + ",width=" + w + ",height=" + h + ",dependent=yes", true);
+				o_colWindow.opener = window;
+				o_colWindow.focus();
+			}
+
+			function TCBuildCell (R, G, B, w, h) {
+				return "<td style=\"border:0px solid red; \" bgcolor=\"#" + this.dec2hex((R << 16) + (G << 8) + B) + "\"><a style=\"border: 0px solid green;\" href=\"javascript:P.S(\"" + this.dec2hex((R << 16) + (G << 8) + B) + "\") onmouseover=\"P.P(\"" + this.dec2hex((R << 16) + (G << 8) + B) + "\")\"><img style=\"border: 0px solid black;\" src=\"img/pixel.gif\" width=\"" + w + "\" height=\"" + h + "\" border=\"0\"></a></td>";
+			}
+
+			function TCSelect(c) {
+				// Removed # from return value.  -JJL
+				this.field.value = c.toUpperCase();
+				storeTheme();
+				this.win.close();
+			}
+
+			function TCPaint(c, b_noPref) {
+				c = (b_noPref ? "" : "#") + c.toUpperCase();
+				if (this.o_samp) 
+					this.o_samp.innerHTML = "<font face=Tahoma size=2>" + c +" <font color=white>" + c + "</font></font>";
+				if(this.doc.layers)
+					this.sample.bgColor = c;
+				else { 
+					if (this.sample.backgroundColor != null) this.sample.backgroundColor = c;
+					else if (this.sample.background != null) this.sample.background = c;
+				}
+			}
+
+			function TCDec2Hex(v) {
+				v = v.toString(16);
+				for(; v.length < 6; v = "0" + v);
+				return v;
+			}
+
+			function TColorPicker(field) {
+				this.show = document.layers ? 
+					function (div) { this.divs[div].visibility = "show" } :
+					function (div) { this.divs[div].visibility = "visible" };
+				this.hide = document.layers ? 
+					function (div) { this.divs[div].visibility = "hide" } :
+					function (div) { this.divs[div].visibility = "hidden" };
+				// event handlers
+				this.S       = TCSelect;
+				this.P       = TCPaint;
+				this.popup   = TCPopup;
+				this.dec2hex = TCDec2Hex;
+				this.bldCell = TCBuildCell;
+				this.divs = [];
+			}
+			
 			</script>';
 		return $str;
 	}
