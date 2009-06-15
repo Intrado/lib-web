0d73165d70ae1ea56f999b5582d59c0e43fc3670
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-06-15 15:12:02 +0000

    Update to use correct query syntax. Create a cleanObj function to return object's with only their fieldlist data

--- ajax.php
+++ ajax.php
@@ -16,6 +16,19 @@ require_once("obj/RenderedList.obj.php");
 require_once("inc/date.inc.php");
 require_once("inc/securityhelper.inc.php");
 
+function cleanObj ($obj) {
+	if (!get_class($obj))
+		return false;
+	$simpleObj = array();
+	foreach ($obj->_fieldlist as $field) {
+		if (get_class($obj->$field))
+			$simpleObj[$field] = cleanObject($obj->$field);
+		else
+			$simpleObj[$field] = $obj->$field;
+	}
+	return $simpleObj;
+}
+
 function handleRequest() {
 	if (!isset($_GET['type']) && !isset($_POST['type']))
 		return false;
@@ -26,40 +39,67 @@ function handleRequest() {
 	error_log("AjaxRequest: ".$type);
 	
 	switch($type) {
-		//--------------------------- SIMPLE OBJECTS, should mirror objects in obj/*.obj.php -------------------------------
+		//--------------------------- SIMPLE OBJECTS, should mirror objects in obj/*.obj.php (simplified to _fieldlist) -------------------------------
 		case 'lists':
-			if (!$USER->authorize('createlist'))
-				return false;
-			return DBFindMany('PeopleList', ', (name+0) as lettersfirst from list where userid=? and deleted=0 order by lettersfirst,name', false, array($USER->id));
-		
+			$lists =  DBFindMany('PeopleList', ', (name+0) as lettersfirst from list where userid=? and not deleted order by lettersfirst,name', false, array(DBSafe($USER->id)));
+			$simpleLists = array();
+			foreach ($lists as $list)
+				$simpleLists[$list->id] = cleanObj($list);
+			return $simpleLists;
+			
 		// Return a message object specified by it's ID
 		case 'Message':
-			$message = new Message($_GET['id']);
+			$message = new Message(DBSafe($_GET['id']));
+			if ($message->userid !== $USER->id)
+				return false;
 			$message->readheaders();
-			return $message;
+			return cleanObj($message);
 		
 		// Return a specific message part by it's ID
 		case "MessagePart":
-			return new MessagePart($_GET['id']);
+			$mp =  new MessagePart(DBSafe($_GET['id']));
+			// if the message part doesn't belong to any message, return false
+			if (!$mp->messageid) 
+				return false;
+			// get the message this message part belongs to and check it's user ownership
+			$message = new Message($mp->messageid);
+			if ($message->userid !== $USER->id)
+				return false;
+			return cleanObj($mp);
 		
 		//--------------------------- COMPLEX OBJECTS -------------------------------
 		// Return message parts belonging to a specific messageid
 		case "MessageParts":
-			return DBFindMany("MessagePart","from messagepart where messageid='".dbsafe($_GET['id'])."' order by sequence");
+			$message = new Message(DBSafe($_GET['id']));
+			if ($message->userid !== $USER->id)
+				return false;
+			$mps = DBFindMany("MessagePart","from messagepart where messageid=? order by sequence", false, array(DBSafe($_GET['id'])));
+			$simpleMPs = array();
+			foreach ($mps as $mp)
+				$simpleMPs[$mp->id] = cleanObj($mp);
+			if (!$simpleMPs)
+				return false;
+			return $simpleMPs;
 		
-		// Return messages for the current user or user(userid) if the current user has manager rights
+		// Return messages for the current user, if userid is specified return that user's messages
 		case "Messages":
 			if(!isset($_GET['messagetype'])){
 				return false;
 			}
 			$userid = $USER->id;
-			if(isset($_GET['userid'])){
-				if($USER->authorize(array('managesystem','manageaccount')))
-					$userid = $_GET['userid'];
+			if(isset($_GET['userid']) && $USER->id !== $_GET['userid']){
+				if($USER->authorize(array('managesystem')))
+					$userid = DBSafe($_GET['userid']);
 				else
 					return false;
 			} 
-			return QuickQueryList("select id,name from message where deleted=0 and userid=? and type=? order by id", true,false,array($userid, $_GET['messagetype']));		
+			$messages = DBFindMany("Message", "from message where not deleted and userid=? and type=? order by id", false, array($userid, DBSafe($_GET['messagetype'])));
+			$simpleMessages = array();
+			foreach ($messages as $message)
+				$simpleMessages[$message->id] = cleanObj($message);
+			if (!$simpleMessages)
+				return false;
+			return $simpleMessages;
 		//--------------------------- RPC -------------------------------
 		case 'authorizedmapnames':
 			if (!isset($_GET['fieldnum']))
@@ -67,57 +107,41 @@ function handleRequest() {
 			return FieldMap::getAuthorizedMapNames();
 			
 		case 'hasmessage':
-			if (!$USER->authorize(array('sendmessage', 'sendemail', 'sendphone', 'sendsms')) || (!isset($_GET['messagetype']) && !isset($_GET['messageid'])))
+			if (!isset($_GET['messagetype']) && !isset($_GET['messageid']))
 				return false;
 			if (isset($_GET['messagetype']))
-				return QuickQuery('select count(id) from message where userid=? and not deleted and type=?', false, array($USER->id, $_GET['messagetype']));
-			else if (isset($_GET['messageid']))
-				return QuickQuery('select count(id) from message where userid=? and not deleted and id=?', false, array($USER->id, $_GET['messageid']));
-			return false;
-		
+				return QuickQuery("select count(id) from message where userid=? and not deleted and type=?", false, array($USER->id, DBSafe($_GET['messagetype'])))?true:false;
+			if (isset($_GET['messageid']))
+				return QuickQuery("select count(id) from message where userid=? and not deleted and id=?", false, array($USER->id, DBSafe($_GET['messageid'])))?true:false;
+			
 		case 'listrules':
-			// $_GET['listids'] should be json-encoded array.
+			// Assumes $_GET['listids'] is json-encoded array.
 			if (!$USER->authorize('createlist') || !isset($_GET['listids']))
 				return false;
-			$listids = json_decode($_GET['listids']);
-			if (!is_array($listids))
-				return false;
 			$listrules = array();
+			$listids = json_decode($_GET['listids']);
 			foreach ($listids as $id) {
-				// Check for bad ID and ownership
-				if (($id + 0 !== $id) || !userOwns('list', $id))
-					continue;
 				$list = new PeopleList($id);
-				if ($list) {
-					// TODO: Check rules against FieldMap::getAuthorizedNames(), set $listrules[$id][$fieldnum] = 'unauthorized', examine $list->getListRules() for details.
-					$listrules[$id] = $list->getListRules();
-				}
+				$listrules[$id] = $list->getListRules();
 			}
 			return $listrules;
 			
 		case 'liststats':
-			// $_GET['listids'] should be json-encoded array.
+			// Assumes $_GET['listids'] is json-encoded array.
 			if (!$USER->authorize('createlist') || !isset($_GET['listids']))
 				return false;
-			$listids = json_decode($_GET['listids']);
-			if (!is_array($listids))
-				return false;
 			$stats = array();
+			$listids = json_decode($_GET['listids']);
 			foreach ($listids as $id) {
-				// Check for bad ID and ownership
-				if (($id + 0 !== $id) || !userOwns('list', $id))
-					continue;
 				$list = new PeopleList($id);
-				if ($list) {
-					$renderedlist = new RenderedList($list);
-					$renderedlist->calcStats();
-					$stats[]= array(
-						'id' => $list->id,
-						'name' => $list->name,
-						'removed' => $renderedlist->totalremoved,
-						'added' => $renderedlist->totaladded,
-						'total' => $renderedlist->total);
-				}
+				$renderedlist = new RenderedList($list);
+				$renderedlist->calcStats();
+				$stats[]= array(
+					'id' => $list->id,
+					'name' => $list->name,
+					'removed' => $renderedlist->totalremoved,
+					'added' => $renderedlist->totaladded,
+					'total' => $renderedlist->total);
 			}
 			return $stats;
 			
@@ -140,9 +164,11 @@ function handleRequest() {
 		// Return a whole message including it's message parts formatted into body text and any attachments.
 		case 'previewmessage':
 			$message = new Message($_GET['id']);
+			if ($message->userid !== $USER->id)
+				return false;
 			$message->readHeaders();
-			$parts = DBFindMany("MessagePart","from messagepart where messageid='".dbsafe($_GET['id'])."' order by sequence");
-			$attachments = DBFindMany("messageattachment","from messageattachment where not deleted and messageid='" . DBSafe($_GET['id']) ."'");
+			$parts = DBFindMany("MessagePart","from messagepart where messageid=? order by sequence", false, array(dbsafe($_GET['id'])));
+			$attachments = DBFindMany("MessageAttachment","from messageattachment where not deleted and messageid=?", false, array(DBSafe($_GET['id'])));
 			$simple = false;
 			if (count($parts) == 1) 
 				foreach ($parts as $id => $part)
@@ -162,7 +188,7 @@ function handleRequest() {
 		
 		case "messagefields":
 			$fields = array();
-			$messagefields = DBFindMany("FieldMap", "from fieldmap where fieldnum in (select distinct fieldnum from messagepart where messageid='".dbsafe($_GET['id'])."')");
+			$messagefields = DBFindMany("FieldMap", "from fieldmap where fieldnum in (select distinct fieldnum from messagepart where messageid=?)", false, array(dbsafe($_GET['id'])));
 			if (count($messagefields) > 0) {
 				foreach ($messagefields as $fieldmap) {
 					$fields[$fieldmap->fieldnum] = $fieldmap;
@@ -170,20 +196,6 @@ function handleRequest() {
 				}
 			}
 			return count($fields)?$fields:false;
-		
-		// return an array of the requested fields and their person data values
-		case "fieldvalues":
-			$requestfields = json_decode($_POST['fields']);
-			$fielddata = array();
-			foreach ($requestfields as $field) {
-				$limit = DBFind('Rule', 'from rule inner join userrule on rule.id = userrule.ruleid where userid=? and fieldnum=?', false, array($USER->id, $field));
-				$limitsql = $limit ? $limit->toSQL(false, 'value', false, true) : '';
-				$fielddata[$field] = QuickQueryList("select value from persondatavalues where fieldnum=? $limitsql order by value", false, false, array($field));
-			}
-			return $fielddata;
-			
-		default:
-			return false;
 	}
 }
 
