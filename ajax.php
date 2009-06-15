0f50d043f8c36ba32f2153971d333dbadcc8043a
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-06-15 15:31:15 +0000

    Commit error handling to listrules and liststats

--- ajax.php
+++ ajax.php
@@ -115,33 +115,48 @@ function handleRequest() {
 				return QuickQuery("select count(id) from message where userid=? and not deleted and id=?", false, array($USER->id, DBSafe($_GET['messageid'])))?true:false;
 			
 		case 'listrules':
-			// Assumes $_GET['listids'] is json-encoded array.
+			// $_GET['listids'] should be json-encoded array.
 			if (!$USER->authorize('createlist') || !isset($_GET['listids']))
 				return false;
-			$listrules = array();
 			$listids = json_decode($_GET['listids']);
+			if (!is_array($listids))
+				return false;
+			$listrules = array();
 			foreach ($listids as $id) {
+				// Check for bad ID and ownership
+				if (($id + 0 !== $id) || !userOwns('list', $id))
+					continue;
 				$list = new PeopleList($id);
-				$listrules[$id] = $list->getListRules();
+				if ($list) {
+					// TODO: Check rules against FieldMap::getAuthorizedNames(), set $listrules[$id][$fieldnum] = 'unauthorized', examine $list->getListRules() for details.
+					$listrules[$id] = $list->getListRules();
+				}
 			}
 			return $listrules;
 			
 		case 'liststats':
-			// Assumes $_GET['listids'] is json-encoded array.
+			// $_GET['listids'] should be json-encoded array.
 			if (!$USER->authorize('createlist') || !isset($_GET['listids']))
 				return false;
-			$stats = array();
 			$listids = json_decode($_GET['listids']);
+			if (!is_array($listids))
+				return false;
+			$stats = array();
 			foreach ($listids as $id) {
+				// Check for bad ID and ownership
+				if (($id + 0 !== $id) || !userOwns('list', $id))
+					continue;
 				$list = new PeopleList($id);
-				$renderedlist = new RenderedList($list);
-				$renderedlist->calcStats();
-				$stats[]= array(
-					'id' => $list->id,
-					'name' => $list->name,
-					'removed' => $renderedlist->totalremoved,
-					'added' => $renderedlist->totaladded,
-					'total' => $renderedlist->total);
+				if ($list) {
+					$renderedlist = new RenderedList($list);
+					$renderedlist->calcStats();
+					$stats[]= array(
+						'id' => $list->id,
+						'name' => $list->name,
+						'removed' => $renderedlist->totalremoved,
+						'added' => $renderedlist->totaladded,
+						'total' => $renderedlist->total);
+				}
 			}
 			return $stats;
 			
