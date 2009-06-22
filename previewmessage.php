7952cc577d991a48a133279112ef1357707b61af
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-06-22 12:43:23 +0000

    Change done button to close.

--- previewmessage.php
+++ previewmessage.php
@@ -197,7 +197,7 @@ if (count($formdata)) echo $form->render();
 // If there is no formdata (no field inserts) then just play the message
 if (!count($formdata)) {?>
 	<div style="float:left; margin: 5px">
-		<?=icon_button(_L('Done'),"cross","window.close()",null,null)?>
+		<?=icon_button(_L('Close'),"cross","window.close()",null,null)?>
 	</div>
 	<div align="center" style="clear:left">
 		<OBJECT ID="MediaPlayer" WIDTH=320 HEIGHT=42
