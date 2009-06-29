f09af74c10d6320f889d7a5cbf6783774508b480
Martin Arnberg <marnberg@schoolmessenger.com>
2009-06-29 13:24:04 +0000

    Building on the translation widget controls

--- obj/traslationitem.obj.php
+++ obj/traslationitem.obj.php
@@ -16,12 +16,71 @@ class TranslationItem extends FormItem {
 			$gender = $value["gender"];
 			$value = $value["value"];
 		}
-	
-		$str = '<input id="'.$n.'" name="'.$n.'" type="text" value="'.escapehtml($value).'"/>';
-		$str .= icon_button(_L("Play"),"fugue/control","
-				var content = $('" . $n . "').getValue();
-					if(content != '')
-						popup('previewmessage.php?text=' + encodeURIComponent(content) + '&language=$language&gender=$gender', 400, 400);");
+
+		$displaylanguage = ucfirst($language);
+		
+		
+		$str = '
+			<table>
+				<tr>
+					<td valign="top">
+						<input id="'.$n.'translatecheck" name="'.$n.'checkbox" type="checkbox"
+							 onchange=" $(\''.$n.'icons\').toggle(); 
+							 			$(\''.$n.'textfields\').toggle();
+										$(\''.$n.'controls\').toggle();" 
+										checked/> 
+					</td>
+					<td valign="top" align="right" width="40px">
+						<div id="'.$n.'icons">
+							<div id="'.$n.'editlock" style="display: none;">
+								<img src="img/padlock.gif">
+							</div>
+							<img src="img/pixel.gif" width="10" height="1">
+						</div>
+					</td>
+					<td valign="top">
+						<div id="'.$n.'textfields">
+							<textarea id="'.$n.'text" name="'.$n.'text" rows="3" cols="50" disabled />'.escapehtml($value).'</textarea>	
+							<br />
+							<input id="'.$n.'override" name="'.$n.'checkbox" type="checkbox"
+												 onchange="$(\''.$n.'editlock\').toggle();
+												 			var langtext = $(\''.$n.'text\');
+												 			langtext.disabled = !langtext.disabled;
+												 "/>
+							Override Translation 
+							
+							<div id="'.$n.'retranslation" style="display: none;margin-top: 30px;">
+								<table>
+									<tr>
+										<td>' 
+											. icon_button(_L('Refresh %1$s to English Translation', $displaylanguage),"fugue/arrow_circle_double_135","") . 
+										'</td>
+									</tr>
+									<tr>							
+										<td>
+											<textarea id="'.$n.'retranslation" name="'.$n.'retranslation" rows="3" cols="50"/></textarea>
+										</td>
+									</tr>
+								</table>	
+							</div>
+						</div>
+					</td>
+					<td valign="top">
+						<div id="'.$n.'controls">'
+							. icon_button(_L("Play"),"fugue/control","
+									var content = $('" . $n . "text').getValue();
+										if(content != '')
+											popup('previewmessage.php?text=' + encodeURIComponent(content) + '&language=$language&gender=$gender', 400, 400);")
+							. icon_button(_L("Show in English"),"fugue/magnifier","
+									$('" . $n . "retranslation').toggle();
+							") .
+						'</div>
+					</td>			
+				</tr>
+			</table>
+			<hr>
+				';				
+				
 		return $str;
 	}
 }
