de2f35e152db82a954b48e6c70297dc194b363b3
Martin Arnberg <marnberg@schoolmessenger.com>
2009-07-08 18:45:05 +0000

    Updating converted phone message builder

--- messagephone.php
+++ messagephone.php
@@ -15,6 +15,7 @@ require_once("obj/Voice.obj.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessagePart.obj.php");
 require_once("obj/FieldMap.obj.php");
+require_once("messageitems.inc.php");
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -47,123 +48,6 @@ if (isset($_GET['id'])) {
 // Optional Form Items And Validators
 ////////////////////////////////////////////////////////////////////////////////
 
-class MessageWidget extends FormItem {
-	function render ($value) {
-		$n = $this->form->name."_".$this->name;	
-	
-		$str = '
-			<div>
-			<table>
-				<tr>
-					<td valign="top" rowspan="5">
-						<textarea id="'.$n.'" name="'.$n.'" rows="12" cols="50" />'.escapehtml($value).'</textarea>	'
-					 .  icon_button(_L("Play"),"fugue/control","var content = $('" . $n . "').getValue();
-																	var language = $('" . $this->form->name . "_language').getValue();
-																	var voice = 'Female';
-																	if($('" . $this->form->name . "_voice-2').checked) {
-																		voice = 'Male';
-																	}
-																	if(content != '')
-																		popup('previewmessage.php?text=' + encodeURIComponent(content) + '&language=' + encodeURIComponent(language) + '&gender=' + encodeURIComponent(voice), 400, 400);")
-				. '</td>
-				</tr>
-				<tr>	
-					<td valign="top" colspan="2">
-						<b>Insert Audio Recording:</b>	
-					</td>
-				</tr>
-				<tr>
-					<td valign="top" class="bottomBorder">
-						<select id="'.$n.'recording" name="'.$n.'recording">
-							<option value="">-- Select an Audio File --</option>';				
-		foreach($this->args['audiofiles'] as $audiofile) {
-			$str .= '							<option value="' . $audiofile->id . '">' . escapehtml($audiofile->name) . '</option>';
-		}					
-		$str .= '		</select>'
-						. icon_button(_L("Insert"),"fugue/arrow_turn_180","
-									var sel = $('" . $n . "recording');							
-									if (sel.options[sel.selectedIndex].value > 0) { 
-										textInsert('{{' + sel.options[sel.selectedIndex].text + '}}', $('$n'));}") 
-						. icon_button(_L("Play"),"fugue/control","
-									var sel = $('" . $n . "recording');	
-									if(sel.selectedIndex >= 1) { 
-										popup('previewaudio.php?close=1&id=' + sel.options[sel.selectedIndex].value, 400, 400);}")						
-						. '
-					</td>	
-
-				</tr>
-				<tr>
-					<td valign="top">
-						<b>Insert Data Field:</b> 	
-					</td>
-				</tr>
-				<tr>					
-					<td valign="top">
-						<table border="0" cellpadding="1" cellspacing="0" style="font-size: 9px; margin-top: 5px;">
-							<tr>
-								<td>
-									<span style="font-size: 9px;">Default&nbsp;Value:</span><br />
-									<input id="'.$n.'datadefault" name="'.$n.'datavalue" type="text" size="10" value=""/>
-								</td>
-								<td>
-									<span style="font-size: 9px;">Data&nbsp;Field:</span><br />
-									<select id="'.$n.'datafield" name="'.$n.'language">
-										<option value="">-- Select a Field --</option>';								
-		foreach($this->args['fields'] as $field)
-		{
-			$str .= '							<option value="' . $field . '">' . $field . '</option>';
-		}
-		$str .=	'						</select>
-								</td>
-							</tr>
-						</table>	
-									'. icon_button(_L("Insert"),"fugue/arrow_turn_180","
-												sel = $('" . $n . "datafield'); def = $('" . $n . "datadefault').value; textInsert('<<' + sel.options[sel.selectedIndex].text + (def ? ':' : '') + def + '>>', $('$n'));") 
-									. '		
-												
-				</td>
-				</tr>
-			</table>
-		</div>';
-		return $str;
-	}
-}
-
-class ValMessageWidget extends Validator {
-	var $onlyserverside = true;
-	function validate ($value, $args) {
-		$message = new Message();
-		$errors = array();	
-		$message->parse($value,$errors);  // Fill in with voice id later
-		if (count($errors) > 0)	{			
-			//error('There was an error parsing the message', $errors);
-			$str = "There was an error parsing the message: ";
-			foreach($errors as $error)
-			{
-				$str .= "\n" . $error;
-			}
-			
-			return $str;
-		} else {
-			return true;
-		}
-	}
-}
-
-class ValMessageName extends Validator {
-	
-	var $onlyserverside = true;
-	function validate ($value, $args) {	
-		global $USER;
-		$existsid = QuickQuery("select id from message where name=? and type='phone' and userid=? and deleted=0",false,array($value,$USER->id));		
-		if($existsid && $existsid != $_SESSION['messageid']) {
-			return "A message named $value already exists";
-		}
-		return true;
-	
-	}
-	
-}
 
 ////////////////////////////////////////////////////////////////////////////////
 // Form Data
@@ -202,7 +86,7 @@ $formdata = array(
 		"value" => $messagename,
 		"validators" => array(
 			array("ValRequired","ValLength","min" => 3,"max" => 50),
-			array("ValMessageName","userid" => $USER->id)
+			array("ValMessageName","type" => "phone")
 		),
 		"control" => array("TextField","size" => 30, "maxlength" => 51),
 		"helpstep" => 1
@@ -214,6 +98,8 @@ $formdata = array(
 		"control" => array("TextField","size" => 30, "maxlength" => 51),
 		"helpstep" => 1
 	),
+	"&nbsp;"
+	,
 	"language" => array(
 		"label" => _L('Language'),
 		"value" => ucfirst($messagevoice["language"]),
@@ -228,14 +114,14 @@ $formdata = array(
 		"control" => array("RadioButton","values" => array ("Female" => "Female","Male" => "Male")),
 		"helpstep" => 2
 	),
-	"messagewidget" => array(
+	"messagebody" => array(
 		"label" => _L('Message Content'),
 		"value" => $messagebody,
 		"validators" => array(
 			array("ValRequired"),
-			array("ValMessageWidget")
+			array("ValMessageBody")
 		),
-		"control" => array("MessageWidget", "audiofiles" => $audiofiles,"fields" => $insertfields),
+		"control" => array("MessageBody", "audiofiles" => $audiofiles,"fields" => $insertfields),
 		"helpstep" => 3
 	)
 );
@@ -292,7 +178,7 @@ if ($button = $form->getSubmit()) { //checks for submit and merges in post data
 			exit("nope!"); //TODO
 		}
 		$errors = array();	
-		$parts = $message->parse($postdata["messagewidget"],$errors);
+		$parts = $message->parse($postdata["messagebody"],$errors);
 
 		$message->name = trim($postdata["messagename"]);
 		$message->description = trim($postdata["description"]);
@@ -327,14 +213,14 @@ function fmt_somefield ($obj, $field) {
 ////////////////////////////////////////////////////////////////////////////////
 // Display
 ////////////////////////////////////////////////////////////////////////////////
-$PAGE = "Notification:PhoneMessage";
+$PAGE = _L("notifications").":"._L("messages");
 $TITLE = _L('Phone Message Builder: New Message');
 
 include_once("nav.inc.php");
 
 ?>
 <script type="text/javascript">
-<? Validator::load_validators(array("ValMessageWidget","ValMessageName")); ?>
+<? Validator::load_validators(array("ValMessageBody","ValMessageName")); ?>
 </script>
 <?
 
