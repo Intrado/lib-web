7af196543989a1729fb6bb6f4b7040e22b5587c3
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-07-08 17:05:53 +0000

    add back putRuleFormData and getRuleFromForm until all dependent pages have been converted to use the new rulewidget.

--- inc/rulesutils.inc.php
+++ inc/rulesutils.inc.php
@@ -1,5 +1,59 @@
 <?
 
+// reads form data for new rule
+// if one exists, return the rule object
+function getRuleFromForm($f, $s){
+	
+	$rule = null;
+	$fieldnum = GetFormData($f,$s,"newrulefieldnum");
+	
+	if ($fieldnum != "" && $fieldnum != -1) {
+		$type = GetFormData($f,$s,"newruletype");
+		$logic = GetFormData($f,$s,"newrulelogical_$type");
+		$op = GetFormData($f,$s,"newruleoperator_$type");
+		$value = GetFormData($f,$s,"newrulevalue_" . $fieldnum);
+		if(!is_array($value)) {
+			$value = trim($value);
+		}
+		$value2 = TrimFormData($f,$s,"newrulevalue2_" . $fieldnum);
+		$value3 = TrimFormData($f,$s,"newrulevalue3_" . $fieldnum);
+		$value4 = TrimFormData($f,$s,"newrulevalue4_" . $fieldnum);
+		$value5 = TrimFormData($f,$s,"newrulevalue5_" . $fieldnum);
+
+		// TODO: Why is this "if" check necessary?
+		if (count($value) > 0) {
+			$val = $value;
+			if ($op == "num_range")
+				$val = array($value, $value2);
+			if ($type == "reldate" && $op != "reldate") {
+				if ($op == "eq")
+					$val = $value2;
+				if ($op == "date_range")
+					$val = array($value2, $value3);
+				if ($op == "date_offset")
+					$val = $value4;
+				if ($op == "reldate_range")
+					$val = array($value4,$value5);
+			}
+			$rule = Rule::initFrom($fieldnum, $logic, $op, prepareRuleVal($type, $op, $val));
+			error_log(json_encode($rule));
+		}
+	}
+	return $rule;
+}
+
+// putformdata for ruleeditform
+function putRuleFormData($f, $s){
+	PutFormData($f,$s,"newrulefieldnum","");
+	PutFormData($f,$s,"newruletype","text","text",1,50);
+	PutFormData($f,$s,"newrulelogical_text","and","text",1,50);
+	PutFormData($f,$s,"newrulelogical_multisearch","and","text",1,50);
+	PutFormData($f,$s,"newruleoperator_text","sw","text",1,50);
+	PutFormData($f,$s,"newruleoperator_multisearch","in","text",1,50);
+	PutFormData($f,$s,"newruleoperator_numeric","num_eq","text",1,50);
+	PutFormData($f,$s,"newrulelogical_numeric","and","text",1,50);
+}
+
 // Takes an array of rules and formats them for display in an array
 function displayRules($rules){
 	$rulesdisplay = array();
