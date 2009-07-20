29776d8b4553e0809f493e34c08dd1771612a112
Ben Hencke <bhencke@schoolmessenger.com>
2009-07-20 13:03:24 +0000

    support zip files for downloading, uses different headers, checking for PK zip header and magic numbers

--- taskdownload.php
+++ taskdownload.php
@@ -29,11 +29,20 @@ if (!$USER->authorize('managetasks')) {
 $id = $_GET['id'] + 0;
 $import = new Import($id);
 
-header("Pragma: private");
-header("Cache-Control: private");
-header("Content-disposition: attachment; filename=data.csv");
-header("Content-type: application/vnd.ms-excel");
+//see if this is a zip file using PK zip header magic numbers see http://www.garykessler.net/library/file_sigs.html
+$data =  $import->download();
+if (strlen($data) > 4 && $data[0] == "P" && $data[1] == "K" && $data[2] == "\x03" && $data[3] == "\x04") {
+	header("Pragma: private");
+	header("Cache-Control: private");
+	header("Content-disposition: attachment; filename=data.zip");
+	header("Content-type: application/zip");
+} else {
+	header("Pragma: private");
+	header("Cache-Control: private");
+	header("Content-disposition: attachment; filename=data.csv");
+	header("Content-type: application/vnd.ms-excel");
+}
 
-echo $import->download();
+echo $data;
 
 ?>
