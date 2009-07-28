44fe03bf992f17c0273001a48e22ba60ac759b55
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-07-28 17:07:44 +0000

    First draft message link preview.

--- messagelink.php
+++ messagelink.php
@@ -1,64 +1,59 @@
 <?
 // Cannot use form.inc because no session has started
+setlocale(LC_ALL, 'en_US.UTF-8');
+mb_internal_encoding('UTF-8');
+$SETTINGS = parse_ini_file("inc/settings.ini.php",true);
 
-$isindexpage = true;
-require_once("inc/common.inc.php");
-require_once("inc/html.inc.php");
-require_once("inc/table.inc.php");
+require_once("XML/RPC.php");
+require_once("inc/db.inc.php");
 require_once("inc/auth.inc.php");
-
+require_once("inc/utils.inc.php");
 
 $code = '';
 if (isset($_GET['s'])) {
 	$code = $_GET['s'];
 }
 
-if ($SETTINGS['feature']['has_ssl']) {
-	if ($IS_COMMSUITE)
-		$secureurl = "https://" . $_SERVER["SERVER_NAME"] . "/messagelink.php?s=" . $code;
-	/*CSDELETEMARKER_START*/
-	else
-		$secureurl = "https://" . $_SERVER["SERVER_NAME"] . "/$CUSTOMERURL/messagelink.php?s=" . $code;
-	/*CSDELETEMARKER_END*/
-
-	if ($SETTINGS['feature']['force_ssl'] && !isset($_SERVER["HTTPS"])) {
-		redirect($secureurl);
-	}
-}
-
 // find the message from authserver for this code
-$messageid = loginMessageLink($code);
+$messageinfo = loginMessageLink($code);
 
-
-$TITLE = "Message";
-//primary colors are pulled in login top
-include_once("logintop.inc.php");
-if (!$messageid) {
+$TITLE = ($messageinfo)?getSystemSetting("displayname"):"Not Found";
 ?>
-	<table  style="color: #365F8D;" >
-		<tr>
-			<td>&nbsp;</td>
-			<td>
-				<div style="margin:5px; font-size:12px">
-					Sorry, an error occurred.
-				</div>
-			</td>
-		</tr>
-	</table>
+<html>
+<head>
+	<meta http-equiv="Content-type" value="text/html; charset=UTF-8" />
+	<title>SchoolMessenger - <?=$TITLE?></title>
+</head>
+<body style='padding: 0; margin: 0px; margin: 15px;font-family: "Lucida Grande", verdana, arial, helvetica, sans-serif;'>
+<?
+if (!$messageinfo) {
+?>
+	<div>
+		<h1>The requested information was not found.</h1>
+		<p>The message your looking for doesn't exist anymore or has expired.</p>
+	</div>
 <?
 } else {
 ?>
-	<table  style="color: #365F8D;" >
-		<tr>
-			<td>&nbsp;</td>
-			<td>
-				<div style="margin:5px; font-size:12px">
-					Your message id was found <?=$messageid?>
-				</div>
-			</td>
-		</tr>
-	</table>
+	<div>
+		<img src="logo.img.php?hash=<?=crc32("cid".getSystemSetting("_logocontentid"))?>" />
+	</div>
+	<div style="margin-left: 15px">
+		<h3><?=$TITLE?></h3>
+	</div>
+	<div style="margin:5px">
+		<OBJECT ID="MediaPlayer" WIDTH=320 HEIGHT=42
+		CLASSID="CLSID:22D6F312-B0F6-11D0-94AB-0080C74C7E95"
+		STANDBY="Loading Windows Media Player components..."
+		TYPE="application/x-oleobject">
+
+		<PARAM NAME="FileName" VALUE="messagelink_preview.wav.php?jobcode=<?=$code?>">
+		<param name="controller" value="true">
+		<EMBED SRC="messagelink_preview.wav.php?jobcode=<?=$code?>" AUTOSTART="TRUE"></EMBED>
+		</OBJECT>
+	</div>
 <?
 }
-include_once("loginbottom.inc.php");
 ?>
+</body>
+</html>
