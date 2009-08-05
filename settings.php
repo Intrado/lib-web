bd6329aa14dcaecd805965eb8c1032ce1d2fdc1d
Martin Arnberg <marnberg@schoolmessenger.com>
2009-08-05 20:49:42 +0000

    moving message intro setting to jobfeatures

--- settings.php
+++ settings.php
@@ -62,7 +62,6 @@ startWindow("Options", 'padding: 3px;');
 ?>
 						<tr><td><a href='systemwidealertmessage.php'>Systemwide Alert Message</a></td></tr>
 						<tr><td><a href='customerinfo.php'>Customer Information</a></td></tr>
-						<tr><td><a href='messageintro.php'>Message Intro</a></td></tr>	
 <?
 					}
 					if($USER->authorize('metadata')){
@@ -92,6 +91,7 @@ startWindow("Options", 'padding: 3px;');
 					<tr><td><a href='disablerepeatingjobs.php'>Enable/Disable Repeating Jobs</a></td></tr>
 					<tr><td><a href='jobsettings.php'>Job Settings</a></td></tr>
 					<tr><td><a href='jobtypemanagement.php'>Job Types</a></td></tr>
+					<tr><td><a href='messageintro.php'>Message Intro</a></td></tr>		
 				</table>
 			</td>
 			<td>
