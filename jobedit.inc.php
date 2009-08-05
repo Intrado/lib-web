854738afd72adb3676df761d401b0993f8f0f1bd
Martin Arnberg <marnberg@schoolmessenger.com>
2009-08-05 23:39:22 +0000

    bugfix 2913

--- jobedit.inc.php
+++ jobedit.inc.php
@@ -222,6 +222,8 @@ if(CheckFormSubmit($f,$s) || CheckFormSubmit($f,'phone') || CheckFormSubmit($f,'
 		} else if((bool)GetFormData($f,"email","newlangemail") XOR (bool)GetFormData($f,"email","newmessemail")) {
 			error('Select both a language and a message');
 			$hasemaildetailerror = true;
+		} else if($sendsms && GetFormData($f, $s,"smsradio") == "create" && !ereg("^[a-zA-Z0-9\<\>\?\,\.\/\{\}\|\~\!\@\#\$\%\&\(\)\_\+\']*$", GetFormData($f, $s, "smstextarea"))) {
+			error('Invalid character(s) in SMS Message.');	
 		} else {
 			//submit changes
 
