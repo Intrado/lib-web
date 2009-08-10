1c53dbd3700b38fc2375cc9afa032245ba51831c
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-08-10 20:37:53 +0000

    bugfix 3019

--- obj/JobSummaryReport.obj.php
+++ obj/JobSummaryReport.obj.php
@@ -198,7 +198,7 @@ class JobSummaryReport extends ReportGenerator{
 				$jobtypenames = array();
 				foreach($jobtypes as $jobtype){
 					$jobtypeobj = new JobType($jobtype);
-					$jobtypenames[] = $jobtypeobj->name;
+					$jobtypenames[] = escapehtml($jobtypeobj->name);
 				}
 				$jobtypenames = implode(", ",$jobtypenames);
 ?>
