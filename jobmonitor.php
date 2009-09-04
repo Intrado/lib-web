9568dc82541109dff96ceee2d53af1ca5d22bdf4
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-09-04 23:01:54 +0000

    pass 2, jobmonitor now auto refreshes using Ajax.PeriodicalUpdater, stops refreshing once the job is complete or cancelled. Also makes a distinction between embed(for use in jobwizard)/popup.

--- jobmonitor.php
+++ jobmonitor.php
@@ -22,7 +22,7 @@ if (!$USER->authorize("sendphone")) {
 }
 */
 
-$jobid = DBSafe($_GET['jobid']);
+$jobid = $_GET['jobid'] + 0;
 if (!userOwns("job",$jobid) && !$USER->authorize('viewsystemreports')) {
 	redirect("unauthorized.php");
 }
@@ -31,9 +31,8 @@ if (!userOwns("job",$jobid) && !$USER->authorize('viewsystemreports')) {
 // Data Handling
 ////////////////////////////////////////////////////////////////////////////////
 
+$noupdate = isset($_GET['noupdate']) && $_GET['noupdate'] == true;
 $job = new Job($jobid);
-
-$noupdate = isset($_GET['noupdate']) ? "noupdate" : false;
 
 ////////////////////////////////////////////////////////////////////////////////
 // Display Functions
@@ -44,45 +43,68 @@ $noupdate = isset($_GET['noupdate']) ? "noupdate" : false;
 // Display
 ////////////////////////////////////////////////////////////////////////////////
 
-$TITLE = escapehtml($job->name);
 
-include_once("popup.inc.php");
+if (!isset($_GET['notpopup'])) {
+	$TITLE = escapehtml($job->name);
+	include_once("popup.inc.php");
 
-button_bar(button('Done', 'window.close()'),
-		button("Refresh","UpdateTimer();"));
-
-//startWindow('Job Status', 'padding: 3px;');
-displayJobSummary($jobid,readonlyDBConnect());
-
-?>
-
-<?startWindow(_L("Phone"));?>
-	<img id="realtimePhone" src="graph_job.png.php?type=phone&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
-<?endWindow();?>
-
-<?startWindow(_L("Email"));?>
-	<img id="realtimeEmail" src="graph_job.png.php?type=email&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
-<?endWindow();?>
-
-<?startWindow(_L("SMS"));?>
-	<img id="realtimeSMS" src="graph_job.png.php?type=sms&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
-<?endWindow();?>
-<?
-
-//endWindow();
-print('<br>');
-
-include_once("popupbottom.inc.php");
-if (!$noupdate) {
-?>
-<script type='text/javascript'>
-<!--
-function UpdateTimer() {
-	$('realtimePhone').src = 'graph_job.png.php?type=phone&jobid=<?=$_GET['jobid']?>&foo=' + new Date();
-	$('realtimeEmail').src = 'graph_job.png.php?type=email&jobid=<?=$_GET['jobid']?>&foo=' + new Date();
-	$('realtimeSMS').src = 'graph_job.png.php?type=sms&jobid=<?=$_GET['jobid']?>&foo=' + new Date();
-	setTimeout('UpdateTimer()', 15000);
+	button_bar(button('Done', 'window.close()'),
+		button("Refresh","window.location='jobmonitor.php?jobid={$job->id}&noupdate={$noupdate}';"));
 }
--->
-</script>
-<? } ?>
+
+if (!$noupdate)
+	print('<div id="jobmonitor">');
+
+displayJobSummary($job->id,readonlyDBConnect());
+
+if (!in_array($job->status, array("new", "scheduled"))) {
+	if ($job->sendphone) { ?>
+		<div>
+			<img src="graph_job.png.php?type=phone&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
+		</div>
+	<? }
+	if ($job->sendemail) { ?>
+		<div>
+			<img src="graph_job.png.php?type=email&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
+		</div>
+	<? }
+	if ($job->sendsms) { ?>
+		<div>
+			<img src="graph_job.png.php?type=sms&jobid=<?=$_GET['jobid']?>&junk=<?= rand() ?>" />
+		</div>
+	<? }
+}
+
+if (in_array($job->status, array("cancelled", "complete"))) {?>
+	<script type='text/javascript'>
+		jobmonitorstop = true;
+	</script>
+<? }
+
+if (!$noupdate) {
+	print('</div><br>'); ?>
+
+	<script type='text/javascript'>
+		jobmonitorupdater = new Ajax.PeriodicalUpdater('jobmonitor', 'jobmonitor.php', {
+			evalScripts: true,
+			method: 'get',
+			parameters: {
+				notpopup: true,
+				noupdate: true, // Ajax.PeriodicalUpdater will take care of updating from now on.
+				jobid: <?=$job->id?>
+			},
+			onSuccess: function() {
+				if (jobmonitorstop && jobmonitorupdater) {
+					jobmonitorupdater.stop();
+				}
+			},
+			frequency: 15,
+			decay: 1 // Decay value of 1 means no decay.
+		});
+	</script>
+<? }
+
+if (!isset($_GET['notpopup'])) {
+	include_once("popupbottom.inc.php");
+}
+?>
