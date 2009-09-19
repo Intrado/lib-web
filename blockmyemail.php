aa4eab7d2e420956718f93e64b8e5b444ec3521a
Ben Hencke <bhencke@schoolmessenger.com>
2009-09-19 08:18:32 +0000

    fix with IE, move to post, use hidden field to pass code

--- blockmyemail.php
+++ blockmyemail.php
@@ -25,8 +25,8 @@ $submit = false;
 
 if (isset($_GET['s'])) {
 	$code = $_GET['s'];
-} else if (isset($_GET['submit'])) {
-	$code = $_GET['submit'];
+} else if (isset($_POST['submit'])) {
+	$code = $_POST['code'];
 	$submit = true;
 }
 
@@ -105,8 +105,9 @@ if ($badcode) {
 			<p>Once blocked, you will no longer receive messages from <?=$customer?> to the following address.</p>
 			<div><div style="float:left; margin-right: 5px">Email:</div><div style="padding-left: 3px; padding-right: 3px; width: auto; float: left"><?=$email?></div></div>
 			<div style="clear: both; margin-bottom: 10px"><img src="img/pixel.gif" /></div>
-			<form id="blockmyemail">
-			<?=submit_button("Block my email",$code,"fugue/slash")?>
+			<form method="POST" action="blockmyemail.php">
+			<input type="hidden" name="code" value="<?=escapehtml($code)?>">
+			<?=submit_button("Block my email","submit","fugue/slash")?>
 			</form>
 		</div>
 	<?
