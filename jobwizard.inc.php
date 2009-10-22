1a1a4aba062249855c2ca961c5777ed3c072e81c
Nickolas Heckman <nheckman@schoolmessenger.com>
2009-10-22 15:55:30 +0000

    Fix wording on call window validator

--- jobwizard.inc.php
+++ jobwizard.inc.php
@@ -412,7 +412,7 @@ class ValTimeWindowCallEarly extends Validator {
 	var $onlyserverside = true;
 	function validate ($value, $args, $requiredvalues) {
 		if ((strtotime($value) + 3600) > strtotime($requiredvalues['calllate']))
-			return $this->label. " ". _L('There must be a minimum of one-half hour between start and end time');
+			return $this->label. " ". _L('There must be a minimum of one hour between start and end time');
 		return true;
 	}
 }
@@ -421,7 +421,7 @@ class ValTimeWindowCallLate extends Validator {
 	var $onlyserverside = true;
 	function validate ($value, $args, $requiredvalues) {
 		if ((strtotime($value) - 3600) < strtotime($requiredvalues['callearly']))
-			return $this->label. " ". _L('There must be a minimum of one-half hour between start and end time');
+			return $this->label. " ". _L('There must be a minimum of one hour between start and end time');
 		return true;
 	}
 }
