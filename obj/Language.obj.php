30847a1056c234574479fb545c12cb8cbf4810b9
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-12-14 19:05:53 +0000

    * Removed Language2.obj.php, Message2.obj.php, and MessageBody2.fi.php. ** updated messagegroup.php to include original files. * Updated Language.obj.php, Message.obj.php, MessageBody.fi.php to have temporary classes that will be removed once the db schema for 7.5 is finalized.

--- obj/Language.obj.php
+++ obj/Language.obj.php
@@ -12,4 +12,23 @@ class Language extends DBMappedObject {
 	}
 }
 
+
+
+
+// TODO: Replace with Language.obj.php once migration/db schema is finalized. For now, named Language2.obj.php for use with MessageGroupForm editor.
+
+class Language2 extends DBMappedObject {
+	var $name;
+	var $code;
+	var $ttsvoiceid;
+	var $enabled;
+
+	function Language2 ($id = NULL) {
+		$this->_tablename = "language";
+		$this->_fieldlist = array("name", "code", "ttsvoiceid", "enabled");
+		//call super's constructor
+		DBMappedObject::DBMappedObject($id);
+	}
+}
+
 ?>
