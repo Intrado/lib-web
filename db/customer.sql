2b16751376d4058bc9a5a6d3f13e6cf78bb61166
Ben Hencke <bhencke@schoolmessenger.com>
2009-12-21 19:45:15 +0000

    messagegroup

--- db/customer.sql
+++ db/customer.sql
@@ -1603,3 +1603,19 @@ CREATE TABLE IF NOT EXISTS `personassociation` (
   KEY `personid` (`personid`)
 ) ENGINE=InnoDB
 $$$
+
+CREATE TABLE IF NOT EXISTS `messagegroup` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `userid` int(11) NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `description` varchar(50) NOT NULL,
+  `modified` datetime NOT NULL,
+  `lastused` datetime DEFAULT NULL,
+  `deleted` tinyint(4) NOT NULL DEFAULT '0',
+  `permanent` tinyint NOT NULL DEFAULT 1,
+  `deleted` tinyint NOT NULL DEFAULT 0, 
+  PRIMARY KEY  (`id`),
+  KEY `userid` (`userid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+$$$
+
