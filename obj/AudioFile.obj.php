0c0f534187fe3f6c251699538fe4909337ad7e84
Kee-Yip Chan <kchan@schoolmessenger.com>
2009-12-22 19:30:55 +0000

    Created a temporary AudioFile2 for use in messagegroup.php; will be renamed to AudioFile once data migration and schema is finalized.

--- obj/AudioFile.obj.php
+++ obj/AudioFile.obj.php
@@ -19,4 +19,25 @@ class AudioFile extends DBMappedObject {
 
 }
 
+
+class AudioFile2 extends DBMappedObject {
+
+	var $userid;
+	var $name = "";
+	var $description = "";
+	var $contentid;
+	var $recorddate;
+	var $deleted = 0;
+	var $permanent = 0;
+	var $messagegroupid = 0;
+
+	function AudioFile ($id = NULL) {
+		$this->_allownulls = true;
+		$this->_tablename = "audiofile";
+		$this->_fieldlist = array("userid", "name", "messagegroupid", "description", "contentid", "recorddate", "deleted", "permanent");
+		DBMappedObject::DBMappedObject($id);
+	}
+
+}
+
 ?>
