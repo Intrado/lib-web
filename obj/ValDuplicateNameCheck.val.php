a8d8b012f7220604e1cbfe2d4601204482cb6d23
Martin Arnberg <marnberg@schoolmessenger.com>
2009-12-28 20:23:18 +0000

    Extend validator to handle job name validation

--- obj/ValDuplicateNameCheck.val.php
+++ obj/ValDuplicateNameCheck.val.php
@@ -1,12 +1,21 @@
 <?
-// TODO make generic to work with list, jobs etc...
+// TODO make generic to work with list etc...
 class ValDuplicateNameCheck extends Validator {
 	var $onlyserverside = true;
 	function validate ($value, $args) {	
 		global $USER;
-		$existsid = QuickQuery("select id from message where name=? and type=? and userid=? and deleted=0",false,array($value,$args["type"],$USER->id));		
-		if($existsid && $existsid != $_SESSION['messageid']) {
-			return "A message named $value already exists";
+		$type = $args["type"];
+
+		if(in_array($type, array("phone","email","sms"))) {
+			$existsid = QuickQuery("select id from message where name=? and type=? and userid=? and deleted=0",false,array($value,$args["type"],$USER->id));
+			if($existsid && $existsid != $_SESSION['messageid']) {
+				return "$this->label: ". _L('There is already a message with this name. Please choose another.');
+			}
+		} else if($type == "job") {
+			$existsid = QuickQuery("select id from job where not deleted and userid=? and name=? and status in ('new','scheduled','processing','procactive','active')",false,array($USER->id, $value));
+			if($existsid && $existsid != $_SESSION['jobid']) {
+				return "$this->label: ". _L('There is already an active notification with this name. Please choose another.');
+			}
 		}
 		return true;
 	}
