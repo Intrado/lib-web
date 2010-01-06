01298f8ad9eeaa85797ec0a4a2670bf2941011bc
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-06 01:37:12 +0000

    Updated messagegroup with utility functions

--- obj/MessageGroup.obj.php
+++ obj/MessageGroup.obj.php
@@ -24,6 +24,61 @@ class MessageGroup extends DBMappedObject {
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
+	
+	function hasMessage($type, $subtype = null, $languagecode = null) {
+		global $USER;
+		
+		$query = 'select count(id) from message where userid=? and messagegroupid=? and not deleted and type=? ';
+		$args = array($USER->id, $this->id, $type);
+		
+		if (is_string($subtype)) {
+			$query .= ' and subtype=? ';
+			$args[] = $subtype;
+		}
+		if (is_string($languagecode)) {
+			$query .= ' and languagecode=? ';
+			$args[] = $languagecode;
+		}
+		
+		return QuickQuery($query, false, $args) ? true : false;
+	}
+	
+	function getOneEnabledMessage($type) {
+		return DBFind('Message2', 'from message where not deleted and type=? and messagegroupid=?', false, array($type, $this->id));
+	}
+
+	function getGlobalPreferredGender($default) {
+		if (!$phonemessage = $this->getOneEnabledMessage('phone'))
+			return $default;
+		return QuickQuery('select v.gender from messagepart mp join ttsvoice v on mp.voiceid=v.id where mp.messageid=? limit 1', false, array($phonemessage->id));
+	}
+
+	function getGlobalEmailAttachments() {
+		if (!$emailmessage = $this->getOneEnabledMessage('email'))
+			return array();
+		return DBFindMany('MessageAttachment', 'from messageattachment where not deleted and messageid=?', false, array($emailmessage->id));
+	}
+	
+	function getGlobalEmailHeaders($default) {
+		if (!$emailmessage = $this->getOneEnabledMessage('email'))
+			return $default;
+		$emailmessage->readHeaders();
+		
+		return array(
+			'subject' => $emailmessage->subject,
+			'fromname' => $emailmessage->fromname,
+			'fromemail' => $emailmessage->fromemail
+		);
+	}
+	
+	function getMessageText($type, $subtype, $languagecode, $autotranslate) {
+		if (!$message = DBFind('Message2', 'from message where not deleted and type=? and subtype=? and languagecode=? and messagegroupid=? and autotranslate=?', false, array($type, $subtype, $languagecode, $this->id, $autotranslate)))
+			return '';
+		
+		$parts = DBFindMany('MessagePart', 'from messagepart where messageid=?', false, array($message->id));
+		
+		return $message->format($parts);
+	}
 }
 
 ?>
