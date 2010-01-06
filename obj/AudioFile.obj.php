036617d37b55ad7fb06abf105b2ea221347d0192
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-06 01:30:28 +0000

    removed AudioFile2

--- obj/AudioFile.obj.php
+++ obj/AudioFile.obj.php
@@ -9,26 +9,6 @@ class AudioFile extends DBMappedObject {
 	var $recorddate;
 	var $deleted = 0;
 	var $permanent = 0;
-
-	function AudioFile ($id = NULL) {
-		$this->_allownulls = true;
-		$this->_tablename = "audiofile";
-		$this->_fieldlist = array("userid", "name", "description", "contentid", "recorddate", "deleted", "permanent");
-		DBMappedObject::DBMappedObject($id);
-	}
-
-}
-
-
-class AudioFile2 extends DBMappedObject {
-
-	var $userid;
-	var $name = "";
-	var $description = "";
-	var $contentid;
-	var $recorddate;
-	var $deleted = 0;
-	var $permanent = 0;
 	var $messagegroupid = 0;
 
 	function AudioFile ($id = NULL) {
