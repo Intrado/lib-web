f4277f9f97d6d2bbfca59ad690def5f531fe5bf9
Ben Hencke <bhencke@schoolmessenger.com>
2010-01-06 01:05:42 +0000

    refactor

--- obj/Voice.obj.php
+++ obj/Voice.obj.php
@@ -3,46 +3,48 @@
 class Voice extends DBMappedObject {
 
 	var $language;
+	var $languagecode;
 	var $gender;
-
+	
 	function Voice ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "ttsvoice";
-		$this->_fieldlist = array("language", "gender");
+		$this->_fieldlist = array("language", "languagecode", "gender");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
 
-	// return array of Voice objects (indexed "language:gender") based on customer language table
+	// return array of Voice objects (indexed "languagecode:gender") based on what's enabled in customer language table
 	static function getTTSVoices() {
-		$voices = DBFindMany("Voice","from ttsvoice t join language l where l.name = t.language order by t.id", "t");
-		$retval = array();
-		foreach ($voices as $voice) {
-			$retval[$voice->language.":".$voice->gender] = $voice;
+		static $voices = false;	
+		if ($voices === false) {
+			$tmp = DBFindMany("Voice","from ttsvoice t join language l on (t.languagecode = l.code) order by t.id", "t");
+			$voices = array();
+			foreach ($tmp as $voice) {
+				$voices[$voice->languagecode.":".$voice->gender] = $voice;
+			}
 		}
-		return $retval;
+		return $voices;
 	}
 
-	// return array of language names (indexed by language code if $paired) that are supported languages for tts, based on customer language table
-	static function getTTSLanguages($paired = false) {
-		if ($paired)
-			return QuickQueryList("select distinct l.code, l.name from ttsvoice t join language l where l.name = t.language order by t.id", true);
-		else
-			return QuickQueryList("select distinct l.name from ttsvoice t join language l where l.name = t.language order by t.id");
+	// return array of language codes that are supported languages for tts, based on what's enabled in customer language table
+	static function getTTSLanguageCodes() {
+		$codes = array();
+		foreach (Voice::GetTTSVoices() as $voice) {
+			$codes[] = $voice->languagecode;
+		}
 	}
 	
 	static function getPreferredVoice($languagecode, $gender) {
-		$voiceid = QuickQuery("select t.id from language l join ttsvoice t on l.ttsvoiceid = t.id where l.code=? and t.gender=?",false,array($languagecode,$gender));
-
-		if($voiceid === false ) {
-			if($gender == "Female") {
-				$voiceid = QuickQuery("select t.id from language l join ttsvoice t on l.ttsvoiceid = t.id where l.code=? and t.gender='Male'",false,array($languagecode));
-			} else if($gender == "Male") {
-				$voiceid = QuickQuery("select t.id from language l join ttsvoice t on l.ttsvoiceid = t.id where l.code=? and t.gender='Female'",false,array($languagecode));
-			}
-		}
-		if($voiceid	=== false)
-			$voiceid = 1; // default to english
+		$voices = Voice::GetTTSVoices();
+		if (isset($voices["$languagecode:$gender"]))
+			return $voices["$languagecode:$gender"]->id;
+		else if (isset($voices["$languagecode:female"]))
+			return $voices["$languagecode:female"]->id;
+		else if (isset($voices["$languagecode:male"]))
+			return $voices["$languagecode:male"]->id;
+		else
+			return 1; // default to english
 	}
 }
 
