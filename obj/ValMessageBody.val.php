3726388de71d49c1f9c819774c53d9e0146a4d34
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-08 17:54:06 +0000

    add support for defaultlanguagecode.

--- obj/ValMessageBody.val.php
+++ obj/ValMessageBody.val.php
@@ -2,10 +2,17 @@
 class ValMessageBody extends Validator {
 	var $onlyserverside = true;
 	function validate ($value, $args) {
+		if (isset($_SESSION['messagegroupid'])) {
+			$messagegroup = new MessageGroup($_SESSION['messagegroupid']);
+			// Unless the user is editing the default message, show an error if the messagegroup has no default message.
+			if ($args['languagecode'] != $messagegroup->defaultlanguagecode && !$messagegroup->hasDefaultMessage($args['type'], $args['subtype']))
+				return _L("Please first create the %s message.", Language::getName($messagegroup->defaultlanguagecode));
+		}
+		
 		$message = new Message();
 		$errors = array();	
 		$message->parse($value,$errors);  // Fill in with voice id later
-		if (count($errors) > 0)	{			
+		if (count($errors) > 0)	{
 			$str = "There was an error parsing the message: ";
 			foreach($errors as $error)
 			{
