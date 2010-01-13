f72d680168e0a5036bb5315dd7b18a23551b0645
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-13 02:38:56 +0000

    update to support defaultlanguagecode, also this validator can be used for validating a translationitem's text

--- obj/ValMessageBody.val.php
+++ obj/ValMessageBody.val.php
@@ -1,17 +1,51 @@
 <?
 class ValMessageBody extends Validator {
 	var $onlyserverside = true;
-	function validate ($value, $args) {
+	function validate ($value, $args, $requiredvalues) {
 		if (isset($_SESSION['messagegroupid'])) {
 			$messagegroup = new MessageGroup($_SESSION['messagegroupid']);
+			$errormessagecreatefirst = _L("Please first create the %s message.", Language::getName($messagegroup->defaultlanguagecode));
 			// Unless the user is editing the default message, show an error if the messagegroup has no default message.
-			if ($args['languagecode'] != $messagegroup->defaultlanguagecode && !$messagegroup->hasDefaultMessage($args['type'], $args['subtype']))
-				return _L("Please first create the %s message.", Language::getName($messagegroup->defaultlanguagecode));
+			if (!$messagegroup->hasDefaultMessage($args['type'], $args['subtype'])) {
+				if ($args['languagecode'] == 'autotranslator') { // For autotranslator, $requiredvalues are jsonencoded from TranslationItem.
+					if (empty($requiredvalues))
+						return $errormessagecreatefirst;
+				
+					$editingdefault = false;
+					$defaultincluded = false;
+					foreach ($requiredvalues as $value) {
+						$translationitemdata = json_decode($value);
+						if ($translationitemdata->language == $messagegroup->defaultlanguagecode) {
+							$defaultincluded = true;
+							if ($translationitemdata->enabled) {
+								$editingdefault = true;
+								break;
+							}
+						}
+					}
+					if (!$defaultincluded)
+						return $errormessagecreatefirst;
+					if (!$editingdefault)
+						return _L("Please include %s or first create its message separately.", Language::getName($messagegroup->defaultlanguagecode));
+				} else if ($args['languagecode'] != $messagegroup->defaultlanguagecode) {
+					return $errormessagecreatefirst;
+				}
+			}
+		}
+		
+		if (!empty($args['translationitem'])) {
+			$msgdata = json_decode($value);
+			if ($msgdata->override || !$msgdata->enabled)
+				$text = $msgdata->text;
+			else if ($msgdata->enabled && !$msgdata->override)
+				$text = $msgdata->englishText;
+		} else {
+			$text = $value;
 		}
 		
 		$message = new Message();
 		$errors = array();	
-		$parts = $message->parse($value,$errors);  // Fill in with voice id later
+		$parts = $message->parse($text,$errors);  // Fill in with voice id later
 		if (count($errors) > 0)	{
 			$str = "There was an error parsing the message: ";
 			foreach($errors as $error)
