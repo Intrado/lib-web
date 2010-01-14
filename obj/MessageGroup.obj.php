5f0c255312c23a99ded53cfe78ede5d2049ef366
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-14 19:20:09 +0000

    Return the default is the gender was not found.

--- obj/MessageGroup.obj.php
+++ obj/MessageGroup.obj.php
@@ -56,7 +56,11 @@ class MessageGroup extends DBMappedObject {
 	function getGlobalPreferredGender($default) {
 		if (!$phonemessage = $this->getOneEnabledMessage('phone'))
 			return $default;
-		return QuickQuery('select v.gender from messagepart mp join ttsvoice v on mp.voiceid=v.id where mp.messageid=? and mp.voiceid is not null order by sequence limit 1', false, array($phonemessage->id));
+		$gender = QuickQuery('select v.gender from messagepart mp join ttsvoice v on mp.voiceid=v.id where mp.messageid=? and mp.voiceid is not null order by sequence limit 1', false, array($phonemessage->id));
+		if ($gender)
+			return $gender;
+		else
+			return $default;
 	}
 
 	function getGlobalEmailAttachments() {
