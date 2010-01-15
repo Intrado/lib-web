99969134d36e3824c9135f2062aff235f3d09a8f
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-15 19:28:56 +0000

    Moved htmleditor routines into its own file, out of utils.js

---
+++ script/htmleditor.js
@@ -0,0 +1,152 @@
+
+// Returns the textarea that the html editor is currently replacing.
+function getHtmlEditorObject() {
+	if ((typeof CKEDITOR == 'undefined') || !CKEDITOR) {
+		return null;
+	}
+
+	var instance;
+	if ((typeof CKEDITOR.instances == 'undefined') || !CKEDITOR.instances || !(instance = CKEDITOR.instances['reusableckeditor']))
+		return null;
+
+	var container = $('cke_reusableckeditor');
+	
+	if (!container)
+		return null;
+
+	var textarea = container.previous();
+	var textareauseshtmleditor = textarea && textarea.match('textarea.HtmlEditor');
+	
+	return {'instance': instance, 'container': container, 'currenttextarea': textareauseshtmleditor ? textarea : null};
+}
+
+var currenthtmleditorkeylistener = null; // Global variable used to keep track of the key listener so that it can be unregistered when registerHtmlEditorKeyListener() is called with a new listener.
+var pendinghtmleditorkeylistener = null; // Will register this listener when the instance is ready, 
+function registerHtmlEditorKeyListener(listener) {
+	var htmleditorobject = getHtmlEditorObject();
+	if (!htmleditorobject) {
+		currenthtmleditorkeylistener = null;
+		pendinghtmleditorkeylistener = listener; // Will register this listener when the instance is ready, called in applyHtmlEditor().
+		
+		return;
+	}
+	
+	if (currenthtmleditorkeylistener)
+		htmleditorobject.instance.removeListener('key', currenthtmleditorkeylistener);
+	if (listener)
+		htmleditorobject.instance.on('key', listener);
+	
+	currenthtmleditorkeylistener = listener;
+}
+
+// Updates the textarea that the html editor replaces with the latest content.
+// Returns null if the html editor is not loaded.
+// Returns an object containing the html editor instance and also the html editor's container.
+function saveHtmlEditorContent(existinghtmleditorobject) {
+	var htmleditorobject = existinghtmleditorobject || getHtmlEditorObject();
+	if (!htmleditorobject) {
+		return null;
+	}
+	
+	var textarea = htmleditorobject.currenttextarea;
+	if (textarea) {
+		var tempdiv = new Element('div').insert(htmleditorobject.instance.getData());
+
+		// Unstyle any image elements having src="viewimage.php?id=.."
+		var images = tempdiv.select('img');
+		for (var i = 0, count = images.length; i < count; i++) {
+			var image = images[i];
+			var matches = image.src.match(/viewimage\.php\?id=(\d+)/);
+			if (matches.length == 2) {
+				image.replace('<img src="viewimage.php?id=' + matches[1] + '">');
+			}
+		}
+
+		var html = tempdiv.innerHTML.replace(/&lt;&lt;/g, '<<').replace(/&gt;&gt;/g, '>>');
+
+		if (images.length < 1) {
+			// CKEditor inserts blank tags even if the user has deleted everything.
+			if (html.stripTags().strip() == '')
+				html = '';
+		}
+
+		textarea.value = html;
+		textarea.fire('HtmlEditor:SavedContent');
+	}
+	
+	return htmleditorobject;
+}
+
+function hideHtmlEditor() {
+	if ($('cke_reusableckeditor'))
+		$('reusableckeditorhider').insert($('cke_reusableckeditor'));
+}
+
+function clearHtmlEditorContent() {
+	var htmleditorobject = getHtmlEditorObject();
+	if (!htmleditorobject)
+		return;
+	
+	htmleditorobject.instance.setData('');
+}
+
+// Loads the html editor if necessary.
+// NOTE: It is assumed that there be only a single html editor on the page; CKEditor is buggy with multiple instances.
+function applyHtmlEditor(textarea) {
+	textarea = $(textarea);
+
+	var editorobject = getHtmlEditorObject();
+	if (!editorobject) {
+		if ($('reusableckeditor'))
+			return; // The editor instance is still loading.
+
+		textarea.insert({'before': '<span class="HTMLEditorAjaxLoader"><img src="img/ajax-loader.gif"/> Please wait while the HTML editor loads. </span>'});
+		textarea.hide();
+
+		var reusableckeditor = new Element('div', {'id':'reusableckeditor'});
+		if (document.body) {
+			document.body.insert(new Element('div', {'id':'reusableckeditorhider'}).hide().insert(reusableckeditor));
+			CKEDITOR.replace(reusableckeditor, {
+				'customConfig': '', // Prevent ckeditor from trying to load an external configuration file, should improve startup time.
+				'removePlugins': 'wsc,scayt,smiley,showblocks,flash',
+				'toolbar': [
+					['Preview','Print'],
+					['Undo','Redo','-','SelectAll','Cut','Copy','Paste','PasteText','PasteFromWord','Find','Replace'],
+					'/',
+					['Styles','Format'],
+					['NumberedList','BulletedList','JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock','Outdent','Indent'],
+					'/',
+					['Font','FontSize','Bold', 'Italic', 'Underline','Strike','Subscript','Superscript',  'TextColor','BGColor', 'RemoveFormat'],
+					['SpecialChar','Blockquote','Link', 'Image','Table','HorizontalRule','PageBreak']
+				],
+				'disableObjectResizing': true,
+				'resize_enabled': false,
+				'width': '100%',
+				'filebrowserImageUploadUrl' : 'uploadimage.php',
+				'on': {
+					'instanceReady': function(event) {
+						this.previous('.HTMLEditorAjaxLoader').remove();
+						applyHtmlEditor(this);
+						registerHtmlEditorKeyListener(pendinghtmleditorkeylistener);
+						pendinghtmleditorkeylistener = null;
+					}.bindAsEventListener(textarea)
+				}
+			});
+		} else {
+			document.observe('dom:loaded', function(event) {
+				applyHtmlEditor(this);
+			}.bindAseventListener(textaera));
+		}
+
+		return;
+	}
+	
+	if (!editorobject.currenttextarea || editorobject.currenttextarea.identify() != textarea.identify()) {
+		saveHtmlEditorContent(editorobject);
+	}
+
+	var html = textarea.value.replace("<<", "&lt;&lt;").replace(">>", "&gt;&gt;");
+	editorobject.instance.setData(html);
+	
+	textarea.hide().addClassName('HtmlEditor').insert({'after':editorobject.container});
+}
