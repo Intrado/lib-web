ad949b2ac038ddb9047b6932725f3f7464f228ba
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2010-01-20 21:28:33 +0000

    agent and customeragent tables new disk database!

---
+++ db/disk.sql
@@ -0,0 +1,15 @@
+CREATE TABLE `agent` (
+ `id` int(11) NOT NULL auto_increment,
+ `uuid` varchar(255) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL,
+ `name` varchar(50) NOT NULL,
+ `numpollthreads` smallint(6) NOT NULL,
+ PRIMARY KEY  (`id`),
+ UNIQUE KEY `uuid` (`uuid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `customeragent` (
+ `customerid` int(11) NOT NULL,
+ `agentid` int(11) NOT NULL,
+ PRIMARY KEY  (`customerid`,`agentid`)
+) ENGINE=InnoDB;
+
