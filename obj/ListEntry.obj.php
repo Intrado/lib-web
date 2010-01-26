a2d6bc3486b51dfa089cd20bdc01364f3b4ef47a
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-26 23:28:10 +0000

    Schema update

--- obj/ListEntry.obj.php
+++ obj/ListEntry.obj.php
@@ -3,16 +3,18 @@
 class ListEntry extends DBMappedObject {
 
 	var $listid;
-	var $type;
+	var $type; // 'rule', 'organization', 'section'
 	var $ruleid;
 	var $personid;
+	var $organizationid;
+	var $sectionid;
 
 	var $rule;
 
 	function ListEntry ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "listentry";
-		$this->_fieldlist = array("listid","type","ruleid","personid");
+		$this->_fieldlist = array("listid","type","ruleid","personid","organizationid","sectionid");
 		$this->_childobjects = array("rule");
 		$this->_childclasses = array("Rule");
 		//call super's constructor
