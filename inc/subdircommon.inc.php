febad0395e5c7cf4772df1e9d86096f857149660
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-01-26 23:40:44 +0000

    Added require_once() for Organization.obj.php to facilitate user organization restrictions

--- inc/subdircommon.inc.php
+++ inc/subdircommon.inc.php
@@ -29,6 +29,7 @@ require_once("../obj/User.obj.php");
 require_once("../obj/Access.obj.php");
 require_once("../obj/Permission.obj.php");
 require_once("../obj/Rule.obj.php"); //for search and sec profile rules
+require_once("../obj/Organization.obj.php"); //for search and sec profile rules
 
 if (!isset($isindexpage) || !$isindexpage) {
 	doStartSession();
