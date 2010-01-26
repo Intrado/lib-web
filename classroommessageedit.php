22a16cbeacf8d8e0de815717da90c090bae40aa8
Martin Arnberg <marnberg@schoolmessenger.com>
2010-01-26 00:03:17 +0000

    Targeted message has moved to classroom message pages. classroommessagemanager is the overview page and classroommessgeedit and classroommessagecategory creates and edits the messages and categories. classroom.inc.php is used to store common array for category icons but may be used for other common classroom stuff after refactoring

---
+++ classroommessageedit.php
@@ -0,0 +1,241 @@
+<?
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+require_once("inc/common.inc.php");
+require_once("inc/securityhelper.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/html.inc.php");
+require_once("inc/utils.inc.php");
+require_once("obj/Validator.obj.php");
+require_once("obj/Form.obj.php");
+require_once("obj/FormItem.obj.php");
+require_once("obj/MessageGroup.obj.php");
+require_once("obj/Message.obj.php");
+require_once("obj/MessagePart.obj.php");
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+if (!$USER->authorize('managesystem')) {
+	redirect('unauthorized.php');
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+if (isset($_GET['id'])) {
+	if($_GET['id'] == "new") {
+		$_SESSION["targetedmessageid"] = null;
+	} else {
+		$_SESSION["targetedmessageid"] = $_GET['id'] + 0;
+	}
+	redirect("classroommessageedit.php");
+}
+
+$id = $_SESSION["targetedmessageid"];
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data
+////////////////////////////////////////////////////////////////////////////////
+
+$value = "";
+$languages = QuickQueryMultiRow("select * from language");
+$values = array();
+
+if($id) {
+	$targetedmesssage = QuickQueryRow("select messagekey, overridemessagegroupid, targetedmessagecategoryid from targetedmessage where id=?",false,false,array($id));
+} else {
+	$targetedmesssage = false;
+}
+
+if(isset($targetedmesssage[1])) {
+	$languagemessages = QuickQueryList("select m.languagecode, p.txt from message m, messagepart p
+			where m.messagegroupid = ? and
+					m.id = p.messageid", true,false,array($targetedmesssage[1]));
+}
+
+$categories = QuickQueryList("select id, name from targetedmessagecategory where deleted = 0",true);
+$categories = $categories?(array("" => "-- Select a Category --") + $categories):array("" => "-- Select a Category --");
+
+$formdata = array();
+
+if(!isset($messagedatacache)) { 
+	$messagedatacache = array();
+}
+
+
+$formdata["category"] = array(
+	"label" => _L("Category"),
+	"value" => isset($targetedmesssage[2])?$targetedmesssage[2]:"",
+	"validators" => array(
+		array("ValRequired"),
+		array("ValInArray", "values" => array_keys($categories))
+	),
+	"control" => array("SelectMenu","values" => $categories),
+	"helpstep" => 1
+);
+foreach($languages as $language) {
+	$code = $language[2];
+
+	$formdata[$code] = array(
+		"label" => $language[1],
+		"value" => "",
+		"validators" => array(
+			array("ValLength","min" => 3,"max" => 150)
+		),
+		"control" => array("TextField","size" => 50, "maxlength" => 150),
+		"helpstep" => 2
+	);
+	if(isset($languagemessages[$code]) && $languagemessages[$code] != "") {
+		// Populate the form with message data and complete with default data
+
+		//error_log("custom" . $code);
+		$formdata[$code]["value"] = $languagemessages[$code];
+		//$formdata[$code]["value"] = "Custom Data " . (isset($languagemessages[$code])?$languagemessages[$code]:"");
+	} else {
+		//error_log("default" . $code);
+		// Populate with default data
+		$filename = "messagedata/" . $code . "/data.php";
+		if(file_exists($filename))
+			include_once($filename);
+
+		if(isset($messagedatacache[$code]) && isset($messagedatacache[$code][$targetedmesssage[0]])) {
+			$formdata[$code]["value"] = $messagedatacache[$code][$targetedmesssage[0]];
+		} // else no default data found value is set to empty
+	}
+}
+
+$helpsteps = array (
+	_L('Category'),
+	_L('Type the Message in as many languages as you are able to.')
+);
+
+$buttons = array(submit_button(_L('Save'),"submit","tick"),
+	icon_button(_L('Cancel'),"cross",null,"classroommessagemanager.php"));
+$form = new Form("classroom",$formdata,$helpsteps,$buttons);
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+//check and handle an ajax request (will exit early)
+//or merge in related post data
+$form->handleRequest();
+
+$datachange = false;
+$errors = false;
+//check for form submission
+if ($button = $form->getSubmit()) { //checks for submit and merges in post data
+	$ajax = $form->isAjaxSubmit(); //whether or not this requires an ajax response
+
+	if ($form->checkForDataChange()) {
+		$datachange = true;
+	} else if (($errors = $form->validate()) === false) { //checks all of the items in this form
+			$postdata = $form->getData(); //gets assoc array of all values {name:value,...}
+			Query("BEGIN");
+			if($targetedmesssage[0]) {
+				QuickUpdate("update targetedmessage set targetedmessagecategoryid = ? where messagekey = ?",false,array($postdata["category"],$targetedmesssage[0]));
+			}
+
+			$messagegroupid = false;
+			//save data here
+			foreach($languages as $language) {
+				$code = $language[2];
+				$newvalue = $postdata[$code];
+				$message = false;
+				//
+				if($targetedmesssage[1]) {
+					$message = DBFind("Message", "from message m where m.messagegroupid = ? and languagecode = ?",false, array($targetedmesssage[1],$code));
+					$messagegroupid = $targetedmesssage[1];
+				} else {
+					if(	isset($messagedatacache[$code]) &&
+						isset($messagedatacache[$code][$targetedmesssage[0]]) &&
+						$messagedatacache[$code][$targetedmesssage[0]] == $newvalue
+						) {
+							// There is a default value for this message/language and the value has not changed
+
+							continue;
+					} else {
+						if($newvalue != '' && !$messagegroupid) {
+							// create a new message group
+
+							$messagegroup = new MessageGroup();
+							$messagegroup->userid =  $USER->id;
+							$messagegroup->name = "Custom Classroom";
+							$messagegroup->description = '';
+							$messagegroup->modified = date("Y-m-d H:i:s", time());
+							$messagegroup->deleted = 1;
+							$messagegroup->create();
+							$messagegroupid = $messagegroup->id;
+
+							if(isset($targetedmesssage[0])) {
+								error_log("update targetedmessage set overridemessagegroupid = $messagegroupid where messagekey = " . $targetedmesssage[0]);
+								QuickUpdate("update targetedmessage set overridemessagegroupid = ? where messagekey = ?",false,array($messagegroupid,$targetedmesssage[0]));
+							} else {
+								error_log("insert into targete table $code");
+								QuickUpdate("insert into targetedmessage (messagekey,targetedmessagecategoryid,overridemessagegroupid) values (?,?,?)",false,array(substr($newvalue, 0, 10) . "-" .  $messagegroup->modified,$postdata["category"],$messagegroupid));
+							}
+						}
+					}
+				}
+				if($messagegroupid) {
+					if($message === false) {
+						// create a new message
+						$message = new Message();
+						$message->messagegroupid = $messagegroupid;
+						$message->userid = $USER->id;
+						$message->name = "Custom Classroom";
+						$message->description = '';
+						$message->type = 'email';
+						$message->subtype = 'plain';
+						$message->data = '';
+						$message->modifydate = date("Y-m-d H:i:s", time());
+						$message->deleted = 1;
+						$message->autotranslate = 'none';
+						$message->languagecode = $code;
+						$message->create();
+
+						$messagepart = new MessagePart();
+						$messagepart->messageid = $message->id;
+						$messagepart->type = 'T';
+						$messagepart->txt = $newvalue;
+						$messagepart->sequence = 0;
+						$messagepart->create();
+					} else {
+						$message->modifydate = date("Y-m-d H:i:s", time());
+						$message->update();
+						$messagepart = DBFind("MessagePart","from messagepart where messageid = ? and sequence = 0",false,array($message->id));
+						if($messagepart) {
+							$messagepart->txt = $newvalue;
+							$messagepart->update();
+						}
+					}
+				}
+			}
+
+
+			Query("COMMIT");
+			if ($ajax)
+				$form->sendTo("classroommessagemanager.php");
+			else
+				redirect("classroommessagemanager.php");
+		}
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Display
+////////////////////////////////////////////////////////////////////////////////
+$PAGE = "admin:settings";
+$TITLE = _L('Classroom Message Edit');
+
+include_once("nav.inc.php");
+
+startWindow(_L('Language Variations for Classroom Message'));
+echo $form->render();
+endWindow();
+include_once("navbottom.inc.php");
+?>
---
+++ classroommessagemanager.php
@@ -0,0 +1,350 @@
+<?
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+include_once("inc/common.inc.php");
+include_once("inc/securityhelper.inc.php");
+include_once("obj/PeopleList.obj.php");
+require_once("inc/table.inc.php");
+require_once("inc/utils.inc.php");
+include_once("inc/form.inc.php");
+include_once("inc/html.inc.php");
+include_once("inc/formatters.inc.php");
+require_once("inc/classroom.inc.php");
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+if (!$USER->authorize('managesystem')) {
+	redirect('unauthorized.php');
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+if (isset($_GET['enable']) && isset($_GET['id'])) {
+	session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
+	header('Content-Type: application/json');
+	$result = QuickUpdate("update targetedmessage set enabled=? where id=?",false,array(($_GET['enable']=="false"?0:1),$_GET['id']));
+	echo json_encode($result!==false);
+	exit();
+} else if(isset($_GET['deletecategoryid'])) {
+	session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
+	header('Content-Type: application/json');
+	$items = QuickQuery("select count(*) from targetedmessage where targetedmessagecategoryid = ? and deleted = 0",false,array($_GET['deletecategoryid']));
+
+	if($items === "0")
+		QuickUpdate("update targetedmessagecategory set deleted=1 where id=?",false,array($_GET['deletecategoryid']));
+
+
+	//echo json_encode($items === "0" && QuickUpdate("delete from targetedmessagecategory where id=?",false,array($_GET['deletecategoryid'])));
+	echo json_encode($items === "0");
+	exit();
+}
+
+$categories = QuickQueryMultiRow("select id, name, image from targetedmessagecategory where deleted = 0",true);
+$categoriesjson = array();
+
+foreach($categories as $category) {
+	$obj = null;
+	$obj->name = $category["name"];
+	if(isset($category["image"]) && isset($classroomcategoryicons[$category["image"]]))
+		$obj->img = "img/icons/" . $classroomcategoryicons[$category["image"]]  . ".gif";
+	else
+		$obj->img = "img/pixel.gif";
+	$categoriesjson[$category["id"]] = $obj;
+}
+
+$ajax = isset($_GET['ajax']);
+
+$items = array();
+if($ajax === true) {
+	session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
+	header('Content-Type: application/json');
+
+	if (!isset($_GET['category'])) {
+		echo json_encode(false);
+		exit();
+	}
+
+	if (isset($_GET['moveid']) && isset($_GET['movetoid'])) {
+		if(in_array($_GET['movetoid'], array_keys($categoriesjson)))
+			QuickUpdate("update targetedmessage set targetedmessagecategoryid=? where id=?", false, array($_GET['movetoid'],$_GET['moveid']));
+	} else if(isset($_GET['deletemessageid'])) {
+		QuickUpdate("update targetedmessage set deleted=1 where id=?",false,array($_GET['deletemessageid']));
+	}
+
+	$getcategory = $_GET['category'];
+
+	$start = 0 + (isset($_GET['pagestart']) ? $_GET['pagestart'] : 0);
+	$limit = 100;
+
+	$items = QuickQueryMultiRow("select SQL_CALC_FOUND_ROWS id, messagekey, targetedmessagecategoryid, overridemessagegroupid, enabled from targetedmessage where targetedmessagecategoryid = ? and deleted = 0 order by id limit $start, $limit",true,false,array($getcategory));
+
+	$total = QuickQuery("select FOUND_ROWS()");
+
+	$customtxt = QuickQueryList("select t.id, p.txt from targetedmessage t, message m, messagepart p
+										where t.targetedmessagecategoryid = ? and t.deleted = 0 and
+											t.overridemessagegroupid = m.messagegroupid and
+											m.languagecode = 'en' and
+											p.messageid = m.id and p.sequence = 0",true,false,array($getcategory));
+
+	$numpages = ceil($total/$limit);
+	$curpage = ceil($start/$limit) + 1;
+	$displayend = ($start + $limit) > $total ? $total : ($start + $limit);
+	$displaystart = ($total) ? $start +1 : 0;
+
+	$data->pageinfo = array($numpages,$limit,$curpage, "Showing $displaystart - $displayend of $total records on $numpages pages ");
+	$data->list = array();
+
+	$filename = "messagedata/en/data.php";
+	if(file_exists($filename))
+		include_once($filename);
+
+	while(!empty($items)) {
+		$item = array_shift($items);
+
+		if(isset($item["overridemessagegroupid"]) && isset($customtxt[$item["id"]])) {
+			$title = $customtxt[$item["id"]];
+		} else if(isset($messagedatacache["en"]) && isset($messagedatacache["en"][$item["messagekey"]])) {
+				$title = $messagedatacache["en"][$item["messagekey"]];
+		} else {
+			$title = ""; // Could not find message for this message key.
+		}		
+		$data->list[] = array(
+			"id" => $item["id"],
+			"enabled" => ($item["enabled"]==1),
+			"title" => $title
+		);
+	}
+	echo json_encode($data);
+	exit();
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Display Functions
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Display
+////////////////////////////////////////////////////////////////////////////////
+
+$PAGE = "admin:settings";
+$TITLE = "Classroom Message Manager";
+
+include_once("nav.inc.php");
+
+startWindow('Classroom Messages');
+
+echo icon_button(_L('Create Category'),"add",null,"classroommessagecategory.php?id=new",'style="margin:10px;"');
+
+?>
+<div style="clear:both;"></div>
+
+
+<div id='tabsContainer' style='vertical-align:middle;'></div>
+
+<div id="libraryContent">
+
+<?
+
+foreach($categories as $category) {
+	echo "<div id='lib-" . $category["id"] . "'>
+			" .
+			action_links (
+				action_link("Edit", "pencil", "classroommessagecategory.php?id=" . $category["id"]),
+				action_link("Delete", "cross", null,"deletecategory('". $category["id"] . "');return false;")
+			)
+			. "
+			<h3>Massages</h3>
+			" .
+			icon_button(_L('Create Message'),"add",null,"classroommessageedit.php?id=new")
+			. "
+			<div id='pagewrappertop-" . $category["id"] . "'></div>
+			<div style='clear:both;'></div>
+			<table id='items-" . $category["id"] . "'>
+				<tr><td></td></tr>
+			</table>
+			<div id='pagewrapperbottom-" . $category["id"] . "'></div>
+		</div>";
+}
+?>
+
+</div>
+
+<?
+//echo icon_button(_L('Done'),"tick",null,"settings.php");
+
+endWindow();
+
+
+
+
+?>
+<script type="text/javascript" src="script/accordion.js"></script>
+
+<script type="text/javascript" language="javascript">
+
+var categoryinfo = $H(<?= json_encode($categoriesjson) ?>);
+var tabs;
+
+var activepage = 0;
+
+function updateenabled(input) {
+	new Ajax.Request('classroommessagemanager.php', {
+		method:'get',
+		parameters: {enable: input.checked,id:input.id.substring(7)},
+		onSuccess: function (response) {
+			var result = response.responseJSON;
+			if(result) {
+				input.checked = !input.checked;
+			}
+		}
+	});
+}
+
+function move(current, item) {
+	var value = item.getValue();
+	if(value == "") {
+		var options = '<option value="">-- Move To --</option>';
+		categoryinfo.each(function(category) {
+			if(category.key != current)
+				options += '<option value="' + category.key + '">' + category.value.name + '</option>';
+		});
+		item.update(options);
+	} else {
+		value = {action:"move",messageid:item.id.substring(5),tocategory:value};
+		updatecategory(current,value);
+	}
+}
+
+
+function deletemessage(current, item) {
+	var confirm = confirmDelete();
+	if(confirm){
+		var value = {action:"deletemessage",deletemessageid:item.id.substring(7)};
+		updatecategory(current,value);
+	}
+}
+function deletecategory(id) {
+	var confirm = confirmDelete();
+	if(confirm){
+		new Ajax.Request('classroommessagemanager.php', {
+		method:'get',
+		parameters: {deletecategoryid:id},
+		onSuccess: function (response) {
+			var result = response.responseJSON;
+			if(!result)
+				alert('<?= _L("Unable to delete category, All messages contained within the category must be moved or deleted.") ?>');
+			else
+				window.location = 'classroommessagemanager.php';
+		}
+	});
+	}
+}
+
+function updatecategory(category, actioninfo) {
+
+	var sendvars = {
+		ajax:"true",
+		category:category,
+		pagestart:activepage
+	};
+	if(actioninfo != undefined) {
+		if(actioninfo.action == "move") {
+			sendvars.moveid = actioninfo.messageid;
+			sendvars.movetoid = actioninfo.tocategory;
+		} else if(actioninfo.action == "deletemessage") {
+			sendvars.deletemessageid = actioninfo.deletemessageid;
+		}
+	}
+
+	new Ajax.Request('classroommessagemanager.php', {
+		method:'get',
+		parameters: sendvars,
+		onSuccess: function (response) {
+			var result = response.responseJSON;
+			if(result) {
+				var size = result.list.length;
+
+				var items = new Element('tbody',{width:'100%'});
+
+				var header = new Element('tr').addClassName("listHeader");
+
+				header.insert(new Element('th').update('Enabled'));
+				header.insert(new Element('th',{width:'100%',align:'left'}).update('Message'));
+				header.insert(new Element('th',{align:'left'}).update('Actions'));
+
+
+				items.insert(header);
+
+				for(i=0;i<size;i++){
+					var item = result.list[i];
+					var row = new Element('tr');
+					if(i%2)
+						row.addClassName("listAlt");
+					row.insert(new Element('td',{align:"right"}).update(new Element('input',{id:'enable-' + item.id,type:'checkbox',checked:item.enabled,onclick:'updateenabled(this);return false;'})));
+					row.insert(new Element('td').update(item.title));
+					row.insert(new Element('td',{style:"white-space: nowrap;"}).update(
+					'<a href="classroommessageedit.php?id=' + item.id + '"  class="actionlink" title="Edit" ><img src="img/icons/pencil.gif" alt="Edit">Edit</a>&nbsp;|&nbsp;<a id="delete-' + item.id + '" href="#"  class="actionlink" title="delete" onclick="deletemessage(' + category + ',this); return false;" ><img src="img/icons/cross.gif" alt="delete">Delete</a>&nbsp;|&nbsp;<select id="move-' + item.id + '" onclick="move(' + category + ',this)"/><option value="">-- Move To --</option></select>'
+					));
+					items.insert(row);
+				}
+
+				$('items-' + category).update(items);
+
+				var pagetop = new Element('div',{style: 'float:right;'}).update(result.pageinfo[3]);
+				var pagebottom = new Element('div',{style: 'float:right;'}).update(result.pageinfo[3]);
+				var selecttop = new Element('select', {onchange: 'activepage = this.value;updatecategory(\'' + category + '\');'});
+				var selectbottom = new Element('select', {onchange: 'activepage = this.value;updatecategory(\'' + category + '\');'});
+				for (var x = 0; x < result.pageinfo[0]; x++) {
+					var offset = x * result.pageinfo[1];
+					var psel = result.pageinfo[2] == x+1;
+					selecttop.insert(new Element('option', {value: offset,selected:psel}).update('Page ' + (x+1)));
+					selectbottom.insert(new Element('option', {value: offset,selected:psel}).update('Page ' + (x+1)));
+				}
+				pagetop.insert(selecttop);
+				pagebottom.insert(selectbottom);
+				$('pagewrappertop-' + category).update(pagetop);
+				$('pagewrapperbottom-' + category).update(pagebottom);
+			}
+		}
+	});
+}
+
+document.observe('dom:loaded', function() {
+	// Load tabs
+	tabs = new Tabs('tabsContainer',{});
+	categoryinfo.each(function(category) {
+		var conentid = "lib-" + category.key;
+		category.value.img
+
+		tabs.add_section(conentid);
+		tabs.update_section(conentid, {
+			"title": category.value.name,
+			"icon": category.value.img,
+			"content": $(conentid).remove()
+		});
+	});
+	var first = categoryinfo.keys().first();
+	tabs.show_section('lib-' + first);
+	updatecategory(first);
+
+	tabs.container.observe('Tabs:ClickTitle', function(event) {
+		activepage = 0;
+		updatecategory(event.memo.section.substring(4));
+	});
+
+
+});
+</script>
+
+<?
+
+include_once("navbottom.inc.php");
