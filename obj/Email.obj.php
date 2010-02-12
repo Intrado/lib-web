6f94476e91aa500619eb303fc6b92b0e7ff564d9
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2010-02-12 05:52:43 +0000

    add editlockdate and update() method

--- obj/Email.obj.php
+++ obj/Email.obj.php
@@ -6,12 +6,33 @@ class Email extends DBMappedObject {
 	var $email;
 	var $sequence;
 	var $editlock;
+	var $editlockdate;
 
 	function Email ($id = NULL) {
 		$this->_tablename = "email";
-		$this->_fieldlist = array("personid", "email", "sequence", "editlock");
+		$this->_allownulls = true;
+		$this->_fieldlist = array("personid", "email", "sequence", "editlock","editlockdate");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
+	}
+
+	function update ($specificfields = NULL, $updatechildren = false) {
+		if (isset($this->id)) {
+			$originalObject = new Email($this->id);
+			if (($originalObject->email != $this->email) ||
+				($originalObject->editlock != $this->editlock)) {
+					if ($this->editlock)
+						$this->editlockdate = date("Y-m-d H:i:s", time());
+					else
+						$this->editlockdate = null;
+			}
+		} else {
+					if ($this->editlock)
+						$this->editlockdate = date("Y-m-d H:i:s", time());
+					else
+						$this->editlockdate = null;
+		}
+		DBMappedObject::update($specificfields, $updatechildren);
 	}
 
 }
--- obj/Sms.obj.php
+++ obj/Sms.obj.php
@@ -6,13 +6,33 @@ class Sms extends DBMappedObject{
 	var $sms;
 	var $sequence;
 	var $editlock;
+	var $editlockdate;
 
 	function Sms ($id = NULL) {
 		$this->_tablename = "sms";
-		$this->_fieldlist = array("personid", "sms", "sequence","editlock");
+		$this->_allownulls = true;
+		$this->_fieldlist = array("personid", "sms", "sequence","editlock","editlockdate");
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
 	}
 
+	function update ($specificfields = NULL, $updatechildren = false) {
+		if (isset($this->id)) {
+			$originalObject = new Sms($this->id);
+			if (($originalObject->sms != $this->sms) ||
+				($originalObject->editlock != $this->editlock)) {
+					if ($this->editlock)
+						$this->editlockdate = date("Y-m-d H:i:s", time());
+					else
+						$this->editlockdate = null;
+			}
+		} else {
+					if ($this->editlock)
+						$this->editlockdate = date("Y-m-d H:i:s", time());
+					else
+						$this->editlockdate = null;
+		}
+		DBMappedObject::update($specificfields, $updatechildren);
+	}
 
 }
