b273aff7b72f85f037d3d3eb7ab181f35c3aada1
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-02-12 22:49:36 +0000

    Validator for sections.

--- obj/ValSections.val.php
+++ obj/ValSections.val.php
@@ -3,10 +3,45 @@
 class ValSections extends Validator {
 	var $onlyserverside = true;
 	
-	function validate($value) {
+	function validate($sectionids) {
 		global $USER;
 		
-		// TODO: Validate against $USER->sections()
+		$msgInvalidSections = _L("%s contains unauthorized sections.", $this->label);
+		
+		if (!is_array($sectionids))
+			return $msgInvalidSections;
+		
+		$organizationid = QuickQuery('select organizationid from section where id = ?', false, array(reset($sectionids)));
+		
+		if (!$USER->authorizeOrganization($organizationid))
+			return $msgInvalidSections;
+		
+		// If the user has section associations, only retrieve those sections that also belong to this organization.
+		// Otherwise retrieve all sections from this organization.
+		if (QuickQuery('select count(*) from userassociation where userid = ? and type = "section" limit 1', false, array($USER->id)) > 0) {
+			// Return an array of id=>skey pairs.
+			$authorizedsectionids = QuickQueryList("
+				select section.id
+				from section
+					inner join userassociation
+						on (userassociation.sectionid = section.id)
+				where userid=? and organizationid = ?",
+				false, false, array($USER->id, $organizationid)
+			);
+		} else {
+			// Return an array of id=>skey pairs.
+			$authorizedsectionids = QuickQueryList("
+				select id
+				from section
+				where organizationid = ?",
+				false, false, array($organizationid)
+			);
+		}
+		
+		foreach ($sectionids as $id) {
+			if (!in_array($id, $authorizedsectionids))
+				return $msgInvalidSections;
+		}
 		
 		return true;
 	}
