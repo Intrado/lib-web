dba0109090cb3d9ef72b9b9bf664165982a01b5f
Kee-Yip Chan <kchan@schoolmessenger.com>
2010-02-17 19:51:35 +0000

    Rename a messagegroup instead of a message.

--- messagerename.php
+++ messagerename.php
@@ -4,13 +4,13 @@
 ////////////////////////////////////////////////////////////////////////////////
 include_once("inc/common.inc.php");
 include_once("inc/securityhelper.inc.php");
-include_once("obj/Message.obj.php");
 include_once("inc/table.inc.php");
 include_once("inc/html.inc.php");
 include_once("inc/utils.inc.php");
 include_once("inc/form.inc.php");
 include_once("obj/Message.obj.php");
 include_once("obj/MessagePart.obj.php");
+include_once("obj/MessageGroup.obj.php");
 include_once("obj/AudioFile.obj.php");
 include_once("obj/Voice.obj.php");
 include_once("obj/FieldMap.obj.php");
@@ -26,9 +26,9 @@ if (! ($USER->authorize("sendemail") || $USER->authorize("sendphone") || $USER->
 // Data Handling
 ////////////////////////////////////////////////////////////////////////////////
 
-//get the message to edit from the request params or session
+//get the messagegroup to edit from the request params or session
 if (isset($_GET['id'])) {
-	setCurrentMessage($_GET['id']);
+	setCurrentMessageGroup($_GET['id']);
 	redirect("messagerename.php");
 }
 
@@ -51,26 +51,26 @@ if(CheckFormSubmit($form,$section))
 		MergeSectionFormData($form, $section);
 
 
-		$message = new Message($_SESSION['messageid']);
-		//get the ID of any message with the same name and type
+		$messagegroup = new MessageGroup($_SESSION['messagegroupid']);
+		//get the ID of any messagegroup with the same name.
 		$name = trim(GetFormData($form,$section,"name"));
 		if ( empty($name) ) {
 			PutFormData($form,$section,"name",'',"text",1,50,true);
 		}
 		
-		$existsid = QuickQuery("select id from message where name='" . DBSafe($name) . "' and type='$message->type' and userid='$USER->id' and deleted=0");
+		$existsid = QuickQuery("select id from messagegroup where name=? and userid=? and deleted=0", false, array($name, $USER->id));
 
 		//do check
 		if( CheckFormSection($form, $section) ) {
 			error('There was a problem trying to save your changes', 'Please verify that all required field information has been entered properly');
-		} else if ($existsid && $existsid != $_SESSION['messageid']) {
+		} else if ($existsid && $existsid != $_SESSION['messagegroupid']) {
 			error('A message named \'' . $name . '\' already exists');
 		} else {
 
-			$message->name = $name;
-			$message->description = trim(GetFormData($form,$section,"description"));
+			$messagegroup->name = $name;
+			$messagegroup->description = trim(GetFormData($form,$section,"description"));
 			
-			$message->update();
+			$messagegroup->update();
 			ClearFormData($form);
 			redirect('messages.php');
 		}
@@ -83,13 +83,13 @@ if( $reloadform )
 {
 	ClearFormData($form);
 
-	$message = new Message($_SESSION['messageid']);
+	$messagegroup = new MessageGroup($_SESSION['messagegroupid']);
 
 	$fields = array(
 			array("name","text",0,50,true),
 			array("description","text",0,50)
 			);
-	PopulateForm($form,$section,$message,$fields);
+	PopulateForm($form,$section,$messagegroup,$fields);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
