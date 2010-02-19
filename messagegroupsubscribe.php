e47098b6654b48eacc62a8a1ed8a74ba32700b15
Nickolas Heckman <nheckman@schoolmessenger.com>
2010-02-19 00:17:16 +0000

    messagegroupsubscribe should NOT be a popup! Oops!

--- messagegroupsubscribe.php
+++ messagegroupsubscribe.php
@@ -5,6 +5,7 @@
 require_once("inc/common.inc.php");
 require_once("inc/html.inc.php");
 require_once("inc/table.inc.php");
+require_once("inc/formatters.inc.php");
 require_once("obj/Publish.obj.php");
 require_once("obj/MessageGroup.obj.php");
 require_once("obj/User.obj.php");
@@ -23,16 +24,16 @@ if (!$USER->authorize('subscribemessagegroup')) {
 if (isset($_GET['publishid']) && isset($_GET['subscribe'])) {
 	// check that this is a vaid publish id and the user can subscribe
 	$publish = DBFind("Publish", "from publish where id = ? and action = 'publish' and type = 'messagegroup'", false, array($_GET['publishid']));
-	if ($publish && $USER->authorize('subscribemessagegroup')) {
-		// get the message group for this published item
-		$msgGroup = DBFind("MessageGroup", "from messagegroup where id = ?", false, array($publish->messagegroupid));
+	// see that they don't subscribe more than once
+	$publishSubscription = DBFind("Publish", "from publish where userid = ? and action = 'subscribe' and type = 'messagegroup' and messagegroupid = ?", false, array($_GET['publishid'], $publish->messagegroupid));
+	if ($publish && $USER->authorize('subscribemessagegroup') && !$publishSubscription) {
 		// create a new publish dbmo
-		$publish = new Publish();
-		$publish->userid = $USER->id;
-		$publish->action = 'subscribe';
-		$publish->type = 'messagegroup';
-		$publish->messagegroupid = $msgGroup->id;
-		$publish->create();
+		$subscribe = new Publish();
+		$subscribe->userid = $USER->id;
+		$subscribe->action = 'subscribe';
+		$subscribe->type = 'messagegroup';
+		$subscribe->messagegroupid = $publish->messagegroupid;
+		$subscribe->create();
 	}
 }
 // requests to remove a publishid
@@ -56,29 +57,32 @@ function fmt_msgname ($row, $index) {
 function fmt_actions ($row, $index) {
 	global $subscribed;
 	global $start;
-	$preview = action_link("Preview", "fugue/control", 'messagegroupview.php?id=' . $row['mgid']);
+	$preview = action_link("Preview", "fugue/control", false, "popup('messagegroupviewpopup.php?id=" . $row['mgid'] ."', 700, 500)");
 	if (isset($subscribed[$row['mgid']]))
-		return action_links($preview, action_link("Un-Subscribe", "fugue/star__minus", "messagegroupsubscribepopup.php?publishid=". $subscribed[$row['mgid']] ."&remove&pagestart=$start"));
+		return action_links($preview, action_link("Un-Subscribe", "fugue/star__minus", "messagegroupsubscribe.php?publishid=". $subscribed[$row['mgid']] ."&remove&pagestart=$start"));
 	else
-		return action_links($preview, action_link("Subscribe", "fugue/star__plus", "messagegroupsubscribepopup.php?publishid=". $row[$index] ."&subscribe&pagestart=$start"));
+		return action_links($preview, action_link("Subscribe", "fugue/star__plus", "messagegroupsubscribe.php?publishid=". $row[$index] ."&subscribe&pagestart=$start"));
 }
 
 $titles = array(
 	"mgname" => 'Message Name',
+	"mgdescription" => 'Description',
+	"mgmodified" => 'Modified',
 	"owner" => 'Owner',
 	"id" => 'Action');
 $formatters = array(
 	"mgname" => "fmt_msgname",
+	"mgmodified" => "fmt_date",
 	"id" => "fmt_actions");
 
 $start = 0 + (isset($_GET['pagestart']) ? $_GET['pagestart'] : 0);
-$limit = 10;
+$limit = 100;
 
 $data = array();
 // get all the published messages
 $data = QuickQueryMultiRow(
 	"select SQL_CALC_FOUND_ROWS 
-		p.id as id, mg.id as mgid, mg.name as mgname, u.login as owner
+		p.id as id, mg.id as mgid, mg.name as mgname, mg.description as mgdescription, mg.modified as mgmodified, u.login as owner
 	from publish p
 	join messagegroup mg on
 		(p.messagegroupid = mg.id and not mg.deleted)
@@ -98,12 +102,14 @@ $subscribed = QuickQueryList("select messagegroupid, id from publish where actio
 ////////////////////////////////////////////////////////////////////////////////
 // Display
 ////////////////////////////////////////////////////////////////////////////////
+$PAGE = "notifications:messages";
+$TITLE = "Subscribe";
 
-require_once("popup.inc.php");
+include_once("nav.inc.php");
 
 startWindow(_L("Subscribe to message"));
 ?><div style="padding-top: 5px; padding-bottom: 5px; float:left"><?
-	buttons(icon_button("Done", "fugue/tick", "window.opener.document.location='messages.php';window.close();"));
+	buttons(icon_button("Done", "fugue/tick", "document.location='messages.php';"));
 ?></div><?
 // if there are any messages to subscribe to
 if (count($data)) {
@@ -119,5 +125,5 @@ if (count($data)) {
 }
 endWindow();
 
-require_once("popupbottom.inc.php");
+include_once("navbottom.inc.php");
 ?>
