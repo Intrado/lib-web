13237b96e303d95f56c29d2e1f966971d64fcb9d
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2010-02-25 20:16:23 +0000

    use the ../db/update_SMAdmin_access.sql to create schoolmessenger profile access

--- manager/managerutils.inc.php
+++ manager/managerutils.inc.php
@@ -13,65 +13,28 @@ function genpassword($digits = 15) {
  * create SchooMessenger user and profile
  * (used by both manager newcustomer and the commsuite migration scripts)
  */
-function createSMUserProfile($newdb) {
-				try {
-					$query = "insert into access (name) values ('SchoolMessenger Admin')";
-					QuickUpdate($query, $newdb);
-					$accessid = $newdb->lastInsertId();
-				} catch (PDOException $e) {
-					die("ERROR: ".$e->getMessage());
-				}
+function createSMUserProfile($newdb, $newdbname = "") {
+	try {
+		$query = "insert into access (name) values ('SchoolMessenger Admin')";
+		QuickUpdate($query, $newdb);
+		$accessid = $newdb->lastInsertId();
+	} catch (PDOException $e) {
+		die("ERROR: ".$e->getMessage());
+	}
 
-				$query = "INSERT INTO `permission` (accessid,name,value) VALUES "
-						. "($accessid, 'loginweb', '1'),"
-						. "($accessid, 'manageprofile', '1'),"
-						. "($accessid, 'manageaccount', '1'),"
-						. "($accessid, 'managesystem', '1'),"
-						. "($accessid, 'loginphone', '1'),"
-						. "($accessid, 'startstats', '1'),"
-						. "($accessid, 'startshort', '1'),"
-						. "($accessid, 'starteasy', '1'),"
-						. "($accessid, 'sendprint', '0'),"
-						. "($accessid, 'callmax', '10'),"
-						. "($accessid, 'sendemail', '1'),"
-						. "($accessid, 'sendphone', '1'),"
-						. "($accessid, 'sendsms', '1'),"
-						. "($accessid, 'sendmulti', '1'),"
-						. "($accessid, 'leavemessage', '1'),"
-						. "($accessid, 'survey', '1'),"
-						. "($accessid, 'createlist', '1'),"
-						. "($accessid, 'createrepeat', '1'),"
-						. "($accessid, 'createreport', '1'),"
-						. "($accessid, 'maxjobdays', '7'),"
-						. "($accessid, 'viewsystemreports', '1'),"
-						. "($accessid, 'viewusagestats', '1'),"
-						. "($accessid, 'viewcalldistribution', '1'),"
-						. "($accessid, 'managesystemjobs', '1'),"
-						. "($accessid, 'managemyaccount', '1'),"
-						. "($accessid, 'viewcontacts', '1'),"
-						. "($accessid, 'viewsystemactive', '1'),"
-						. "($accessid, 'viewsystemrepeating', '1'),"
-						. "($accessid, 'viewsystemcompleted', '1'),"
-						. "($accessid, 'listuploadids', '1'),"
-						. "($accessid, 'listuploadcontacts', '1'),"
-						. "($accessid, 'setcallerid', '1'),"
-						. "($accessid, 'blocknumbers', '1'),"
-						. "($accessid, 'callblockingperms', 'editall'),"
-						. "($accessid, 'metadata', '1'),"
-						. "($accessid, 'portalaccess', '1'),"
-						. "($accessid, 'generatebulktokens', '1'),"
-						. "($accessid, 'managetasks', '1'), "
-						. "($accessid, 'managecontactdetailsettings', '1'),"
-						. "($accessid, 'messageconfirmation', '1'),"
-						. "($accessid, 'targetedmessage', '1'),"
-						. "($accessid, 'targetedcomment', '1')";
-				QuickUpdate($query, $newdb);
+	$tablequeries = explode("$$$",file_get_contents("../db/update_SMAdmin_access.sql"));
+	foreach ($tablequeries as $tablequery) {
+		if (trim($tablequery)) {
+			Query($tablequery,$newdb)
+				or dieWithError("Failed to execute statement \n$tablequery\n\nfor $newdbname", $newdb);
+		}
+	}
 
-				$query = "INSERT INTO `user` (`accessid`, `login`,
-							`firstname`, `lastname`, `enabled`, `deleted`) VALUES
-							( '$accessid' , 'schoolmessenger',
-							'School', 'Messenger', 1 ,0)";
-				QuickUpdate($query, $newdb);
+	$query = "INSERT INTO `user` (`accessid`, `login`,
+				`firstname`, `lastname`, `enabled`, `deleted`) VALUES
+				( '$accessid' , 'schoolmessenger',
+				'School', 'Messenger', 1 ,0)";
+	QuickUpdate($query, $newdb);
 }
 
 // create the subscriber application database user
