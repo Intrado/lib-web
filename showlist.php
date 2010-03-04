1b414183c3fd87414b2b1608c87146cb1859329b
Ben Hencke <bhencke@schoolmessenger.com>
2010-03-04 18:29:46 +0000

    rewrite list preview/search code

--- showlist.php
+++ showlist.php
@@ -4,7 +4,23 @@
 ////////////////////////////////////////////////////////////////////////////////
 require_once("inc/common.inc.php");
 include_once("inc/securityhelper.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/utils.inc.php");
+require_once("inc/html.inc.php");
+require_once("inc/date.inc.php");
+require_once("inc/formatters.inc.php");
+require_once("obj/Person.obj.php");
+require_once("obj/FieldMap.obj.php");
+require_once("obj/Rule.obj.php");
 require_once("obj/PeopleList.obj.php");
+require_once("obj/RenderedList.obj.php");
+require_once("obj/Phone.obj.php");
+require_once("obj/Language.obj.php");
+require_once("obj/UserSetting.obj.php");
+
+
+require_once("inc/reportutils.inc.php"); //used by list.inc.php
+require_once("list.inc.php");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Authorization
@@ -13,11 +29,46 @@ if (!$USER->authorize('createlist')) {
 	redirect('unauthorized.php');
 }
 
-$_SESSION['listsearchpreview'] = true;
+////////////////////////////////////////////////////////////////////////////////
+// Action/Request Processing
+////////////////////////////////////////////////////////////////////////////////
 
+//get the message to edit from the request params or session
 if (isset($_GET['id'])) {
 	setCurrentList($_GET['id']);
-	redirect("search.php");
+	$_SESSION['listreferer'] = $_SERVER['HTTP_REFERER'];
+	redirect();
 }
 
+handle_list_checkbox_ajax(); //for handling check/uncheck from the list
+
+
+$list = new PeopleList($_SESSION['listid']);
+
+$renderedlist = new RenderedList2();
+$renderedlist->initWithList($list);
+$renderedlist->pagelimit = 100;
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Display
+////////////////////////////////////////////////////////////////////////////////
+
+$PAGE = "notifications:lists";
+$TITLE = 'List Preview: ' . escapehtml(QuickQuery("select name from list where id = $_SESSION[listid]"));
+
+include_once("nav.inc.php");
+
+buttons(button('Refresh', '', $_SERVER["REQUEST_URI"] ), button("Done","",$_SESSION['listreferer']));
+
+startWindow("Preview");
+
+showRenderedListTable($renderedlist, $list);
+
+endWindow();
+
+buttons(button('Refresh', '', $_SERVER["REQUEST_URI"] ), button("Done","","list.php"));
+
+include_once("navbottom.inc.php");
 ?>
