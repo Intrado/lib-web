1cde09b5b21ff3379c4bd875c94e5ae5db0595a6
Ben Hencke <bhencke@schoolmessenger.com>
2010-03-04 18:18:58 +0000

    refactor for new renderedlist stuff, and optimize logic so that it doesn't calculate more lists than it needs to.

--- obj/ValLists.val.php
+++ obj/ValLists.val.php
@@ -1,39 +1,43 @@
 <?
 
-
 class ValLists extends Validator {
 	var $onlyserverside = true;
 
 	function validate ($value, $args) {
 		global $USER;
+
 		if (strpos($value, 'pending') !== false)
 			return _L('Please finish adding this rule, or unselect the field');
 
 		$listids = json_decode($value);
-		if (empty($listids))
+		if ($listids == null || count($listids) == 0)
 			return _L("Please add a list");
-
-		if(isset($args['allowempty']) && $args['allowempty'] == true) {
-			return true;
-		}
-
-		$allempty = true;
+			
+		//check ownership of each list
 		foreach ($listids as $listid) {
-			if ($listid === 'addme') {
-				$allempty = false;
+			if ($listid === 'addme')
 				continue;
-			}
 			if (!userOwns('list', $listid))
 				return _L('You have specified an invalid list');
-			$list = new PeopleList($listid + 0);
-			$renderedlist = new RenderedList($list);
-			$renderedlist->calcStats();
-			if ($renderedlist->total >= 1)
-				$allempty = false;
 		}
-		if ($allempty)
-			return _L('All of the selected lists are empty');
-		return true;
+		
+		if (isset($args['skipemptycheck']) && $args['skipemptycheck'])
+			return true;
+		
+		//check to see if they are all empty
+		foreach ($listids as $listid) {
+			if ($listid === 'addme') {
+				return true; //stop looking as soon as we find any non empty list
+			}
+		
+			$list = new PeopleList($listid);
+			$renderedlist = new RenderedList2($list);
+			$renderedlist->initWithList($list);
+			$renderedlist->pagelimit = 0; //save a bit of memory by not trying to get anyone, just calc totals
+			if ($renderedlist->getTotal() > 0)
+				return true; //stop looking as soon as we find any non empty list
+		}
+		return _L('All of the lists are empty');
 	}
 }
 
