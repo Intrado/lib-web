44b2c6f1e1f6b8218694792f33ffb20257e23584
Ben Hencke <bhencke@schoolmessenger.com>
2010-03-04 18:27:25 +0000

    rewrite list preview/search code, in list checkboxes, formatters, etc. work in progress, several functions still need to be replaced.

--- list.inc.php
+++ list.inc.php
@@ -1,5 +1,175 @@
 <?
 
+
+/**
+ * Displays a table of the rederedlist data and field togglers.
+ * May also display list checkbox if list is specified.
+ * @param $renderedlist
+ * @param $list
+ * @return unknown_type
+ */
+function showRenderedListTable($renderedlist, $list = false) {
+	global $PAGEINLISTMAP;
+	static $tableidcounter = 1;
+	
+	$pagestart = (isset($_GET['pagestart']) ? $_GET['pagestart'] + 0 : 0);
+	$renderedlist->pageoffset = $pagestart;
+	$data = $renderedlist->getPageData();
+	$total = $renderedlist->getTotal();
+	
+	if ($list)
+		$PAGEINLISTMAP = $renderedlist->getPageInListMap($list);
+	
+	$titles = array();
+	$formatters = array();
+	$repeatedcolumns = array(2,3); //sequence, destination
+	$groupby = 0; //personid
+	
+	//set up formatters and titles for the basic fields.
+	if ($list) {
+		$titles[0] = "In List";
+		$formatters[0] = "fmt_checkbox";	
+	}
+	
+	$titles[1] = "Unique ID";
+	$formatters[1] = "fmt_persontip";
+	
+	$titles[6] = FieldMap::getName(FieldMap::getFirstNameField());
+	$titles[7] = FieldMap::getName(FieldMap::getLastNameField());
+
+	$titles[2] = "Sequence";
+	$formatters[2] = "fmt_list_destination_sequence";
+	
+	$titles[3] = "Destination";
+	$formatters[3] = "fmt_destination";
+	
+	$titles[5] = "Organization";
+	
+	//after that, show F fields, then G fields
+	//optional F fields start at index 8 (skip f01, f02)
+	//save some data for field show/hide tool
+	
+	//show field togglers, reuse whats in reportutils.inc.php (needs to be refactored)
+	//FIXME UGLY HACK: need to set global session var to control behavior of this function
+	//FIXME UGLY HACK: this function also has the side effect of loading $_SESSION['report']['fields'] display prefs
+	$_SESSION['saved_report'] = false; //this causes checkbox states to be loaded/saved in userprefs
+	
+	$tableid = "renderedlist". $tableidcounter++;
+	$optionalfields = array_merge(FieldMap::getOptionalAuthorizedFieldMapsLike('f'), FieldMap::getAuthorizedFieldMapsLike('g'));
+	$optionalfieldstart = $list ? 6 : 5; //table col of last non optional field
+	select_metadata($tableid,$optionalfieldstart,$optionalfields);
+	
+	//now use session display prefs to set up titles and whatnot for the optional fields
+	$i = 8;
+	foreach ($optionalfields as $field) {
+		//add a formatter for language field
+		if ($field->fieldnum == FieldMap::getLanguageField())
+			$formatters[$i] = "fmt_languagecode";
+		
+		if (isset($_SESSION['report']['fields'][$field->fieldnum]) && $_SESSION['report']['fields'][$field->fieldnum])
+			$titles[$i++] = $field->name;
+		else
+			$titles[$i++] = "@" . $field->name;
+	}
+
+	showPageMenu($total,$pagestart,$renderedlist->pagelimit);
+	echo '<table id="'.$tableid.'" width="100%" cellpadding="3" cellspacing="1" class="list">';
+	showTable($data, $titles, $formatters, $repeatedcolumns, $groupby);
+	echo "\n</table>";
+	showPageMenu($total,$pagestart,$renderedlist->pagelimit);
+}
+
+/**
+ * Reads $_GET['addpersonid'] or isset($_GET['removepersonid'], and handles them accordingly to the current list.
+ * Will exit early when handling an ajax request.
+ * @return unknown_type
+ */
+function handle_list_checkbox_ajax () {
+	global $USER;
+	
+	if (!isset($_GET['ajax']))
+		return;
+	
+	if (isset($_GET['addpersonid'])) {
+		error_log("addpersonid");
+		
+		$id = $_GET['addpersonid'];
+		$existingtype = QuickQuery("select type from listentry where personid=? and listid=?", false, array($id, $_SESSION['listid']));
+		if ($existingtype == "negate") {
+			//must be a skip, so delete the skip entry
+			QuickUpdate("delete from listentry where personid=? and listid=?",false,array($id, $_SESSION['listid']));
+		} else if ($existingtype == null) {
+			//see if user can see this person
+			if ($USER->canSeePerson($id)) {
+				QuickUpdate("insert into listentry (listid, type, personid) values (?,'add',?)",false,array($_SESSION['listid'], $id));
+			}
+		} else {
+			header('Content-Type: application/json');
+			exit(json_encode(false));
+		}
+		header('Content-Type: application/json');
+		exit(json_encode(true));
+	}
+	
+	if (isset($_GET['removepersonid'])) {
+		error_log("removepersonid");
+		$id = $_GET['removepersonid'];
+		$existingtype = QuickQuery("select type from listentry where personid=? and listid=?", false, array($id, $_SESSION['listid']));
+		if ($existingtype == "add") {
+			//must be an add, so delete the add entry
+			QuickUpdate("delete from listentry where personid=? and listid=?",false,array($id, $_SESSION['listid']));
+		} else if ($existingtype == null) {
+			QuickUpdate("insert into listentry (listid, type, personid) values (?,'negate',?)",false,array($_SESSION['listid'], $id));
+		} else {
+			header('Content-Type: application/json');
+			exit(json_encode(false));
+		}
+		
+		header('Content-Type: application/json');
+		exit(json_encode(true));
+	}
+
+}
+
+
+function fmt_persontip ($row, $index) {
+	global $USER;
+
+	$pkey = escapehtml($row[1]);
+	$personid = $row[0];
+	
+	return "<a href=\"viewcontact.php?id=$personid\" class=\"actionlink\">" 
+		. "<img src=\"img/icons/diagona/16/049.gif\" /> $pkey</a>";
+}
+
+
+function fmt_list_destination_sequence($row, $index){
+	// destination type is +2 to the sequence index
+	$typeindex = $index+2;
+	if($row[$typeindex] != "" || $row[$typeindex] != false){
+		return destination_label($row[$typeindex], $row[$index]);
+	} else {
+		return "";
+	}
+}
+
+
+function fmt_checkbox($row, $index) {
+	global $PAGEINLISTMAP;
+	
+	$personid = $row[$index];
+
+	$checked = '';
+	if (isset($PAGEINLISTMAP[$personid]))
+		$checked = 'checked';
+	
+	$onclick = "do_ajax_listbox(this, $personid);";
+	return "<input type=\"checkbox\" onclick=\"$onclick\" $checked />";
+}
+
+
+/************************ PAST THIS POINT THERE BE DRAGONS ************************************/
+
 function list_clear_search_session($keep = false) {
 	if ($keep != 'listsearchshowall')
 		$_SESSION['listsearchshowall'] = false;
@@ -58,14 +228,7 @@ function list_remove_person($personid) {
 	}
 }
 
-function fmt_list_destination_sequence($row, $index){
-	// index 8 is the type of destination: phone, email, or sms.
-	if($row[8] != "" || $row[8] != false){
-		return destination_label($row[8], $row[$index]);
-	} else {
-		return "";
-	}
-}
+
 
 function list_handle_ajax_table($renderedlist, $validContainers) {
 	global $USER;
@@ -100,26 +263,26 @@ function list_prepare_ajax_table($containerID, $renderedlist) {
 
 	switch ($containerID) {
 		case 'listAdditionsContainer':
-			$renderedlist->prepareAdditionsMode(100);
+			$renderedlist->prepareAdditionsMode();
 			break;
 		case 'listSkipsContainer':
-			$renderedlist->prepareSkipsMode(100);
+			$renderedlist->prepareSkipsMode();
 			break;
 		case 'listPreviewContainer':
 		case 'listSearchContainer':
 			if (!empty($_SESSION['listsearchshowall'])) {
-				$renderedlist->prepareRulesMode(100, false);
+				$renderedlist->prepareRulesMode(false);
 			} else if (!empty($_SESSION['listsearchperson'])) {
-				$renderedlist->preparePeopleMode(100, $_SESSION['listsearchpkey'], $_SESSION['listsearchphone'], $_SESSION['listsearchemail']);
+				$renderedlist->preparePeopleMode($_SESSION['listsearchpkey'], $_SESSION['listsearchphone'], $_SESSION['listsearchemail']);
 			} else if (!empty($_SESSION['listsearchrules'])) {
 				$rules = $_SESSION['listsearchrules'];
 				$organizationids = isset($rules['organization']) ? $rules['organization']['val'] : false;
 				unset($rules['organization']);
 				
-				$renderedlist->prepareRulesMode(100, $rules, $organizationids);
+				$renderedlist->prepareRulesMode($rules, $organizationids);
 			} else {
 				if ($containerID == 'listPreviewContainer')
-					$renderedlist->prepareRulesMode(100, false);
+					$renderedlist->prepareRulesMode(false);
 				else
 					return _L('No search criteria');
 			}
@@ -203,7 +366,8 @@ function list_prepare_ajax_table($containerID, $renderedlist) {
 			return _L('Nobody in your list!');
 	}
 
-	return ajax_table_show_menu($containerID, $renderedlist->total, $renderedlist->pageoffset, $renderedlist->pagelimit) . ajax_show_table($containerID, $data, $titles, $formatters, $sorting, isset($repeatedColumns) ? $repeatedColumns : false, isset($groupBy) ? $groupBy : false, ($searchable ? 3 : 0), ($searchable ? true : false), ($searchable ? false : true));
+	return ajax_table_show_menu($containerID, $renderedlist->total, $renderedlist->pageoffset, $renderedlist->pagelimit) 
+		. ajax_show_table($containerID, $data, $titles, $formatters, $sorting, isset($repeatedColumns) ? $repeatedColumns : false, isset($groupBy) ? $groupBy : false, ($searchable ? 3 : 0), ($searchable ? true : false), ($searchable ? false : true));
 }
 
 function list_get_results_html($containerID, $renderedlist) {
