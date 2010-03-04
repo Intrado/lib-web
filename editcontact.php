d247da4853b81b9b6460dfc1cd79f2a8b612dcb6
Ben Hencke <bhencke@schoolmessenger.com>
2010-03-04 18:24:48 +0000

    include_once -> require_once

--- editcontact.php
+++ editcontact.php
@@ -3,21 +3,21 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Includes
 ////////////////////////////////////////////////////////////////////////////////
-include_once("inc/common.inc.php");
-include_once("inc/utils.inc.php");
-include_once("inc/date.inc.php");
-include_once("inc/securityhelper.inc.php");
-include_once("inc/form.inc.php");
-include_once("inc/html.inc.php");
-include_once("inc/table.inc.php");
-include_once("obj/FieldMap.obj.php");
-include_once("obj/Person.obj.php");
-include_once("obj/Address.obj.php");
-include_once("obj/Phone.obj.php");
-include_once("obj/Email.obj.php");
-include_once("obj/Language.obj.php");
-include_once("obj/JobType.obj.php");
-include_once("obj/Sms.obj.php");
+require_once("inc/common.inc.php");
+require_once("inc/utils.inc.php");
+require_once("inc/date.inc.php");
+require_once("inc/securityhelper.inc.php");
+require_once("inc/form.inc.php");
+require_once("inc/html.inc.php");
+require_once("inc/table.inc.php");
+require_once("obj/FieldMap.obj.php");
+require_once("obj/Person.obj.php");
+require_once("obj/Address.obj.php");
+require_once("obj/Phone.obj.php");
+require_once("obj/Email.obj.php");
+require_once("obj/Language.obj.php");
+require_once("obj/JobType.obj.php");
+require_once("obj/Sms.obj.php");
 
 ////////////////////////////////////////////////////////////////////////////////
 // Authorization
@@ -31,5 +31,5 @@ if(isset($_SERVER['HTTP_REFERER']) && strpos($_SERVER['HTTP_REFERER'],"viewconta
 	$_SESSION['contact_referer'] = $_SERVER['HTTP_REFERER'];
 }
 $method = "edit";
-include("contactdetails.inc.php");
+require_once("contactdetails.inc.php");
 ?>
