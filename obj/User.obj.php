fd64f34056d56ff791fc343bd6fe207bcf3ff01c
Ben Hencke <bhencke@schoolmessenger.com>
2010-03-04 18:33:56 +0000

    replace old user sql with new functions. one returns a join for organization/section ids, based on search criteria (used for lists and contact search). getrulesql merges in search rules and gives extra where clause bits, canseeperson convienience function for places where code used to check individuals with the rule sql.

--- obj/User.obj.php
+++ obj/User.obj.php
@@ -108,11 +108,158 @@ class User extends DBMappedObject {
 		
 		return $USERSORGANIZATIONS[$this->id];
 	}
+	
+	
+	/**
+	 * Takes a list of organizationids or sections, and returns a SQL bit suitable for adding in to a query on the person table as an optional join.
+	 * Filters out any sections or organizations the user doesn't have access to, making join as simple as possible.
+	 * $searchorgids and $searchsectionids are mutually exclusive, only one should be specified.
+	 * The joinsql may cause an impossible query if all of the criteria are invalid.
+	 * Calling code should verify that at least one restriction exists by checking that rules, orgs, sections are not all empty.
+	 * Note that of course adding a join to person may result in duplicate records returned due to nature of joins.
+	 * 
+	 * @param $searchorgids list of organization ids to search/filter on.
+	 * @param $searchsectionids list of sectionids to search/filter on. 
+	 * @param $personalias alias of the person table, false if not aliased
+	 * @return string containing a join clause to the person table on personassociation
+	 */
+	function getPersonAssociationJoinSql ($searchorgids = array(), $searchsectionids = array(), $personalias = false) {
+		
+		//put orgids in indexed array
+		if (count($searchorgids) > 0) {
+			$searchorgids = array_fill_keys($searchorgids,true);
+		}
+		
+		//load user restriction info on orgs, sections	
+		$userorgids = QuickQueryList("select organizationid,1 from userassociation where type='organization' and userid = ?",true,false,array($this->id));
+		$usersectionorgs = QuickQueryList("select ua.sectionid, s.organizationid from userassociation ua inner join section s on (s.id=ua.sectionid) where ua.userid = ?",true,false,array($this->id));
 
-	function userSQL ($alias = false) {
-		$r = Rule::makeQuery($this->getRules(), $alias);
-		return $r;
+		$isunrestricted = count($userorgids) == 0 && count($usersectionorgs) == 0;
+		$aliasid = ($personalias ? $personalias . ".id" : "person.id");
+
+		$joinsql = "";
+		
+		// ############## Section Mode ##############
+		if (count($searchsectionids) > 0) {
+			
+			//first load org info for all searcsectionids
+			$tmp = QuickQueryList("select id, organizationid from section where id in (" . DBParamListString(count($searchsectionids)) . ")" ,true,false,$searchsectionids);
+			//create new array with sectionids as keys, all pointing to zeros. then fill in any found orgids. sections not found still point to zero.
+			$filtersectionids = array_fill_keys($searchsectionids,0); //any unfound sections will have orgid=0
+			foreach ($tmp as $sectionid => $orgid)
+				$filtersectionids[$sectionid] = $orgid;
+			
+			//check all specified sections against user restrictions
+			if (!$isunrestricted) {
+				foreach ($filtersectionids as $sectionid => $orgid) {
+					
+					//see if the sectionid matches one of the user's sections
+					if (isset($usersectionorgs[$sectionid]))
+						continue;
+					//see if this section is a child of any of the user orgs
+					if (isset($userorgids[$orgid]))
+						continue;
+					unset($filtersectionids[$sectionid]);
+				}
+			}
+			if (count($filtersectionids) == 0)
+				$joinsql = "inner join personassociation pa on (0) /* bad sections */ "; //impossible query, we must have removed everything they were looking for!
+			else
+				$joinsql = "inner join personassociation pa on (pa.personid = $aliasid "
+						. "and pa.sectionid in (" . implode(",",array_keys($filtersectionids)) . "))";
+		
+		// ############## Organization Mode ##############
+		} else if (count($searchorgids) > 0) {
+			
+			$joinsql = "inner join personassociation pa on (pa.personid = $aliasid";
+			
+			$joinorgids = array();
+			$joinsectionids = array();
+			
+			//if user is unrestricted, just add all orgs
+			if ($isunrestricted) {
+				$joinorgids = array_keys($searchorgids);
+			} else {	
+				//if user is restricted to some orgs, add all matching orgs
+				foreach ($searchorgids as $orgid => $dummy) {
+					if (isset($userorgids[$orgid])) {
+						//add to joinorgids and remove from search (so we can find leftovers as sections)
+						$joinorgids[] = $orgid;
+						unset($searchorgids[$orgid]);
+					}
+				}
+				//downgrade any remaining search orgs to sections that match user restrictions
+				foreach ($usersectionorgs as $sectionid => $orgid) {
+					if (isset($searchorgids[$orgid]))
+						$joinsectionids[] = $sectionid;
+					
+				}
+			}
+			
+			$components = array();
+			if (count($joinorgids) > 0)
+				$components[] = "pa.organizationid in (" . implode(",",$joinorgids) . ")";	
+			if (count($joinsectionids) > 0)
+				$components[] = "pa.sectionid in (" . implode(",",$joinsectionids) . ")";	
+			
+			if (count($components) == 0)
+				$joinsql .= " and 0 "; //impossible query, nothing valid to search on
+			else
+				$joinsql .= " and ( " . implode(" or ", $components) . " ) ";
+			
+			$joinsql .= ")";
+			
+		// ############## All Mode ##############
+		} else {
+			//list has no org/section search, join on just user restrictions
+			if ($isunrestricted) {
+				$joinsql = ""; //no join needed if unrestricted user on unrestricted list (note: something else needs to make sure they at least have a rule somewhere)
+			} else {
+				$joinsql = "inner join personassociation pa on (pa.personid = $aliasid";
+				
+				$components = array();
+				if (count($userorgids) > 0)
+					$components[] = "pa.organizationid in (" . implode(",",array_keys($userorgids)) . ")";	
+				if (count($usersectionorgs) > 0)
+					$components[] = "pa.sectionid in (" . implode(",",array_keys($usersectionorgs)) . ")";	
+				
+				//$components can't be empty if user is unrestricted, dont need to check again
+				$joinsql .= " and ( " . implode(" or ", $components) . " ) )";
+			}
+		}
+		
+		return $joinsql;
 	}
+	
+	/**
+	 * Returns sql for rules specified combined with rules from user restriction.
+	 * Calling code should verify that at least one restriction exists by checking that rules, orgs, sections are not all empty.
+	 * 
+	 * @param $personalias alias of the person table, false if not aliased
+	 * @param $searchrules array of Rule objects to add to the user's rules (ie for lists, etc)
+	 * @param $isreport true if query should be using report tables for historic data
+	 * @return unknown_type
+	 */
+	function getRuleSql ($searchrules = array(), $personalias = false, $isreport = false) {
+		return Rule::makeQuery(array_merge($this->getRules(), $searchrules), $personalias, false, $isreport);
+	}
+	
+	
+	/**
+	 * Checks to see if the specified personid is visible to the user. Checks addressbook, orgs, and rules.
+	 * @param $personid
+	 * @return unknown_type
+	 */
+	function canSeePerson ($personid) {
+		$joinsql = $this->getPersonAssociationJoinSql(array(), array(), "p");
+		$rulesql = $this->getRuleSql(array(), "p");
+		$query = "select 1 from person p \n"
+				."	$joinsql \n"
+				."	where p.id=? and (p.userid=0 OR p.userid=? or (1 $rulesql))  \n";
+		
+		return QuickQuery($query,false,array($personid,$this->id));
+	}
+	
 
 	//see if the login is used
 	function checkDuplicateLogin ($newlogin, $id) {
