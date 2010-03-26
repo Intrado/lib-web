39c40077f262b6f48f1fb434ef01e045c03c0c7b
Nickolas Heckman <nheckman@schoolmessenger.com>
2010-03-26 17:29:00 +0000

    update validator, don't bother with partial rule validation.

--- obj/ValRules.val.php
+++ obj/ValRules.val.php
@@ -4,11 +4,7 @@ class ValRules extends Validator {
 	var $onlyserverside = true;
 	
 	function validate ($valueJSON, $args) {
-		$msgPleaseFinish = addslashes(_L("Please finish adding your rule"));
 		$msgRuleAlreadyExists = addslashes(_L("Rule already exists"));
-		
-		if ($valueJSON == 'pending')
-			return $msgPleaseFinish;
 
 		$ruledata = json_decode($valueJSON);
 		
@@ -16,17 +12,14 @@ class ValRules extends Validator {
 			return true;
 		
 		if (is_array($ruledata)) {
-			$rulesfor = array();
 			foreach ($ruledata as $data) {
 				if (isset($data->fieldnum) && isset($rulesfor[$data->fieldnum])) { // Do not allow more than one rule per fieldnum
 					return $msgRuleAlreadyExists;
 				} else {
-					$error = $this->validateOneRule($data, $args);
-					if ($error !== true)
-						return $error;
+					$validation = $this->validateOneRule($data, $args);
+					if ($validation !== true)
+						return $validation;
 				}
-				
-				$rulesfor[$data->fieldnum] = true;
 			}
 		} else {
 			return $this->validateOneRule($ruledata, $args);
@@ -38,22 +31,32 @@ class ValRules extends Validator {
 	function validateOneRule($data, $args) {
 		global $USER;
 		
+		// get the allowed field types if they are set
+		$allowedFieldTypes = isset($args['allowedFieldTypes'])?$args['allowedFieldTypes']:array();
+		
+		// get the current field's type. F, G and C fields just use the first character of the fieldnum
+		$fieldtype = ($data->fieldnum == 'organization')?'organization':$data->fieldnum[0];
+		
 		$msgIncompleteRule = addslashes(_L("Incomplete rule data"));
 		$msgUnauthorizedFieldmap = addslashes(_L("Unauthorized fieldmap"));
 		$msgUnauthorizedOrganization = addslashes(_L("Unauthorized organization"));
 		
+		// if any of the rule data isn't set
 		if (!isset($data->fieldnum, $data->logical, $data->op, $data->val)) {
 			return $msgIncompleteRule;
+		// if the field is disallowed due to allowed field types specified and it not being present
+		} else if ($allowedFieldTypes && !in_array($fieldtype, $allowedFieldTypes)) {
+			return $msgUnauthorizedFieldmap;
+		// if the requested field is an organization, be sure the user has access to the specified organizations
 		} else if ($data->fieldnum == 'organization') {
 		
 			$validorgkeys = Organization::getAuthorizedOrgKeys();
 			
 			foreach ($data->val as $id) {
-				if (!ctype_digit($id) || !isset($validorgkeys[$id]))
+				if (!isset($validorgkeys[$id]))
 					return $msgUnauthorizedOrganization;
 			}
-		} else if (isset($args['allowedFieldTypes']) && !in_array($data->fieldnum[0], $args['allowedFieldTypes'])) {
-			return $msgUnauthorizedFieldmap;
+		// finally, for any non organization fields, check that it is a valid rule by initing it
 		} else if (!Rule::initFrom($data->fieldnum, $data->logical, $data->op, $data->val)) {
 			return $msgUnauthorizedFieldmap;
 		}
