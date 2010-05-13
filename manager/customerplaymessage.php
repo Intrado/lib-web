e18aff5bd292f5245a802f5ab8756b1eee6aeff3
Martin Arnberg <marnberg@schoolmessenger.com>
2010-05-13 23:51:40 +0000

    simplify the method of getting messagid

--- manager/customerplaymessage.php
+++ manager/customerplaymessage.php
@@ -30,22 +30,14 @@ $_dbcon = DBConnect($custinfo[0], $custinfo[1], $custinfo[2], "c_" . $_SESSION['
 if (!$_dbcon) {
 	exit("Connection failed for customer: $custinfo[0], db: c_" .  $_SESSION['previewmessage_customerid']);
 }
-$messages = QuickQueryList("select m.id, m.autotranslate, m.* from job j inner join messagegroup mg on (j.messagegroupid = mg.id)
-							inner join message m on (mg.id = m.messagegroupid and mg.defaultlanguagecode = m.languagecode) where j.id =? and m.type = 'phone'", true, false, array($_SESSION['previewmessage_jobid'] ));
-$messageid = false;
-if ($messages) {
-	$mode = false;
-	// Select the right message out of the possible messages in the messagegroup
-	// overridden ---> translated ---> none
-	foreach ($messages as $id => $autotranslate) {
-		if ($autotranslate == "overridden" || 
-			($autotranslate == "translated" && $mode != "overridden") ||
-			($autotranslate == "none" && !$mode)) {
-				$mode = $autotranslate;
-				$messageid = $id;
-		}
-	}
-}
+
+$query = "select m.id from job j 
+inner join messagegroup mg on (j.messagegroupid = mg.id)
+inner join message m on (mg.id = m.messagegroupid and mg.defaultlanguagecode = m.languagecode and m.type = 'phone' and m.autotranslate != 'source')
+where j.id =?";
+
+$messageid = QuickQuery($query, false, array($_SESSION['previewmessage_jobid']));
+
 
 //find all unique fields used in this message
 $messagefields = array();
