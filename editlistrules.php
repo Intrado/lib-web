c18e87288d738f0a9b7b982f39aa7e8726024b05
Martin Arnberg <marnberg@schoolmessenger.com>
2010-06-04 18:56:48 +0000

    Need include files in inc files. This is needed to fix bug 3468

--- editlistrules.php
+++ editlistrules.php
@@ -1,48 +1,4 @@
 <?
-////////////////////////////////////////////////////////////////////////////////
-// Includes
-////////////////////////////////////////////////////////////////////////////////
-require_once("inc/common.inc.php");
-require_once("inc/securityhelper.inc.php");
-require_once("inc/table.inc.php");
-require_once("inc/html.inc.php");
-require_once("inc/form.inc.php");
-require_once("inc/date.inc.php");
-require_once("obj/Language.obj.php");
-require_once("obj/Person.obj.php");
-require_once("obj/Address.obj.php");
-require_once("obj/Phone.obj.php");
-require_once("obj/Email.obj.php");
-require_once("obj/Sms.obj.php");
-require_once("obj/PeopleList.obj.php");
-require_once("obj/AudioFile.obj.php");
-require_once("obj/FieldMap.obj.php");
-require_once("obj/Rule.obj.php");
-require_once("obj/ListEntry.obj.php");
-require_once("obj/RenderedList.obj.php");
-require_once("ruleeditform.inc.php");
-require_once("inc/rulesutils.inc.php");
-require_once("obj/Validator.obj.php");
-require_once("obj/ValSections.val.php");
-require_once("obj/ValRules.val.php");
-require_once("obj/Form.obj.php");
-require_once("obj/FormItem.obj.php");
-require_once("obj/FormRuleWidget.fi.php");
-require_once("obj/SectionWidget.fi.php");
-require_once("inc/rulesutils.inc.php");
-require_once("inc/formatters.inc.php");
-require_once("obj/JobType.obj.php");
-require_once("inc/list.inc.php");
-
-////////////////////////////////////////////////////////////////////////////////
-// Authorization
-////////////////////////////////////////////////////////////////////////////////
-
-// Do Authentication in include file
-
-////////////////////////////////////////////////////////////////////////////////
-// Action/Request Processing
-////////////////////////////////////////////////////////////////////////////////
 
 $_SESSION['previewfrom'] = 'editlistrules.php';
 
--- jobrepeating.php
+++ jobrepeating.php
@@ -1,36 +1,7 @@
 <?
-////////////////////////////////////////////////////////////////////////////////
-// Includes
-////////////////////////////////////////////////////////////////////////////////
-
-require_once("inc/common.inc.php");
-require_once("inc/securityhelper.inc.php");
-require_once("inc/table.inc.php");
-require_once("inc/html.inc.php");
-require_once("inc/utils.inc.php");
-require_once("inc/date.inc.php");
-require_once("obj/Validator.obj.php");
-require_once("obj/Form.obj.php");
-require_once("obj/FormItem.obj.php");
-require_once("obj/Job.obj.php");
-require_once("obj/JobType.obj.php");
-require_once("obj/Phone.obj.php"); // Required by job
-require_once("obj/PeopleList.obj.php");
-require_once("obj/Person.obj.php");
-require_once("obj/RenderedList.obj.php");
-require_once("obj/FieldMap.obj.php");
-include_once("obj/Schedule.obj.php");
-require_once("obj/ValDuplicateNameCheck.val.php");
-require_once("obj/MessageGroupSelectMenu.fi.php");
-require_once("obj/WeekRepeat.fi.php");
-require_once("obj/WeekRepeat.val.php");
-require_once("obj/ValLists.val.php");
-require_once("obj/ValTimeWindowCallEarly.val.php");
-require_once("obj/ValTimeWindowCallLate.val.php");
-require_once("obj/ValNonEmptyMessage.val.php");
-require_once("obj/FormListSelect.fi.php");
 
 $JOBTYPE = "repeating";
 
 include("jobedit.inc.php");
 
+?>
