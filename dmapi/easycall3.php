da7348fcf77aa5bfbd83b8521b1e90c8730b2b43
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2010-06-04 00:13:49 +0000

    support for cisco phone multi-lingual record

--- dmapi/easycall3.php
+++ dmapi/easycall3.php
@@ -4,6 +4,7 @@ require_once("../inc/date.inc.php");
 include_once("../obj/Job.obj.php");
 include_once("../obj/User.obj.php");
 include_once("../obj/SpecialTask.obj.php");
+include_once("../obj/MessageGroup.obj.php");
 include_once("../obj/Message.obj.php");
 include_once("../obj/MessagePart.obj.php");
 include_once("../obj/AudioFile.obj.php");
@@ -23,51 +24,25 @@ if($REQUEST_TYPE == "new"){
 
 		$USER = new User($specialtask->getData('userid'));
 		$ACCESS = $_SESSION['access'] = new Access($USER->accessid);
-		$messages = array();
-		$languages = array();
-		$count = $specialtask->getData("count");
-		for($i = 0; $i < $count; $i++){
-			$messnum = "message" . $i;
-			$messages[$i] = $specialtask->getData($messnum);
-			$langnum = "language" . $i;
-			$languages[$i] = $specialtask->getData($langnum);
-		}
-
-		$job = Job::jobWithDefaults();
-		//get the job name, type, and messageid
 
 		$name = $specialtask->getData('name');
-
 		if (!$name)
 			$name = "EasyCall - " . date("M j, Y g:i a");
+
+		$job = Job::jobWithDefaults();
+
 		$job->name = $name;
 		$job->description = "EasyCall - " . date("M j, Y g:i a");
 		$type = $specialtask->getData('jobtypeid');
 		$job->jobtypeid = $type;
-		$job->type = "phone";
+		$job->type = "notification";
+		$job->messagegroupid = $_SESSION['messagegroupid'];
 		$numdays = $specialtask->getData('jobdays');
 		$job->enddate = date("Y-m-d", strtotime($job->startdate) + (($numdays - 1) * 86400));
 		$job->setOptionValue("retry",$specialtask->getData('jobretries'));
-
-		if($messages) {
-
-			foreach($messages as $index => $message){
-				if($languages[$index] == "Default"){
-					$job->phonemessageid = $message;
-					$job->create();
-				} else {
-					$joblang = new JobLanguage();
-					$joblang->type = "phone";
-					$joblang->language = $languages[$index];
-					$joblang->messageid = $message;
-					$joblang->jobid = $job->id;
-					if ($joblang->language && $joblang->messageid) {
-						$joblang->create();
-					}
-				}
-			}
-		}
-		if($job->id){
+		$job->create();
+		
+		if ($job->id) {
 			QuickUpdate("insert into joblist (jobid, listid) values (?,?)", false, array($job->id, $specialtask->getData('listid')));
 			$specialtask->setData('jobid', $job->id);
 			chdir("../");
