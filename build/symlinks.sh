e5328eaf86ef6a5de7175e330c1ef4efdcdd8696
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2010-07-15 23:50:24 +0000

    create the symlinnks for parentportal and subscriber folders to the img, css, media, script

---
+++ build/symlinks.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+ln -s /usr/commsuite/www/css /usr/commsuite/www/parentportal/css
+ln -s /usr/commsuite/www/img /usr/commsuite/www/parentportal/img
+ln -s /usr/commsuite/www/media /usr/commsuite/www/parentportal/media
+ln -s /usr/commsuite/www/script /usr/commsuite/www/parentportal/script
+
+
+ln -s /usr/commsuite/www/css /usr/commsuite/www/subscriber/css
+ln -s /usr/commsuite/www/img /usr/commsuite/www/subscriber/img
+ln -s /usr/commsuite/www/media /usr/commsuite/www/subscriber/media
+ln -s /usr/commsuite/www/script /usr/commsuite/www/subscriber/script
+
+
