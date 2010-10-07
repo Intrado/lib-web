315a96de05db1103841469619a22aec408579b25
Martin Arnberg <marnberg@schoolmessenger.com>
2010-10-07 20:55:11 +0000

    Cleanup so schoolmessenger logo is shown even if an error occurs

--- messagelinklogo.img.php
+++ messagelinklogo.img.php
@@ -21,26 +21,24 @@ try {
 	$client = new MessageLinkClient($appserverprotocol);
 	// Open up the connection
 	$appservertransport->open();
-	
-	try {
-		$logo = $client->getLogo($code);
-		$data = $logo->data;
-		$contenttype = $logo->contenttype;
-	} catch (messagelink_MessageLinkCodeNotFoundException $e) {
-		error_log("Unable to find the messagelinkcode: " . $code);
-		$data = file_get_contents("img/logo_small.gif");
-		$contenttype = "image/gif";
-	}
-	header("Content-type: " . $contenttype);
-	header("Pragma: ");
-	header("Cache-Control: private");
-	header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire in 1 hour, but if theme changes so will hash pointing to this file
-	echo $data;
-	
+	$logo = $client->getLogo($code);
+	$data = $logo->data;
+	$contenttype = $logo->contenttype;
 	$appservertransport->close();
+} catch (messagelink_MessageLinkCodeNotFoundException $e) {
+	error_log("Unable to find the messagelinkcode: " . $code);
+	$data = file_get_contents("img/logo_small.gif");
+	$contenttype = "image/gif";
 } catch (TException $tx) {
 	// a general thrift exception, like no such server
 	error_log("Exception Connection to AppServer (" . $tx->getMessage() . ")");
+	$data = file_get_contents("img/logo_small.gif");
+	$contenttype = "image/gif";
 }
+header("Content-type: " . $contenttype);
+header("Pragma: ");
+header("Cache-Control: private");
+header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire in 1 hour, but if theme changes so will hash pointing to this file
+echo $data;
 
 ?>
