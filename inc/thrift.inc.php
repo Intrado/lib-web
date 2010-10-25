313c9f42a1c9f0191703b7ba2b1f612fb30dfc22
Martin Arnberg <marnberg@schoolmessenger.com>
2010-10-25 19:02:31 +0000

    Increasing the default thrift timeout and making it possible to set timeout through settings.inc.php under the appserver setting

--- inc/thrift.inc.php
+++ inc/thrift.inc.php
@@ -19,28 +19,16 @@ try {
 		$appserverhost = explode(":",$SETTINGS['appserver']['host']);
 		if(count($appserverhost) == 2) {
 			$appserversocket = new TSocket($appserverhost[0], $appserverhost[1]);
+			//$appserversocket->setDebug(true);
+			if(isset($SETTINGS['appserver']['timeout'])) {
+				$appserversocket->setRecvTimeout($SETTINGS['appserver']['timeout']);
+			} else {
+				$appserversocket->setRecvTimeout(60000);
+			}
 			$appservertransport = new TFramedTransport($appserversocket);
 			//$appserverprotocol = new TBinaryProtocol($appservertransport);
 			
 			$appserverprotocol = new TBinaryProtocolAccelerated($appservertransport);
-			
-			/*
-			 * The TBinaryProtocolAccelerated extension contains a bug that will cause an
-			 * error message on the server side when using framed transport
-			 * https://issues.apache.org/jira/browse/THRIFT-867
-			 * 
-			 * Fix by comment out flush() for ~PHPOutputTransport()
-			 * 
-			class PHPOutputTransport : public PHPTransport {
-			public:
-			  PHPOutputTransport(zval* _p, size_t _buffer_size = 8192) {
-			    construct_with_zval(_p, _buffer_size);
-			  }
-
-			  ~PHPOutputTransport() {
-			//    flush();
-			  }
-			 */
 			
 		}
 	}
