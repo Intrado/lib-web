58002d554ff0bfe1831383dfc9ccb639733e0e05
Nickolas Heckman <nheckman@schoolmessenger.com>
2010-12-08 19:29:14 +0000

    Add ajax request for sumitting access tokens. This lets us wipe it out with an ajax request.

--- ajaxtwitter.php
+++ ajaxtwitter.php
@@ -10,18 +10,17 @@ global $USER;
 
 header('Content-Type: application/json');
 
+if (!$USER->authorize('twitterpost'))
+	echo false;
+
 if (!isset($_GET['type']) && !isset($_POST['type']))
-	return false;
+	echo false;
 
 $type = isset($_GET['type'])?$_GET['type']:$_POST['type'];
 
 // get twitter connection
-$twitterdata = json_decode($USER->getSetting("tw_access_token", false));
-if ($twitterdata) {
-	$twitter = new Twitter($twitterdata->oauth_token, $twitterdata->oauth_token_secret);
-} else {
-	$twitter = new Twitter();
-}
+$twitter = new Twitter($USER->getSetting("tw_access_token", false));
+
 $twitter->decode_json = false;
 
 switch ($type) {
@@ -29,9 +28,13 @@ switch ($type) {
 		$userdata = $twitter->getUserData();
 		echo $userdata;
 		break;
+		
+	case "store_access_token":
+		if (isset($_POST['access_token']))
+			$USER->setSetting("tw_access_token", (($_POST['access_token'] == "false")?false:$_POST['access_token']));
 	
 	default:
-		return false;
+		echo false;
 }
 
 
