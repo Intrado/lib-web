cc925a886ec66098f7466b4fbe848bc86f89ab50
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-02-10 16:53:31 +0000

    Adjust output for two files, one for data and one for metadata. Document script.

--- build/dump_db_content.php
+++ build/dump_db_content.php
@@ -1,10 +1,24 @@
 <?
+/* Dump customer content from CommSuite database into the local filesystem
+ * The idea is, these content files can be used for loading into an alternate 
+ * storage system such as Cassandra via another tool.
+ * 
+ * Content will be extrated into two files for each content record. A .dat
+ * file will contain the data and a .metadata file will contain the file 
+ * metadata, such as type, with the following destination format
+ * $destfolder/<customerid>/<contentid>.dat
+ * $destfolder/<customerid>/<contentid>.metadata
+ * 
+ * - Nickolas
+ */
 
+// authserver connection information (only read access required)
 $authhost="localhost:3306";
 $authuser="root";
 $authpass="asp123";
 $authdb="authserver";
 
+// destination for extracted content files
 $destfolder = "/tmp/content/";
 
 $auth = mysql_connect($authhost, $authuser, $authpass)
@@ -19,45 +33,40 @@ while($row = mysql_fetch_row($res)){
 	$data[] = $row;
 }
 
+// iterate all customers
 foreach($data as $customer){
+	$customerid = $customer[0];
+	
+	// connect to the customer db
 	$custdb = mysql_connect($customer[1], $customer[2], $customer[3])
 				or die("Could not connect to customer: " . mysql_error($custdb));
 	mysql_select_db("c_$customer[0]", $custdb)
 				or die("Could not select customer db: " . mysql_error($custdb));
 
+	// select the contentid and type
 	$res = mysql_query("select id, contenttype from content", $custdb);
 	$contentrecords = array();
 	while ($row = mysql_fetch_row($res)) {
 		$contentrecords[] = $row;
 	}
 	
+	// for each contentid
 	foreach ($contentrecords as $content) {
+		$contentid = $content[0];
+		$contenttype = $content[1];
 		
-		$folder = $destfolder . str_replace("/", "_", $content[1]) . "/" . $customer[0] . "/";
-		$file = $content[0];
-//		switch ($content[1]) {
-//			case "image/gif":
-//				$file .= ".gif";
-//				break;
-//			case "audio/wav":
-//				$file .= ".wav";
-//				break;
-//			case "text/plain":
-//				$file .= ".txt";
-//				break;
-//			case "application/zip":
-//				$file .= ".zip";
-//				break;
-//			default:
-//				$type = explode("/", $content[1]);
-//				$file .= "." . $type[1];
-//		}
+		// construct the file paths for the dat and metadata files
+		$folder = $destfolder . $customerid . "/";
+		$datfile = $contentid . ".dat";
+		$metadatafile = $contentid . ".metadata";
+		
 		if (!file_exists($folder))
 			mkdir($folder, 0755, true);
 		
-		$f = fopen($folder . $file, "w");
+		$f = fopen($folder . $datfile, "w");
 		
-		$res = mysql_query("select data from content where id = " . $content[0], $custdb);
+		// un-base64 the data and write it to the dat file
+		$res = mysql_query("select data from content where id = " . $contentid, $custdb);
 		$b64data = mysql_fetch_row($res);
 		$data = base64_decode($b64data[0]);
 		$b64data = "";
@@ -66,7 +75,13 @@ foreach($data as $customer){
 		$data = "";
 		
 		fclose($f);
-	}
-}
+		
+		// create a metadata file with the contenttype and owner=system
+		$f = fopen($folder . $metadatafile, "w");
+		fwrite($f, "type=" . $contenttype . "\n" . "owner=system" . "\n");
+		
+		fclose($f);
+	} // end for each contentid
+} // end for each customer
 
 ?>
