6cd118f0af49e6204a03a2d73c44fbfc9d0013fe
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2011-03-28 21:08:33 +0000

    symlink thrift for parentportal and subscriber to call appserver to render email with templates

--- build/symlinks.sh
+++ build/symlinks.sh
@@ -4,11 +4,12 @@ ln -s /usr/commsuite/www/css /usr/commsuite/www/parentportal/css
 ln -s /usr/commsuite/www/img /usr/commsuite/www/parentportal/img
 ln -s /usr/commsuite/www/media /usr/commsuite/www/parentportal/media
 ln -s /usr/commsuite/www/script /usr/commsuite/www/parentportal/script
-
+ln -s /usr/commsuite/www/thrift /usr/commsuite/www/parentportal/thrift
 
 ln -s /usr/commsuite/www/css /usr/commsuite/www/subscriber/css
 ln -s /usr/commsuite/www/img /usr/commsuite/www/subscriber/img
 ln -s /usr/commsuite/www/media /usr/commsuite/www/subscriber/media
 ln -s /usr/commsuite/www/script /usr/commsuite/www/subscriber/script
+ln -s /usr/commsuite/www/thrift /usr/commsuite/www/subscriber/thrift
 
 
