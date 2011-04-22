453b5bbae3cd831717b68fae38a416d41cf8ae17
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-04-22 22:45:56 +0000

    Allow html formatted radioname if ishtml is passed as true

--- obj/FormItem.obj.php
+++ obj/FormItem.obj.php
@@ -117,13 +117,17 @@ class CheckBox extends FormItem {
 class RadioButton extends FormItem {
 	function render ($value) {
 		$n = $this->form->name."_".$this->name;
+		// is the radioname data html formatted?
+		$ishtml = false;
+		if (isset($this->args['ishtml']) && $this->args['ishtml'])
+			$ishtml = true; 
 		$str = '<div id='.$n.' class="radiobox">';
 		$hoverdata = array();
 		$counter = 1;
 		$autoselect = count($this->args['values']) == 1; //if there is only one value, autoselect it
 		foreach ($this->args['values'] as $radiovalue => $radioname) {
 			$id = $n.'-'.$counter;
-			$str .= '<input id="'.$id.'" name="'.$n.'" type="radio" value="'.escapehtml($radiovalue).'" '.($value == $radiovalue || $autoselect ? 'checked' : '').' /><label id="'.$id.'-label" for="'.$id.'">'.escapehtml($radioname).'</label><br />
+			$str .= '<input id="'.$id.'" name="'.$n.'" type="radio" value="'.escapehtml($radiovalue).'" '.($value == $radiovalue || $autoselect ? 'checked' : '').' /><label id="'.$id.'-label" for="'.$id.'">'.($ishtml?$radioname:escapehtml($radioname)).'</label><br />
 			';
 			if (isset($this->args['hover'])) {
 				$hoverdata[$id] = $this->args['hover'][$radiovalue];
