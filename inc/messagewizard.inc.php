9de65ce300a701eeeb38ccb00935c374d7bf8405
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-05-02 20:12:29 +0000

    Check user authorization for individual message types. Remove defunct AutoTranslateForm and include it's contents in the wizard step.

--- inc/messagewizard.inc.php
+++ inc/messagewizard.inc.php
@@ -288,6 +288,10 @@ class MsgWiz_phoneText extends WizStep {
 
 	//returns true if this step is enabled
 	function isEnabled($postdata, $step) {
+		global $USER;
+		if (!$USER->authorize("sendphone"))
+			return false;
+			
 		if (isset($postdata["/start"]["messagetype"]) && $postdata["/start"]["messagetype"] == "sendphone"
 				&& isset($postdata["/method"]["method"]) && $postdata["/method"]["method"] == "write")
 			return true;
@@ -337,6 +341,11 @@ class MsgWiz_phoneEasyCall extends WizStep {
 
 	//returns true if this step is enabled
 	function isEnabled($postdata, $step) {
+		global $USER;
+		
+		if (!$USER->authorize("sendphone") || !$USER->authorize("starteasy"))
+			return false;
+			
 		if (isset($postdata["/start"]["messagetype"]) && $postdata["/start"]["messagetype"] == "sendphone"
 				&& isset($postdata["/method"]["method"]) && $postdata["/method"]["method"] == "record")
 			return true;
@@ -503,6 +512,10 @@ class MsgWiz_emailText extends WizStep {
 
 	//returns true if this step is enabled
 	function isEnabled($postdata, $step) {
+		global $USER;
+		if (!$USER->authorize("sendemail"))
+			return false;
+			
 		if (isset($postdata["/start"]["messagetype"]) && $postdata["/start"]["messagetype"] == "sendemail")
 			return true;
 		
@@ -512,28 +525,130 @@ class MsgWiz_emailText extends WizStep {
 
 
 class MsgWiz_translatePreview extends WizStep {
+	function escapeFieldInserts($text) {
+		return str_replace(">>", "&#062;&#062;", str_replace("<<", "&#060;&#060;", $text));
+	}
+	
 	function getForm($postdata, $curstep) {
+		global $TRANSLATIONLANGUAGECODES;
+		
 		// msgdata from phone or email
 		switch ($postdata["/start"]["messagetype"]) {
 			case "sendphone":
 				if (isset($postdata["/method"]["method"]) && $postdata["/method"]["method"] == "write") {
 					$msg = json_decode($postdata['/create/phonetext']['message']);
-					$msgdata = $msg->message;
+					$sourcetext = $msg->message;
 				} else {
-					$msgdata = $postdata['/create/phoneadvanced']['message'];
+					$sourcetext = $postdata['/create/phoneadvanced']['message'];
 				}
 				$msgtype = "phone";
 				break;
 			case "sendemail":
-				$msgdata = $postdata['/create/emailtext']['message'];
+				$sourcetext = $postdata['/create/emailtext']['message'];
 				$msgtype = "email";
 				break;
 				
 			default:
+				$sourcetext = "";
 				$msgtype = "";
 		}
 		
-		return new AutoTranslateForm("translatePreview", $this->title, $msgdata, $msgtype);
+		static $translations = false;
+		static $translationlanguages = false;
+
+		$warning = "";
+		if(mb_strlen($sourcetext) > 4000) {
+			$warning = _L('Warning. Only the first 4000 characters are translated.');
+		}
+
+		//Get available languages
+		switch ($msgtype) {
+			case "phone":
+				$translationlanguages = Voice::getTTSLanguageMap();
+				$voices = Voice::getTTSVoices();
+				break;
+			
+			default:
+				$alllanguages = Language::getLanguageMap();
+				$translationlanguages = array_intersect_key($alllanguages, array_flip($TRANSLATIONLANGUAGECODES));
+				$voices = array();
+		}
+		unset($translationlanguages['en']);
+		$translationlanguagecodes = array_keys($translationlanguages);
+		$translations = translate_fromenglish(makeTranslatableString($sourcetext),$translationlanguagecodes);
+
+		// Form Fields.
+		$formdata = array($this->title);
+
+		if ($warning)
+			$formdata["warning"] = array(
+				"label" => _L("Warning"),
+				"control" => array("FormHtml","html"=>'<div style="font-size: medium; color: red">'.escapehtml($warning).'</div><br>'),
+				"helpstep" => 1
+			);
+
+		$formdata["englishtext"] = array(
+			"label" => _L("English"),
+			"control" => array("FormHtml","html"=>'<div style="font-size: medium;">'.$this->escapeFieldInserts($sourcetext).'</div><br>'),
+			"helpstep" => 1
+		);
+
+		// Debug output when no translation is available
+		if(!$translations) {
+			$formdata["Translationinfo"] = array(
+				"label" => _L("Info"),
+				"control" => array("FormHtml","html"=>'<div style="font-size: medium;">'._L('No Translations Available').'</div><br>'),
+				"helpstep" => 2
+			);
+		} else {
+			if(is_array($translations)){
+				foreach($translations as $obj){
+					$languagecode = array_shift($translationlanguagecodes);
+					$formdata[] = Language::getName($languagecode);
+					$formdata[$languagecode] = array(
+						"label" => _L("Enabled"),
+						"fieldhelp" => _L('Check this box to automatically translate your message using Google Translate.'),
+						"value" => 1,
+						"validators" => array(),
+						"control" => array("CheckBoxWithHtmlPreview", "checkedhtml" => $this->escapeFieldInserts($obj->responseData->translatedText), "uncheckedhtml" => addslashes(_L("People tagged with this language will receive the English version."))),
+						"helpstep" => 2
+					);
+				}
+			} else {
+				$languagecode = reset($translationlanguagecodes);
+					$formdata[] = Language::getName($languagecode);
+				$formdata[$languagecode] = array(
+						"label" => _L("Enabled"),
+						"fieldhelp" => _L('Check this box to automatically translate your message using Google Translate.'),
+						"value" => 1,
+						"validators" => array(),
+						"control" => array("CheckBoxWithHtmlPreview", "checkedhtml" => $this->escapeFieldInserts($translations->translatedText), "uncheckedhtml" => addslashes(_L("People tagged with this language will receive the English version."))),
+						"helpstep" => 2
+					);
+			}
+		}
+		if(!isset($formdata["Translationinfo"])) {
+			$formdata["Translationinfo"] = array(
+				"label" => " ",
+				"control" => array("FormHtml","html"=>'
+					<div id="branding">
+						<div style="color: rgb(103, 103, 103);float: right;" class="gBranding">
+							<span style="vertical-align: middle; font-family: arial,sans-serif; font-size: 11px;" class="gBrandingText">
+								'._L('Translation powered by').'<img style="padding-left: 1px; vertical-align: middle;" alt="Google" src="' . (isset($_SERVER['HTTPS'])?"https":"http") . '://www.google.com/uds/css/small-logo.png">
+							</span>
+						</div>
+					</div>
+				'),
+				"helpstep" => 2
+			);
+		}
+
+		$helpsteps = array(
+			_L("This is the message that all contacts will receive if they do not have any other language message specified"),
+			_L("This is an automated translation powered by Google Translate. Please note that although machine translation is always improving, it is not perfect yet. You can try reverse translation for an idea of how well your message was translated.")
+		);
+		
+		return new Form("translatePreview",$formdata,$helpsteps);
 	}
 
 	//returns true if this step is enabled
@@ -577,6 +692,10 @@ class MsgWiz_smsText extends WizStep {
 
 	//returns true if this step is enabled
 	function isEnabled($postdata, $step) {
+		global $USER;
+		if (!$USER->authorize("sendsms"))
+			return false;
+			
 		if (isset($postdata["/start"]["messagetype"]) && $postdata["/start"]["messagetype"] == "sendsms")
 			return true;
 		
