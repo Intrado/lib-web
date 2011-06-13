bd3da07a773c9bb1389b5598d3acd503740e07af
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-06-13 15:13:27 +0000

    Be more specific in the validator error message. Indicate to the user what the default language is currently set to.

--- obj/ValMessageGroup.val.php
+++ obj/ValMessageGroup.val.php
@@ -5,7 +5,10 @@ class ValMessageGroup extends Validator {
 	function validate ($value) {
 		$mg = new MessageGroup($value);
 		if(!$mg->isValid()) {
-			return _L("The selected message is missing one or more parts and cannot be used.");
+			if ($mg->defaultlanguagecode)
+				return _L("The default language, %s, for the selected message is missing one or more parts and cannot be used.", Language::getName($mg->defaultlanguagecode));
+			else
+				return _L("The selected message is missing one or more parts and cannot be used.", Language::getName($mg->defaultlanguagecode));
 		}
 		return true;
 	}
