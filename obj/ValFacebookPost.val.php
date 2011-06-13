710df157e8fd0d1461cea46d38637feab68cdcc5
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-06-13 22:59:28 +0000

    Finish enhancing validator to work with page and wall restrictions. Implement javascript version.

--- obj/ValFacebookPost.val.php
+++ obj/ValFacebookPost.val.php
@@ -1,7 +1,6 @@
 <?
 
 class ValFacebookPost extends Validator {
-	var $onlyserverside = true;
 	
 	function validate ($value, $args) {
 		global $USER;
@@ -10,27 +9,6 @@ class ValFacebookPost extends Validator {
 		
 		$fbdata = json_decode($value);
 		
-		// get the authorized pages
-		$authpages = array();
-		$authwall = false;
-		if (isset($args['authpages']))
-			$authpages = $args['authpages'];
-		if (isset($args['authwall']))
-			$authwall = $args['authwall'];
-		
-		// check to see if any pages are selected
-		$haspage = false;
-		foreach ($fbdata->page as $pageid => $token) {
-			$haspage = true;
-			// check authorized pages to see if the ones selected are allowed
-			if ($pageid == "me" && !$authwall)
-				return $this->label. " ". _L("has an invalid selection. Personal wall posting is disabled.");
-			else if ($authpages && !in_array($pageid, $authpages))
-				return $this->label. " ". _L("has an invalid posting location selected. Page is not authorized.");
-		}
-		if (!$haspage)
-			return $this->label. " ". _L("must have one or more pages to post to.");
-		
 		// check that there is message text and it is within the allowed length
 		if (!$fbdata->message)
 			return $this->label. " ". _L("needs a message to post to your Facebook pages.");
@@ -38,8 +16,62 @@ class ValFacebookPost extends Validator {
 		if (mb_strlen($fbdata->message) > 420)
 			return $this->label. " ". _L("message may not excede 420 characters.");
 		
+		// get the authorized pages
+		// don't trust args, look up the authorized pages
+		$authpages = getFbAuthorizedPages();
+		$authwall = getSystemSetting("fbauthorizewall");
+		
+		// check to see if any pages are selected
+		$haspage = false;
+		foreach ($fbdata->page as $pageid => $token) {
+			error_log($pageid);
+			$haspage = true;
+			// check authorized pages to see if the ones selected are allowed
+			if ($pageid == "me") {
+				if (!$authwall)
+					return $this->label. " ". _L("has an invalid selection. Personal wall posting is disabled.");
+			} else if ($authpages && !in_array($pageid, $authpages)) {
+				return $this->label. " ". _L("has an invalid posting location selected. Page is not authorized.");
+			}
+		}
+		if (!$haspage)
+			return $this->label. " ". _L("must have one or more pages to post to.");
+		
+		
 		return true;
 	}
-	// TODO: javascript validator
+
+	function getJSValidator () {
+		return
+			'function (name, label, value, args) {
+				var authpages = args.authpages;
+				var authwall = (args.authwall == 1);
+				var pages = value.evalJSON();
+				
+				if (pages.message == "")
+					return label + " '. _L("needs a message to post to your Facebook pages.") .'";
+				if (pages.message.length > 420)
+					return label + " '. _L("message may not excede 420 characters.") .'";
+				if ($H(pages.page).size() == 0)
+					return label + " '. _L("must have one or more pages to post to.") .'";
+				
+				var validatormessage = true;
+				$H(pages.page).keys().each(function (pageid) {
+					if (pageid == "me") {
+						if (!authwall)
+							validatormessage = label + " '. _L("has an invalid selection. Personal wall posting is disabled.") .'";
+					} else {
+						var validpage = false;
+						$A(authpages).each(function (authpageid) {
+							if (pageid == authpageid)
+								validpage = true;
+						});
+						if (!validpage)
+							validatormessage = label + " '. _L("has an invalid posting location selected. Page is not authorized.") .'";
+					}
+				});
+				return validatormessage;
+			}';
+	}
 }
 ?>
