bf277108597ef9007fadffb609fc6ffc16eebc8e
Ben Hencke <bhencke@schoolmessenger.com>
2011-06-24 18:47:54 +0000

    setting db name to pagelink, not variable (no reason to be). plus it was in the wrong field

--- db/users.sql
+++ db/users.sql
@@ -5,9 +5,9 @@
 
 -- pagelink user for finding codes. used by appserver
 CREATE USER 'pagelink_ro'@'%' IDENTIFIED BY '$some_random_password';
-GRANT SELECT ON `pagelink`.`$pagedbname` TO 'pagelink_ro'@'%';
+GRANT SELECT ON `pagelink`.`pagelink` TO 'pagelink_ro'@'%';
 
 -- pagelink user for creating links. used by redialer
 CREATE USER 'pagelink_rw'@'%' IDENTIFIED BY '$some_random_password';
-GRANT SELECT, INSERT, UPDATE, DELETE ON `pagelink`.`$pagedbname` TO 'pagelink_rw'@'%';
+GRANT SELECT, INSERT, UPDATE, DELETE ON `pagelink`.`pagelink` TO 'pagelink_rw'@'%';
 
