234a878288ed1f099ade09e61e44a8ecb18558e5
Martin Arnberg <marnberg@schoolmessenger.com>
2011-08-22 21:25:08 +0000

    Adding jobstatus filter for active jobs page. Added scheduled and processing jobs page to make active jobs page shorter. Refactored to new forms

--- manager/customeractivejobs.php
+++ manager/customeractivejobs.php
@@ -1,60 +1,160 @@
 <?
-include_once("common.inc.php");
-include_once("../inc/table.inc.php");
-
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+require_once("common.inc.php");
+require_once("../inc/table.inc.php");
+require_once("../inc/html.inc.php");
+require_once("../inc/utils.inc.php");
+require_once("../obj/Validator.obj.php");
+require_once("../obj/Form.obj.php");
+require_once("../obj/FormItem.obj.php");
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
 
 if (!$MANAGERUSER->authorized("activejobs"))
 	exit("Not Authorized");
 
-if(isset($_GET['cid'])){
-	$customerid = $_GET['cid'] + 0;
-	$extrasql = " and j.customerid = $customerid ";
-	if(isset($_GET['user'])){
-		$userid = $_GET['user'] + 0;
-		$extrasql .= " and j.userid = $userid ";
+
+if (isset($_GET["clear"])) {
+	unset($_SESSION["customeractivejobsfiler"]);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Action/Request Processing
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data
+////////////////////////////////////////////////////////////////////////////////
+// default settings
+$settings = array(
+	"dispatchtype" => 'system',
+	"priorities" => array(1,2,3),
+	"jobstatus" => "active"
+);
+if (isset($_SESSION["customeractivejobsfiler"])) {
+	$settings = array_merge($settings,json_decode($_SESSION["customeractivejobsfiler"],true));
+}
+
+
+$helpstepnum = 1;
+$dispatchtypes = array('customer' => 'Customer','system' => 'System');
+$formdata["dispatchtype"] = array(
+	"label" => _L('Dispatch Type'),
+	"value" => $settings['dispatchtype'],
+	"validators" => array(
+		array("ValRequired"),
+		array("ValInArray", "values" => array_keys($dispatchtypes))
+	),
+	"control" => array("SelectMenu", "values" => $dispatchtypes),
+	"helpstep" => $helpstepnum
+);
+$prinames = array (1 => "Emergency", 2 => "High", 3 => "General");
+$formdata["priorities"] = array(
+	"label" => _L('Priority'),
+	"value" => $settings['priorities'],
+	"validators" => array(
+		array("ValRequired"),
+		array("ValInArray", "values" => array_keys($prinames))
+	),
+	"control" => array("MultiCheckBox", "values" => $prinames),
+	"helpstep" => $helpstepnum
+);
+
+if (isset($_SESSION['customeractivejobsfiler'])) {
+	$buttons = array(submit_button(_L('Refresh'),"submit","arrow_refresh"));
+} else {
+	$buttons = array(submit_button(_L('Show Jobs'),"submit","magnifier"));	
+}
+$form = new Form("templateform",$formdata,false,$buttons);
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+//check and handle an ajax request (will exit early)
+//or merge in related post data
+$form->handleRequest();
+
+$datachange = false;
+$errors = false;
+//check for form submission
+if ($button = $form->getSubmit()) { //checks for submit and merges in post data
+	$ajax = $form->isAjaxSubmit(); //whether or not this requires an ajax response	
+	
+	if ($form->checkForDataChange()) {
+		$datachange = true;
+	} else if (($errors = $form->validate()) === false) { //checks all of the items in this form
+		$postdata = $form->getData(); //gets assoc array of all values {name:value,...}
+		$_SESSION['customeractivejobsfiler'] = json_encode($postdata);
+		if ($ajax)
+			$form->sendTo("customeractivejobs.php");
+		else
+			redirect("customeractivejobs.php");
 	}
-} else {
+}
+
+if (isset($_SESSION['customeractivejobsfiler'])) {
+	$customers = QuickQueryList("select id, urlcomponent from customer",true);
+
+	$res = Query("select id, dbhost, dbusername, dbpassword from shard order by id");
+	$shards = array();
+	while($row = DBGetRow($res)){
+		$dsn = 'mysql:dbname=aspshard;host='.$row[1];
+		$db = new PDO($dsn, $row[2], $row[3]);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+		$shards[$row[0]] = $db;
+	}
+
+	$calldata = array();
+	$activejobs = array();
 	$extrasql = "";
+
+	if(isset($_GET['cid'])){
+		$customerid = $_GET['cid'] + 0;
+		$extrasql .= " and j.customerid = $customerid ";
+		if(isset($_GET['user'])){
+			$userid = $_GET['user'] + 0;
+			$extrasql .= " and j.userid = $userid ";
+		}
+	}
+	if ($settings['dispatchtype'] == 'customer'){
+		$extrasql .= " and j.dispatchtype = 'customer' ";
+	} else {
+		$extrasql .= " and j.dispatchtype = 'system' ";
+	}
+
+	foreach ($shards as $shardid => $sharddb) {
+		Query("use aspshard", $sharddb);
+		$query = "select j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence,
+						jt.status, j.phonetaskcount, j.timeslices, count(*)
+				from qjobtask jt
+				straight_join qjob j on (j.id = jt.jobid and j.customerid = jt.customerid)
+				where 1 $extrasql
+				group by jt.status, jt.customerid, jt.jobid, jt.type, jt.attempts, jt.sequence
+				order by j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence
+				";
+		$res = Query($query,$sharddb);
+		while ($row = DBGetRow($res)) {
+
+			$calldata[$row[0]][$row[1]][$row[2]][$row[3]][$row[4]][$row[5]][$row[6]] = $row[9];
+
+			$activejobs[$row[1]][$row[2]]["phonetaskcount"] = $row[7];
+			@$activejobs[$row[1]][$row[2]]["phonetaskremaining"] += $row[9];
+
+
+			$activejobs[$row[1]][$row[2]]["timeslices"] = $row[8];
+		}
+	}
 }
 
-if (isset($_GET['customer'])){
-	$dispatchtype = 'customer';
-	$extrasql .= " and j.dispatchtype = 'customer' ";
-} else if (isset($_GET['system'])) {
-	$dispatchtype = 'system';
-	$extrasql .= " and j.dispatchtype = 'system' ";	
-} else {
-	$dispatchtype = 'system';
-	$extrasql .= " and j.dispatchtype = 'system' ";	
-}
 
+////////////////////////////////////////////////////////////////////////////////
+// Display Functions
+////////////////////////////////////////////////////////////////////////////////
 
-/*
-
-//TODO filter for a customer
-
-
-foreach shard:
-
-select j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence,
-		jt.status, j.phonetaskcount, j.timeslices, count(*)
-from qjobtask jt
-straight_join qjob j on (j.id = jt.jobid and j.customerid = jt.customerid)
-group by jt.status, jt.customerid, jt.jobid, jt.type, jt.attempts, jt.sequence
-
-order by j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence, jt.status
-
-
-
-show by
-syspri
-	customer
-		job (totalphone, timeslices)
-			type
-				attempt
-					sequence
-
-*/
 
 function fmt_html ($row,$index) {
 	return $row[$index];
@@ -73,264 +173,203 @@ function fmt_custurl($row, $index){
 }
 
 //index 2 is jobid
-function fmt_play_link($row, $index){
+function fmt_play_activejobs($row, $index){
 	$url = "";
 	if($row[2])
-		$url = "<a onclick='popup(\"customerplaymessage.php?customerid=" . $row[14] . "&jobid=" . $row[2] . "\", 400, 500); return false;' href=\"#\" title='Play Message'><img src='mimg/s-play.png' border=0></a>";
+		$url = "<a onclick='popup(\"customerplaymessage.php?customerid=" . $row[14] . "&jobid=" . $row[2] . "\", 400, 500); return false;' href=\"#\" alt='' title='Play Message'><img src='mimg/s-play.png' border=0></a>";
 	return $url;
 }
 
-
-$customers = QuickQueryList("select id, urlcomponent from customer",true);
-
-$res = Query("select id, dbhost, dbusername, dbpassword from shard order by id");
-$shards = array();
-while($row = DBGetRow($res)){
-	$dsn = 'mysql:dbname=aspshard;host='.$row[1];
-	$db = new PDO($dsn, $row[2], $row[3]);
-	$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
-	$shards[$row[0]] = $db;
-}
-
-$calldata = array();
-$jobs = array();
-$schedjobs = array();
-foreach ($shards as $shardid => $sharddb) {
-	Query("use aspshard", $sharddb);
-	$query = "select j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence,
-					jt.status, j.phonetaskcount, j.timeslices, count(*)
-			from qjobtask jt
-			straight_join qjob j on (j.id = jt.jobid and j.customerid = jt.customerid)
-			where 1 $extrasql
-			group by jt.status, jt.customerid, jt.jobid, jt.type, jt.attempts, jt.sequence
-			order by j.systempriority, j.customerid, j.id, jt.type, jt.attempts, jt.sequence
-			";
-	$res = Query($query,$sharddb);
-	while ($row = DBGetRow($res)) {
-
-		$calldata[$row[0]][$row[1]][$row[2]][$row[3]][$row[4]][$row[5]][$row[6]] = $row[9];
-
-		$jobs[$row[1]][$row[2]]["phonetaskcount"] = $row[7];
-		@$jobs[$row[1]][$row[2]]["phonetaskremaining"] += $row[9];
-
-
-		$jobs[$row[1]][$row[2]]["timeslices"] = $row[8];
-	}
-
-
-	QuickUpdate("set time_zone='GMT'",$sharddb);
-
-	$query = "select systempriority, customerid, id, startdate, starttime, timezone,
-			convert_tz(addtime(startdate,starttime),timezone,'SYSTEM') systemstarttime
-			from qjob where status='scheduled'
-			order by systemstarttime , systempriority, customerid, id";
-			
-	$res = Query($query,$sharddb);
-	while ($row = DBGetRow($res)) {
-		$secondstostart = strtotime($row[6]) - time();
-		$seconds = abs($secondstostart);	
-		$days = floor($seconds/86400);
-		$seconds -= $days*86400;
-		$hours = floor($seconds/3600);
-		$seconds -= $hours*3600;
-		$minutes = floor($seconds/60);
-		$seconds -= $minutes*60;
-		
-		$timetorun = str_pad($hours,2, "0",STR_PAD_LEFT) . ":" . str_pad($minutes,2, "0",STR_PAD_LEFT) . ":" . str_pad($minutes,2, "0",STR_PAD_LEFT) . ($days ? " + $days Days" : "");
-		if($secondstostart < 0)
-			$timetorun = " - " . $timetorun;
-		
-		$schedjobs[$row[0]][$secondstostart][] = array ($row[1], $customers[$row[1]], $row[2], $row[3], $row[4], $row[5], $timetorun);
-	}
-}
-
-///////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
 // Display
-///////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+$TITLE = _L('Customer Jobs');
 
-include("nav.inc.php");
+include_once("nav.inc.php");
+
+// Optional Load Custom Form Validators
 ?>
-
-Task dispatch type:&nbsp;
-<select id='dispatchtypeselect' onchange="window.location='customeractivejobs.php?'+this.options[this.selectedIndex].value;">
-<option value='system' <?=($dispatchtype=='system')?"selected":""?>>Asp</option>
-<option value='customer' <?=($dispatchtype=='customer')?"selected":""?>>SmartCall</option>
-</select>
-
+<script type="text/javascript">
+<? Validator::load_validators(array()); ?>
+</script>
+<ul><li><a href="customersubmittedjobs.php?clear">View Scheduled and Processing Jobs</a></li></ul>
 <?
 
-$prinames = array (1 => "Emergency", 2 => "High", 3 => "General");
-$pricolors = array (1 => "#ff0000", 2 => "#ffff00", 3 => "#0000ff");
+startWindow(_L('Active Jobs Filter'));
+echo $form->render();
+endWindow();
 
-
-for ($pri = 1; $pri <=3 ; $pri++) {
-	if (!isset($calldata[$pri]) && !isset($schedjobs[$pri]))
-		continue;
-?>
-	<h2 style="border: 3px solid <?= $pricolors[$pri] ?>;"><?=$prinames[$pri]?><hr>
-<?
-	if (isset($calldata[$pri])) {
-
-	$pricalldata = $calldata[$pri];
-
+$foundjobs = false;
+if (isset($_SESSION['customeractivejobsfiler'])) {
+	startWindow(_L('Jobs'));
+	$pricolors = array (1 => "#ff0000", 2 => "#ffff00", 3 => "#0000ff");
+	
 	$data = array();
-	$pritotals = array();
-	foreach ($pricalldata as $customerid => $custcalldata) {
-		$showcust = true;
-		foreach ($custcalldata as $jobid => $jobcalldata) {
-			$showjob = true;
-
-			@$pritotals["phonetaskremaining"] += $jobs[$customerid][$jobid]["phonetaskremaining"];
-			@$pritotals["phonetaskcount"] += $jobs[$customerid][$jobid]["phonetaskcount"];
-
-			$slicesize = $jobs[$customerid][$jobid]["timeslices"];
-			if ($slicesize) {
-				$slicesize = (int) max(2,($jobs[$customerid][$jobid]["phonetaskcount"] / $slicesize));
-				@$pritotals["slicesize"] += $slicesize;
-			} else {
-				$slicesize = "&#8734;";
-			}
-
-			foreach ($jobcalldata as $type => $typecalldata) {
-				$showtype = true;
-				foreach ($typecalldata as $attempt => $attemptcalldata) {
-					$showattempt = true;
-					foreach ($attemptcalldata as $sequence => $sequencecalldata) {
-
-
-
-
-						$row = @array(
-								$showcust ? $customerid : "",
-								$showcust ? $customers[$customerid] : "",
-								$showjob ? $jobid : "",
-								$showjob ? $jobs[$customerid][$jobid]["phonetaskremaining"] : "",
-								$showjob ? $jobs[$customerid][$jobid]["phonetaskcount"] : "",
-								$showjob ? $slicesize : "",
-								$showtype ? $type : "",
-								$showattempt ? $attempt : "",
-								$sequence,
-								$sequencecalldata['active'],
-								$sequencecalldata['assigned'],
-								$sequencecalldata['progress'],
-								$sequencecalldata['pending'],
-								$sequencecalldata['waiting'],
-								$customerid
-							);
-						$data[] = $row;
-
-
-						@$pritotals["active"] += $sequencecalldata['active'];
-						@$pritotals["assigned"] += $sequencecalldata['assigned'];
-						@$pritotals["progress"] += $sequencecalldata['progress'];
-						@$pritotals["pending"] += $sequencecalldata['pending'];
-						@$pritotals["waiting"] += $sequencecalldata['waiting'];
-
-						$showcust = $showjob = $showtype = $showattempt = false;
+	$summarydata = array();
+	
+	foreach($settings['priorities'] as $pri) {
+		if (isset($calldata[$pri])) {
+			$foundjobs = true;
+			$pricalldata = $calldata[$pri];
+			$pridata = array();
+			$pritotals = array();
+			foreach ($pricalldata as $customerid => $custcalldata) {
+				$showcust = true;
+				foreach ($custcalldata as $jobid => $jobcalldata) {
+					$showjob = true;
+		
+					@$pritotals["phonetaskremaining"] += $activejobs[$customerid][$jobid]["phonetaskremaining"];
+					@$pritotals["phonetaskcount"] += $activejobs[$customerid][$jobid]["phonetaskcount"];
+		
+					$slicesize = $activejobs[$customerid][$jobid]["timeslices"];
+					if ($slicesize) {
+						$slicesize = (int) max(2,($activejobs[$customerid][$jobid]["phonetaskcount"] / $slicesize));
+						@$pritotals["slicesize"] += $slicesize;
+					} else {
+						$slicesize = "&#8734;";
+					}
+		
+					foreach ($jobcalldata as $type => $typecalldata) {
+						$showtype = true;
+						foreach ($typecalldata as $attempt => $attemptcalldata) {
+							$showattempt = true;
+							foreach ($attemptcalldata as $sequence => $sequencecalldata) {
+								$row = @array(
+										$showcust ? $customerid : "",
+										$showcust ? $customers[$customerid] : "",
+										$showjob ? $jobid : "",
+										$showjob ? $activejobs[$customerid][$jobid]["phonetaskremaining"] : "",
+										$showjob ? $activejobs[$customerid][$jobid]["phonetaskcount"] : "",
+										$showjob ? $slicesize : "",
+										$showtype ? $type : "",
+										$showattempt ? $attempt : "",
+										$sequence,
+										$sequencecalldata['active'],
+										$sequencecalldata['assigned'],
+										$sequencecalldata['progress'],
+										$sequencecalldata['pending'],
+										$sequencecalldata['waiting'],
+										$customerid
+									);
+								$pridata[] = $row;
+		
+		
+								@$pritotals["active"] += $sequencecalldata['active'];
+								@$pritotals["assigned"] += $sequencecalldata['assigned'];
+								@$pritotals["progress"] += $sequencecalldata['progress'];
+								@$pritotals["pending"] += $sequencecalldata['pending'];
+								@$pritotals["waiting"] += $sequencecalldata['waiting'];
+		
+								$showcust = $showjob = $showtype = $showattempt = false;
+							}
+						}
 					}
 				}
 			}
+			$totalsrow = array (
+				"<b>Total</b>",
+				"",
+				"",
+				$pritotals["phonetaskremaining"],
+				$pritotals["phonetaskcount"],
+				$pritotals["slicesize"] ? $pritotals["slicesize"] : "&#8734;",
+				"",
+				"",
+				"",
+				$pritotals["active"],
+				$pritotals["assigned"],
+				$pritotals["progress"],
+				$pritotals["pending"],
+				$pritotals["waiting"]
+			);
+			$pridata[] = $totalsrow;
+			$data[$pri] = $pridata;
+			$summarydata[] = array (
+				"<b>$prinames[$pri] Total</b>",
+				$pritotals["phonetaskremaining"],
+				$pritotals["phonetaskcount"],
+				$pritotals["slicesize"] ? $pritotals["slicesize"] : "&#8734;",
+				$pritotals["active"],
+				$pritotals["assigned"],
+				$pritotals["progress"],
+				$pritotals["pending"],
+				$pritotals["waiting"]
+			);
 		}
 	}
-
-
-	$totalsrow = array ("<b>Total</b>",
-						"",
-						"",
-						$pritotals["phonetaskremaining"],
-						$pritotals["phonetaskcount"],
-						$pritotals["slicesize"] ? $pritotals["slicesize"] : "&#8734;",
-						"",
-						"",
-						"",
-						$pritotals["active"],
-						$pritotals["assigned"],
-						$pritotals["progress"],
-						$pritotals["pending"],
-						$pritotals["waiting"]
-					);
-	$data[] = $totalsrow;
-
-
-
-?>
-	Active Jobs:
-	<table>
-<?
-
-	$titles = array("Customer id",
-					"Customer url",
-					"Job id",
-					"job ph remain",
-					"job ph total",
-					"job throttle",
-					"Type",
-					"attempt",
-					"sequence",
-					"Active",
-					"Assigned",
-					"Progress",
-					"Pending",
-					"Waiting",
-					"Play Message"
-				);
-	$formatters = array (0 => "fmt_html",
-						1 => "fmt_custurl",
-						3 => "fmt_number",
-						4 => "fmt_number",
-						5 => "fmt_html",
-						9 => "fmt_number",
-						10 => "fmt_number",
-						11 => "fmt_number",
-						12 => "fmt_number",
-						13 => "fmt_number",
-						14 => "fmt_play_link"
-
+	if (count($data)) {
+		$titles = array(
+			"Priority",
+			"job ph remain",
+			"job ph total",
+			"job throttle",
+			"Active",
+			"Assigned",
+			"Progress",
+			"Pending",
+			"Waiting",
 		);
-
-	showTable($data, $titles, $formatters);
-
-?>
-	</table>
-
-<?
-	}
-
-	if (isset($schedjobs[$pri])) {
-?>
-		<hr>Scheduled jobs:
-		<table border=1>
-<?
-			$titles = array ("Customer id",
-						"Customer url",
-						"Job id",
-						"Start Date",
-						"Start Time",
-						"Timezone",
-						"Time until run"
-					);
-		ksort($schedjobs[$pri]);
-		$scheddata = array();
-		foreach ($schedjobs[$pri] as $schedstart => $schedjob) 
-			foreach ($schedjob as $job)
-				$scheddata[] = $job;
+		$formatters = array (
+			0 => "fmt_html",
+			1 => "fmt_number",
+			2 => "fmt_number",
+			3 => "fmt_number",
+			4 => "fmt_number",
+			5 => "fmt_number",
+			6 => "fmt_number",
+			7 => "fmt_number",
+			8 => "fmt_number"
+		);
+		echo "<h2 style=\"border: 3px solid black;\">";
+		echo "Active Jobs Summary: <table>";
+		showTable($summarydata, $titles, $formatters);
+		echo "</table>";
+		echo "</h2>";
 		
-		showTable($scheddata, $titles, array(1 => "fmt_custurl"));
-	
-?>
-		</table>
-<?
+		$titles = array(
+			"Customer id",
+			"Customer url",
+			"Job id",
+			"job ph remain",
+			"job ph total",
+			"job throttle",
+			"Type",
+			"attempt",
+			"sequence",
+			"Active",
+			"Assigned",
+			"Progress",
+			"Pending",
+			"Waiting",
+			"Play Message"
+		);
+		$formatters = array (
+			0 => "fmt_html",
+			1 => "fmt_custurl",
+			3 => "fmt_number",
+			4 => "fmt_number",
+			5 => "fmt_html",
+			9 => "fmt_number",
+			10 => "fmt_number",
+			11 => "fmt_number",
+			12 => "fmt_number",
+			13 => "fmt_number",
+			14 => "fmt_play_activejobs"
+		);
+		foreach($data as $pri => $pridata) {
+			echo "<h2 style=\"border: 3px solid $pricolors[$pri];\">$prinames[$pri]<hr />";
+			echo "Active Jobs: <table>";
+			showTable($pridata, $titles, $formatters);
+			echo "</table>";
+			echo "</h2>";
+		}
 	}
-?>
-	</h2>
-<?
-
+	
+	if (!$foundjobs) {
+		echo _L("No jobs found for the current filter settings");
+	}
+	endWindow();
 }
 
-?>
-
-
-<div > All time stamps are in customer time. </div>
-<?
-include("navbottom.inc.php");
+include_once("navbottom.inc.php");
 ?>
---
+++ manager/customersubmittedjobs.php
@@ -0,0 +1,208 @@
+<?
+////////////////////////////////////////////////////////////////////////////////
+// Includes
+////////////////////////////////////////////////////////////////////////////////
+require_once("common.inc.php");
+require_once("../inc/table.inc.php");
+require_once("../inc/html.inc.php");
+require_once("../inc/utils.inc.php");
+require_once("../obj/Validator.obj.php");
+require_once("../obj/Form.obj.php");
+require_once("../obj/FormItem.obj.php");
+////////////////////////////////////////////////////////////////////////////////
+// Authorization
+////////////////////////////////////////////////////////////////////////////////
+
+if (!$MANAGERUSER->authorized("activejobs"))
+	exit("Not Authorized");
+
+
+if (isset($_GET["clear"])) {
+	unset($_SESSION["customerjobsfiler"]);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+// Action/Request Processing
+////////////////////////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data
+////////////////////////////////////////////////////////////////////////////////
+// default settings
+$settings = array(
+	"jobstatus" => "scheduled"
+);
+if (isset($_SESSION["customerjobsfiler"])) {
+	$settings = array_merge($settings,json_decode($_SESSION["customerjobsfiler"],true));
+}
+
+$jobstatus = array("scheduled" => "Scheduled","processing" => "Processing");
+$formdata["jobstatus"] = array(
+	"label" => _L('Job Status'),
+	"value" => $settings['jobstatus'],
+	"validators" => array(
+		array("ValRequired"),
+		array("ValInArray", "values" => array_keys($jobstatus))
+	),
+	"control" => array("SelectMenu", "values" => $jobstatus),
+	"helpstep" => 1
+);
+
+if (isset($_SESSION['customerjobsfiler'])) {
+	$buttons = array(submit_button(_L('Refresh'),"submit","arrow_refresh"));
+} else {
+	$buttons = array(submit_button(_L('Show Jobs'),"submit","magnifier"));	
+}
+$form = new Form("templateform",$formdata,false,$buttons);
+
+////////////////////////////////////////////////////////////////////////////////
+// Form Data Handling
+////////////////////////////////////////////////////////////////////////////////
+
+//check and handle an ajax request (will exit early)
+//or merge in related post data
+$form->handleRequest();
+
+$datachange = false;
+$errors = false;
+//check for form submission
+if ($button = $form->getSubmit()) { //checks for submit and merges in post data
+	$ajax = $form->isAjaxSubmit(); //whether or not this requires an ajax response	
+	
+	if ($form->checkForDataChange()) {
+		$datachange = true;
+	} else if (($errors = $form->validate()) === false) { //checks all of the items in this form
+		$postdata = $form->getData(); //gets assoc array of all values {name:value,...}
+		
+		$_SESSION['customerjobsfiler'] = json_encode($postdata);
+		if ($ajax)
+			$form->sendTo("customersubmittedjobs.php");
+		else
+			redirect("customersubmittedjobs.php");
+	}
+}
+
+if (isset($_SESSION['customerjobsfiler'])) {
+	$customers = QuickQueryList("select id, urlcomponent from customer",true);
+
+	$res = Query("select id, dbhost, dbusername, dbpassword from shard order by id");
+	$shards = array();
+	while($row = DBGetRow($res)){
+		$dsn = 'mysql:dbname=aspshard;host='.$row[1];
+		$db = new PDO($dsn, $row[2], $row[3]);
+		$db->setAttribute(PDO::ATTR_EMULATE_PREPARES, true);
+		$shards[$row[0]] = $db;
+	}
+
+	$schedjobs = array();
+	$extrasql = "";
+
+	if(isset($_GET['cid'])){
+		$customerid = $_GET['cid'] + 0;
+		$extrasql .= " and j.customerid = $customerid ";
+		if(isset($_GET['user'])){
+			$userid = $_GET['user'] + 0;
+			$extrasql .= " and j.userid = $userid ";
+		}
+	}
+
+	foreach ($shards as $shardid => $sharddb) {
+		Query("use aspshard", $sharddb);
+		QuickUpdate("set time_zone='GMT'",$sharddb);
+		$args=array($settings['jobstatus']);
+		if ($settings['jobstatus'] == "processing") {
+			$extrasql .= " and status='procactive' or";
+		} else {
+			$extrasql .= " and";
+		}
+		$query = "select systempriority, customerid, id, startdate, starttime, timezone,
+				convert_tz(addtime(startdate,starttime),timezone,'SYSTEM') systemstarttime
+				from qjob j where 1 $extrasql status=? 
+				order by systemstarttime , systempriority, customerid, id";				
+		$res = Query($query,$sharddb, array($settings['jobstatus']));
+		while ($row = DBGetRow($res)) {
+			$secondstostart = strtotime($row[6]) - time();
+			$seconds = abs($secondstostart);
+			$days = floor($seconds/86400);
+			$seconds -= $days*86400;
+			$hours = floor($seconds/3600);
+			$seconds -= $hours*3600;
+			$minutes = floor($seconds/60);
+			$seconds -= $minutes*60;
+
+			$timetorun = str_pad($hours,2, "0",STR_PAD_LEFT) . ":" . str_pad($minutes,2, "0",STR_PAD_LEFT) . ":" . str_pad($minutes,2, "0",STR_PAD_LEFT) . ($days ? " + $days Days" : "");
+			if($secondstostart < 0)
+			$timetorun = " - " . $timetorun;
+
+			$schedjobs[$secondstostart][] = array ($row[1], $customers[$row[1]], $row[2], $row[3], $row[4], $row[5], $timetorun);
+		}
+	}
+}
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Display Functions
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+
+function fmt_custurl($row, $index){
+	$url = "<a href=\"customerlink.php?id=" . $row[0] ."\" target=\"_blank\">" . $row[1] . "</a>";
+	return $url;
+}
+
+
+//index 0 is jobid
+function fmt_play_jobs($row, $index){
+	$url = "";
+	if($row[0])
+	$url = "<a onclick='popup(\"customerplaymessage.php?customerid=" . $row[0] . "&jobid=" . $row[2] . "\", 400, 500); return false;' href=\"#\" alt='' title='Play Message'><img src='mimg/s-play.png' border=0></a>";
+	return $url;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+// Display
+////////////////////////////////////////////////////////////////////////////////
+$TITLE = _L('Customer Jobs');
+
+include_once("nav.inc.php");
+
+// Optional Load Custom Form Validators
+?>
+<script type="text/javascript">
+<? Validator::load_validators(array()); ?>
+</script>
+<?
+
+startWindow(_L('Scheduled/Processing Jobs Filter'));
+echo $form->render();
+endWindow();
+
+if (isset($_SESSION['customerjobsfiler'])) {
+	startWindow(_L('Jobs'));
+	$titles = array ("Customer id",
+					"Customer url",
+					"Job id",
+					"Start Date",
+					"Start Time",
+					"Timezone",
+					"Time until run",
+					"Play Message"
+				);
+	ksort($schedjobs);
+	$scheddata = array();
+	foreach ($schedjobs as $schedstart => $schedjob) 
+		foreach ($schedjob as $job)
+			$scheddata[] = $job;
+	
+	echo "<hr>{$jobstatus[$settings['jobstatus']]} jobs: <table border=\"1\">";
+	showTable($scheddata, $titles, array(1 => "fmt_custurl",7 => "fmt_play_jobs"));
+	echo "</table>";
+	endWindow();
+}
+
+include_once("navbottom.inc.php");
+?>
