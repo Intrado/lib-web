ca155ea0e915de588d4b1640dc9f1838f3c5bcc6
Nickolas Heckman <nheckman@schoolmessenger.com>
2011-09-15 16:35:01 +0000

    Instead of throwing exceptions, return errors.

--- manager/JmxClient.obj.php
+++ manager/JmxClient.obj.php
@@ -8,22 +8,22 @@
 class JmxClient {
 	var $jolokiaUrl;
 	
+	// Initialize the client with the remote host's jetty url. Ex: http://localhost:8086
 	function JmxClient($jettyUrl) {
 		$this->jolokiaUrl = "$jettyUrl/jolokia";
 	}
 	
+	// read the value of an mbean, or an individual attribute and return it.
 	function read($mbean, $attribute = false) {
-		$response = $this->_request($mbean, 'read', $attribute);
-		if ($response)
-			return $response['value'];
-		else
-			return false;
+		return $this->_request($mbean, 'read', $attribute);
 	}
 	
+	// execute an operation, providing an array of the required arguments.
 	function exec($mbean, $operation, $arguments = array()) {
 		return $this->_request($mbean, 'exec', false, $operation, $arguments);
 	}
 	
+	// handle all request types via http post
 	private function _request($mbean, $type, $attribute = false, $operation = false, $arguments = array()) {
 		$postdata = array(
 				'method' => 'POST',
@@ -39,15 +39,30 @@ class JmxClient {
 		$http = array(
 				'method' => 'POST',
 				'content' => json_encode($postdata));
+		
+		// turn on error tracking so we can return the errors generated
+		ini_set('track_errors', 1);
+		
 		$ctx = stream_context_create(array('http' => $http));
 		$fp = @fopen($this->jolokiaUrl, 'rb', false, $ctx);
-		if (!$fp)
-			throw new Exception("Problem with {$this->jolokiaUrl}, $php_errormsg");
-		$response = @stream_get_contents($fp);
-		if ($response === false)
-			throw new Exception("Problem reading data from {$this->jolokiaUrl}, $php_errormsg");
 		
-		return json_decode($response, true);
+		$result = array();
+		if (!$fp) {
+			$result["error"] = $php_errormsg;
+		} else {
+			$response = @stream_get_contents($fp);
+			if ($response === false)
+				$result["error"] = $php_errormsg;
+		}
+		// turn error tracking back off
+		ini_set('track_errors', 0);
 		
+		if (isset($result['error']))
+			return $result;
+		
+		$resdata = json_decode($response, true);
+		$result['value'] = $resdata['value'];
+		
+		return $result;
 	}
 }
