4a87b4cc7f13fddafa656ffa21fe4d15280be503
Ben Hencke <bhencke@schoolmessenger.com>
2011-10-13 16:59:19 +0000

    prevent php from messing with cache headers while using session

--- css.php
+++ css.php
@@ -1,6 +1,8 @@
 <?
 $SESSION_READONLY = true;
 
+session_cache_limiter(false); //disable automatic cache headers when sessions are used
+
 header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire in 1 hour, but if theme changes so will hash pointing to this file
 header("Content-Type: text/css");
 header("Cache-Control: private");
