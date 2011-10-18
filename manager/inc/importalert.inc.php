fc504964cf4b8104964928db5bdf90550f4761ba
Martin Arnberg <marnberg@schoolmessenger.com>
2011-10-18 22:22:18 +0000

    Add missing include file for import alert

---
+++ manager/inc/importalert.inc.php
@@ -0,0 +1,23 @@
+<?
+
+function formatAlert($name, $operation, $testvalue, $actualvalue) {
+	switch($name) {
+		case "daysold":
+			$str = "Import is delayed. Expected import within {$testvalue} days. Triggered at {$actualvalue} days";
+			break;
+		case "size":
+			$str = "File size is too " . ($operation=="gt"?"big":"small") .
+						 ". Expected " . ($operation=="gt"?"less":"more") . " than {$testvalue} bytes. Actual size: {$actualvalue} bytes";
+			break;
+		case "importtime":
+			$midnight_today = mktime(0,0,0);
+			$testvalue = date("g:i a",$midnight_today + $testvalue);
+			$actualvalue = date("g:i a",$midnight_today + $actualvalue);
+			$str = "Imported too " . ($operation=="gt"?"late":"early") .
+				". Expected import " . ($operation=="gt"?"before":"after") . " $testvalue. Imported at: $actualvalue";
+			break;
+	}
+	return $str;
+}
+
+?>
