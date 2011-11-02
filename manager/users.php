83412576df0444b1b76b06a6bc32a76709f7bba5
Martin Arnberg <marnberg@schoolmessenger.com>
2011-11-02 17:04:54 +0000

    Add link to show soft deleted manager users

--- manager/users.php
+++ manager/users.php
@@ -19,18 +19,27 @@ if (!$MANAGERUSER->authorized("superuser"))
 ////////////////////////////////////////////////////////////////////////////////'
 if (isset($_GET['delete'])) {
 	QuickUpdate("update aspadminuser set deleted=1 where id=?",false,array($_GET['delete']));
-	notice(_L("User deleted"));
+	notice(_L("User Deleted"));
 }
-
-
+if (isset($_GET['undelete'])) {
+	QuickUpdate("update aspadminuser set deleted=0 where id=?",false,array($_GET['undelete']));
+	notice(_L("User Undeleted"));
+}
+$showdeleted = isset($_GET["deleted"]);
 
 ////////////////////////////////////////////////////////////////////////////////
 // Formatters
 ////////////////////////////////////////////////////////////////////////////////
 function fmt_user_actions ($obj, $name) {
+	global $showdeleted;
 	$actionlinks = array();
 	$actionlinks[] = action_link("Edit", "pencil","user.php?id=$obj->id");
-	$actionlinks[] = action_link("Delete", "cross","users.php?delete=$obj->id","return confirmDelete();");
+	
+	if (!$showdeleted)
+		$actionlinks[] = action_link("Delete", "cross","users.php?delete=$obj->id","return confirmDelete();");
+	else
+		$actionlinks[] = action_link("Undelete", "accept","users.php?undelete=$obj->id");
+	
 	return action_links($actionlinks);
 }
 
@@ -38,7 +47,7 @@ function fmt_user_actions ($obj, $name) {
 // Data
 ////////////////////////////////////////////////////////////////////////////////
 
-$users = DBFindMany("AspAdminUser", "from aspadminuser where deleted=0 order by login");
+$users = DBFindMany("AspAdminUser", "from aspadminuser where deleted=? order by login",false,array($showdeleted?1:0));
 
 ////////////////////////////////////////////////////////////////////////////////
 // Display
@@ -48,7 +57,7 @@ include_once("nav.inc.php");
 startWindow(_L("Users"));
 
 echo "<div style='padding:10px'>" . icon_button("Add User", "add",null,"user.php?id=new") . "</div><br /><hr />";
-
+echo $showdeleted?"<a href='users.php'>Show Active Users</a>":"<a href='users.php?deleted'>Show Deleted Users</a>";
 if ($users) {
 	$titles = array(
 			"firstname" => "#First Name",
