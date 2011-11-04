dcf0c8040b44e52619f9d57edf0e11fa5cd43adb
Martin Arnberg <marnberg@schoolmessenger.com>
2011-11-04 22:50:16 +0000

    Fix domain validation and remove code duplication.

--- inc/utils.inc.php
+++ inc/utils.inc.php
@@ -246,35 +246,10 @@ function base64url_decode($string) {
 }
 
 function getDomainRegExp() {
-    ##################################################################################
-	# Beginning of Creative Commons Email Parser Code
-	##################################################################################
-
-	$qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]';
-
-	$dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]';
-
-	$atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+';
-
-    $quoted_pair = '\\x5c[\\x00-\\x7f]';
-
-    $domain_literal = "\\x5b(?:$dtext|$quoted_pair)*\\x5d";
-
-    $quoted_string = "\\x22(?:$qtext|$quoted_pair)*\\x22";
-
-    $domain_ref = $atom;
-
-    $sub_domain = "(?:$domain_ref|$domain_literal)";
-
-    $word = "(?:$atom|$quoted_string)";
-	// original code allows a domain to only contain a single sub_domain.
-	// changed to require 2 domain parts ex.  "example.com"  instead of just "example"
-    $domain = "(?:$sub_domain(?:\\x2e$sub_domain)+)";
-
-	return $domain;
+	return getEmailRegExp(true);
 }
 
-function getEmailRegExp() {
+function getEmailRegExp($domainonly = false) {
 	#
     # RFC822 Email Parser
     #
@@ -301,7 +276,7 @@ function getEmailRegExp() {
 
     $quoted_string = "\\x22(?:$qtext|$quoted_pair)*\\x22";
 
-    $domain_ref = $atom;
+	$domain_ref = '[^\\x00-\\x2c\\x2e\\x2f\\x3a-\\x40\\x5b-\\x5e\\x60\\x7b-\\xff]+';
 
     $sub_domain = "(?:$domain_ref|$domain_literal)";
 
@@ -309,6 +284,9 @@ function getEmailRegExp() {
 	// original code allows a domain to only contain a single sub_domain.
 	// changed to require 2 domain parts ex.  "example.com"  instead of just "example"
     $domain = "(?:$sub_domain(?:\\x2e$sub_domain)+)";
+	
+	if ($domainonly)
+		return $domain;
 
     $local_part = "$word(?:\\x2e$word)*";
 
