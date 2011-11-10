3d581e77218bdefae7198b59a5b83fedcd0c0882
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2011-11-10 19:42:09 +0000

    fix without any redirect() bugfix 0004574: kona "Oops, sorry service unavailable" page when authserver is down

--- inc/auth.inc.php
+++ inc/auth.inc.php
@@ -24,7 +24,20 @@ function pearxmlrpc($method, $params, $returndata = false) {
 	}
 	
 	if (!$resp) {
-		redirect("serviceunavailable.php"); // authserver down, exit now
+		header('HTTP/1.1 503 Service Temporarily Unavailable');
+		echo "
+			<html>
+				<head>
+					<title>503 Service Temporarily Unavailable</title>
+				</head>
+				<body>
+					<h1>Service Temporarily Unavailable</h1>
+
+					The server is temporarily unable to service your request. Please try again later.
+				</body>
+			</html>
+			";
+		exit();
 	} else if ($resp->faultCode()) {
 		error_log($method . ' Fault Code: ' . $resp->faultCode() . ' Fault Reason: ' . $resp->faultString());
 	} else {
--- parentportal/authportal.inc.php
+++ parentportal/authportal.inc.php
@@ -23,7 +23,20 @@ function pearxmlrpc($method, $params) {
 	}
 	
 	if (!$resp) {
-		redirect("serviceunavailable.php"); // authserver down, exit now
+		header('HTTP/1.1 503 Service Temporarily Unavailable');
+		echo "
+			<html>
+				<head>
+					<title>503 Service Temporarily Unavailable</title>
+				</head>
+				<body>
+					<h1>Service Temporarily Unavailable</h1>
+
+					The server is temporarily unable to service your request. Please try again later.
+				</body>
+			</html>
+			";
+		exit();
 	} else if ($resp->faultCode()) {
 		error_log($method . ' Fault Code: ' . $resp->faultCode() . ' Fault Reason: ' . $resp->faultString());
 	} else {
