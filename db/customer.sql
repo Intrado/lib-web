4aebf61cfe32e9a32b74406c597c266cc94a7362
Ben Hencke <bhencke@schoolmessenger.com>
2011-11-28 18:56:14 +0000

    proper statement separator

--- db/customer.sql
+++ db/customer.sql
@@ -1726,5 +1726,6 @@ $$$
 INSERT INTO fieldmap (id, fieldnum, name, options) VALUES 
 	(NULL , '$d01', '%Date%', 'text,systemvar'),
 	(NULL , '$d02', '%Tomorrow\'s Date%', 'text,systemvar'),
-	(NULL , '$d03', '%Yesterday\'s Date%', 'text,systemvar');
+	(NULL , '$d03', '%Yesterday\'s Date%', 'text,systemvar')
+$$$
 -- END 8.2/1
