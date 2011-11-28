ca52a2e1c8cb5af25217eb1cd3b4e48b92bb08ce
Ben Hencke <bhencke@schoolmessenger.com>
2011-11-28 18:55:23 +0000

    8.2/1

--- db/customer.sql
+++ db/customer.sql
@@ -1719,3 +1719,12 @@ $$$
 update setting set value='8.1/10' where name='_dbversion'
 $$$
 -- END REV 8.1/10
+
+
+
+-- START 8.2/1
+INSERT INTO fieldmap (id, fieldnum, name, options) VALUES 
+	(NULL , '$d01', '%Date%', 'text,systemvar'),
+	(NULL , '$d02', '%Tomorrow\'s Date%', 'text,systemvar'),
+	(NULL , '$d03', '%Yesterday\'s Date%', 'text,systemvar');
+-- END 8.2/1
