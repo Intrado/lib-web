1c36584074307db70c8a55df3d3256e7033ae5e2
Martin Arnberg <marnberg@schoolmessenger.com>
2011-11-29 12:12:27 +0000

    Adding monitor and monitorfilter tables set rev to 8.2/2

--- build/upgrades/db_8-2.php
+++ build/upgrades/db_8-2.php
@@ -7,7 +7,9 @@ function upgrade_8_2 ($rev, $shardid, $customerid, $db) {
 		case 0:
 			echo "|";
 			apply_sql("upgrades/db_8-2_pre.sql", $customerid, $db, 1);
-			
+		case 1:
+			echo "|";
+			apply_sql("upgrades/db_8-2_pre.sql", $customerid, $db, 2);
 	}
 	
 	return true;	
--- build/upgrades/db_8-2_pre.sql
+++ build/upgrades/db_8-2_pre.sql
@@ -6,5 +6,23 @@ INSERT INTO fieldmap (id, fieldnum, name, options) VALUES
 	(NULL , '$d03', '%Yesterday\'s Date%', 'text,systemvar');
 	
 	
+-- $rev 2
+
+CREATE TABLE `monitor` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`userid` int(11) NOT NULL,
+	`type` enum('job-active','job-firstpass','job-complete') NOT NULL,
+	`action` enum('email') NOT NULL DEFAULT 'email',
+	PRIMARY KEY (`id`),
+	KEY `type` (`type`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8;
 
 
+CREATE TABLE `monitorfilter` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`monitorid` int(11) NOT NULL,
+	`type` enum('userid','jobtypeid') NOT NULL,
+	`val` text,
+	PRIMARY KEY (`id`),
+	KEY `monitorid` (`monitorid`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
