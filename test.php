f7e97ab504246ad20ea210783f189e12b6a20180
Ben Hencke <bhencke@schoolmessenger.com>
2011-12-30 22:24:38 +0000

    memcacheify the start page for complete job stats and list size calculations (and invalidating on list mod date changing). Active jobs stats not cached since these are expected to change continously.

--- test.php
+++ test.php
@@ -1,5 +1,28 @@
-<?
+<pre><?
 
 
+//$SESSION_READONLY = true;
 
-?>
+require_once("inc/common.inc.php");
+
+class MyObject {
+	var $foo = "foo";
+	var $bar = "bar";
+}
+
+
+function generateSomething($value) {
+	sleep(1);
+	
+	$o = new MyObject();
+	$o->foo = $value;
+	
+	return $o;
+}
+
+
+$value = gen2cache(60, null, "generateSomething", 55);
+
+var_dump($value);
+
+?></pre>
