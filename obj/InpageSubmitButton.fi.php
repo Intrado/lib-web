aa032b0058b831e61ad2183d62fe0372a5a58495
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-01-13 19:36:23 +0000

    Allow setting the submit value when using the inpage submit button.

--- obj/InpageSubmitButton.fi.php
+++ obj/InpageSubmitButton.fi.php
@@ -3,7 +3,8 @@ class InpageSubmitButton extends FormItem {
 	function render ($value) {
 		$n = $this->form->name."_".$this->name;
 		$str = '<input id="'.$n.'" name="'.$n.'" type="hidden" value=""/>';
-		return $str.submit_button($this->args['name'], 'inpagesubmit', $this->args['icon']);
+		$submitvalue = (isset($this->args['submitvalue'])?$this->args['submitvalue']:'inpagesubmit');
+		return $str.submit_button($this->args['name'], $submitvalue, $this->args['icon']);
 	}
 }
 ?>
