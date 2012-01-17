ecf621164ef5aa31795b519fd4a3e7fc1dc489c0
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-01-17 23:01:51 +0000

    Provide links in the title and labels. Autoplay swf and mp3 content.

--- page/feedwidget.js
+++ page/feedwidget.js
@@ -20,11 +20,24 @@ function genFeed() {
 		
 		// get the rss xml
 		var feedxml = feeddata.responseXML.getElementsByTagName("rss")[0];
-		
+
 		// find the main title and add it to the document
 		var feedtitle = document.createElement("h2");
 		feedtitle.setAttribute("style", vars.head);
-		feedtitle.appendChild(document.createTextNode(feedxml.getElementsByTagName("title")[0].childNodes[0].nodeValue));
+		var title = document.createTextNode(feedxml.getElementsByTagName("title")[0].childNodes[0].nodeValue);
+		
+		// get/set the link (if there is one)
+		var feedlink = feedxml.getElementsByTagName("link")[0];
+		if (feedlink.firstChild) {
+			var href = document.createElement("a");
+			href.setAttribute("href",feedlink.firstChild.nodeValue);
+			href.setAttribute("target","_blank");
+			href.appendChild(title)
+			feedtitle.appendChild(href);
+		} else {
+			feedtitle.appendChild(title);
+		}
+		
 		feeddiv.appendChild(feedtitle);
 		
 		// put the feed items in a list
@@ -38,6 +51,8 @@ function genFeed() {
 		var feeditemmedia;
 		var feeditemdescription;
 		var descdiv;
+		var itemtitle;
+		var itemlabel;
 		var linkhref;
 		var mediadiv = null;
 		var mediahref;
@@ -46,25 +61,37 @@ function genFeed() {
 		var mp3url;
 		var itemdiscription;
 		for (var i = 0; i < feeditems.length; i++) {
-			// add the label for the item
-			itemli = document.createElement("li");
-			itemli.appendChild(document.createTextNode(feeditems[i].getElementsByTagName("title")[0].firstChild.nodeValue));
-			// insert the description
+			// get the label for the item
+			itemtitle = document.createTextNode(feeditems[i].getElementsByTagName("title")[0].firstChild.nodeValue);
+			// create the description
 			descdiv = document.createElement("div");
-			descdiv.setAttribute("style",vars.desc)
+			descdiv.setAttribute("style",vars.desc);
 			descdiv.appendChild(document.createTextNode(feeditems[i].getElementsByTagName("description")[0].firstChild.nodeValue));
 			// add link if there is one
 			feeditemlink = feeditems[i].getElementsByTagName("link")[0];
 			if (feeditemlink.firstChild) {
+				// add a link to the label
+				itemlabel = document.createElement("a");
+				itemlabel.setAttribute("href",feeditemlink.firstChild.nodeValue);
+				itemlabel.setAttribute("target","_blank");
+				itemlabel.setAttribute("style","color:inherit;");
+				itemlabel.appendChild(itemtitle);
+				
+				// add a link to the description
 				linkhref = document.createElement("a");
 				linkhref.setAttribute("href",feeditemlink.firstChild.nodeValue);
 				linkhref.setAttribute("target","_blank");
-				linkhref.appendChild(document.createTextNode("More..."));
+				linkhref.appendChild(document.createTextNode("Details..."));
 				descdiv.appendChild(document.createTextNode("  "));
 				descdiv.appendChild(linkhref);
+			} else {
+				itemlabel = itemtitle;
 			}
-				
+			// add a list item
+			itemli = document.createElement("li");
+			itemli.appendChild(itemlabel);
 			itemli.appendChild(descdiv);
+			
 			// find the media items
 			feeditemmediagroup = feeditems[i].getElementsByTagName("media:group")[0];
 			if (feeditemmediagroup == undefined) // WebKit, Gecko
@@ -82,7 +109,7 @@ function genFeed() {
 					if (feeditemmedia[m].attributes.getNamedItem("type").value == "audio/mpeg")
 						mp3url = feeditemmedia[m].attributes.getNamedItem("url").value;
 					else if (feeditemmedia[m].attributes.getNamedItem("type").value == "application/x-shockwave-flash")
-						swfurl = feeditemmedia[m].attributes.getNamedItem("url").value;
+						swfurl = feeditemmedia[m].attributes.getNamedItem("url").value + "&as=1";
 				}
 				
 				if (swfurl || mp3url ) {
@@ -124,7 +151,7 @@ function insertPlayerObject(container,swfurl,mp3url) {
 	if (hasflash && swfurl) {
 		container.innerHTML = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="165" height="38"><param name="movie" value="'+swfurl+'" /><object type="application/x-shockwave-flash" data="'+swfurl+'" width="165" height="38"><param name="movie" value="'+swfurl+'"/></object></object>';
 	} else if (mp3url) {
-		container.innerHTML = '<object classid="clsid:22D6F312-B0F6-11D0-94AB-0080C74C7E95" width="165" height="45"><param name="type" value="audio/mpeg" /><param name="src" value="'+mp3url+'" /><param name="autostart" value="0" /><object type="audio/mpeg" data="'+mp3url+'" width="165" height="45" autoplay="false"></object></object>';
+		container.innerHTML = '<object classid="clsid:22D6F312-B0F6-11D0-94AB-0080C74C7E95" width="165" height="45"><param name="type" value="audio/mpeg" /><param name="src" value="'+mp3url+'" /><param name="autostart" value="true" /><object type="audio/mpeg" data="'+mp3url+'" width="165" height="45" autoplay="false"></object></object>';
 	}
 }
 
