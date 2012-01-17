be0636af88a5be2c0281ce65b869f6e6c5f7d110
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-01-17 19:26:54 +0000

    New validator for the ColorPicker form item.

---
+++ obj/ColorPicker.val.php
@@ -0,0 +1,23 @@
+<?
+
+class ValColorPicker extends Validator {
+	function validate ($value, $args) {
+		if (!strlen($value))
+			return $this->label." "._L("cannot be blank.");
+		if (!preg_match('/^[0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F]$/', $value))
+			return $this->label." "._L("must be a color.");
+		return true;
+	}
+
+	function getJSValidator () {
+		return
+			'function (name, label, value, args) {
+				if (!value.length)
+					return label + " '.addslashes(_L('cannot be blank.')).'";
+				if (!value.match(/^[0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F][0-9,a-f,A-F]$/))
+					return label + " '.addslashes(_L('must be a color.')).'";
+				return true;
+			}';
+	}
+}
+?>
