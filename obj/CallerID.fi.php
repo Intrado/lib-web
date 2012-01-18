84b0367ab780c4df15fa3b973ec5a21d61e85440
Martin Arnberg <marnberg@schoolmessenger.com>
2012-01-18 01:56:16 +0000

    Adding caller id form item

---
+++ obj/CallerID.fi.php
@@ -0,0 +1,107 @@
+<? 
+
+
+class CallerID extends FormItem {
+
+	function render ($value) {
+		$n = $this->form->name."_".$this->name;
+		$includeSelectMenu = isset($this->args['selectvalues']) && count($this->args['selectvalues']);
+		$includeInputBox =  isset($this->args['allowedit']) && $this->args['allowedit'];
+		
+		$max = isset($this->args['maxlength']) ? 'maxlength="'.$this->args['maxlength'].'"' : "";
+		$size = isset($this->args['size']) ? 'size="'.$this->args['size'].'"' : "";
+		
+		$str = '<input id="'.$n.'" name="'.$n.'" type="hidden" value="'.escapehtml($value).'" '.$max.' '.$size.'/>';
+		$includeInputBoxBox = false;
+		if ($includeSelectMenu) {
+			if (!$includeInputBox && count($this->args['selectvalues']) == 1 && current($this->args['selectvalues']) == $value) {
+				$str .= Phone::format($value);
+				$includeSelectMenu = false;
+			} else {
+				$str .= '<select id="'.$n.'select" name="'.$n.'select">';
+				$str .= '<option value="" ' . ($value == ""?'selected':'') . ' > -- ' . _L('Select Caller ID') . ' -- </option>';
+				$notInList = !in_array($value, $this->args['selectvalues']);
+				if ($includeInputBox) {
+					$includeInputBoxBox = $notInList;
+					$str .= '<option value="other" ' . ($includeInputBoxBox?'selected':''). '>' . _L('Other') . '</option>';
+				} else {
+					if ($notInList) {
+						$str .= '<option value="' . $value . '" selected disabled>' . escapehtml(Phone::format($value)) . '</option>';
+					}
+				}
+				
+				foreach ($this->args['selectvalues'] as $selectvalue) {
+					$checked = $value == $selectvalue;
+					$str .= '<option value="'.escapehtml($selectvalue).'" '.($checked ? 'selected' : '').' >'.escapehtml(Phone::format($selectvalue)).'</option>';
+				}
+				$str .= '</select>';
+			}
+		} else {
+			$includeInputBoxBox = true;
+		}
+		
+		if ($includeInputBox) {
+			$str .= '&nbsp;<input id="'.$n.'freeform" name="'.$n.'freeform" type="text" value="'. ($includeInputBoxBox?escapehtml(Phone::format($value)):'') .'" '.$max.' '.$size.' style="' . (!$includeInputBoxBox?'display:none':''). ';"/>';
+		}
+		return $str;
+	}
+
+	function renderJavascript($value) {
+		$n = $this->form->name."_".$this->name;
+		$includeSelectMenu = isset($this->args['selectvalues']) && count($this->args['selectvalues']);
+		$includeInputBox =  isset($this->args['allowedit']) && $this->args['allowedit'];
+		
+		// Remove select menu when only one item and no freeform is allowed
+		$includeSelectMenu = !(!$includeInputBox && count($this->args['selectvalues']) == 1 && current($this->args['selectvalues']) == $value);
+		
+		$str = '';
+		if ($includeSelectMenu) {
+			if ($includeInputBox) {
+				$str .= '
+					$(\''.$n.'select\').observe("change",function(event) {
+						if($(\''.$n.'select\').value==\'other\') {
+							$(\''.$n.'freeform\').show();
+						} else {
+							$(\''.$n.'freeform\').hide();
+							$(\''.$n.'\').value = $(\''.$n.'select\').value;
+							form_do_validation($("' . $this->form->name . '"), $("' . $n . '"));
+						} 
+					});';
+			} else {
+				$str .= '
+					$(\''.$n.'select\').observe("change",function(event) {
+						$(\''.$n.'\').value = $(\''.$n.'select\').value;
+						form_do_validation($("' . $this->form->name . '"), $("' . $n . '"));
+					});';
+			}
+		
+		}
+		if ($includeInputBox) {
+			$str .= '
+				$(\''.$n.'freeform\').observe("change",function(event) {
+					e = event.element();
+					$(\''.$n.'\').value = e.value;
+					form_do_validation($("' . $this->form->name . '"), $("' . $n . '"));
+				});';
+		}
+		
+		return $str;
+	}
+}
+
+
+class ValCallerID extends Validator {
+	var $onlyserverside = true;
+
+	function validate ($value, $args) {
+		global $USER;
+		$callerid = Phone::parse($value);
+		if (!canSetCallerid($callerid)) {
+			return _L('Callerid is not Authorized');
+		}
+		return true;
+
+	}
+}
+
+?>
