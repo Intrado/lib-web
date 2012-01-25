50fc077ba74c494ef7b2cd32e970ebb5e3a5a842
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-01-25 23:51:12 +0000

    Custom validator for text area form item with check box.

---
+++ obj/TextAreaWithEnableCheckbox.val.php
@@ -0,0 +1,52 @@
+<?
+/* Validator for TextAreaWithEnableCheckbox form item
+ *  You can have an empty value and it will be valid
+ *  non-empty value is of format:
+ *    {"subject":<subject text>,"message":<message text>}
+ *  
+ * Possible args
+ *  requiresubject - subject must be filled out if a value is present
+ *  
+ * Nickolas Heckman
+ */
+class ValTextAreaWithEnableCheckbox extends Validator {
+	function validate ($value, $args) {
+		// if the value is blank, that's fine. ValRequired should prevent this when it's required.
+		if ($value == "")
+			return true;
+		
+		// get the data
+		$jsvalue = json_decode($value, true);
+		
+		if (isset($args['requiresubject']) && $args['requiresubject'] && !$jsvalue['subject'])
+			return "$this->label: ". escapehtml(_L('subject is required'));
+		
+		if (!$jsvalue['message'])
+			return "$this->label: ". escapehtml(_L('message text is required'));
+		
+		return true;
+	}
+	
+	function getJSValidator () {
+		return '
+			function (name, label, value, args) {
+				// if blank, no problem
+				if (value == "")
+					return true;
+				
+				// parse the data
+				var jsvalue = value.evalJSON(true);
+				
+				if (args.requiresubject && !jsvalue.subject)
+					return label + " " +"'.escapehtml(_L('subject is required')).'";
+				
+				if (!jsvalue.message)
+					return label + " " +"'.escapehtml(_L('message text is required')).'";
+				
+				return true;
+			}
+		';
+	}
+}
+
+?>
