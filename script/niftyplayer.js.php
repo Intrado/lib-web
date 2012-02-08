af6fdf0755f588196b83d1b01d2aefbc970d1011
Martin Arnberg <marnberg@schoolmessenger.com>
2012-02-08 00:45:53 +0000

    Use same flash detection as on feedwidget for embedplayer to fix flash detection for safari. Fix flahs branding

--- script/niftyplayer.js.php
+++ script/niftyplayer.js.php
@@ -4,6 +4,26 @@ header("Expires: " . gmdate('D, d M Y H:i:s', time() + 60*60) . " GMT"); //exire
 header("Content-Type: text/javascript");
 header("Cache-Control: private");
 ?>
+// borrowed and modified from http://www.kirupa.com/developer/mx/detection.htm
+var hasflash = false;
+var contentVersion = 8;
+var plugin = (navigator.mimeTypes && navigator.mimeTypes["application/x-shockwave-flash"]) ? navigator.mimeTypes["application/x-shockwave-flash"].enabledPlugin : 0;
+if (plugin) {
+	var words = navigator.plugins["Shockwave Flash"].description.split(" ");
+	for (var i = 0; i < words.length; ++i) {
+		if (isNaN(parseInt(words[i])))
+			continue;
+		var pluginVersion = words[i]; 
+	}
+	hasflash = pluginVersion >= contentVersion;
+} else if (navigator.userAgent && navigator.userAgent.indexOf("MSIE")>=0 && (navigator.appVersion.indexOf("Win") != -1)) {
+	document.write('<SCR' + 'IPT LANGUAGE=VBScript\> \n'); //FS hide this from IE4.5 Mac by splitting the tag
+	document.write('on error resume next \n');
+	document.write('hasflash = ( IsObject(CreateObject("ShockwaveFlash.ShockwaveFlash." & contentVersion)))\n');
+	document.write('</SCR' + 'IPT\> \n');
+}
+
+
 // Script for NiftyPlayer 1.7, by tvst from varal.org
 // Released under the MIT License: http://www.opensource.org/licenses/mit-license.php
 
@@ -112,52 +132,49 @@ function niftyplayer(name)
  * Embed the NiftyPlayer and load audio if flash player was embedded sucessfully. Otherwise use the native mp3 player.
  */
 
-function hasFlashPlayer(){
-	var ischrome = navigator.userAgent.toLowerCase().indexOf('chrome') > -1;
-	return ischrome || (typeof(FlashHelper.getMovie('niftyPlayer1')) != 'undefined' && typeof(FlashHelper.getMovie('niftyPlayer1').PercentLoaded) != 'undefined');
-}
-
 function embedPlayer(url,target,parts) {
-	var requestfiles = url;
-	if(typeof(parts) != "undefined") {
-		if(parts > 1) {
-			var tmp = [];
-			tmp.push(url + '&partnum=1');
-			var urlpart = '|'+ url + '&partnum=';
-			for(var i=2; i <= parts;i++) {
-				tmp.push(i);
+	if(hasflash) {
+		var requestfiles = url;
+		if(typeof(parts) != "undefined") {
+			if(parts > 1) {
+				var tmp = [];
+				tmp.push(url + '&partnum=1');
+				var urlpart = '|'+ url + '&partnum=';
+				for(var i=2; i <= parts;i++) {
+					tmp.push(i);
+				}
+				requestfiles = tmp.join(urlpart);
+			} else {
+				requestfiles = url + '&partnum=1';
 			}
-			requestfiles = tmp.join(urlpart);
-		} else {
-			requestfiles = url + '&partnum=1';
-		}
-	} 
-	$(target).update('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="<?= isset($_SERVER['HTTPS'])?"https":"http" ?>://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="165" height="38" id="niftyPlayer1" align="">' +
-		'<param name=movie value="media/niftyplayer.swf?file=' + encodeURIComponent(requestfiles)  + '&as=1">' + 
-		'<param name=quality value=high>' + 
-		'<param name=bgcolor value=#FFFFFF>' + 
-		'<embed src="media/niftyplayer.swf?file=' + encodeURIComponent(requestfiles) + '&as=1" quality=high bgcolor=#FFFFFF width="165" height="38" name="niftyPlayer1" align="" type="application/x-shockwave-flash" pluginspage="<?= isset($_SERVER['HTTPS'])?"https":"http" ?>://get.adobe.com/flashplayer">' +
-		'</embed>' + 
-		'</object>');
-if(!hasFlashPlayer()) {
+		} 
+		$(target).update('<object classid="clsid:D27CDB6E-AE6D-11cf-96B8-444553540000" codebase="<?= isset($_SERVER['HTTPS'])?"https":"http" ?>://download.macromedia.com/pub/shockwave/cabs/flash/swflash.cab#version=8,0,0,0" width="165" height="38" id="niftyPlayer1" align="">' +
+			'<param name=movie value="media/niftyplayer.swf?file=' + encodeURIComponent(requestfiles)  + '&as=1">' + 
+			'<param name=quality value=high>' + 
+			'<param name=bgcolor value=#FFFFFF>' + 
+			'<embed src="media/niftyplayer.swf?file=' + encodeURIComponent(requestfiles) + '&as=1" quality=high bgcolor=#FFFFFF width="165" height="38" name="niftyPlayer1" align="" type="application/x-shockwave-flash" pluginspage="<?= isset($_SERVER['HTTPS'])?"https":"http" ?>://get.adobe.com/flashplayer">' +
+			'</embed>' + 
+			'</object>');
+	} else {
 <?
-
-$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
-
-if($android) {
+		$android = strpos($_SERVER['HTTP_USER_AGENT'],"Android");
+		if($android) {
 ?>
-	$(target).update("Unable to play message. Please install Flash for Android 2.2 or higher or click the link to download the message.");
-<? } else { ?>
-	$(target).update("<object classid='clsid:22D6F312-B0F6-11D0-94AB-0080C74C7E95' width='280' height='45'>"+
-		"<param name='type' value='audio/mpeg'>"+
-		"<param name='src' value='" + url + "'>"+
-		"<param name='autostart' value='1'>"+
-		"<param name='showcontrols' value='1'>"+
-		"<param name='showstatusbar' value='0'>"+
-		"<embed src ='" + url + "' type='audio/mpeg' autoplay='true' autostart='1' width='280' height='45' controller='1' showstatusbar='0' bgcolor='#ffffff'></embed>"+ 
-		"</object><br /><a href='http://get.adobe.com/flashplayer/'>Click here to install or upgrade your flash player</a>");
-<? } ?>
+			$(target).update("Unable to play message. Please install Flash for Android 2.2 or higher or click the link to download the message.");
+<?		} else { ?>
+			$(target).update("<object classid='clsid:22D6F312-B0F6-11D0-94AB-0080C74C7E95' width='280' height='45'>"+
+				"<param name='type' value='audio/mpeg'>"+
+				"<param name='src' value='" + url + "'>"+
+				"<param name='autostart' value='1'>"+
+				"<param name='showcontrols' value='1'>"+
+				"<param name='showstatusbar' value='0'>"+
+				"<embed src ='" + url + "' type='audio/mpeg' autoplay='true' autostart='1' width='280' height='45' controller='1' showstatusbar='0' bgcolor='#ffffff'></embed>"+ 
+				"</object><br /><a href='http://get.adobe.com/flashplayer/'>Click here to install or upgrade your Flash&reg; player</a>");
+<?		} ?>
 	}
 }
+
+
+
 
 
