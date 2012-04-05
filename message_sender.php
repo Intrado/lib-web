cb6190762bf6c6381b912a18aacdef56dc396b7a
Tony Farnell <tony@aw20.co.uk>
2012-04-05 16:40:23 +0000

    new files and set up for the new message sender form

---
+++ message_sender.php
@@ -0,0 +1,14 @@
+<?php 
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+
+include_once("inc/common.inc.php");
+
+$PAGE = "notifications:lists";
+$TITLE = "List Builder";
+
+include("nav.inc.php");
+
+include("message_sender/index.php");
+
+include("navbottom.inc.php"); ?>
---
+++ message_sender/index.php
@@ -0,0 +1,303 @@
+<div class="window messagescreen cf">
+
+	<div class="window_title_wrap">
+	<h3 class="window_title">New Notification</h3>
+	<ul class="msg_steps cf">
+	<li><a id="tab1" class="" href=""><strong>Subject &amp; Recipients</strong> Basic information and recipient lists.</a></li>
+	<li><a id="tab2" class="" href=""><strong>Message Content</strong> Choose existing or add Email, Phone, SMS or Social</a></li>
+	<li><a id="tab3" class="" href=""><strong>Review &amp; Send</strong> Send now, or schedule and send later</a></li>
+	</ul>
+	</div>
+	
+	<div id="msg_section_1" class="window_panel">
+	
+	<h3 class="flag">Notification Info</h3>
+	
+	<form>
+	
+	<fieldset>
+	<label for="form_subject">Subject</label>
+		<div class="controls">
+		<input type="text" id="form_subject" name="subject"/>
+		<p>e.g. "PTA Meeting Reminder"</p>
+		</div>
+	</fieldset>
+	
+	<fieldset class="cf">
+		<label for="form_type">Type</label>
+		<div class="controls">
+		<select id="form_type" name="type">
+		<option value="general">General Announcement</option>
+		<option value="attend">Attendance</option>
+		<option value="emergency">Emergency</option>
+		</select>
+		</div>
+	</fieldset>
+	</form>
+	
+	<h3 class="flag">Recipient Lists</h3>
+	
+	<div class="add_recipients">	
+		<div class="add_btn">
+		<a class="btn" href="">Pick from Existing Lists</a>
+		or
+		<a class="btn" href="">Build a List Using Rules</a>
+		</div>
+	
+		<table class="recipient_lists">
+			<thead>
+				<tr>
+					<th colspan="2">List Name</th>
+					<th>Count</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<td>x</td>
+					<td>List name goes here</td>
+					<td>1000</td>
+				</tr>
+				<tr>
+					<td>x</td>
+					<td>List name goes here</td>
+					<td>1000</td>
+				</tr>
+				<tr>
+					<td colspan="2">Total</td>
+					<td>2000</td>
+				</tr>
+			</tbody>
+		</table>
+	
+		<form class="cf">
+		<input class="addme" type="checkbox" id="form_myself"/><label class="addme" for="form_myself">Add Myself</label>
+		</form>
+	</div><!-- end add_recipients -->
+	
+	<div class="msg_confirm"><a class="btn" href="#">Continue</a></div>
+	
+	</div><!-- end window_panel -->
+	
+	
+	<div id="msg_section_2" class="window_panel">
+	<p>Create the content for this message, or <a href="">load a saved message</a></p>
+	<ul class="msg_content_nav cf">
+	<li><a id="ctrl_phone" href="#">Add <span>Phone</span></a></li>
+	<li><a id="ctrl_email" href="#">Add <span>Email</span></a></li>
+	<li><a id="ctrl_sms" href="#">Add <span>SMS</span></a></li>
+	<li><a id="ctrl_social" href="#">Add <span>Social</span></a></li>
+	</ul>
+	
+	<div class="tab_content">
+		<!-- Add the phone panel -->
+		<div id="tab_phone" class="tab_panel">
+		<form>
+		<fieldset class="check">
+			<label for="form_type">Switch Audio Type</label>
+			<div class="controls">
+			<a class="btn audioleft" href="#">Call Me to Record</a><a class="btn audioright" href="#">Text-to-Speech</a>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_number">Number to Call</label>
+			<div class="controls">
+			<input class="small" type="text" id="form_number" name="form_number"/>
+			<a class="btn record" href="#">Call Now to Record</a>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_scratch">Scratch Pad <span>(optional)</span></label>
+			<div class="controls">
+			<textarea id="form_scratch" name="form_scratch"></textarea>
+			<p>You can use this to write notes about what you'd like to say. This information isn't saved anywhere.</p>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<div class="controls">
+			<a href="#">Advanced Options</a>
+			</div>
+		</fieldset>
+		
+		<fieldset class="form_actions">
+			<div class="controls">
+			<a class="btn" href="#">Save Phone Message</a>
+			<a class="btn" href="#">Cancel</a>
+			</div>
+		</fieldset>
+		</form>
+		</div>
+		
+		<!-- Add the email panel -->
+		<div id="tab_email" class="tab_panel">
+		<form>
+		<fieldset>
+			<label for="form_name">From Name</label>
+			<div class="controls">
+			<input type="text" id="form_name" name="from_name"/>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_email">From Email</label>
+			<div class="controls">
+			<input type="text" id="form_email" name="from_email"/>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_mailsubject">Subject</label>
+			<div class="controls">
+			<input type="text" id="form_mailsubject" name="form_mailsubject"/>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_body">Body</label>
+			<div class="controls">
+			<textarea id="form_body" name="form_body"></textarea>
+			</div>
+		</fieldset>
+		
+		<fieldset>
+			<label for="form_translate">Translate</label>
+			<input type="checkbox" id="form_translate" value=""/>
+			<a href="">Show Translations</a>
+		</fieldset>
+		
+		<fieldset class="form_actions">
+			<div class="controls">
+			<a class="btn" href="#">Save Email Message</a>
+			<a class="btn" href="#">Cancel</a>
+			</div>
+		</fieldset>
+		</form>
+		</div>
+		
+		<!-- Add the sms panel -->
+		<div id="tab_sms" class="tab_panel">
+		<form>
+		<fieldset>
+			<label for="form_sms">SMS Text</label>
+			<div class="controls">
+			<textarea id="form_sms" name="form_sms"></textarea>
+			<p><a href="#">Spell Check</a> <span>160 Characters left</span></p>
+			</div>
+		</fieldset>
+		
+		<fieldset class="form_actions">
+			<div class="controls">
+			<a class="btn" href="#">Save SMS Message</a>
+			<a class="btn" href="#">Cancel</a>
+			</div>
+		</fieldset>
+		</form>
+		</div>
+		
+		<!-- Add the social network panel -->
+		<div id="tab_social" class="tab_panel">
+		<form>
+		<fieldset class="check">
+			<div class="controls">
+			<input class="addme" type="checkbox" id="form_audio" name="form_audio" />
+			<label class="addme" for="form_audio">Include a link to the audio message</label>
+			</div>
+		</fieldset>
+		
+		<fieldset class="check">
+			<div class="controls">
+			<input class="addme" type="checkbox" id="form_facebook" name="form_facebook" />
+			<label class="addme" for="form_facebook"><strong>Post to Facebook</strong></label>
+			</div>
+		</fieldset>
+		
+		<fieldset class="check">
+			<div class="controls">
+			<input class="addme" type="checkbox" id="form_twitter" name="form_twitter" />
+			<label class="addme" for="form_twitter"><strong>Post to Twitter</strong></label>
+			</div>
+		</fieldset>
+		
+		<fieldset class="checklast">
+			<div class="controls">
+			<input class="addme" type="checkbox" id="form_feed" name="form_feed" />
+			<label class="addme" for="form_feed"><strong>Post to Feeds</strong></label>
+			</div>
+		</fieldset>
+		
+		<fieldset class="form_actions">
+			<div class="controls">
+			<a class="btn" href="#">Save Social Messages</a>
+			<a class="btn" href="#">Cancel</a>
+			</div>
+		</fieldset>
+		</form>
+		</div>
+	</div><!-- end tab_content -->
+	
+	<div class="msg_confirm"><a class="btn" href="#">Continue</a></div>
+	
+	</div><!-- end window_panel -->
+	
+	
+	<div id="msg_section_3" class="window_panel">
+	<p><strong>Subject</strong> Holidays Reminder</p>
+	<p><strong>Type</strong> General Annoucement</p>
+	<p><strong>Recipients 2000</strong></p>
+	
+	<div class="msg_confirm"><a href="#">save for later</a> or <a class="btn" href="#">Send Message</a></div>
+	</div><!-- end window_panel -->
+
+</div><!-- end window messagescreen-->
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+<script type="text/javascript">
+$(document).ready(function() {
+	$('#msg_section_2').hide();
+	$('#msg_section_3').hide();
+	$('#tab1').parent().addClass("active");
+
+	$('#tab1').click(function(){
+		$('ul.msg_steps').children().removeClass();
+		$('#tab1').parent().addClass("active");
+		
+		$('#msg_section_1').show();
+		$('#msg_section_2').hide();
+		$('#msg_section_3').hide();
+		return false;
+		});
+
+	$('#tab2').click(function(){
+		$('ul.msg_steps').children().removeClass();
+		$('#tab2').parent().addClass("active");
+		
+		$('#msg_section_1').hide();
+		$('#msg_section_2').show();
+		$('#msg_section_3').hide();
+		return false;
+		});
+
+	$('#tab3').click(function(){
+		$('ul.msg_steps').children().removeClass();
+		$('#tab3').parent().addClass("active");
+		
+		$('#msg_section_1').hide();
+		$('#msg_section_2').hide();
+		$('#msg_section_3').show();
+		return false;
+		});
+
+	$('#tab_phone').hide();
+	$('#tab_email').hide();
+	$('#tab_sms').hide();
+	$('#tab_social').hide();
+
+	$('#ctrl_phone').click(function(){
+		$('#ctrl_phone').parent().addClass("active");
+		return false;
+	});
+});
+</script>
+
