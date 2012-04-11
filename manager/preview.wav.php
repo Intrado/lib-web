2323a9a0ff6bba6505e553db5b5865e149563f31
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-04-11 18:35:20 +0000

    Make phone message preview in manager work again.

--- manager/preview.wav.php
+++ manager/preview.wav.php
@@ -4,12 +4,21 @@ include_once("common.inc.php");
 
 include_once('../inc/securityhelper.inc.php');
 include_once('../inc/content.inc.php');
+include_once('../inc/appserver.inc.php');
 include_once("../obj/Content.obj.php");
 include_once("../obj/Message.obj.php");
 include_once("../obj/MessagePart.obj.php");
 include_once("../obj/AudioFile.obj.php");
 include_once("../obj/Voice.obj.php");
 include_once("../obj/FieldMap.obj.php");
+
+// load the thrift api requirements.
+require_once('thrift/Thrift.php');
+require_once $GLOBALS['THRIFT_ROOT'].'/protocol/TBinaryProtocol.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/transport/TSocket.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/transport/TBufferedTransport.php';
+require_once $GLOBALS['THRIFT_ROOT'].'/transport/TFramedTransport.php';
+require_once($GLOBALS['THRIFT_ROOT'].'/packages/commsuite/CommSuite.php');
 
 session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
 
@@ -27,6 +36,48 @@ if (isset($_GET['id']) && isset($_GET['customerid'])) {
 		if(isset($_REQUEST[$fieldnum]))
 			$fields[$fieldnum] = $_REQUEST[$fieldnum];
 	}
-	Message::playAudio($id, $fields);
+	
+	$parts = DBFindMany('MessagePart', 'from messagepart where messageid=? order by sequence', false, array($id));
+	$renderedparts = Message::renderPhoneParts($parts, $fields);
+	
+	// -- get the wav files --
+	$wavfiles = array();
+	
+	foreach ($renderedparts as $part) {
+		if ($part[0] == "a") {
+			list($contenttype,$data) = contentGetForCustomerId($customerid, $part[1]);
+			$wavfiles[] = writeWav($data);
+		} else if ($part[0] == "t") {
+			$voice = $voices[$part[2]];
+			list($contenttype,$data) = renderTts($part[1],$voice->language,$voice->gender);
+			$wavfiles[] = writeWav($data);
+		}
+	}
+	
+	//finally, merge the wav files
+	$outname = secure_tmpname("preview",".wav");
+	
+	$messageparts = empty($wavfiles)?'':'"' . implode('" "',$wavfiles) . '" ';
+			$cmd = 'sox ' . $messageparts . ' "' . $outname . '"';
+			$result = exec($cmd, $res1, $res2);
+	
+	foreach ($wavfiles as $file)
+	@unlink($file);
+	
+	if (!$res2 && file_exists($outname)) {
+		$data = file_get_contents ($outname); // readfile seems to cause problems
+		header("HTTP/1.0 200 OK");
+		header("Content-Type: audio/wav");
+		if (isset($_GET['download']))
+			header("Content-disposition: attachment; filename=message.wav");
+		header('Pragma: private');
+		header('Cache-control: private, must-revalidate');
+		header("Content-Length: " . strlen($data));
+		header("Connection: close");
+		echo $data;
+	} else {
+		echo _L("An error occurred trying to generate the preview file. Please try again.");
+	}
+	@unlink($outname);
 }
 ?>
