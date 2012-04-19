77224830aef2e010556c1eca6be7b44ae5578a53
Tony Farnell <tony@aw20.co.uk>
2012-04-19 14:07:45 +0000

    style added to updated login form

--- manager/index.php
+++ manager/index.php
@@ -49,6 +49,28 @@ if(isset($_POST["submit"])) {
 <head>
 	<title>Manager Login</title>
 	<link rel="SHORTCUT ICON" href="mimg/manager_favicon.ico" />
+	
+	<style>
+	em { position: absolute; top: 170px; left: 50%; width: 330px; margin-left: -165px; text-align: center; }
+	.incorrect { position: absolute; top: 170px; left: 50%; width: 330px; margin-left: -165px; text-align: center; color: red; }
+	form { position: absolute; top: 200px; left: 50%; background: #f1f1f1; width: 300px; margin-left: -165px; padding: 15px 15px 5px 15px; 
+	border-radius: 8px; box-shadow: 0 1px 3px 0 rgba(0,0,0,0.5); }
+	form fieldset { border: none; margin: 0 0 8px 0; padding: 0; }
+	form label { display: inline; float: left; width: 80px; margin: 0 10px 0 0; padding: 6px 0; text-align: right; font-size: 14px; }
+	form input { display: inline; float: right; width: 200px; padding: 5px 4px; border: 1px solid #ccc; font-size: 14px; border-radius: 4px; }
+	form input[type="submit"] { background: #7da7d9; width: 100px; border: 1px solid #628bbc; color: #fff; 
+	-webkit-box-shadow: 0px 1px 2px 0px #fff;
+     -moz-box-shadow: 0px 1px 2px 0px #fff;
+          box-shadow: 0px 1px 2px 0px #fff;}
+	form input[type="submit"]:hover { background: #89b3e5; 
+  -webkit-box-shadow: 0px 1px 1px -1px #999, inset 0px 2px 2px 0px rgba(255,255,255,0.5);
+     -moz-box-shadow: 0px 1px 1px -1px #999, inset 0px 2px 2px 0px rgba(255,255,255,0.5);
+          box-shadow: 0px 1px 1px -1px #999, inset 0px 2px 2px 0px rgba(255,255,255,0.5); }
+  form input[type="submit"]:active { background: #6d98cc; border: 1px solid #527aa9; color: #f9f9f9; 
+  -webkit-box-shadow: inset 0px 2px 4px 0px rgba(0,0,0,0.2);
+     -moz-box-shadow: inset 0px 2px 4px 0px rgba(0,0,0,0.2);
+          box-shadow: inset 0px 2px 4px 0px rgba(0,0,0,0.2); }
+	</style>
 </head>
 <body onload="
 var l = document.forms[0].login;
@@ -57,7 +79,7 @@ l.focus();">
 <?
 if ($badlogin) {
 ?>
-	<div style="color: red;">Incorrect username/password. Please try again.</div>
+	<div class="incorrect">Incorrect username/password. Please try again.</div>
 <?
 } else if (isset($_GET['reason'])) {
 	switch ($_GET['reason']) {
@@ -70,9 +92,15 @@ if ($badlogin) {
 ?>
 
 	<form method="POST" action="index.php">
-		<p>Login: <input type="text" name="login" /> </p>
-		<p>Password: <input type="password" name="password" /> </p>
-		<p><input type="submit" name="submit" /></p>
+	<fieldset>
+		<label for="login">Login:</label><input type="text" name="login" id="login"/>
+	</fieldset>
+	<fieldset>
+		<label for="pass">Password:</label> <input type="password" name="password" id="pass"/>
+	</fieldset>
+	<fieldset>
+		<input type="submit" name="submit" value="Sign In"/>
+	</fieldset>
 	</form>
 </body>
 </html>
