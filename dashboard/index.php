add0a6581b107a4b82b957fb032701ca4100f88c
Tony Farnell <tony@aw20.co.uk>
2012-05-17 16:04:16 +0000

    new layout of dashboard based upon ui design

---
+++ dashboard/index.php
@@ -0,0 +1,81 @@
+<div class="container cf">
+
+	<div class="wrapper">
+	
+	<div class="main_activity">
+		<div class="users cf">
+			<p>Show activity for</p>
+			<select>
+				<option value="">Everyone</option>
+				<option value="">Me</option>
+			</select>
+		</div>
+		
+		<div class="window summary">
+			<header>
+			<h2>Activity Summary</h2>
+			<div class="btngroup">
+				<button class="active">7 Days</button>
+				<button>Month</button>
+				<button>Year</button>
+				<button>Range</button>
+			</div>
+			</header>
+			
+			<section class="cf">
+			<div class="col">
+				<h4>Broadcasts</h4>
+				<p>231</p>
+				<p>2 Languages</p>
+				<p>34 Senders</p>
+			</div>
+			
+			<div class="col bloc">
+				<h4>Message Content</h4>
+			</div>
+			
+			<div class="col bloc">
+				<h4>Top Types</h4>
+			</div>
+			
+			<div class="col bloc">
+				<h4>Top Senders</h4>
+			</div>
+			</section>
+		</div>
+		
+		<div class="window broadcasts">
+			<header><h2>Broadcasts</h2></header>
+			
+			<section>
+			<h3>In Progress <span>(Sending Now)</span></h3>
+			
+			<h3>On Deck <span>(Sending Soon)</span></h3>
+			
+			<h3>Completed <span>(Already Sent)</span></h3>
+			</section>
+		</div>
+	</div><!-- end main_activity -->
+	
+	
+	<div class="main_aside">
+		<a class="bigbtn" href="message_sender.php"><span>New Broadcast</span></a>
+	
+		<div class="templates">
+			<h3>Broadcast Templates</h3>
+			<ul>
+			<li><a href="">General Announcement</a></li>
+			<li><a href="">Snow Delay</a></li>
+			<li><a href="">School Closure</a></li>
+			</ul>
+		</div>
+	
+		<div class="help">
+			<h3>Need Help?</h3>
+			<p>Visit the <a href="">help section</a> or call (800) 920-3897. Also be sure to <a href="">give us feedback</a> about the new version.</p>
+		</div>
+	</div><!-- end main_aside -->
+	
+	</div><!-- end wrapper -->
+
+</div><!-- end dashboard -->
