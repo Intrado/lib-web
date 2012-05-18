dfc87ddf372f63b29c302776008cf84564e2dfdc
Tony Farnell <tony@aw20.co.uk>
2012-05-18 13:20:18 +0000

    dummy content added for new dashboard

--- dashboard/index.php
+++ dashboard/index.php
@@ -4,11 +4,11 @@
 	
 	<div class="main_activity">
 		<div class="users cf">
-			<p>Show activity for</p>
+			<p>Show activity for
 			<select>
 				<option value="">Everyone</option>
 				<option value="">Me</option>
-			</select>
+			</select></p>
 		</div>
 		
 		<div class="window summary">
@@ -25,21 +25,39 @@
 			<section class="cf">
 			<div class="col">
 				<h4>Broadcasts</h4>
-				<p>231</p>
+				<p><strong>231</strong></p>
 				<p>2 Languages</p>
 				<p>34 Senders</p>
 			</div>
 			
 			<div class="col bloc">
 				<h4>Message Content</h4>
+				<ul>
+				<li><img src="themes/newui/phone-blue.png"/> 62</li>
+				<li><img src="themes/newui/email-red.png"/> 22</li>
+				<li><img src="themes/newui/sms-orange.png"/> 13</li>
+				<li><img src="themes/newui/social-green.png"/> 3</li>
+				</ul>
 			</div>
 			
 			<div class="col bloc">
 				<h4>Top Types</h4>
+				<ul>
+				<li><span>412</span> General Outre&hellip;</li>
+				<li><span>365</span> Attendance</li>
+				<li><span>91</span> Newsletter</li>
+				<li><span>32</span> PTO</li>
+				</ul>
 			</div>
 			
 			<div class="col bloc">
 				<h4>Top Senders</h4>
+				<ul>
+				<li><span>188</span> <a href="">Stevie Smith</a></li>
+				<li><span>32</span> <a href="">Gary Baldi</a></li>
+				<li><span>19</span> <a href="">Robyn Banks</a></li>
+				<li><span>12</span> <a href="">Davey Jones</a></li>
+				</ul>
 			</div>
 			</section>
 		</div>
@@ -49,10 +67,94 @@
 			
 			<section>
 			<h3>In Progress <span>(Sending Now)</span></h3>
+			<table class="info">
+				<thead>
+					<tr>
+					<th>Status</th>
+					<th>Author</th>
+					<th>Subject</th>
+					<th>Rcpts.</th>
+					<th>Content</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<td>Status</td>
+					<td>Gary Baldi</td>
+					<td>Harvest Festival Bake Sale</td>
+					<td>18659</td>
+					<td><img src="themes/newui/phone-grey.png"/> <img src="themes/newui/email-grey.png"/> <img src="themes/newui/sms-grey.png"/> <img src="themes/newui/social-grey.png"/></td>
+					</tr>
+				</tbody>
+			</table>
 			
 			<h3>On Deck <span>(Sending Soon)</span></h3>
+			<table class="info">
+				<thead>
+					<tr>
+					<th>Scheduled For</th>
+					<th>Author</th>
+					<th>Subject</th>
+					<th>Rcpts.</th>
+					<th>Content</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<td>Monday, 28/5/12</td>
+					<td>Davey Jones</td>
+					<td>Queens Jubilee Celebration</td>
+					<td>3761</td>
+					<td><img src="themes/newui/phone-grey.png"/> <img src="themes/newui/email-grey.png"/> <img src="themes/newui/social-grey.png"/></td>
+					</tr>
+
+					<tr>
+					<td>Friday, 1/6/12</td>
+					<td>Gary Baldi</td>
+					<td>July 4th Fundraiser</td>
+					<td>3761</td>
+					<td><img src="themes/newui/email-grey.png"/> <img src="themes/newui/sms-grey.png"/> <img src="themes/newui/social-grey.png"/></td>
+					</tr>
+				</tbody>
+			</table>
 			
 			<h3>Completed <span>(Already Sent)</span></h3>
+			<table class="info">
+				<thead>
+					<tr>
+					<th>Sent On</th>
+					<th>Author</th>
+					<th>Subject</th>
+					<th>Rcpts.</th>
+					<th>Content</th>
+					</tr>
+				</thead>
+				<tbody>
+					<tr>
+					<td>Monday, 7/6/12</td>
+					<td>Gary Baldi</td>
+					<td>Chinese Takeaway Day</td>
+					<td>1800</td>
+					<td><img src="themes/newui/phone-grey.png"/> <img src="themes/newui/email-grey.png"/> <img src="themes/newui/sms-grey.png"/> <img src="themes/newui/social-grey.png"/></td>
+					</tr>
+					
+					<tr>
+					<td>Thursday, 12/5/12</td>
+					<td>Davey Jones</td>
+					<td>May Day Bank Holiday</td>
+					<td>1800</td>
+					<td><img src="themes/newui/phone-grey.png"/> <img src="themes/newui/email-grey.png"/> <img src="themes/newui/sms-grey.png"/></td>
+					</tr>
+					
+					<tr>
+					<td>Friday, 6/4/12</td>
+					<td>Robyn Banks</td>
+					<td>April Bank Holiday dates</td>
+					<td>1800</td>
+					<td><img src="themes/newui/phone-grey.png"/> <img src="themes/newui/email-grey.png"/> <img src="themes/newui/sms-grey.png"/></td>
+					</tr>
+				</tbody>
+			</table>
 			</section>
 		</div>
 	</div><!-- end main_activity -->
@@ -78,4 +180,4 @@
 	
 	</div><!-- end wrapper -->
 
-</div><!-- end dashboard -->
+</div><!-- end container -->
