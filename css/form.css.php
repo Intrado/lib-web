5356755ac3387be2c715bb6d218b0fa91762451a
Tony Farnell <tony@aw20.co.uk>
2012-05-30 15:09:32 +0000

    had to add a width to ul.radiobox for the contact manager settings page for mantis bug 0005021

--- css/form.css.php
+++ css/form.css.php
@@ -119,7 +119,7 @@ border-top: 1px solid #ccc; border-right: 1px solid #ccc; border-bottom: none; b
 .newform .formcontrol .gBranding {}
 .newform .formcontrol .gBrandingText { font-size: 11px; }
 
-.newform .formcontrol .radiobox { float: left; display: inline; list-style-type: none; padding: 0; margin: 0 20px 0 0; }
+.newform .formcontrol .radiobox { float: left; display: inline; width: 250px; list-style-type: none; padding: 0; margin: 0 20px 0 0; }
 .newform .formcontrol .radiobox li { padding: 2px 0; }
 .newform .formcontrol .radiobox input[type="radio"],
 .newform .formcontrol .radiobox input[type="checkbox"] { margin: 0 5px 0 0; }
