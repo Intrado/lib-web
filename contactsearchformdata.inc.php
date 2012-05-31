97a854faf09091f8d0c9ae03f5afe8a8d90b7c97
Tony Farnell <tony@aw20.co.uk>
2012-05-31 16:37:38 +0000

    add class of search option to each surrounding div to help radio button layout

--- contactsearchformdata.inc.php
+++ contactsearchformdata.inc.php
@@ -112,11 +112,11 @@ else
 
 
 $togglehtml = '
-	<div><input name="searchbymode" id="searchByRules" type="radio" '. ($searchmode == "rules" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=rules\'">
+	<div class="searchoption"><input name="searchbymode" id="searchByRules" type="radio" '. ($searchmode == "rules" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=rules\'">
 	<label for="searchByRules"> Search by Rules </label></div>
-	<div><input name="searchbymode" id="searchByPerson" type="radio" '. ($searchmode == "individual" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=individual\'">
+	<div class="searchoption"><input name="searchbymode" id="searchByPerson" type="radio" '. ($searchmode == "individual" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=individual\'">
 	<label for="searchByPerson"> Search for Person </label></div>
-	<div><input name="searchbymode" id="searchShowAll" type="radio" '. ($searchmode == "showall" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=showall\'">
+	<div class="searchoption"><input name="searchbymode" id="searchShowAll" type="radio" '. ($searchmode == "showall" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=showall\'">
 	<label for="searchShowAll"> Show All Contacts </label></div>';
 
 
@@ -124,7 +124,7 @@ $contactsearchbuttons = array();
 
 if (getSystemSetting('_hasenrollment')) {
 	$togglehtml .= '
-		<div><input name="searchbymode" id="searchBySections" type="radio" '. ($searchmode == "sections" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=sections\'">
+		<div class="searchoption"><input name="searchbymode" id="searchBySections" type="radio" '. ($searchmode == "sections" ? "checked" : "") . ' onclick="window.location=\'?listsearchmode=sections\'">
 		<label for="searchBySections"> Search by Sections </label></div>';
 }
 
