86dd2d64cbc00662c090ed32076fdd98471b2121
Simon Dawdry <simon@aw20.co.uk>
2012-06-13 20:55:28 +0000

    added includes

--- _previewmodal.php
+++ _previewmodal.php
@@ -2,6 +2,7 @@
 require_once("inc/common.inc.php");
 require_once("inc/html.inc.php");
 require_once("obj/Form.obj.php");
+require_once("obj/FormItem.obj.php");
 require_once("obj/FieldMap.obj.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessageGroup.obj.php");
@@ -12,6 +13,7 @@ require_once("obj/AudioFile.obj.php");
 require_once("obj/Phone.obj.php");
 require_once("inc/previewfields.inc.php");
 require_once("obj/PreviewModal.obj.php");
+require_once("obj/Validator.obj.php");
 
 require_once("obj/FormItem.obj.php");
 require_once("obj/Validator.obj.php");
