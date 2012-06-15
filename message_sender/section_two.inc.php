e58469855cc6419049472dec6aa18e7e2f91a6d7
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-06-15 15:30:46 +0000

    Facebook page actionlinks are styled as such.

--- message_sender/section_two.inc.php
+++ message_sender/section_two.inc.php
@@ -316,7 +316,7 @@
 									</button>
 								</div>
 								
-								<div id="msgsndr_fbpageactionlinks" class="hidden">
+								<div id="msgsndr_fbpageactionlinks" class="hidden actionlinks">
 									<a id="msgsndr_fbpageall" class="actionlink">Select All</a>
 									<a id="msgsndr_fbpagenone" class="actionlink">Remove All</a>
 								</div>
