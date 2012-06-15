a1ccbdab86edb6ae23e92be343203757485a2ed7
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-06-15 15:32:53 +0000

    Add pages label

--- message_sender/section_two.inc.php
+++ message_sender/section_two.inc.php
@@ -297,6 +297,7 @@
 
 						
 						<fieldset> 
+							<label for="msgsndr_form_fbpage">Page(s)</label>
 							<div id="msgsndr_fbpages" class="controls fb_reset">
 							<input id="msgsndr_fbpage" type="hidden" value="" name="social_fbpages">
 							<input id="msgsndr_fbpageauthpages" type="hidden" value="" name="social_fbpagesauthpages">
