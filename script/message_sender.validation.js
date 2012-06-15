bea75157724c5444bd11dedaa0e910a9e84423b5
Simon Dawdry <simon@aw20.co.uk>
2012-06-15 14:48:33 +0000

    postdata updates (wip)

--- script/message_sender.validation.js
+++ script/message_sender.validation.js
@@ -166,6 +166,13 @@ jQuery.noConflict();
       });  
     };
 
+    function formatPhone(number){ // must be a 10 digit number with no spaces passed in
+      var phone = number;
+      var phonePartOne = '(' + phone.substring(0,3) + ') ';
+      var phonePartTwo = phone.substring(3,6) + '-';
+      var phonePartThree = phone.substring(6,10);
+      return phonePartOne + phonePartTwo + phonePartThree;
+    };
 
 
       // call the functions
@@ -726,13 +733,16 @@ jQuery.noConflict();
         if ($.isEmptyObject(data) != true) {
           if (typeof(data.facebook) != 'undefined' && data.facebook.accessToken){
             fbToken = data.facebook.accessToken;
-            //$('#msgsndr_fbpage').attr('value', fbToken);
-          } else {
-            //$('#msgsndr_fbpage').attr('value', []);
-          }
-            if (typeof(data.twitter) != 'undefined' || data.twitter != ''){
+            }
+            if (typeof(data.twitter) != 'undefined' && data.twitter != ''){
               twToken = data.twitter;
               $('#msgsndr_twittername').append('Posting as <a class="" href="http://twitter.com/'+twToken.screenName+'">@'+twToken.screenName+'</a>');
+            } else {
+              $('#msgsndr_twittername').append('<a id="msgsndr_twitterauth" href="popuptwitterauth.php" class="btn"><img src="img/icons/custom/twitter.gif" alt="twitter"/> Add Twitter Account</a>');
+              $('#msgsndr_twitterauth').on('click', function(){
+                event.preventDefault();
+                window.open($(this).prop('href'), '', 'height=300, width=600');
+              });
             }
           }
         }
@@ -991,16 +1001,64 @@ jQuery.noConflict();
         "addme_phone":"msgsndr_addmephone",
         "addme_email":"msgsndr_addmeemail",
         "addme_sms":"msgsndr_addmesms",
-        "lists_listids":"msgsndr_listids", // check what this is in the new listpicker, currently passing in test data
+        "lists":"msgsndr_listids", // check what this is in the new listpicker, currently passing in test data
         "has_phone":"msgsndr_hasphone", // true/false
         "phone_type":"msgsndr_phonemessagetype", // callme or text
         "phone_number":"msgsndr_phonemessagecallme", // callme messages object
         //"phone_voiceresponse":"", // true/false
         "phone_callconfirmation":"msgsndr_phonemessagepost",// true/false
-        "phone_text":"msgsndr_phonemessagetext",
-        "phone_translate":"msgsndr_phonemessagetexttranslate", // true/false
-        "phone_translation_es":"msgsndr_phonemessagetexttranslateestext", //translations example
-        // need to account for at least all tts languages, if not all of them
+        "phone_translate":"msgsndr_phonemessagetext", // english version
+        "phone_tts_translate":"msgsndr_phonemessagetexttranslate", // true/false
+        "phone_translate_es":"msgsndr_phonemessagetexttranslateestext",
+        "phone_translate_af":"msgsndr_phonemessagetexttranslateaftext",
+        "phone_translate_sq":"msgsndr_phonemessagetexttranslatesqtext",
+        "phone_translate_ar":"msgsndr_phonemessagetexttranslateartext",
+        "phone_translate_be":"msgsndr_phonemessagetexttranslatebetext",
+        "phone_translate_bg":"msgsndr_phonemessagetexttranslatebgtext",
+        "phone_translate_ca":"msgsndr_phonemessagetexttranslatecatext",
+        "phone_translate_zh":"msgsndr_phonemessagetexttranslatezhtext",
+        "phone_translate_hr":"msgsndr_phonemessagetexttranslateaftext",
+        "phone_translate_cs":"msgsndr_phonemessagetexttranslatecstext",
+        "phone_translate_da":"msgsndr_phonemessagetexttranslatedatext",
+        "phone_translate_nl":"msgsndr_phonemessagetexttranslatenltext",
+        "phone_translate_et":"msgsndr_phonemessagetexttranslateettext",
+        "phone_translate_fil":"msgsndr_phonemessagetexttranslatefiltext",
+        "phone_translate_fi":"msgsndr_phonemessagetexttranslatefitext",
+        "phone_translate_fr":"msgsndr_phonemessagetexttranslatefrtext",
+        "phone_translate_gl":"msgsndr_phonemessagetexttranslategltext",
+        "phone_translate_de":"msgsndr_phonemessagetexttranslatedetext",
+        "phone_translate_el":"msgsndr_phonemessagetexttranslateeltext",
+        "phone_translate_ht":"msgsndr_phonemessagetexttranslatehttext",
+        "phone_translate_iw":"msgsndr_phonemessagetexttranslateiwtext",
+        "phone_translate_hi":"msgsndr_phonemessagetexttranslatehitext",
+        "phone_translate_hu":"msgsndr_phonemessagetexttranslatehutext",
+        "phone_translate_is":"msgsndr_phonemessagetexttranslateistext",
+        "phone_translate_id":"msgsndr_phonemessagetexttranslateidtext",
+        "phone_translate_ga":"msgsndr_phonemessagetexttranslategatext",
+        "phone_translate_it":"msgsndr_phonemessagetexttranslateittext",
+        "phone_translate_ja":"msgsndr_phonemessagetexttranslatejatext",
+        "phone_translate_ko":"msgsndr_phonemessagetexttranslatekotext",
+        "phone_translate_lv":"msgsndr_phonemessagetexttranslatelvtext",
+        "phone_translate_lt":"msgsndr_phonemessagetexttranslatelttext",
+        "phone_translate_mk":"msgsndr_phonemessagetexttranslatemktext",
+        "phone_translate_ms":"msgsndr_phonemessagetexttranslatemstext",
+        "phone_translate_mt":"msgsndr_phonemessagetexttranslatemttext",
+        "phone_translate_no":"msgsndr_phonemessagetexttranslatenotext",
+        "phone_translate_fa":"msgsndr_phonemessagetexttranslatefatext",
+        "phone_translate_pt":"msgsndr_phonemessagetexttranslatepttext",
+        "phone_translate_ro":"msgsndr_phonemessagetexttranslaterotext",
+        "phone_translate_ru":"msgsndr_phonemessagetexttranslaterutext",
+        "phone_translate_sr":"msgsndr_phonemessagetexttranslatesrtext",
+        "phone_translate_sk":"msgsndr_phonemessagetexttranslatesktext",
+        "phone_translate_sl":"msgsndr_phonemessagetexttranslatesltext",
+        "phone_translate_sw":"msgsndr_phonemessagetexttranslateswtext",
+        "phone_translate_sv":"msgsndr_phonemessagetexttranslatesvtext",
+        "phone_translate_th":"msgsndr_phonemessagetexttranslatethtext",
+        "phone_translate_tr":"msgsndr_phonemessagetexttranslatetrtext",
+        "phone_translate_uk":"msgsndr_phonemessagetexttranslateuktext",
+        "phone_translate_vi":"msgsndr_phonemessagetexttranslatevitext",
+        "phone_translate_cy":"msgsndr_phonemessagetexttranslatecytext",
+        "phone_translate_yi":"msgsndr_phonemessagetexttranslateyitext",
         "has_email":"msgsndr_hasemail", // true/false
         "email_name":"msgsndr_emailmessagefromname",
         "email_address":"msgsndr_emailmessagefromemail",
@@ -1008,8 +1066,57 @@ jQuery.noConflict();
         "email_attachment":"msgsndr_emailmessageattachment",
         "email_message":"msgsndr_emailmessagetext", // data from CK editor panel
         "email_translate":"msgsndr_emailmessagetexttranslate", // convert this from 1/0 to true/false
-        "email_translation_es":"msgsndr_emailmessagetexttranslateestext", // translations example
-        // translations need to be added dynamically 
+        "email_translate_es":"msgsndr_emailmessagetexttranslateestext", // translates example
+        "email_translate_es":"msgsndr_emailmessagetexttranslateestext",
+        "email_translate_af":"msgsndr_emailmessagetexttranslateaftext",
+        "email_translate_sq":"msgsndr_emailmessagetexttranslatesqtext",
+        "email_translate_ar":"msgsndr_emailmessagetexttranslateartext",
+        "email_translate_be":"msgsndr_emailmessagetexttranslatebetext",
+        "email_translate_bg":"msgsndr_emailmessagetexttranslatebgtext",
+        "email_translate_ca":"msgsndr_emailmessagetexttranslatecatext",
+        "email_translate_zh":"msgsndr_emailmessagetexttranslatezhtext",
+        "email_translate_hr":"msgsndr_emailmessagetexttranslateaftext",
+        "email_translate_cs":"msgsndr_emailmessagetexttranslatecstext",
+        "email_translate_da":"msgsndr_emailmessagetexttranslatedatext",
+        "email_translate_nl":"msgsndr_emailmessagetexttranslatenltext",
+        "email_translate_et":"msgsndr_emailmessagetexttranslateettext",
+        "email_translate_fil":"msgsndr_emailmessagetexttranslatefiltext",
+        "email_translate_fi":"msgsndr_emailmessagetexttranslatefitext",
+        "email_translate_fr":"msgsndr_emailmessagetexttranslatefrtext",
+        "email_translate_gl":"msgsndr_emailmessagetexttranslategltext",
+        "email_translate_de":"msgsndr_emailmessagetexttranslatedetext",
+        "email_translate_el":"msgsndr_emailmessagetexttranslateeltext",
+        "email_translate_ht":"msgsndr_emailmessagetexttranslatehttext",
+        "email_translate_iw":"msgsndr_emailmessagetexttranslateiwtext",
+        "email_translate_hi":"msgsndr_emailmessagetexttranslatehitext",
+        "email_translate_hu":"msgsndr_emailmessagetexttranslatehutext",
+        "email_translate_is":"msgsndr_emailmessagetexttranslateistext",
+        "email_translate_id":"msgsndr_emailmessagetexttranslateidtext",
+        "email_translate_ga":"msgsndr_emailmessagetexttranslategatext",
+        "email_translate_it":"msgsndr_emailmessagetexttranslateittext",
+        "email_translate_ja":"msgsndr_emailmessagetexttranslatejatext",
+        "email_translate_ko":"msgsndr_emailmessagetexttranslatekotext",
+        "email_translate_lv":"msgsndr_emailmessagetexttranslatelvtext",
+        "email_translate_lt":"msgsndr_emailmessagetexttranslatelttext",
+        "email_translate_mk":"msgsndr_emailmessagetexttranslatemktext",
+        "email_translate_ms":"msgsndr_emailmessagetexttranslatemstext",
+        "email_translate_mt":"msgsndr_emailmessagetexttranslatemttext",
+        "email_translate_no":"msgsndr_emailmessagetexttranslatenotext",
+        "email_translate_fa":"msgsndr_emailmessagetexttranslatefatext",
+        "email_translate_pt":"msgsndr_emailmessagetexttranslatepttext",
+        "email_translate_ro":"msgsndr_emailmessagetexttranslaterotext",
+        "email_translate_ru":"msgsndr_emailmessagetexttranslaterutext",
+        "email_translate_sr":"msgsndr_emailmessagetexttranslatesrtext",
+        "email_translate_sk":"msgsndr_emailmessagetexttranslatesktext",
+        "email_translate_sl":"msgsndr_emailmessagetexttranslatesltext",
+        "email_translate_sw":"msgsndr_emailmessagetexttranslateswtext",
+        "email_translate_sv":"msgsndr_emailmessagetexttranslatesvtext",
+        "email_translate_th":"msgsndr_emailmessagetexttranslatethtext",
+        "email_translate_tr":"msgsndr_emailmessagetexttranslatetrtext",
+        "email_translate_uk":"msgsndr_emailmessagetexttranslateuktext",
+        "email_translate_vi":"msgsndr_emailmessagetexttranslatevitext",
+        "email_translate_cy":"msgsndr_emailmessagetexttranslatecytext",
+        "email_translate_yi":"msgsndr_emailmessagetexttranslateyitext",
         "has_sms":"msgsndr_hassms", // true/false
         "sms_text":"msgsndr_smsmessagetext",
         "has_facebook":"msgsndr_hasfacebook", // true/false
