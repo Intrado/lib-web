9ae9facf08b414018123901605dc348ed078dde5
Tony Farnell <tony@aw20.co.uk>
2012-06-18 13:08:56 +0000

    mantis bug 0005138 - widen the search box used on system - contacts - search by person page to show all help text within the input

--- css/form.css.php
+++ css/form.css.php
@@ -133,7 +133,7 @@ border-top: 1px solid #ccc; border-right: 1px solid #ccc; border-bottom: none; b
 .newform .formcontrol #accessprofile_subscribe,
 .newform .formcontrol #accessprofile_datafields,
 .newform .formcontrol #reportcallssearch_organizationids { float: none; display: block; margin-left: 1em; }
-.newform .formcontrol #listsearch_quickaddsearch { width: 280px; }
+.newform .formcontrol #listsearch_quickaddsearch { width: 330px; }
 
 .newform .formcontrol .radio_box { width: 250px; list-style-type: none; padding: 0; margin: 0 20px 0 0; }
 
