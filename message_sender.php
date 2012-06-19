83c5afe91f61e2c055f4bdeaed1dfab06b797d7f
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-06-19 20:16:17 +0000

    Start implementing template loading.

--- message_sender.php
+++ message_sender.php
@@ -1,8 +1,24 @@
 <? 
 include_once("inc/common.inc.php");
 
+if (isset($_GET['template']) && $_GET['template'] && isset($_GET['subject']) && $_GET['subject']) {
+	$_SESSION['message_sender'] = array();
+	$_SESSION['message_sender']['template'] = array(
+		"subject" => $_GET['subject'],
+		"lists" => (isset($_GET['lists'])?$_GET['lists']:"[]"),
+		"jobtypeid" => (isset($_GET['jobtypeid'])?$_GET['jobtypeid']:0),
+		"messagegroupid" => (isset($_GET['messagegroupid'])?$_GET['messagegroupid']:0));
+	redirect();
+} else if (isset($_GET['new'])) {
+	unset($_SESSION['message_sender']);
+	redirect();
+}
+
 $PAGE = "notifications:jobs";
-$TITLE = "New Broadcast";
+if (isset($_SESSION['message_sender']['template']['subject']))
+	$TITLE = _L("Broadcast Template: %s", $_SESSION['message_sender']['template']['subject']);
+else
+	$TITLE = _L("New Broadcast");
 
 // Moved this include into message_sender/index.php 
 //include("nav.inc.php");
