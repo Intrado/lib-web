c13b5111245c23f37c16eef9fe99843c1fcaa850
Tony Farnell <tony@aw20.co.uk>
2012-07-12 09:13:35 +0000

    Removed the cf class from body_wrap and title_wrap to fix bug: 0005357: Screenful of whitespace above classroom's message sender finish screen

--- inc/table.inc.php
+++ inc/table.inc.php
@@ -176,12 +176,12 @@ function startWindow($title) {
 ?>
 
 <div class="window cf">
-	<div class="window_title_wrap cf">
+	<div class="window_title_wrap">
 		<div class="window_title_l"></div>
 		<h2 class="window_title"><?= $title ?></h2>
 		<div class="window_title_r"></div>
 	</div>
-	<div class="window_body_wrap cf">
+	<div class="window_body_wrap">
 		<div class="window_body_l"></div>
 		<div class="window_body cf">
 <?
