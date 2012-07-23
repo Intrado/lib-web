491c18ff8d102c8a73b6ff9523e3fa92a6ebb091
Ben Hencke <bhencke@schoolmessenger.com>
2012-07-23 16:10:58 +0000

    FIXME since arg is known, can check upfront and return a js validator that always fails or always passes, no need to serverside check FIXME also this shouldn't ever be needed, the form items for these shouldn't exist, and form handling code shouldn't try to read values for features the user is not permitted to access

--- obj/ValPermission.val.php
+++ obj/ValPermission.val.php
@@ -1,4 +1,6 @@
 <?
+//FIXME since arg is known, can check upfront and return a js validator that always fails or always passes, no need to serverside check
+//FIXME also this shouldn't ever be needed, the form items for these shouldn't exist, and form handling code shouldn't try to read values for features the user is not permitted to access
 class ValPermission extends Validator {
 	var $onlyserverside = true;
 	function validate ($value, $args) {
