2fd339ee257c8e06b10aeabec86c237c48aecdd6
Ben Cox <benc@relianceco.com>
2012-08-17 20:21:10 +0000

    #5495 - updated sidebar text to contain support mailto link

--- inc/help.inc.php
+++ inc/help.inc.php
@@ -3,8 +3,11 @@ function addHelpSection() {
 ?>
 	<div class="help">
 	<h3>Need Help?</h3>
-	<p>Visit the <a href="#" onclick="popup('help/index.php',950,500);">help section</a> or call (<?=substr($_SESSION['_supportphone'],0,3) . ") " . substr($_SESSION['_supportphone'],3,3) . "-" . substr($_SESSION['_supportphone'],6,4);?>. Also be sure to <a href="mailto:feedback@schoolmessenger.com">give us feedback</a> about the new version.</p>
-	</div>
+    <p>Visit the <a href="#" onclick="popup('help/index.php',950,500);">help section</a>,
+        send an <a href="mailto:<?=$_SESSION['_supportemail']?>">email to support</a>,
+        or call (<?=substr($_SESSION['_supportphone'],0,3) . ") " . substr($_SESSION['_supportphone'],3,3) . "-" . substr($_SESSION['_supportphone'],6,4);?>.
+        Also be sure to <a href="mailto:feedback@schoolmessenger.com">give us feedback</a> about the new version.</p>
+    </div>
 <?
 }
 ?>
