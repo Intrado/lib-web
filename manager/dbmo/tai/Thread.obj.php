3bd2de49a6988261bc3b353117afec5f79a5a238
Martin Arnberg <marnberg@schoolmessenger.com>
2012-08-27 22:29:49 +0000

    add threadtype

--- manager/dbmo/tai/Thread.obj.php
+++ manager/dbmo/tai/Thread.obj.php
@@ -8,11 +8,12 @@ class Thread extends DBMappedObject{
 	var $parentthreadid;
 	var $wassentanonymously = 0;
 	var $modifiedtimestamp;
+	var $threadtype;
 
 	function Thread($id = NULL){
 		$this->_allownulls = false;
 		$this->_tablename = "tai_thread";
-		$this->_fieldlist = array("organizationid","originatinguserid", "recipientuserid", "topicid", "parentthreadid","wassentanonymously","modifiedtimestamp");
+		$this->_fieldlist = array("organizationid","originatinguserid", "recipientuserid", "topicid", "parentthreadid","wassentanonymously","modifiedtimestamp","threadtype");
 
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
