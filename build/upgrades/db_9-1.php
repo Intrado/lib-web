27c86c5a200b4f836d81640d2d78f17e2247073c
Nickolas Heckman <nheckman@schoolmessenger.com>
2012-09-05 21:27:54 +0000

    Add missing upgrade step when migrating from revision 1 to 2

--- build/upgrades/db_9-1.php
+++ build/upgrades/db_9-1.php
@@ -8,6 +8,9 @@ function upgrade_9_1 ($rev, $shardid, $customerid, $db) {
 		case 0:
 			echo "|";
 			apply_sql("upgrades/db_9-1_pre.sql", $customerid, $db, 1);
+		case 1:
+			echo "|";
+			apply_sql("upgrades/db_9-1_pre.sql", $customerid, $db, 2);
 	}
 	
 	return true;
