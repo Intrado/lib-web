8964b43e303aabd343684e8bf52d0d9727db5156
nrheckman <nheckman@schoolmessenger.com>
2012-11-02 15:41:44 -0700

    Implements bug 5565, updating the customer page to read from the api endpoint
    Ajax requests now pull from the endpoint, instead of pulling from the php page.

--- dmstatus.php
+++ dmstatus.php
@@ -7,7 +7,6 @@ include_once("inc/securityhelper.inc.php");
 include_once("inc/table.inc.php");
 include_once("inc/html.inc.php");
 include_once("inc/utils.inc.php");
-include_once("inc/memcache.inc.php");
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -29,14 +28,7 @@ if (isset($_GET['dmid'])) {
 // Data Handling
 ////////////////////////////////////////////////////////////////////////////////
 
-list($dmname,$dmuuid) = QuickQueryRow("select name, dmuuid from custdm where dmid=?",false,false,array($dmid));
-
-init_memcache();
-global $mcache;
-$jsonstats = $mcache->get("dmpoststatus/".$dmuuid);
-
-include_once("dmstatusdata.inc.php");
-
+$dmname = QuickQuery("select name from custdm where dmid=?",false,false,array($dmid));
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -50,36 +42,33 @@ include_once("nav.inc.php");
 buttons(icon_button("Back","fugue/arrow_180",null,"dms.php"));
 
 ?>
+<script type="text/javascript">
+	var dmid = <?=$dmid?>;
+</script>
 <script type='text/javascript' src='script/dmstatus.js'></script>
-<?
 
-if ($status == NULL) {
-	echo "There is no status available at this time.";
-} else {
-?>
 <table width="100%"><tr><td valign="top">
 <?
 startWindow("System Statistics");
-echo "<div id='systemstats'>";
-	foreach($statusgroups as $groupname => $groupdata) {
-		showStatusGroup($groupname, $groupdata);
-	}
-echo "</div>";
+include("dmstatusdata.inc.php");
 endWindow();
 ?>
 </td><td valign="top">
 <?
 startWindow("Active Resources");
-echo "<div id='activeresources'></div>";
+?>
+	<div id='activeresources'></div>
+<?
 endWindow();
 
 startWindow("Completed Resources");
-echo "<div id='completedresources'></div>";
+?>
+	<div id='completedresources'></div>
+<?
 endWindow();
 ?>
 </td></tr></table>
 <?
-} // end else status is not null
 buttons();
 include_once("navbottom.inc.php");
 ?>
