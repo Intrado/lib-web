22e6647417a6a10ac3e2b3f39362245bc6ef91af
Martin Arnberg <marnberg@schoolmessenger.com>
2012-11-13 15:04:06 -0800

    Adds missing include

--- classroommessagephonetemplate.php
+++ classroommessagephonetemplate.php
@@ -13,6 +13,7 @@ require_once("obj/FormItem.obj.php");
 require_once("obj/Job.obj.php");
 require_once("obj/Schedule.obj.php");
 require_once("obj/Language.obj.php");
+require_once("obj/AudioFile.obj.php");
 require_once("obj/MessageGroup.obj.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessagePart.obj.php");
