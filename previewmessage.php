29c639b8ce00f698ffac4b96666118f1b7f2ab9d
Martin Arnberg <marnberg@schoolmessenger.com>
2012-11-28 14:07:50 -0800

    Remove unused form items

--- previewmessage.php
+++ previewmessage.php
@@ -19,7 +19,6 @@ require_once("obj/Voice.obj.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessagePart.obj.php");
 require_once("obj/AudioFile.obj.php");
-require_once("obj/FormSelectMessage.fi.php");
 require_once("obj/Language.obj.php");
 require_once("obj/MessageGroup.obj.php");
 
