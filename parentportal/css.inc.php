233a26df88e7a53563a40ad600082001d865ef51
Martin Arnberg <marnberg@schoolmessenger.com>
2012-11-30 12:39:36 -0800

    First stab at css quick fix design enhancements

---
+++ parentportal/css.inc.php
@@ -0,0 +1,29 @@
+<?
+
+?>
+
+/************************************************************************************
+MOBILE
+*************************************************************************************/
+@media screen and (max-width: 600px) {
+	.banner_logo {
+		display: none;
+	}
+	.windowRowHeader { width: 0px;display:none; }
+}
+
+@media screen and (min-width: 600px) {
+	.banner_logo {
+		display: block;
+	}
+
+}
+
+@media screen and (max-width: 700px) {
+	#logininfo { clear:both; float:none; display: block; text-align: left; }
+	#termsinfo { clear:both; float:none; display: block; text-align: left;}
+}
+
+
+
+
