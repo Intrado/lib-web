37370bfd71fa72996ec4764f9b3f749cdf1a6bce
nrheckman <nheckman@schoolmessenger.com>
2012-12-03 14:59:57 -0800

    Corrects bug 5913, where the call early value was incorrectly calculated and ignored the access profile setting
    This could allow sending jobs earlier than the access profile allows if the user bypassed javascript checks clientside. Either purposefully or through browser issues.

--- message_sender.php
+++ message_sender.php
@@ -350,15 +350,20 @@ function getSchedule($postdata) {
 			$calllatesec = strtotime($calllate);
 			$accessCallearlysec = strtotime($accessCallearly);
 			$accessCalllatesec = strtotime($accessCalllate);
-				
-			//get calllate first from user pref, try to ensure it is at least an hour after start, up to access restriction
+
+			// make sure the calculated callearly is not before access profile
+			if ($callearlysec  < $accessCallearlysec)
+				$callearlysec = $accessCallearlysec;
+
+			// try to ensure calllate is at least an hour after start, up to access restriction
 			if ($callearlysec + 3600 > $calllatesec)
 				$calllatesec = $callearlysec + 3600;
 				
 			//make sure the calculated calllate is not past access profile
 			if ($calllatesec  > $accessCalllatesec)
 				$calllatesec = $accessCalllatesec;
-				
+
+			$callearly = date("g:i a", $callearlysec);
 			$calllate = date("g:i a", $calllatesec);
 				
 			$schedule = array(
