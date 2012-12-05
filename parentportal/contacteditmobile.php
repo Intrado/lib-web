bef371eb865f6ab18e96b1ee6c0251899f1c031d
Martin Arnberg <marnberg@schoolmessenger.com>
2012-12-05 10:12:42 -0800

    Fork contactedit page for some mobile experiments

---
+++ parentportal/contacteditmobile.php
@@ -0,0 +1,146 @@
+<?
+//displays checkbox for each jobtype
+function newdisplayJobtypeForm($f, $s, $type, $sequence, $jobtypes){
+	foreach($jobtypes as $jobtype){
+		?><td class="bottomBorder" align="center"><?
+		if($type!="sms" || ($type=="sms" && !$jobtype->issurvey)){
+			echo "<div class=\"jobtypename\">$jobtype->name</div><div>";
+		 		NewFormItem($f, $s, $type . $sequence . "jobtype" . $jobtype->id, "checkbox", 0, 1);
+		  	echo "</div>";
+		} else {
+			echo "&nbsp;";
+		}
+		?></td><?
+	}
+}
+
+
+//expects $PERSONID and $person to be set
+if($PERSONID){
+
+	NewForm($f);
+?>
+	<table cellpadding="3" cellspacing="1" width="100%">
+<?
+		$type = "phone";
+?>
+		<tr class="listHeader">
+			<th align="left" colspan="<?=count($jobtypes)+2; ?>"><?=format_delivery_type($type); ?></th>
+		</tr>
+		<tr class="windowRowHeader" >
+			<th align="left"><?=_L("Contact Type")?></th>
+			<th align="left"><?=_L("Destination")?></th>
+<?
+			foreach($jobtypes as $jobtype){
+				?><th><?=escapehtml($jobtype->name)?></th><?
+			}
+?>
+		</tr>
+<?
+			foreach($phones as $phone){
+?>
+				<tr>
+					<td class="bottomBorder" ><?=escapehtml(destination_label("phone",$phone->sequence))?></td>
+					<td class="bottomBorder" >
+					<? 
+						if(!$lockedphones[$phone->sequence]){ 
+							NewFormItem($f, $s, "phone" . $phone->sequence, "text", 14, null);
+						} else {
+							if($phone->phone)
+								echo Phone::format($phone->phone);
+							else
+								echo "&nbsp;";
+						}
+					?>
+					</td>
+<?
+						newdisplayJobtypeForm($f, $s, "phone", $phone->sequence, $jobtypes);
+?>
+				</tr>
+<?
+			}
+		$type = "email";
+?>
+			<tr class="listHeader">
+				<th align="left"  colspan="<?=count($jobtypes)+3; ?>"><?=format_delivery_type($type); ?></th>
+			</tr>
+			<tr class="windowRowHeader" >
+				<th align="left">Contact&nbsp;Type</th>
+				<th align="left">Destination</th>
+<?
+				foreach($jobtypes as $jobtype){
+					?><th><?=escapehtml($jobtype->name)?></th><?
+				}
+?>
+			</tr>
+<?
+			foreach($emails as $email){
+?>
+				<tr>
+					<td class="bottomBorder" ><?=escapehtml(destination_label("email",$email->sequence))?></td>
+					<td class="bottomBorder" >
+						<? 
+							if(!$lockedemails[$email->sequence]){ 
+								NewFormItem($f, $s, "email" . $email->sequence, "text", 15, 100, 'id="email' . $email->sequence . '"');
+							} else {
+								if($email->email)
+									echo $email->email;
+								else
+									echo "&nbsp;";
+							}
+						?>
+					</td>
+<?
+					newdisplayJobtypeForm($f, $s, "email", $email->sequence, $jobtypes);
+?>									
+				</tr>
+<?
+			}
+			if(getSystemSetting("_hassms")){
+				$type = "sms";
+?>
+				<tr class="listHeader">
+					<th align="left" colspan="<?=count($jobtypes)+3; ?>"><?=format_delivery_type($type); ?></th>
+				</tr>
+				<tr class="windowRowHeader" >
+					<th align="left"><?=_L("Contact Type")?></th>
+					<th align="left"><?=_L("Destination")?></th>
+<?
+					foreach($jobtypes as $jobtype){
+						?><th><?=escapehtml($jobtype->name)?></th><?
+					}
+?>
+				</tr>
+<?
+				foreach($smses as $sms){
+?>
+				<tr>
+					<td class="bottomBorder" ><?=escapehtml(destination_label("sms",$sms->sequence))?></td>
+					<td class="bottomBorder" >
+						<? 
+							if(!$lockedsms[$sms->sequence]){
+								NewFormItem($f, $s, "sms" . $sms->sequence, "text", 14);
+							} else {
+								if($sms->sms)
+									echo Phone::format($sms->sms);
+								else
+									echo "&nbsp;";
+							}
+						?>
+					</td>
+<?
+					newdisplayJobtypeForm($f, $s, "sms", $sms->sequence, $jobtypes);
+?>
+				</tr>
+<?
+				}
+			}
+?>
+	</table>
+	<div><? NewFormItem($f, $s, "savetoall", "checkbox"); ?> <?=_L("Save To All Contacts")?></div>
+	<br>
+<?
+	echo submit($f, $s, _L("Save"));
+	EndForm();
+}
+?>
