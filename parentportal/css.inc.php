5470fb74af0dd9264917be60d9a3c3ff55b3982e
Martin Arnberg <marnberg@schoolmessenger.com>
2012-12-05 10:11:46 -0800

    Additional mobile css tweaks

--- parentportal/css.inc.php
+++ parentportal/css.inc.php
@@ -3,11 +3,17 @@
 ?>
 .content_wrap { min-width: auto;}
 
+.pagetitle {
+	display: none;
+}
 /************************************************************************************
 MOBILE
 *************************************************************************************/
 @media screen and (max-width: 600px) {
 	
+	.contactmanagersplash {
+		display: none;
+	}
 	
 	.banner_logo {
 		/*background:none;*/
@@ -20,20 +26,77 @@ MOBILE
 		margin:0 -206px 0 0;
 		/*width: 400px; height: 300px;*/
 	}
-	.windowRowHeader { width: 0px;display:none; }
+	.windowRowHeader.bottomBorder { width: 0px;display:none; }
 	
+	
+	.banner_links_button {
+		display: block;
+	}
+	.banner_links.minhide {
+		display: none;
+	}
+	
+	.banner_links li.bl_left {
+		display: none;
+	}
+	.banner_links li.bl_right {
+		display: none;
+	}
+		/* main nav */
 	.banner_links {
+		clear: both;
+		position: absolute;
+		top: 40px;
+		z-index: 10000;
+		padding: 0 5px 5px 0px;
+		background: white;
+		border: solid 1px #999;
+	}
+	.banner_links li {
+		background: none;
+		display: inherit;
+		clear: both;
+		float: none;
+		border-right: none;
+		margin: 5px 0 5px 10px;
+	}
+	#banner_links a, 
+	#banner_links ul a {
+		font: inherit;
+		background: none;
+		display: inline;
+		padding: 0;
+		color: #666;
+		border: none;
+	}
+	#banner_links a:hover, 
+	#banner_links ul a:hover {
+		background: none;
+		color: #000;
+	}
 	
+	/* dropdown */
+	#banner_links ul {
+		width: auto;
+		position: static;
+		display: block;
+		border: none;
+		background: inherit;
+	}
+	#banner_links ul li {
+		margin: 3px 0 3px 15px;
 	}
 	
 }
 
 @media screen and (min-width: 600px) {
-/*
-	.banner_logo {
+	.banner_links_button {
+		display: none;
+	}
+	
+	.banner_links.minhide {
 		display: block;
 	}
-*/
 }
 
 @media screen and (max-width: 700px) {
