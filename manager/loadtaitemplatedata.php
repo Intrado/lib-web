e9b594d77b3cb90ee971059e40b8a6b91a84f542
nrheckman <nheckman@schoolmessenger.com>
2012-12-07 14:25:41 -0800

    Adds very generic template for unread message report

--- manager/loadtaitemplatedata.php
+++ manager/loadtaitemplatedata.php
@@ -12,12 +12,32 @@ function loadTaiTemplateData() {
 	$templates['tai_unreadmessagesreport']['html']['en']['subject'] = 'Unread Messages for: ${date}';
 	$templates['tai_unreadmessagesreport']['html']['en']['fromname'] = 'Talk About It (Reports)';
 	$templates['tai_unreadmessagesreport']['html']['en']['fromaddr'] = 'reports@letstai.com';
-	$templates['tai_unreadmessagesreport']['html']['en']['body'] = 'TODO: template body';
+	$templates['tai_unreadmessagesreport']['html']['en']['body'] = '
+<!-- $beginBlock summary -->
+<!-- $beginBlock organization -->\'${okey}\' <!-- $endBlock organization -->
+0-24: ${interval0}
+24-48: ${interval1}
+48-72: ${interval2}
+<!-- $beginBlock topic -->
+${topicname}: ${topictotal}
+<!-- $endBlock topic -->
+total: ${total}
+<!-- $endBlock summary -->';
 
 	$templates['tai_unreadmessagesreport']['plain']['en']['subject'] = 'Unread Messages for: ${date}';
 	$templates['tai_unreadmessagesreport']['plain']['en']['fromname'] = 'Talk About It (Reports)';
 	$templates['tai_unreadmessagesreport']['plain']['en']['fromaddr'] = 'reports@letstai.com';
-	$templates['tai_unreadmessagesreport']['plain']['en']['body'] = 'TODO: template body';
+	$templates['tai_unreadmessagesreport']['plain']['en']['body'] = '
+<!-- $beginBlock summary -->
+<!-- $beginBlock organization -->\'${okey}\' <!-- $endBlock organization -->
+0-24: ${interval0}
+24-48: ${interval1}
+48-72: ${interval2}
+<!-- $beginBlock topic -->
+${topicname}: ${topictotal}
+<!-- $endBlock topic -->
+total: ${total}
+<!-- $endBlock summary -->';
 
 
 	return $templates;
