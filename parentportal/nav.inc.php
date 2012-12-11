2e7f7d6b2892fe19e033d02c2cca1f23cb4e9f0d
Martin Arnberg <marnberg@schoolmessenger.com>
2012-12-11 11:05:49 -0800

    Scale meta data tag for iPhone devices

--- parentportal/nav.inc.php
+++ parentportal/nav.inc.php
@@ -84,7 +84,8 @@ header('Content-type: text/html; charset=UTF-8') ;
 <head>
 	<meta http-equiv="Content-type" value="text/html; charset=UTF-8" />
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0">
+	
 	<title>Contact Manager: <?= $PAGETITLE ?></title>
 	
 	<script src='script/utils.js'></script>
