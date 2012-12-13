5a7b8dadd12d6d13c0c250e932094abc0fceb37c
Ben Hencke <bhencke@schoolmessenger.com>
2012-12-13 15:15:21 -0800

    add google analytics hooks and way to configure

--- nav.inc.php
+++ nav.inc.php
@@ -241,6 +241,24 @@ header('Content-type: text/html; charset=UTF-8') ;
 <!--[if gt IE 8]><!--> <body <?=setBodyClass();?> <?= isset($MESSAGESENDER) && $MESSAGESENDER == true?' id="ms"':''?> ><!--<![endif]-->
 	<script>
 		var _brandtheme = "<?=getBrandTheme();?>";
+
+<?
+	if (isset($SETTINGS['googleanalytics']['trackingid'])) {
+?>
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', '<?= $SETTINGS['googleanalytics']['trackingid'] ?>']);
+		_gaq.push(['_setCookiePath', '/<?= $CUSTOMERURL ?>/']);
+		_gaq.push(['_setCustomVar', 1, 'cust', '<?= $CUSTOMERURL ?>', 2]);
+		_gaq.push(['_setSiteSpeedSampleRate', <?= $SETTINGS['googleanalytics']['samplerate'] ?>]);
+		_gaq.push(['_trackPageview']);
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+<?
+}	
+?>
 	</script>
 
 <!-- ********************************************************************* -->
