5d41ca3c7fb7e59f430b7b7374b187d64be85c9d
Martin Arnberg <marnberg@schoolmessenger.com>
2012-12-12 16:15:01 -0800

    Experimenting with compact on/off preference mode for destination settings

--- parentportal/contacteditmobile.php
+++ parentportal/contacteditmobile.php
@@ -1,17 +1,45 @@
 <?
 //displays checkbox for each jobtype
 function newdisplayJobtypeForm($f, $s, $type, $sequence, $jobtypes){
-	foreach($jobtypes as $jobtype){
-		?><td class="bottomBorder" align="center"><?
-		if($type!="sms" || ($type=="sms" && !$jobtype->issurvey)){
-			echo "<div class=\"jobtypename\">$jobtype->name</div><div>";
-		 		NewFormItem($f, $s, $type . $sequence . "jobtype" . $jobtype->id, "checkbox", 0, 1);
-		  	echo "</div>";
-		} else {
-			echo "&nbsp;";
-		}
-		?></td><?
-	}
+	$id = "t{$type}s{$sequence}";
+	
+	?><td class="bottomBorder" width="100%">
+	
+	<table class="preference_wrap"><tr><td>
+	<div class="onoffswitch">
+    <input type="checkbox" name="onoffswitch" class="onoffswitch-checkbox" id="<?= $id?>_switch" onclick="togglePreferences('<?= $id?>',this.checked);" checked />
+    <label class="onoffswitch-label" for="<?= "t{$type}s{$sequence}"?>_switch">
+    <div class="onoffswitch-inner"></div>
+    <div class="onoffswitch-switch"></div>
+    </label>
+    </div>
+
+    </td><td>
+	<?
+	echo  "<div id=\"{$id}\" class=\"view_preference_button\">" . action_link(_L("Options"), "cog", null,"$('{$id}_content').toggleClassName('minhide');return false;") . "</div>";
+
+	?>
+	</td></tr>
+	</table>
+	
+	<? 
+    echo  "<div id=\"{$id}_content\" class=\"view_preference_content minhide\">";
+    echo "<ul class=\"jobtypepreferences\">";
+    foreach($jobtypes as $jobtype){
+    	if($type!="sms" || ($type=="sms" && !$jobtype->issurvey)){
+    			
+    		echo "<li>";
+    		NewFormItem($f, $s, $type . $sequence . "jobtype" . $jobtype->id, "checkbox", 0, 1);
+    		echo  $jobtype->name . "</li>";
+    	} else {
+    		echo "&nbsp;";
+    	}
+    }
+    echo "</ul>";
+    echo  "</div>";
+    ?>
+	</td>
+	<?
 }
 
 
@@ -24,27 +52,17 @@ if($PERSONID){
 <?
 		$type = "phone";
 ?>
-		<tr class="listHeader">
-			<th align="left" colspan="<?=count($jobtypes)+2; ?>"><?=format_delivery_type($type); ?></th>
-		</tr>
 		<tr class="windowRowHeader" >
-			<th align="left"><?=_L("Contact Type")?></th>
-			<th align="left"><?=_L("Destination")?></th>
-<?
-			foreach($jobtypes as $jobtype){
-				?><th><?=escapehtml($jobtype->name)?></th><?
-			}
-?>
+			<th align="left" colspan="2"><?=format_delivery_type($type); ?></th>
 		</tr>
 <?
 			foreach($phones as $phone){
 ?>
 				<tr>
-					<td class="bottomBorder" ><?=escapehtml(destination_label("phone",$phone->sequence))?></td>
 					<td class="bottomBorder" >
 					<? 
 						if(!$lockedphones[$phone->sequence]){ 
-							NewFormItem($f, $s, "phone" . $phone->sequence, "text", 14, null);
+							NewFormItem($f, $s, "phone" . $phone->sequence, "text", 14,null, " id=\"phones{$phone->sequence}\" class=\"phonepreference\"");
 						} else {
 							if($phone->phone)
 								echo Phone::format($phone->phone);
@@ -61,27 +79,17 @@ if($PERSONID){
 			}
 		$type = "email";
 ?>
-			<tr class="listHeader">
-				<th align="left"  colspan="<?=count($jobtypes)+3; ?>"><?=format_delivery_type($type); ?></th>
-			</tr>
 			<tr class="windowRowHeader" >
-				<th align="left">Contact&nbsp;Type</th>
-				<th align="left">Destination</th>
-<?
-				foreach($jobtypes as $jobtype){
-					?><th><?=escapehtml($jobtype->name)?></th><?
-				}
-?>
+				<th align="left"  colspan="2"><?=format_delivery_type($type); ?></th>
 			</tr>
 <?
 			foreach($emails as $email){
 ?>
 				<tr>
-					<td class="bottomBorder" ><?=escapehtml(destination_label("email",$email->sequence))?></td>
 					<td class="bottomBorder" >
 						<? 
 							if(!$lockedemails[$email->sequence]){ 
-								NewFormItem($f, $s, "email" . $email->sequence, "text", 15, 100, 'id="email' . $email->sequence . '"');
+								NewFormItem($f, $s, "email" . $email->sequence, "text", 15, 100, " id=\"emails{$email->sequence}\" class=\"emailpreference\"");
 							} else {
 								if($email->email)
 									echo $email->email;
@@ -99,27 +107,17 @@ if($PERSONID){
 			if(getSystemSetting("_hassms")){
 				$type = "sms";
 ?>
-				<tr class="listHeader">
-					<th align="left" colspan="<?=count($jobtypes)+3; ?>"><?=format_delivery_type($type); ?></th>
-				</tr>
 				<tr class="windowRowHeader" >
-					<th align="left"><?=_L("Contact Type")?></th>
-					<th align="left"><?=_L("Destination")?></th>
-<?
-					foreach($jobtypes as $jobtype){
-						?><th><?=escapehtml($jobtype->name)?></th><?
-					}
-?>
+					<th align="left" colspan="2"><?=format_delivery_type($type); ?></th>
 				</tr>
 <?
 				foreach($smses as $sms){
 ?>
 				<tr>
-					<td class="bottomBorder" ><?=escapehtml(destination_label("sms",$sms->sequence))?></td>
 					<td class="bottomBorder" >
 						<? 
 							if(!$lockedsms[$sms->sequence]){
-								NewFormItem($f, $s, "sms" . $sms->sequence, "text", 14);
+								NewFormItem($f, $s, "sms" . $sms->sequence, "text", 14,null," id=\"smss{$sms->sequence}\" class=\"smspreference\"");
 							} else {
 								if($sms->sms)
 									echo Phone::format($sms->sms);
@@ -139,8 +137,72 @@ if($PERSONID){
 	</table>
 	<div><? NewFormItem($f, $s, "savetoall", "checkbox"); ?> <?=_L("Save To All Contacts")?></div>
 	<br>
+	
 <?
-	echo submit($f, $s, _L("Save"));
+	echo icon_button(_L("Save"),"accept","removeBlankFields();submitForm('$f','$s');");
 	EndForm();
 }
 ?>
+
+	<script>
+
+function removeBlankFields() {
+	$$('input.phonepreference, input.emailpreference, input.smspreference').each(function(item) {
+		if(item.getStyle('color') == "gray") {
+			item.stopObserving('change');
+			item.value = "";
+		}
+	});
+}
+
+
+
+	document.observe('dom:loaded', function() {
+		$$('input.phonepreference').each(function(item) {
+			initiateDestination('<?=_L("Phone")?>',item);			
+		});
+
+		$$('input.emailpreference').each(function(item) {
+			initiateDestination('<?=_L("Email")?>',item);			
+		});
+
+		$$('input.smspreference').each(function(item) {
+			initiateDestination('<?=_L("SMS")?>',item);			
+		});
+	});
+
+	function initiateDestination(type,item) {
+		if (item.value == "") {
+			var prefswitch = $('t' + item.id + '_switch');
+			prefswitch.checked = false;
+		}
+		
+		blankFieldValue(item, type);
+		item.observe('change',function(i) {
+			var prefswitch = $('t' + i.element().id + '_switch');
+			if (i.element().value == "") {
+				prefswitch.checked = false;
+			} else {
+				prefswitch.checked = true;
+				togglePreferences('t' + i.element().id, true);
+			}
+		});
+		item.focus();
+		item.blur();
+	}
+
+	
+	
+	function togglePreferences(id,checked) {
+		value = $(id.substring(1)).getStyle('color');
+		if (value == "gray") {
+			$(id + '_switch').checked = false;
+			return;
+		}
+		
+		var expr = '#' + id + '_content input';
+		$$(expr).each(function(item) {
+			item.checked = checked;
+		});
+	}
+	</script>
