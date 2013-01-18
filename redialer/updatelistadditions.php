b8569d3df89729adc59c990401340c769ebb215c
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2013-01-18 11:41:07 -0800

    updatelistadditions.php called from csapiv2

---
+++ redialer/updatelistadditions.php
@@ -0,0 +1,81 @@
+<?
+// CSAPIv2 calls this to create new list additions
+
+setlocale(LC_ALL, 'en_US.UTF-8');
+mb_internal_encoding('UTF-8');
+
+require_once("../inc/db.inc.php");
+require_once("../inc/utils.inc.php");
+require_once("../inc/memcache.inc.php");
+require_once("../inc/DBMappedObject.php");
+require_once("../inc/DBRelationMap.php");
+require_once("../inc/date.inc.php");
+require_once("../obj/User.obj.php");
+require_once("../obj/Access.obj.php");
+require_once("../obj/Permission.obj.php");
+require_once("../obj/Rule.obj.php");
+require_once("../obj/FieldMap.obj.php");
+require_once("../obj/Organization.obj.php");
+require_once("../obj/PeopleList.obj.php");
+require_once("../obj/Person.obj.php");
+
+
+$custid = $argv[1] +0;
+if ($custid == 0) {
+	echo "missing customer id, Usage: updatelistadditions customerid listid pkeys dbhost:port dbname dbuser dbpass \n";
+	exit(-1);
+}
+
+$listid = $argv[2] +0;
+if ($listid == 0) {
+	echo "missing list id, Usage: updatelistadditions customerid listid pkeys dbhost:port dbname dbuser dbpass \n";
+	exit(-1);
+}
+
+$pkeys = explode(",", $argv[3]);
+
+// gather database connection info
+$db['host'] = $argv[4];
+$db['db'] = $argv[5];
+$db['user'] = $argv[6];
+@ $db['pass'] = $argv[7]; // password is last argument in case it is blank
+
+// 	now connect to the customer database
+global $_dbcon;
+$_dbcon = DBConnect($db['host'], $db['user'], $db['pass'], $db['db']);
+if (!$_dbcon) {
+	echo("Problem connecting to MySQL server at " . $db['host'] . "\n");
+	exit(-1);
+}
+
+if (!QuickQuery("select count(*) from list where id=".$listid)) {
+	echo("Error: list not found\n");
+	exit(-1);
+}
+
+$timezone = QuickQuery("select value from setting where name='timezone'");
+if($timezone){
+	@date_default_timezone_set($timezone);
+	QuickUpdate("set time_zone='" . $timezone . "'");
+}
+
+$list = new PeopleList($listid);
+
+$USER = new User($list->userid);
+
+// add the people
+$numpeople = $list->updateManualAddByPkeys($pkeys);
+
+$result['numpeople'] = $numpeople + 0;
+// success if all people added, else warning
+if ($numpeople == count($pkeys))
+	$result["resultcode"] = "success";
+else {
+	$result["resultcode"] = "warning"; // somple people skipped, not added to list
+	$result["resultdescription"] = "Some people may have been skipped.";
+}
+
+echo json_encode($result);
+
+exit(0); // success
+?>
