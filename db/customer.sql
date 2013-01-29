eff923415dcc0048eafcfe1f5690381bd2cee6bc
Gretel Baumgartner <gbaumgartner@schoolmessenger.com>
2013-01-29 13:28:49 -0800

    woops, forgot to update _dbversion

--- db/customer.sql
+++ db/customer.sql
@@ -2123,4 +2123,7 @@ $$$
 -- START 9.5/2
 ALTER TABLE `setting` CHANGE `name` `name` VARCHAR( 255 ) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL DEFAULT '', CHANGE `value` `value` TEXT CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL
 $$$
+
+update setting set value='9.5/2' where name='_dbversion'
+$$$
 -- END 9.5/2
