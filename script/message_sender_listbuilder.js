71808d554e510c952c6b6aeb7eb903aff889172d
Martin Arnberg <marnberg@schoolmessenger.com>
2013-01-29 17:06:59 -0800

    Fix list table view rendering to display deleted lists

--- script/message_sender_listbuilder.js
+++ script/message_sender_listbuilder.js
@@ -51,11 +51,12 @@
                         }).done(function(dataListStats){
                             data.stats = $.extend(data.stats, dataListStats[id]);
                             base.unfetchedIds.splice(base.unfetchedIds.indexOf(id), 1);
-                            base.buildTable();
+                            
                         });
                     };
                 });
-
+                base.buildTable();
+                
                 // Enable the "pick from lists" button
                 if (hasLists)
                     base.$el.find('[href=#add-recipients-existing_lists]').removeClass('disabled');
@@ -1057,35 +1058,55 @@
         // Build the "picked lists" table ---------------------------
 
         base.buildTable = function(){
-
-            var rows = '';
-            var sum = 0;
-
-            // Add each picked list to the table
-            $.each(base.pickedListIds, function(index, id) {
-
-                // Only do this if the list has stats
-                if (base.lists[id].stats.total !== undefined) {
-
-                    // Recalc total recipients
-                    sum += base.lists[id].stats.total;
-                    
-                    rows += '<tr data-list-id="' + id + '"><td><a rel="tooltip" title="Remove List" class="action remove" href="#"><i class="icon-remove" title="Remove List"></i></a>'; 
-                    // If this list is saved, no save icon, else, show save icon
-                    if (!base.lists[id].isSaved) {
-                        rows += '<a rel="tooltip" title="Save List" class="action save" data-toggle="modal" href="#modal-save-list"><i class="icon-folder-open" title="Save List"></i></a>';
-                    }
-                    rows += '<a rel="tooltip" title="Preview List" class="action preview" data-toggle="modal" href="#modal-preview-list"><i title="Preveiw List"></i></a>';
-                    rows += '</td><td>' + base.lists[id].stats.name + '</td><td>' + base.lists[id].stats.total + '</td></tr>';
-                    
-                };
-            });
-
-            // Last row is the total
-            rows += '<tr><td colspan="2">Total</td><td>' + sum + '</td></tr>';
-            
-            // Add the rows to the table
-            $listTable.find('tbody').html(rows);
+			var sum = 0;
+			var rows = '';
+			
+			var tbody = $listTable.find('tbody');
+			tbody.html("");
+			
+			
+			self.addRow = function(id,name,total,isSaved) {
+				var row = '<tr data-list-id="' + id + '"><td><a rel="tooltip" title="Remove List" class="action remove" href="#"><i class="icon-remove" title="Remove List"></i></a>'; 
+				// If this list is saved, no save icon, else, show save icon
+				if (!isSaved) {
+					row += '<a rel="tooltip" title="Save List" class="action save" data-toggle="modal" href="#modal-save-list"><i class="icon-folder-open" title="Save List"></i></a>';
+				}
+				row += '<a rel="tooltip" title="Preview List" class="action preview" data-toggle="modal" href="#modal-preview-list"><i title="Preveiw List"></i></a>';
+				row += '</td><td>' + name + '</td><td>' + total + '</td></tr>';
+				
+				$listTable.find('tbody').append(row);
+			}
+			
+			
+			self.addRows = function(listids) {
+				return function() {
+					if (listids.length > 0) {
+						var listid = listids.pop();
+						if (base.lists && base.lists[listid] && base.lists[listid].stats.total !== undefined) {
+							var stat = base.lists[listid].stats;				
+							self.addRow(listid,stat.name,stat.total,stat.isSaved);
+							sum += stat.total;
+							// call self with listids
+							self.addRows(listids)();
+						} else {
+							$.ajax({
+								url: 'ajax.php?type=liststats&listids=["' + listid + '"]',
+								async : false
+							}).done(function(dataListStats){
+								var stat = dataListStats[listid];
+								self.addRow(listid,stat.name,stat.total,true);
+								sum += stat.total;
+								self.addRows(listids)();
+							});
+						}
+					}
+				}
+			}
+		
+			var listids = base.pickedListIds.clone();
+			self.addRows(listids)();
+	        
+			tbody.append('<tr><td colspan="2">Total</td><td>' + sum + '</td></tr>');
 
             // TODO: comment out tooltips for now. Have to find the issue with CSS causing positioning issues
             // Activate new tooltips
