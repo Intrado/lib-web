6a73f2a7e1ed02900ee8ff90287c1eac0f0b9166
Martin Arnberg <marnberg@schoolmessenger.com>
2013-02-14 17:09:47 -0800

    Change placeholder layouts

--- layouts/layouts.inc.php
+++ layouts/layouts.inc.php
@@ -2,11 +2,8 @@
 
 $layouts = array(
 		"onecolumn" => _L("One Column"),
-		"onecolumn2" => _L("One Column 2"),
 		"twocolumns" => _L("Two Columns"),
-		"threecolumns" => _L("Three Columns"),
-		"1_2_3" => _L("1:2:3"),
-		"2_3_2" => _L("2:3:2")
+		"newsletter" => _L("Newsletter")
 );
 
 ?>
