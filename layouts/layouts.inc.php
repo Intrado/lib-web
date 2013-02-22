d7612f40522b1dcb3d389cda8409f3a97d47ded5
Jennifer Sawyer <jensawyer@gmail.com>
2013-02-21 16:50:06 -0800

    Adds email layouts and updates layout.inc

--- layouts/layouts.inc.php
+++ layouts/layouts.inc.php
@@ -1,9 +1,14 @@
 <? 
 
 $layouts = array(
-		"onecolumn" => _L("One Column"),
-		"twocolumns" => _L("Two Columns"),
-		"newsletter" => _L("Newsletter")
+		"Contemporary" => _L("Contemporary"),
+		"ContemporaryWithLinkSpace" => _L("Contemporary - with Link Space"),
+		"ContemporaryNewsletter" => _L("Contemporary Newsletter"),
+		"ContemporaryNewsletterWithLinkSpace" => _L("Contemporary Newsletter - with Link Space"),
+		"Fancy" => _L("Fancy"),
+		"FancyNewsletter" => _L("Fancy Newsletter"),
+		"Simplicity" => _L("Simplicity"),
+		"SimplicityNewsletter" => _L("Simplicity Newsletter")
 );
 
 ?>
