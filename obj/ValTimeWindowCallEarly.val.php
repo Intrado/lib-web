d27290f07607db8a6304debb82129fc728f210e0
Martin Arnberg <marnberg@schoolmessenger.com>
2013-02-28 12:44:08 -0800

    #CS-1852 Refactor time window check to use the same validator for job edit, account and profile page for a consistent behavior

--- obj/ValTimeWindowCallEarly.val.php
+++ obj/ValTimeWindowCallEarly.val.php
@@ -9,21 +9,27 @@ class ValTimeWindowCallEarly extends Validator {
 		if (isset($args['calllatefield']))
 			$calllatefield= $args['calllatefield'];
 		
-		$accessCalllate = $ACCESS->getValue("calllate");
+		$accessCalllate = (!isset($args['noProfile']) || $args['noProfile'] !== true)?$ACCESS->getValue("calllate"):false;		
 		if (!$accessCalllate)
 			$accessCalllate = "11:59 pm";
-			
+		
+		$calllate = strtotime($requiredvalues[$calllatefield]);
+		$value = strtotime($value);
+		$isBothSet = $value != -1 && $value !== false && $calllate != -1 && $calllate !== false;
+		if (!$isBothSet)
+			return true;
+		
 		//give an exception for late calls, don't restrict to min hour window
-		$isLateCall = strtotime($requiredvalues[$calllatefield]) >= strtotime($accessCalllate); 
-		$isBeforeEndTime = strtotime($value) < strtotime($requiredvalues[$calllatefield]);
-		$isAtLeastAnHourBeforeEndTime = strtotime($value) <= strtotime($requiredvalues[$calllatefield]) - 3600;
+		$isLateCall = $calllate >= strtotime($accessCalllate); 
+		$isBeforeEndTime = $value < $calllate;
+		$isAtLeastAnHourBeforeEndTime = $value <= $calllate - 3600;
 		
 		if (!$isBeforeEndTime)
-			return _L('%1$s must be before the end time.', $this->label);
+			return _L('\'%1$s\' must be before the end time.', $this->label);
 	
 		if (!$isLateCall && !$isAtLeastAnHourBeforeEndTime)
-			return _L('%1$s must be at least an hour before the end time', $this->label);
-
+			return _L('\'%1$s\' must be at least an hour before the end time', $this->label);
+		
 		return true;
 	}
 }
--- obj/ValTimeWindowCallLate.val.php
+++ obj/ValTimeWindowCallLate.val.php
@@ -9,20 +9,26 @@ class ValTimeWindowCallLate extends Validator {
 		if (isset($args['callearlyfield']))
 			$callearlyfield= $args['callearlyfield'];
 		
-		$accessCalllate = $ACCESS->getValue("calllate");
+		$accessCalllate = (!isset($args['noProfile']) || $args['noProfile'] !== true)?$ACCESS->getValue("calllate"):false;
 		if (!$accessCalllate)
 			$accessCalllate = "11:59 pm";
 		
+		$callearly = strtotime($requiredvalues[$callearlyfield]);
+		$value = strtotime($value);
+		$isBothSet = $value != -1 && $value !== false && $callearly != -1 && $callearly !== false;
+		if (!$isBothSet)
+			return true;
+		
 		//give an exception for late calls, don't restrict to min hour window
-		$isLateCall = strtotime($value) >= strtotime($accessCalllate); 
-		$isAfterStartTime = strtotime($value) > strtotime($requiredvalues[$callearlyfield]);
-		$isAtLeastAnHourAfterStartTime = strtotime($value) >= strtotime($requiredvalues[$callearlyfield]) + 3600;
+		$isLateCall = $value >= strtotime($accessCalllate); 
+		$isAfterStartTime = $value > $callearly;
+		$isAtLeastAnHourAfterStartTime = $value >= $callearly + 3600;
 		
 		if (!$isAfterStartTime)
-			return _L('%1$s must be after the start time.', $this->label);
+			return _L('\'%1$s\' must be after the start time.', $this->label);
 		
 		if (!$isLateCall && !$isAtLeastAnHourAfterStartTime)
-			return _L('%1$s must be at least an hour after the start time', $this->label);
+			return _L('\'%1$s\' must be at least an hour after the start time', $this->label);
 		
 		return true;
 	}
