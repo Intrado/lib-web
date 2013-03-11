2ff4bdf83d77c0efdadb96d7cfd7310f06003c30
Ben Hencke <bhencke@schoolmessenger.com>
2013-03-11 10:17:57 -0700

    fixed authserver timeout so that it can try more than once, improved logging auth.inc.php only allowed repeated attempts within 30 seconds, but xml-rpc call was set to 90 second timeout. increased error sleep interval to 1s from 100ms (log flooding and can probably live with fewer attempts during temp outage) use error_log_helper when available

--- inc/auth.inc.php
+++ inc/auth.inc.php
@@ -12,16 +12,24 @@ function pearxmlrpc($method, $params, $returndata = false) {
 	
 	$isAlive = false;
 	$timetostop = time() + 30; // 30 seconds from now
+	$starttime = time();
+	$attempt = 1;
 	
 	// retry authserver for a while, maybe mid-restart
 	while (!$isAlive && $timetostop > time()) {
-		$resp = $cli->send($msg, 90);
+		$resp = $cli->send($msg, 9); //9 second timeout will let us timeout and try 3 times within a 30 second limit, though the 4th attempt could succeed at 36s
 		if (!$resp) {
-			error_log($method . ' communication error: ' . $cli->errstr);
-			usleep(100000);
+			$msg = $method . ' communication error: "' . $cli->errstr . '". Have been trying for ' . (time() - $starttime) . 's attempt ' . $attempt;
+			if (function_exists("error_log_helper"))
+				error_log_helper($msg);
+			else
+				error_log($msg);
+
+			sleep(1); //in case of immediate failure (instead of timeout), sleep a bit to avoid flooding
 		} else {
 			$isAlive = true;
 		}
+		$attempt++;
 	}
 	
 	if (!$resp) {
