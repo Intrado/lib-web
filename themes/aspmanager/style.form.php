06ac96ec46bda6aa2132db7b1291e695dbca21b1
Sean Kelly <skelly@schoolmessenger.com>
2013-04-12 10:20:15 -0700

    Adds new "aspmanager" theme so that manager has its own look/feel theme that is unaffected by user-facing themes.

---
+++ themes/aspmanager/style.form.php
@@ -0,0 +1,60 @@
+/*---------- forest theme form ----------*/
+
+.newform fieldset { background: #fff; margin-bottom: 1em; border: none; }
+
+.newform .formsectionheader { margin: 5px 0 10px 0; padding: 0; font-size: 14px; line-height: 21px; font-weight: bold; color: #444; text-transform: uppercase; border-bottom: 1px dashed #999; }
+ 
+.newform .formfieldarea { padding: 8px 10px; }
+.newform .formfieldarea .underneathmsg { padding: 0 0 0 150px; font-size: 14px; font-weight: normal; color: #cc0000; }
+
+.newform .formtitle { width: 140px; padding: 5px 20px 0 0; }
+.newform .formtitle .formlabel { float: none; /*display: block;*/ padding: 5px 0px 5px 0; font-size: 14px; line-height: 18px; font-weight: normal; text-align: right; }
+
+.newform .formcontrol input[type="text"] { width: 313px; padding: 4px 5px; font-size: 14px; border: 1px solid #E2E2E2; 
+-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; 
+-moz-background-clip: padding; -webkit-background-clip: padding-box; background-clip: padding-box; }
+.newform .formcontrol input[type="radio"] { margin: 0 5px 5px 0; }
+.newform .formcontrol textarea { display: block; width: 375px; margin: 0 20px 5px 0; padding: 4px 5px; font-size: 14px; line-height: 18px; border: 1px solid #E2E2E2; 
+-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; 
+-moz-background-clip: padding; -webkit-background-clip: padding-box; background-clip: padding-box; }
+.newform .formcontrol input#account_brandthemecustomize { margin: 5px 5px 0 0; }
+
+.newform .formcontrol .domain { padding: 5px 0 0; font-size: 14px; line-height: 18px; }
+
+.newform .formcontrol .translate { max-height: 150px; padding: 5px 0 0; font-size: 14px; line-height: 18px; overflow: auto; }
+.newform .formcontrol .translate p { margin: 0; line-height: 18px; }
+
+.retranslateitems .message { font-size: 14px; line-height: 18px; }
+.retranslateitems .message p { margin: 0; line-height: 18px; }
+
+.newform .formcontrol .gBrandingText { font-size: 11px; }
+
+.newform .formcontrol .radiobox li { padding: 2px 0; }
+.newform .formcontrol .radiobox input[type="radio"],
+.newform .formcontrol .radiobox input[type="checkbox"] { margin: 0 5px 0 0; }
+
+.newform .formcontrol select { margin: 0; padding: 4px 6px; border: 1px solid #E7E7E7; 
+-webkit-border-radius: 5px; -moz-border-radius: 5px; border-radius: 5px; 
+-moz-background-clip: padding; -webkit-background-clip: padding-box; background-clip: padding-box; }
+
+.newform .formcontrol .datafields input { width: 140px; }
+
+.maincontainerleft { margin: 0; }
+.maincontainerseperator { display: none; }
+.maincontainerright { margin: 0; padding: 0; }
+
+#emaileedit_helpsection_5 .formcontrol .maincontainerleft,
+#emaileedit_helpsection_5 .formcontrol .maincontainerright,
+#emaileedit_helpsection_5 .formcontrol .controlcontainer { width: 600px; }
+
+#emailText_helpsection_5 .formcontrol .maincontainerleft,
+#emailText_helpsection_5 .formcontrol .maincontainerright,
+#emailText_helpsection_5 .formcontrol .controlcontainer { width: 455px; }
+
+.formcontrol .datafields { margin: 0 10px 0 0; }
+.formcontrol .datafields select { width: 175px; padding: 3px 6px; font-size: 14px; }
+.formcontrol .datafieldsinsert { margin: 25px 0 0; }
+.formcontrol .datafieldsinsert button { padding: 3px 10px; }
+
+#list_helpsection_3 .formtitle { width: 100%; text-align: left; }
+#list_helpsection_3 .formcontrol { width: 600px; margin: 0; }
