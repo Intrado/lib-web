cf4d8a67f5a93ba3c543aa5de4398333015587ed
Sean Kelly <skelly@schoolmessenger.com>
2013-05-08 10:18:15 -0700

    Disables PHPUnit sample test failure

--- UNITTEST/PHPUnit/application/AllTests.php
+++ UNITTEST/PHPUnit/application/AllTests.php
@@ -19,7 +19,7 @@ class application_AllTests {
 
 		// List of test classes to include; TODO: expand as needed:
 		$classes = Array(
-			'FailSampleTest', // TODO - activate this line if you want to see the CI server catch an error and stop the build process.
+			//'FailSampleTest', // TODO - activate this line if you want to see the CI server catch an error and stop the build process.
 			'ValTranslationLengthTest'
 		);
 
