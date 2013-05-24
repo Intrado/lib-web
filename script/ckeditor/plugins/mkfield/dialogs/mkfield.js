ca70c6f486e54640f1c2360c17403aefb7653db2
Sean Kelly <skelly@schoolmessenger.com>
2013-05-24 13:22:33 -0700

    Updates source code comments to reflect changes that were made to the method by which field names are passed to the Field Insert ("mkfield") plugin for CKEditor.

--- script/ckeditor/plugins/mkfield/dialogs/mkfield.js
+++ script/ckeditor/plugins/mkfield/dialogs/mkfield.js
@@ -8,12 +8,16 @@
  *
  * We use the API to pull field maps for this user. We use the USER class to pull
  * the active session user's ID from PHP, and then we use jQuery to request the
-  + req + ']'* fieldmap for this user as a JSON structure from the API. We then reformat the
- * JSON data into an array of field names needed by CKEditor's UI constructs to
- * assemble a SELECT box with the field names in it.
+  + req + ']'* fieldmap for this user as a JSON structure from the API.
+ * We used to use valid field names from the API directly. Now we get them from
+ * our parent, RCIEditor which must have a setting 'fieldinsert_list' as an array
+ * of name/value pairs where the name is the field type, and the value is the name
+ * of the field as it is to be displayed to the user. If the list is empty (default)
+ * then we will not show any field selection options.
  *
  * EXTERNAL DEPENDENCIES
    * jquery.js
+   * rcieditor.js
  *
  * SMK created 2013-01-03
  */
@@ -31,6 +35,9 @@
 
 	// The list of field names is passed into RCIEditor constructor's
 	// overrideSettings as 'type:name' pairs array; iterate over them...
+	// to reformat the data into an array of field names needed by
+	// CKEditor's UI constructs to assemble a SELECT box with the field
+	// names in it.
 	var data = rcie.getSetting('fieldinsert_list');
 	for (var field in data) {
 		ftypes.push(Array(data[field]));
