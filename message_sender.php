d06fe43c7864ae6c6cb97edcdc1e919fdae6651c
Justin Burns <justin.j.burns@gmail.com>
2013-06-18 21:14:25 -0700

    Adds initial work in progress for IE9 CSS fixes

--- message_sender.php
+++ message_sender.php
@@ -895,9 +895,8 @@ include("nav.inc.php");
 	<div id="messagesender-shell"></div>
 
 	<link rel="stylesheet" href="messagesender/stylesheets/app.css">
-    <!--[if IE 8]>
-        <link rel="stylesheet" type="text/css" href="messagesender/styles/messagesender_ie8.css" >
-    <![endif]-->
+    <!--[if IE 9]><link rel="stylesheet" type="text/css" href="messagesender/styles/messagesender_ie9.css" ><![endif]-->
+    <!--[if IE 8]><link rel="stylesheet" type="text/css" href="messagesender/styles/messagesender_ie8.css" ><![endif]-->
 
 	<script type="text/javascript" src="messagesender/javascripts/vendor.js"></script>
 	<script type="text/javascript" src="messagesender/javascripts/app.js"></script>
