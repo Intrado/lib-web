884de571604ff5925b1ae197784c02775f842305
Sean Kelly <skelly@schoolmessenger.com>
2013-10-17 12:01:29 -0700

    Minor changes to pathing to improve uniformity of PHPUnit test class source code

--- UNITTEST/PHPUnit/application/ValTranslationLengthTest.php
+++ UNITTEST/PHPUnit/application/ValTranslationLengthTest.php
@@ -4,18 +4,17 @@
  * ValTranslationLengthTest.php - PHPUnit test for class ValTranslationLength
  *
  * @package unittests
- * @author Sean M. Kelly, <skelly@schoolmessenger.com>
  * @version 1.0
  */
 
-// Since we don't know what the working directory is, use this script's dir as
-// the base and relatively indicate the object class being tested from there
-require_once(realpath(dirname(__FILE__) . '/../../../obj/Validator.obj.php'));
-require_once(realpath(dirname(__FILE__) . '/../../../obj/ValTranslationLength.val.php'));
+require_once(realpath(dirname(dirname(__FILE__)) .'/konaenv.php'));
+// ----------------------------------------------------------------------------
+require_once("{$konadir}/obj/Validator.obj.php");
+require_once("{$konadir}/obj/ValTranslationLength.val.php");
 
 class ValTranslationLengthTest extends PHPUnit_Framework_TestCase {
 
-	private $CHAR_LIMIT = 5000;
+	const CHAR_LIMIT = 5000;
 
         public function test_Empty() {
 		$obj = new ValTranslationLength();
@@ -24,12 +23,12 @@ class ValTranslationLengthTest extends PHPUnit_Framework_TestCase {
 
         public function test_Exact() {
 		$obj = new ValTranslationLength();
-		$this->assertTrue($obj->validate(str_pad('', $this->CHAR_LIMIT, 'A'), array()), 'Failed on exact length string');
+		$this->assertTrue($obj->validate(str_pad('', self::CHAR_LIMIT, 'A'), array()), 'Failed on exact length string');
         }
 
         public function test_Oversize() {
 		$obj = new ValTranslationLength();
-		$this->assertTrue(is_string($obj->validate(str_pad('', ($this->CHAR_LIMIT + 1), 'X'), array())), 'Failed to catch over-sized data');
+		$this->assertTrue(is_string($obj->validate(str_pad('', (self::CHAR_LIMIT + 1), 'X'), array())), 'Failed to catch over-sized data');
         }
 
 	public function test_JSValidator() {
