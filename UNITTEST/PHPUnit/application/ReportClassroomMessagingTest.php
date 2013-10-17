721ce1124efdc0e7b7e68a37df9802cc905e5c18
Sean Kelly <skelly@schoolmessenger.com>
2013-10-16 17:47:52 -0700

    Refactors unit test for ReportClassroomMessage class based on the refactoring of that class; exercizes several source files now which slightly improves test coverage [CS-5303]

--- UNITTEST/PHPUnit/application/ReportClassroomMessagingTest.php
+++ UNITTEST/PHPUnit/application/ReportClassroomMessagingTest.php
@@ -12,45 +12,52 @@ require_once(realpath(dirname(dirname(__FILE__)) .'/DBStub.php'));
 require_once(realpath(dirname(dirname(__FILE__)) .'/konaenv.php'));
 // ----------------------------------------------------------------------------
 require_once("{$konadir}/inc/utils.inc.php");
+require_once("{$konadir}/obj/Headers.obj.php");
+require_once("{$konadir}/obj/ReportGenerator.obj.php");
 require_once("{$konadir}/obj/ReportClassroomMessaging.obj.php");
+require_once("{$konadir}/obj/Formatters.obj.php");
+require_once("{$konadir}/inc/formatters.inc.php");
+require_once("{$konadir}/messagedata/en/targetedmessage.php");
 
 class ReportClassroomMessagingTest extends PHPUnit_Framework_TestCase {
 
+	const NUM_EXPECTED_FIELDS = 12;
+
 	public function test_summary_report() {
-		$rcm = new ReportClassroomMessaging();
+
+		$options = array();
+		$rcm = new ReportClassroomMessaging($options);
 
 		// (1) Make sure it gives us a result object
-		$options = array();
-		$condition = $rcm->queryexec_csvdata($options);
-		$this->assertTrue($condition, 'queryexec_csvdata() indicated error result');
+		$condition = $rcm->generateQuery();
+		$this->assertTrue($condition, 'generateQuery() indicated error result');
 
 
 		// (2) Make sure it sends CSV to STDOUT - output buffering needed accordingly
 		$fakedata = array(
 			array(
-				"login" => 1,
-				"teacher" => 2,
-				"school" => 3,
-				"section" => 4,
-				"student id" => 5,
-				"student" => 6,
-				"message" => 7,
-				"notes" => 8,
-				"message time" => 9,
-				"last attempt" => 10,
-				'type' => 11,
-				"destination" => 12,
-				"result" => 13,
-				"status" => 14
+				'login' => 1,
+				'teacher' => 2,
+				'orgkey' => 3,
+				'skey' => 4,
+				'studentid' => 5,
+				'student' => 6,
+				'messagekey' => 7,
+				'notes' => 8,
+				'occurrence' => 9,
+				'lastattempt' => 10,
+				'destination' => 11,
+				'result' => 12,
+				'type' => 13
 			)
 		);
-		$rcm->csvdata->__results($fakedata);
+		$rcm->query->__results($fakedata);
 		ob_start();
-		$rcm->send_csvdata();
+		$rcm->runCSV();
 		$csv = ob_get_contents();
 		ob_end_clean();
 		$condition = (strlen($csv) > 0);
-		$this->assertTrue($condition, 'csvdata() did not generate any output');
+		$this->assertTrue($condition, 'runCSV() did not generate any output');
 
 
 		// (3) Make sure the CSV data line has the data we expect in it
@@ -61,21 +68,32 @@ class ReportClassroomMessagingTest extends PHPUnit_Framework_TestCase {
 			$num--;
 		}
 		$condition = ($num == 2);
-		$this->assertTrue($condition, "csvdata() CSV output line count was not 2 ({$num})");
+		$this->assertTrue($condition, "runCSV() CSV output line count was not 2 ({$num})");
 
 
 		// (4) Extract the data from the scond line, eliminating the outer and inner quote marks and delimiters
 		$csvdata = explode('","', substr($csvlines[1], 1, strlen($csvlines[1]) - 2));
-		$condition = (($num = count($csvdata)) == 14);
-		$this->assertTrue($condition, "summary_csv_to_stdout() CSV output line did not have 14 data fields ({$num})");
+		$condition = (self::NUM_EXPECTED_FIELDS == ($num = count($csvdata)));
+		$this->assertTrue($condition, 'summary_csv_to_stdout() CSV output line did not have ' . self::NUM_EXPECTED_FIELDS . " data fields ({$num})");
 
 
 		// (5) Iterate over the CSV values to make sure they are what the query returned
 		$condition = true;
-		for ($i = 0; $i < 13; $i++) {
-			if ($csvdata[$i] != ($i + 1)) {
-				$condition = false;
-				break;
+		for ($i = 0; $i < self::NUM_EXPECTED_FIELDS; $i++) {
+			// Fields 6 and 12 are exceptions; because we use a formatter to
+			// modify their values, we expect them to NOT equal their index+1
+			if (($i == 6) || ($i == 12)) {
+				if ($csvdata[$i] == ($i + 1)) {
+					$condition = false;
+					break;
+				}
+			}
+			else {
+				// Otherwise each field should retain the value that we staged in the fakedata
+				if ($csvdata[$i] != ($i + 1)) {
+					$condition = false;
+					break;
+				}
 			}
 		}
 		$this->assertTrue($condition, 'summary_csv_to_stdout() CSV data had values that did not match what we expected');
