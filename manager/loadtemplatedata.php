a85229155643246d4131f79ee516a33c37afdcd8
Nickolas Heckman <nheckman@schoolmessenger.com>
2013-11-08 08:37:46 -0800

    Tweaks email template, data and associated rendering, 5552

--- manager/loadtemplatedata.php
+++ manager/loadtemplatedata.php
@@ -673,7 +673,7 @@ DO NOT REPLY: This is an automatically generated email. Please do not send a rep
 <body style="background:lightslategrey">
 <table style="max-width:800px;"><tr><td>
     <div style="border:1px solid #2e2e2e;border-radius:8px 8px 8px 8px;box-shadow:0 2px 8px rgba(0, 0, 0, 0.15);background:white;padding:12px;margin:20px">
-        <p style="font-size:20px;font-style:italic;font-weight:bold;margin-top:4px;">${product}® QuickTip</p>
+        <p style="font-size:20px;font-style:italic;font-weight:bold;margin-top:4px;">${productname}&reg; QuickTip</p>
         <p style="font-size:18px;font-weight:bold;">New Tip Alert</p>
         <p>Generated on ${date}</p>
         <div style="border:1px solid #2e2e2e;border-radius:8px 8px 8px 8px;box-shadow:0 2px 8px rgba(0, 0, 0, 0.15);background:white;padding:8px;">
@@ -691,7 +691,7 @@ DO NOT REPLY: This is an automatically generated email. Please do not send a rep
         </div>
         <br>
         <div style="margin-top:20px;width:100%;">
-            <p><i>To view a detailed version of this report, go to <a href="http://asp.schoolmessenger.com/${customerurl}/quicktip.php">${product}</a> and log in.</i></p>
+            <p><i>To view a detailed version of this report, go to <a href="http://asp.schoolmessenger.com/${customerurl}/tips.php">${product}</a> and log in.</i></p>
             <p><i>You are receiving this alert because your profile has "Topic Recipient" enabled. Contact your system administrator for more details about QuickTip alerts.</i></p>
         </div>
     </div>
@@ -704,7 +704,7 @@ DO NOT REPLY: This is an automatically generated email. Please do not send a rep
 	$templates['quicktipalert']['en']['plain']['fromname'] = '${productname}';
 	$templates['quicktipalert']['en']['plain']['fromaddr'] = 'noreply@schoolmessenger.com';
 	$templates['quicktipalert']['en']['plain']['body'] = '
-${product}® QuickTip
+${productname} QuickTip
 New Tip Alert
 Generated on ${date}
 
@@ -717,7 +717,7 @@ ${tipbody}
 
 --------
 You are receiving this alert because your profile has "Topic Recipient" enabled.
-To view a detailed version of this report, go to http://asp.schoolmessenger.com/${customerurl}/quicktip.php and log in.
+To view a detailed version of this report, go to http://asp.schoolmessenger.com/${customerurl}/tips.php and log in.
 ';
 
 return $templates;
