c7796f871fef572d3ee35daa897d1936cfcda7a8
Ben Hencke <bhencke@schoolmessenger.com>
2013-12-02 15:19:40 -0800

    add DDMO for tai message attachments

---
+++ obj/tai/Tai_MessageAttachment.obj.php
@@ -0,0 +1,16 @@
+<?
+class Tai_MessageAttachment extends DBMappedObject {
+	var $messageid;
+	var $contentid;
+	var $filename;
+	var $size;
+
+	function Tai_MessageAttachment ($id = NULL) {
+		$this->_allownulls = true;
+		$this->_tablename = "messageattachment";
+		$this->_fieldlist = array("messageid", "contentid", "filename", "size");
+		DBMappedObject::DBMappedObject($id);
+	}
+
+}
+?>
