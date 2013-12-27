17db8a87476c06badff13e920e1e18965028a832
Sean Kelly <skelly@schoolmessenger.com>
2013-12-26 18:38:25 -0800

    First pass BDD-style unit testing for pdfedit page using home-spun API stubbing based on the DBStub approach. INCOMPLETE CS-5595

---
+++ UNITTEST/PHPUnit/ApiStub.php
@@ -0,0 +1,79 @@
+<?
+
+/**
+ * REST API Client data stubbing class
+ *
+ * Substitute for obj/ApiClient.obj.php
+ *
+ * We're going to leverage the database stubbing class by making the sendRequest method
+ * use a "database query" with a specially formatted, bogus query and respond with result
+ * data that comes from that.
+ *
+ */
+class ApiStub {
+
+	protected $apiHostname = '';
+	protected $apiCustomer = '';
+	protected $apiUser = '';
+	protected $apiAuth = '';
+	protected $customerUrl = '';
+	protected $ApiUrl = '';
+
+	public function __construct($apiHostname, $apiCustomer, $apiUser, $apiAuth) {
+		$this->apiHostname = $apiHostname;
+		$this->apiCustomer = $apiCustomer;
+		$this->apiUser = $apiUser;
+		$this->apiAuth = $apiAuth;
+
+		$this->customerUrl = "https://{$this->apiHostname}/{$this->apiCustomer}/";
+		$this->ApiUrl = "{$this->customerUrl}api/2/users/{$this->apiUser}";
+	}
+
+        public function getApiUrl() {
+		return $this->ApiUrl;
+	}
+
+	public function sendRequest($method, $node, $data = null) {
+
+		// Our bogus query is designed to match a regular expression in the QueryRules
+		// class of DBStub.php - it is in no way intended to include proper SQL syntax
+		$bogusRequestObject = (object) null;
+		$bogusRequestObject->method = $method;
+		$bogusRequestObject->node = $node;
+		$bogusRequestObject->data = $data;
+		$bogusQuery = json_encode($bogusRequestObject);;
+
+		$res = Query($bogusQuery);
+
+		// If QueryRules didn't give us anything useful (i.e. a response with headers/body/code defined)
+		if (! (is_array($res) && count($res))) {
+
+			// Then we'll structure a custom, default reply with nothing interesting in it
+			$res = array(
+				'headers' => 'Content-type: text/plain',
+				'body' => 'No result data was returned for this operation',
+				'code' => 0
+			);
+		}
+
+		return($res);
+	}
+
+	public function get($node = '') {
+		return($this->sendRequest('GET', $node));
+	}
+
+	public function ppiPut($node = '', $data = null) {
+		return($this->sendRequest('PUT', $node, $data));
+	}
+
+	public function post($node = '', $data = null) {
+		return($this->sendRequest('POST', $node, $data));
+	}
+
+	public function delete($node = '') {
+		return($this->sendRequest('DELETE', $node));
+	}
+}
+
+?>
---
+++ UNITTEST/PHPUnit/application/PDFEditPageTest.php
@@ -0,0 +1,165 @@
+<?php
+
+/**
+ * PDFEditPageTest.php - PHPUnit Test Class for PDF Edit Page
+ *
+ * @package unittests
+ * @version 1.0
+ */
+
+require_once(realpath(dirname(dirname(__FILE__)) .'/konaenv.php'));
+require_once(realpath(dirname(dirname(__FILE__)) .'/DBStub.php'));
+require_once(realpath(dirname(dirname(__FILE__)) .'/ApiStub.php'));
+// ----------------------------------------------------------------------------
+
+/**
+ * Test Class for PDF Edit Page
+ */
+class PDFEditPageTest extends PHPUnit_Framework_TestCase {
+	const USER_ID = 1;
+	const ACCESS_ID = 3;
+	const FORMNAME = 'pdfuploader';	// The name of our kona form; if it changes in the class, we'll need to update this
+
+	var $pdfEditPage = null; // The instance of our class under test
+	var $csApi = null;	// An instance of CommsuiteApiClient to satisfy API calls
+
+	/**
+	 * Before any tests run, before we even include the source file for the class under test
+	 * we may want to prepare some things that the file depends on such as environment variables
+	 * and such things that are normally set under an Apache context before loading...
+	 */
+	public static function setUpBeforeClass() {
+		global $konadir, $queryRules, $USER;
+
+
+		// Mock up some superglobal "server" data to make it look like we were requested through apache
+		$_SERVER['SERVER_NAME'] = 'localhost';
+		$_SERVER['REQUEST_URI'] = '/';
+
+		// 2) The user DBMO initialization query for userID=1
+		$queryRules->add('/from user where id/', array(self::USER_ID),
+			array(
+				array(
+					self::ACCESS_ID,
+					'first.last',
+					'',
+					'first',
+					'last',
+					'description',
+					'email',
+					'autoreportemail',
+					'8316001335',
+					1,
+					null,
+					0,
+					0,
+					'staff10124',
+					null,
+					'2013-01-01 12:00:00',
+					null
+				)
+			)
+		);
+
+		// 3) The profile (access) record for this user
+		$queryRules->add('/from access where id/', array(self::ACCESS_ID),
+			array(
+				array(
+					'name',
+					'description'
+				)
+			)
+		);
+
+		// 4) Permissions for this user's profile
+		$queryRules->add('/from permission where accessid/',
+			array(
+				array(
+					1,
+					self::ACCESS_ID,
+					'canpdfburst',
+					1
+				)
+			)
+		);
+
+		// 5) A list of burst template records
+		$queryRules->add('/FROM `bursttemplate` WHERE NOT `deleted`/',
+			array(
+				array(
+					'id' => 1,
+					'name' => 'first template'
+				),
+				array(
+					'id' => 2,
+					'name' => 'second template'
+				)
+			)
+		);
+
+		// 6) API response for a single burst record (with id=1)
+		$queryRules->add('|"method":"GET","node":"\\\/bursts\\\/1","data":null|',
+			array(
+				'headers' => 'Content-type: application/json',
+				'body' => '{"id":1,"name":"testname","filename":"testfile.pdf","size":1234,"status":"new","contentId":1,"ownerUser":{"id":1},"uploadTimeStampms":1234,"burstTemplateId":1}',
+				'code' => 200
+			)
+		);
+
+
+		// Mock up a USER session
+		require_once("{$konadir}/inc/common.inc.php");
+		$USER = new User(self::USER_ID);
+		$_SESSION['access'] = new Access($USER->accessid);
+
+		require_once("{$konadir}/pdfedit.php");
+	}
+
+	// before each test
+	protected function setUp() {
+		global $USER;
+
+		// Stub out API access
+		$apiClient = new ApiStub('localhost', 'unrealcustomer', $USER->id, 'unrealauthcookiedata');
+		$this->csApi = new CommsuiteApiClient($apiClient);
+
+		// Then go for launch!
+		$this->pdfEditPage = new PDFEditPage($this->csApi);
+	}
+
+	// after each test
+	protected function tearDown() {
+		unset($this->pdfEditPage);
+	}
+
+	public function test_isAuthorized() {
+		$this->assertTrue($this->pdfEditPage->isAuthorized(), 'The user should be authorized to access this page');
+	}
+
+	public function test_newUploadForm() {
+		// Load up the edit form with no burst id specified
+		$this->pdfEditPage->beforeLoad();
+		$this->pdfEditPage->load();
+		$this->pdfEditPage->afterLoad();
+		$formhtml = $this->pdfEditPage->render();
+		$this->assertTrue(false !== strpos($formhtml, 'multipart/form-data'), 'Missing expected enctype="multipart/form-data"');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_name'), 'Missing name input field');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_bursttemplateid'), 'Missing burst template select field');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_thefile'), 'Missing file input field');
+	}
+
+	public function test_editExistingForm() {
+		// Load up the edit form with burst id = 1
+		$data = array('id' => 1);
+		$this->pdfEditPage->beforeLoad($data, $data, $data);
+		$this->pdfEditPage->load();
+		$this->pdfEditPage->afterLoad();
+		$formhtml = $this->pdfEditPage->render();
+		$this->assertTrue(false !== strpos($formhtml, 'multipart/form-data'), 'Missing expected enctype="multipart/form-data"');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_name'), 'Missing name input field');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_bursttemplateid'), 'Missing burst template select field');
+		$this->assertTrue(false !== strpos($formhtml, self::FORMNAME . '_thefile'), 'Missing file input field');
+	}
+}
+
+?>
