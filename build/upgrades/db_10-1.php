5c731a472beee361320f74474675a2ccd01f5342
Nickolas Heckman <nheckman@schoolmessenger.com>
2014-01-22 18:56:46 -0800

    Adds field for holding the "secret", CS-5636

--- build/upgrades/db_10-1.php
+++ build/upgrades/db_10-1.php
@@ -80,6 +80,9 @@ function upgrade_10_1 ($rev, $shardid, $customerid, $db) {
 			createLimitedUser($limitedusername, $limitedpassword, $custdbname, $db, $grantedhost);
 			Query("COMMIT", $authdb);
 
+		case 10:
+			echo "|";
+			apply_sql("upgrades/db_10-1_pre.sql", $customerid, $db, 11);
 	}
 	
 	//This statement should appear in each upgrade script, when relevent.
