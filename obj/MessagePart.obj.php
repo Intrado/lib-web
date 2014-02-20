946cfa61d25157f7b20b3fa260f7e10fb4e47e8a
Justin Burns <justin.j.burns@gmail.com>
2014-02-20 13:48:43 -0800

    Adds WIP for adding new MAL (Message Attachment Link) MessagePart for appended  email attachment link in pdfsendmail.php, CS-5957

--- obj/MessagePart.obj.php
+++ obj/MessagePart.obj.php
@@ -5,21 +5,21 @@ class MessagePart extends DBMappedObject {
 	var $messageid;
 	var $type;
 	var $audiofileid;
+	var $imagecontentid;
+	var $messageattachmentid;
 	var $txt;
 	var $fieldnum;
 	var $defaultvalue;
 	var $voiceid;
 	var $sequence;
 	var $maxlen;
-	var $imagecontentid;
-    var $messageattachmentid;
 
     var $audiofile;
 
 	function MessagePart ($id = NULL) {
 		$this->_allownulls = true;
 		$this->_tablename = "messagepart";
-		$this->_fieldlist = array("messageid", "type", "audiofileid", "imagecontentid", "txt", "fieldnum", "defaultvalue", "voiceid", "sequence", "maxlen", "messageattachmentid");
+		$this->_fieldlist = array("messageid", "type", "audiofileid", "imagecontentid", "messageattachmentid", "txt", "fieldnum", "defaultvalue", "voiceid", "sequence", "maxlen");
 
 		//call super's constructor
 		DBMappedObject::DBMappedObject($id);
