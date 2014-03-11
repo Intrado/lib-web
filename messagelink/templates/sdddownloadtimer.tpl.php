b9d0b510904e94845d8f071fc102118b283166a0
Justin Burns <justin.j.burns@gmail.com>
2014-03-10 19:05:24 -0700

    Removes unused templates (due to refactor), CS-5942

--- messagelink/templates/sdddownloadtimer.tpl.php
+++
@@ -1,9 +0,0 @@
-<div class="instruction download">
-	<div id="download-timer-wrapper">
-		<span class="glyphicon glyphicon-download"></span> &nbsp;Your download will begin in <span id="download-count" class="text-danger">5</span> seconds...
-	</div>
-	<p>Problems with the download?  Please use this direct <a class="directlink">link</a>.</p>
-	<div class="alert alert-danger" id="download-error-message-container">
-		<span class="label label-danger"><span class="glyphicon glyphicon-exclamation-sign" style=""></span></span> &nbsp;<span id="download-error-message">An error occurred while trying to retrieve your document. Please try again.</span>
-	</div>
-</div>
