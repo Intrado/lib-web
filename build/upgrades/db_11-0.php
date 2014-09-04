09806d518dc9a7f773784be0fb15b82feb569929
Hasari Tosun <hasari@gmail.com>
2014-09-04 14:08:57 -0600

    CS-6648 : add ICR District opt-out option for existing customer

--- build/upgrades/db_11-0.php
+++ build/upgrades/db_11-0.php
@@ -17,6 +17,17 @@ function upgrade_11_0($rev, $shardid, $customerid, $db) {
 		case 3:
 			echo "|";
 			apply_sql("upgrades/db_11-0_pre.sql", $customerid, $db, 4);
+		case 4:
+			echo "|";
+			apply_sql("upgrades/db_11-0_pre.sql", $customerid, $db, 4);
+
+			  //enable ICRA for all existing customers
+			Query("BEGIN", $db);
+			if (!QuickQuery("select count(*) from setting where name = ?", $db, array("_hasicra"))) {
+				$portal = QuickQuery("select value from setting where name = ?", $db, array("_hasportal"));
+				QuickUpdate("insert into setting (name, value) values ('_hasicra',?)", false, array($portal ? 0 : 1));
+			}
+			Query("COMMIT", $db);
 	}
 	//This statement should appear in each upgrade script, when relevent.
 	apply_sql("../db/update_SMAdmin_access.sql", $customerid, $db);
