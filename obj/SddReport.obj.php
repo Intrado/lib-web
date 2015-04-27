90b35d94bc8246c5884a863dd0512fd993e9c3a5
Bill Karwin <bill@karwin.com>
2015-04-27 15:09:07 -0700

    CS-7698 new report for SDD events.

---
+++ obj/SddReport.obj.php
@@ -0,0 +1,160 @@
+<?
+
+require_once('obj/ReportGenerator.obj.php');
+
+function fmt_sdd_action($row, $index) {
+	$map = array(
+		"SEND" => "Sent",
+		"CLICK" => "Clicked",
+		"DOWNLOAD" => "Downloaded",
+		"BAD_PASSWORD" => "Entered Bad Password"
+	);
+	return isset($map[$row[$index]]) ? $map[$row[$index]] : "Unknown";
+}
+
+class SddReport extends ReportGenerator {
+
+	const DEFAULT_PAGE_SIZE = 100;
+
+	public $queryArgs = false;
+
+	private $titles = array(
+		'0' => "Last Name",
+		'1' => "First Name",
+		'2' => "Sequence",
+		'3' => "Destination",
+		'5' => "Attachment Filename",
+		'6' => "Activity",
+		'7' => "Activity Count",
+		'8' => "Last Timestamp",
+	);
+	private $formatters = array(
+		'2' => "fmt_renderedlist_destination_sequence",
+		'3' => "fmt_email",
+		'6' => "fmt_sdd_action",
+		'8' => "fmt_ms_timestamp"
+	);
+
+	function generateQuery($hackPDF = false){
+		$this->params = $this->reportinstance->getParameters();
+
+		// the query return only messageattachments for which there is >= 1 row in reportsdddelivery.
+		// - sdd_action is a reference to the most recent action of any type for a given attachment and given person.
+		// - sdd_download is a reference to the most recent DOWNLOAD action for a given attachment and given person.
+		$this->query =
+"select sql_calc_found_rows
+p.f02 as lastname,
+p.f01 as firstname,
+rc.sequence,
+rc.email as destination,
+rc.type,
+ba.filename,
+coalesce(sdd_download.action, sdd_action.action) as action,
+coalesce(sdd_download.actionCount, sdd_action.actionCount) as actionCount,
+coalesce(sdd_download.timestampMs, sdd_action.timestampMs) as actionTimestampMs
+from burst as b
+inner join burstattachment as ba on (ba.burstid = b.id)
+inner join messageattachment as ma on (ma.burstattachmentid = ba.id)
+inner join reportsdddelivery as sdd_action on (sdd_action.messageAttachmentId = ma.id)
+left outer join reportsdddelivery as sdd_action2 on (sdd_action2.messageAttachmentId = ma.id and sdd_action2.personid = sdd_action.personid and sdd_action2.timestampMs > sdd_action.timestampMs)
+left outer join reportsdddelivery as sdd_download on (sdd_download.messageAttachmentId = ma.id and sdd_download.personid = sdd_action.personid and sdd_download.action = 'DOWNLOAD')
+left outer join reportsdddelivery as sdd_download2 on (sdd_download2.messageAttachmentId = ma.id and sdd_download2.personid = sdd_action.personid and sdd_download2.action = 'DOWNLOAD' and sdd_download2.timestampMs > sdd_download.timestampMs)
+inner join person as pp on (pp.id = sdd_action.personid)
+inner join reportperson as rp on (rp.jobid = b.jobid and rp.type = 'email' and rp.personid = pp.id)
+inner join reportcontact as rc on (rc.jobid = rp.jobid and rc.type = rp.type and rc.personid = rp.personid)
+inner join person as p on (p.id = rc.recipientpersonid)
+where b.id = ? and sdd_action2.messageattachmentid is null and sdd_download2.messageattachmentid is null
+order by lastname, firstname
+";
+		$this->queryArgs[] = $this->params['id'];
+	}
+
+	function runHtml() {
+
+		$data = array();
+		$pageSize = self::DEFAULT_PAGE_SIZE;
+		$pageStart = isset($this->params['pagestart']) ? (int) $this->params["pagestart"] : 0;
+		$limit = "limit $pageStart, $pageSize";
+
+		$result = Query($this->query . $limit, $this->_readonlyDB, $this->queryArgs);
+		while ($row = DBGetRow($result)) {
+			$data[] = $row;
+		}
+
+		$total = QuickQuery("select found_rows()", $this->_readonlyDB);
+
+		if ($data) {
+			if ($total > $pageSize) {
+				showPageMenu($total, $pageStart, $pageSize);
+			}
+
+		?>
+			<table width="100%" cellpadding="3" cellspacing="1" class="list" id="searchresults">
+		<?
+			showTable($data, $this->titles, $this->formatters);
+		?>
+			</table>
+			<script type="text/javascript">
+			var searchresultstable = new getObj("searchresults").obj;
+			</script>
+		<?
+			if ($total > $pageSize) {
+				showPageMenu($total, $pageStart, $pageSize);
+			}
+		} else {
+		?>
+			<em>No results found.</em>
+		<?
+		}
+	}
+
+	function runCSV($options = false) {
+		if ($options) {
+			if (!$options["filename"]) {
+				return false;
+			}
+			$outputfile = $options["filename"];
+		} else {
+			$outputfile = "php://output";
+			header("Pragma: private");
+			header("Cache-Control: private");
+			header("Content-disposition: attachment; filename=report.csv");
+			header("Content-type: application/vnd.ms-excel");
+		}
+		$fp = fopen($outputfile, "w");
+		if (!$fp) {
+			return false;
+		}
+
+		$headerfields = array_map(function ($value) { return '"'.$value.'"'; }, $this->titles);
+		$header = implode(",", $headerfields);
+		$ok = fwrite($fp, $header . "\r\n");
+		if (!$ok) {
+			return false;
+		}
+
+		session_write_close();//WARNING: we don't keep a lock on the session file, any changes to session data are ignored past this point
+
+		// we don't need to worry about blowing out PHP memory if we fetch rows unbuffered
+		$this->_readonlyDB->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, false);
+		$result = Query($this->query, $this->_readonlyDB, $this->queryArgs);
+		while ($row = DBGetRow($result)) {
+			foreach ($this->formatters as $index => $formatter) {
+				$row[$index] = $formatter($row, $index);
+			}
+
+			$row = array_map(function ($value) {
+				return '"' . $value . '"';
+			}, $row);
+			$ok = fwrite($fp, implode(",", $row) . "\r\n");
+			if (!$ok) {
+				return false;
+			}
+		}
+
+		if ($options) {
+			return fclose($fp);
+		}
+	}
+
+}
---
+++ reportsdd.php
@@ -0,0 +1,118 @@
+<?
+require_once("inc/common.inc.php");
+require_once("inc/form.inc.php");
+require_once("inc/html.inc.php");
+require_once("inc/table.inc.php");
+require_once("inc/securityhelper.inc.php");
+require_once("inc/formatters.inc.php");
+
+require_once('ifc/Page.ifc.php');
+require_once('obj/PageBase.obj.php');
+require_once('obj/SddReport.obj.php');
+require_once('obj/ReportInstance.obj.php');
+
+/**
+ * class ReportPdfDocument
+ * 
+ * @description: TODO
+ * @author: Bill Karwin <bkarwin@schoolmessenger.com>
+ * @date: 4/23/2015
+ */
+class ReportSdd extends PageBase {
+	protected $reportGenerator = null;
+	protected $reportOutput = null;
+	protected $documentId;
+	private $csApi;
+
+	function __construct($csApi) {
+		$this->csApi = $csApi;
+		parent::__construct();
+	}
+	
+	// @override
+	function isAuthorized($get = array(), $post = array()) {
+		global $USER;
+		return getSystemSetting("_haspdfburst", false) && $USER->authorize("canpdfburst") && $USER->authorize("viewsystemreports");
+	}
+
+	// @override
+	public function initialize() {
+		// override page option on PageBase
+		$this->options['page']  = "reports:reports";
+		$this->options['title'] = "Secure Document Delivery";
+		$this->options['windowTitle'] = _L("SDD Document Results");
+		$this->options['format'] = "html";
+	}
+
+	// @override
+	public function beforeLoad(&$get=array(), &$post=array(), &$request=array(), &$session=array()) {
+		if (isset($session['report']) && isset($session['report']['options'])) {
+			$this->options = $session['report']['options'];
+		}
+		if (isset($get['id'])) {
+			$this->options['id'] = (int) $get['id'];
+		}
+		if (!$this->options['id']) {
+			redirect('pdfmanager.php');
+		}
+		$this->documentId = $this->options['id'];
+		$session['report']['options'] = $this->options;
+
+		if (isset($get['csv']) && $get['csv']) {
+			$this->options['format'] = 'csv';
+		}
+		$this->options['pagestart'] = isset($get['pagestart']) ? (int) $get['pagestart'] : 0;
+	}
+
+	// @override
+	public function load() {
+		$this->reportGenerator = new SddReport();
+		$instance = new ReportInstance();
+		$this->reportGenerator->reportinstance = $instance;
+		$this->reportGenerator->set_format($this->options['format']);
+		$instance->setParameters($this->options);
+	}
+
+	// @override
+	public function beforeRender() {
+		if ($this->reportGenerator) {
+			switch ($this->reportGenerator->format) {
+			case 'csv':
+				$this->reportGenerator->generate();
+				exit();
+				break;
+			case 'html':
+			default:
+				ob_start();
+				$this->reportGenerator->generate();
+				$this->reportOutput = ob_get_clean();
+				break;
+			}
+		}
+	}
+
+	// @override
+	public function render() {
+		$html = parent::render() . $this->reportOutput;
+		return ($html);
+	}
+
+	// @override
+	public function sendPageOutput() {
+		$fallbackUrl = "pdfmanager.php";
+		$backButton = icon_button(_L("Back"), "fugue/arrow_180", "location.href='" . (isset($_SERVER['HTTP_REFERER']) ? $_SERVER['HTTP_REFERER'] : $fallbackUrl) . "'");
+		$refreshButton = icon_button(_L('Refresh'),"arrow_refresh","window.location.reload()");
+		$csvButton = icon_button("Download CSV", "page_white_excel", null, "reportsdd.php/report.csv?id={$this->documentId}&csv=true");
+		buttons($backButton, $refreshButton, $csvButton);
+
+		parent::sendPageOutput();
+
+		buttons();
+	}
+
+}
+
+// Initialize and render page
+// ================================================================
+executePage(new ReportSdd($csApi));
+
