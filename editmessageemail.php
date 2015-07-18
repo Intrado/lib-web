b3d762c16073f0a76079136180c6a71b8fae8a4b
Sean Kelly <skelly@schoolmessenger.com>
2015-07-17 18:14:16 -0700

    Adds a couple includes to legacy email message editor to support image resizing CS-7254 CS-8151

--- editmessageemail.php
+++ editmessageemail.php
@@ -14,8 +14,11 @@ require_once("inc/securityhelper.inc.php");
 require_once("inc/table.inc.php");
 require_once("inc/html.inc.php");
 require_once("inc/utils.inc.php");
+include_once("inc/content.inc.php");
+
 require_once("obj/Form.obj.php");
 require_once("obj/FieldMap.obj.php");
+require_once("obj/Content.obj.php");
 require_once("obj/Message.obj.php");
 require_once("obj/MessageGroup.obj.php");
 require_once("obj/MessagePart.obj.php");
