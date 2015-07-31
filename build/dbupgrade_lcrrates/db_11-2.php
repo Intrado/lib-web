af9aeb80c53b04f8924b70745cf9ee9d21af977b
Bill Karwin <bill@karwin.com>
2015-07-31 15:55:40 -0700

    Move 11.2 schema migrations to 11.3 to match product. CS-6789

--- build/dbupgrade_lcrrates/db_11-2.php
+++ build/dbupgrade_lcrrates/db_11-2.php
@@ -13,9 +13,6 @@ function lcrrates_upgrade_11_2($rev, $db) {
 	case 2:
 		echo "|";
 		apply_sql_db("dbupgrade_lcrrates/db_11-2_pre.sql", $db, 3);
-	case 3:
-		echo "|";
-		apply_sql_db("dbupgrade_lcrrates/db_11-2_pre.sql", $db, 4);
 	}
 
 	return true;
