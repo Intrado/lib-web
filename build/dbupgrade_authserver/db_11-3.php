233ae7003a81827c9488244a3bf2572f68bb06f4
Bill Karwin <bill@karwin.com>
2015-07-31 15:55:40 -0700

    Move 11.2 schema migrations to 11.3 to match product. CS-6789

---
+++ build/dbupgrade_authserver/db_11-3.php
@@ -0,0 +1,15 @@
+<?
+
+function authserver_upgrade_11_3($rev, $db) {
+
+	switch ($rev) {
+	default:
+	case 0:
+		echo "|";
+		apply_sql_db("dbupgrade_authserver/db_11-3_pre.sql", $db, 1);
+	}
+
+	return true;
+}
+
+?>
