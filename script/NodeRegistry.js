c13bb0cb22f5c71018a6fdb78fe697b199f84807
Sean Kelly <skelly@schoolmessenger.com>
2015-08-04 13:54:54 -0700

    Introduces a simple, custom JS object "Node Registry" utility script CS-7254 CS-8190
    The basic idea is to be able to take any given JS object and wrap it as a "Node" which gets added to the registry. Each Node can have named events added to it and then any other JS code can easily invoke events against any of the registered nodes without having to directly link those normally disparate pieces of JS together.

---
+++ script/NodeRegistry.js
@@ -0,0 +1,86 @@
+NodeRegistry = function () {
+
+	// private
+	var nodes = {};
+	
+	// semi-private
+	var Node = function (obj) {
+		this.obj = obj;
+		this.events = {};
+
+		// Add a named event handler to this node
+		//
+		// The named event must be handled by a function; either a function is supplied, or
+		// the NAME of a function in obj is supplied, or the event handler will be undefined.
+		//
+		// @param name string name of the event that we want to handle
+		// @param handler mixed callable handler function OR name of a method under obj to use
+		this.setEventHandler = function (name, handler) {
+			if (typeof handler === 'function') {
+				this.events[name] = handler;
+			}
+			else if (typeof this.obj[handler] === 'function') {
+				this.events[name] = this.obj[handler];
+			}
+			return this;
+		};
+
+		// Handle the named event
+		//
+		// Returns true if this node has an event handler for this name and has invoked it,
+		// else false.
+		this.handleEvent = function (name) {
+			if (typeof this.events[name] !== 'function') return false;
+			this.events[name]();
+			return true;
+		};
+	};
+
+	// public
+	var NodeRegistry = {
+
+		makeNode: function (obj) {
+			return new Node(obj);
+		},
+
+		addNode: function (id, node) {
+			if (NodeRegistry.hasNode(id)) return false;
+			nodes[id] = node;
+			return node;
+		},
+
+		getNode: function (id) {
+			return NodeRegistry.hasNode(id) ? nodes[id] : null;
+		},
+
+		hasNode: function (id) {
+			return nodes[id] ? true : false;
+		},
+
+		removeNode: function (id) {
+			if (NodeRegistry.hasNode(id)) return;
+			delete nodes[id];
+		},
+
+		// Fire the named event
+		//
+		// If a node id is supplied, just fire the event on that one, otherwise iterate over
+		// all nodes and fire the event on each one.
+		fireEvent: function (name, id) {
+console.log('fire! ' + name);
+			if (id) {
+				if (! NodeRegistry.hasNode(id)) return false;
+				NodeRegistry.getNode(id).handleEvent(name);
+			}
+			else {
+				for (var nodeId in nodes) {
+					NodeRegistry.getNode(nodeId).handleEvent(name);
+				}
+				
+			}
+		}
+	};
+
+	return NodeRegistry;
+}();
+
