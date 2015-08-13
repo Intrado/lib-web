b75c3383208f733f78c3d3ac8581bf7d5c75fc4d
Sean Kelly <skelly@schoolmessenger.com>
2015-08-13 15:00:58 -0700

    Creates a new JS helper class for working with Twitter accounts Cs-7441 CS-8144

---
+++ script/TwitterHelper.js
@@ -0,0 +1,63 @@
+TwitterHelper = function () {
+
+	// public
+	var TwitterHelper = {
+		clearValue: function (formitem) {
+			var fi = $(formitem);
+			var user_id = fi.value;
+			fi.value = "";
+			
+			// ajax request to remove it from the db
+			new Ajax.Request("ajaxtwitter.php",
+				{
+					method: "post",
+					parameters: {
+						"type": "delete_access_token",
+						"user_id": user_id
+					}
+				}
+			);
+			
+			// display the connect button
+			$(formitem + "twconnected").setStyle({display: "none"});
+		},
+
+		loadUserData: function (element, user_id) {
+			element = $(element);
+			element.update(new Element("img", { src: "img/ajax-loader.gif" }));
+			new Ajax.Request("ajaxtwitter.php", {
+				method: "get",
+				parameters: {
+					"type": "user",
+					"user_id": user_id
+				},
+				onSuccess: function(r) {
+					var data = r.responseJSON;
+					if (data) {
+						var profile_image = new Element("img", { 
+							src: data.profile_image_url_https,
+							width: "48",
+							height: "48"
+						})
+						var profile_image_box = new Element("div").setStyle({ float: "left" }).insert(profile_image);
+						var e = new Element("div").insert(profile_image_box);
+						var screen_name = new Element("div").setStyle({ "fontWeight": "bold" }).update(data.screen_name.escapeHTML());
+						var name = new Element("div").setStyle({ color: "grey" }).update(data.name.escapeHTML());
+						var profile_box = new Element("div").setStyle({ float: "left", padding: "7px" }).insert(screen_name).insert(name);
+						e.insert(profile_box);
+						element.update(e);
+					} 
+					else {
+						element.update();
+					}
+				},
+				onFailure: function() {
+					element.update();
+				}
+			});
+		}
+	}
+
+	return TwitterHelper;
+}();
+
