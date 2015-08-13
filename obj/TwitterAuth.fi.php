662b1a842e46eb9c4a4079828dc6f38fc0f8a916
Sean Kelly <skelly@schoolmessenger.com>
2015-08-13 13:37:42 -0700

    Refactors TwitterAuth formitem for conformance, cleanliness, and leverage of TwitterTokens CS-7441 CS-8146

--- obj/TwitterAuth.fi.php
+++ obj/TwitterAuth.fi.php
@@ -3,56 +3,62 @@
 
 class TwitterAuth extends FormItem {
 	function render ($value) {
-		global $SETTINGS;
-		global $USER;
 		
 		// NOTE: this form item changes DB values and thus, cannot get it's value from the $value variable
 		// if you set value on the form item you will get errors submitting the form
 		
-		$n = $this->form->name."_".$this->name;
+		$n = $this->form->name . '_' . $this->name;
 
 		$str = $scriptStr = '';
-		$twAccessTokens = json_decode($USER->getSetting("tw_access_token", false));
-		if (is_array($twAccessTokens) && (count($twAccessTokens) > 0)) {
-			for ($xx = 0; $xx < count($twAccessTokens); $xx++) {
+		$twitterTokens = new TwitterTokens();
+		$accessTokens = $twitterTokens->getAllAccessTokens();
+		if (is_array($accessTokens)) {
+			for ($xx = 0; $xx < count($accessTokens); $xx++) {
 
 				// Get the one we're working with; xx is our enumerator for DHTML operations...
 				$dn = $n . "_{$xx}";
-				$twitter = new Twitter($twAccessTokens[$xx], false);
+				$twitter = new Twitter($accessTokens[$xx], false);
 				$validToken = $twitter->hasValidAccessToken();
 
 				// Per-twitter account containers
-				$str .= '<input id="' . $dn . '" name="' . $dn . '" type="hidden" value="' . escapehtml($twAccessTokens[$xx]->user_id) . '"/>';
+				$str .= '<input id="' . $dn . '" name="' . $dn . '" type="hidden" value="' . escapehtml($accessTokens[$xx]->user_id) . '"/>';
 				$str .= '<div id="' . $dn . 'twdetails">';
 				$str .= '<div id="' . $dn . 'twconnected" style="border: 1px dotted grey; padding: 5px;' . (($validToken) ? "" : "display:none;") . '">';
 				$str .= '<div id="' . $dn . 'twuser"></div>';
 				
 				// button to remove access_token
 				$str .= icon_button("Disconnect this Twitter Account", "custom/twitter" ,"twClearValue('" . $dn . "')");
-				
 				$str .= '<div style="clear: both"></div></div>';
-				
-				// disconnected options div
-				$scriptStr .= 'twLoadUserData("' . $dn . 'twuser", "' . escapehtml($twAccessTokens[$xx]->user_id) . '");' . "\n";
 			}
 		}
 
 
 		// Do twitter login to get good auth token
 		$str .= submit_button(_L('Connect to Twitter'), 'twitterauth', 'custom/twitter');
-		/*
-		if (isset($this->args["submit"])) {
-			$str .= submit_button(_L("Connect to Twitter"), "twitterauth", "custom/twitter");
-		}
-		else {
-			$thispage = substr($_SERVER["SCRIPT_NAME"], strrpos($_SERVER["SCRIPT_NAME"], "/") + 1);
-			$str .= icon_button(_L("Connect to Twitter"), "custom/twitter", "", "twitterauth.php/$thispage");
-		}
-		*/
 
 		$str .= '<div style="clear: both"></div></div></div>';
+		return $str;
+	}
 
-		return $str . "\n\n" . '
+	function renderJavascript($value) {
+		$n = $this->form->name . '_' . $this->name;
+
+		$str = '';
+		$twitterTokens = new TwitterTokens();
+		$accessTokens = $twitterTokens->getAllAccessTokens();
+		if (is_array($accessTokens)) {
+			for ($xx = 0; $xx < count($accessTokens); $xx++) {
+
+				// Get the one we're working with; xx is our enumerator for DHTML operations...
+				$dn = $n . "_{$xx}";
+				$str .= 'twLoadUserData("' . $dn . 'twuser", "' . escapehtml($accessTokens[$xx]->user_id) . '");' . "\n";
+			}
+		}
+		return $str;
+	}
+
+	function renderJavascriptLibraries() {
+		return '
 			<script type="text/javascript">
 			function twClearValue(formitem) {
 				var fi = $(formitem);
@@ -108,7 +114,7 @@ class TwitterAuth extends FormItem {
 					}
 				});
 			}
-		' . $scriptStr . '</script>';
+		</script>';
 	}
 }
 
