4629981b00d67c97f90da7eb150a221690a1d280
chanhannigan <chanhannigan@gmail.com>
2015-08-14 11:46:29 -0700

    dds obj to connect to silver api and get categories CS-8175

--- obj/SilverApiClient.obj.php
+++ obj/SilverApiClient.obj.php
@@ -1,17 +1,19 @@
 <?php
 
 /**
- * class CmaApiClient
- *
- * Fetches "categories" from the CMA API (only requirement as of 1/14/2014; others TBD?)
- *
- * @author Justin Burns
- * @date 1/14/2014
+ *  @author: Nathan Nicholas 
  */
-class CmaApiClient {
+
+class SilverApiClient {
 
 	private $apiClient;
 	private $appId;
+	
+	// basic auth credentials=
+	private $username = 'silverappbuilder@relianceco.com';
+	private $password = 'Z5h^w}3F?L(8U2D2';
+	
+	private $sessionId;
 
 	/**
 	 * Constructor - initialize CmaApiClient object
@@ -22,39 +24,35 @@ class CmaApiClient {
 	public function __construct($apiClient, $appId = null) {
 		$this->apiClient = $apiClient;
 		$this->appId = $appId;
+		$this->sessionId = $this->getSessionId($appId);
 	}
-
-	/**
-	 * Gets categories from CMA API for a given customer's CMA app Id
-	*
-	* The CMA categories for an individual app are obtained from
-	* the following CMA endpoint: GET /apps/{appid}/streams/categories
-	*
-	* The response from requesting the endpoint above returns an object with a 'stream' property,
-	* which contains the array of the categories we are seeking.
-	*
-	* for more details, see: https://reliance.atlassian.net/wiki/display/CMA/CMA+Service+API#CMAServiceAPI-GET/apps/{appid}/streams/categories
-	 *
-	 * @return array of objects, ex [{"id":1,"name":"School A"},{"id":2,"name":"School B"}, ...] or false
-	 */
+	
+	// create a header for the basic auth request
+	public function getBasicAuthHeader() {
+		return array(
+			"Authorization: Basic " . base64_encode($this->username . ':' . $this->password)
+		);
+	}
+	
+	// retrieve a session id to use with subsequent requests
+	public function getSessionId() {
+		$res = $this->apiClient->sendRequest('GET',"/api/account/verify_credentials.json?nid={$this->appId}", null, $this->getBasicAuthHeader());
+		
+		return ($res['code'] == 200 ? json_decode($res['body'])->session_id : false);
+	}
+	
+	// retrieve list of categories 
 	public function getCategories() {
-		$res = $this->apiClient->get("/1/apps/{$this->appId}/streams/categories");
-
-		// now get the categories array from the 'stream' prop
-		return ($res['code'] == 200 ? json_decode($res['body'])->stream : false);
+		$res = $this->apiClient->get('/' . $this->appId . '/channels/objects/channels?embed=_notificationGroup&session_id=' . $this->sessionId);
+		
+		return ($res['code'] == 200 ? json_decode($res['body'])->data : false);
 	}
 
-	/**
-	 * Checks whether our appId is valid
-	 *
-	 * Also useful as a CMA API service "ping" for a known-valid appId.
-	 *
-	 * @return boolean true if our current appId is valid according to the remote service, else false
-	 */
-	public function isValidAppId() {
-		$res = $this->apiClient->get("/1/apps/{$this->appId}");
-		return ($res['code'] == 200);
-	}
+	
+//	public function isValidAppId() {
+//		$res = $this->apiClient->get("/1/apps/{$this->appId}");
+//		return ($res['code'] == 200);
+//	}
 }
 
 ?>
