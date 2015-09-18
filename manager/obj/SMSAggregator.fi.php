c7106e6be36cc00639a8d82d81fc4460f7818963
chanhannigan <chanhannigan@gmail.com>
2015-09-18 16:16:07 -0700

    adds form item for new dropdown select UI-42

--- manager/obj/SMSAggregator.fi.php
+++ manager/obj/SMSAggregator.fi.php
@@ -1,46 +1,68 @@
 <?
 class SMSAggregator extends FormItem {
 	
-	function render ($value) {
+	// flag required by FormItem
+	function render ($selectvalue) {
+		
+		$formData = $this->args['values']['form'];
 		
 		// code copied from SelectMenu form item in Form.obj.php
 		$n = $this->form->name."_".$this->name;
 		$size = isset($this->args['size']) ? 'size="'.$this->args['size'].'"' : "";
+		
 		$str = '<select id='.$n.' name="'.$n.'" '.$size .' >';
-		foreach ($this->args['values'] as $selectvalue => $selectname) {
-			$checked = $value == $selectvalue;
-			$str .= '<option value="'.escapehtml($selectvalue).'" '.($checked ? 'selected' : '').' >'.escapehtml($selectname).'</option>
-				';
+		foreach ($formData as $key => $groupName) {
+			$str .= '<option value="'.escapehtml($key).'" '.($key  == $selectvalue ? 'selected' : '').' >'.escapehtml($groupName).'</option>';
 		}
 		$str .= '</select>';
 		
 		// then we add a couple divs for displaying vendor and short code information
-		$str .='<div id="smsShortCodeData" style></div>';
+		$str .='<div style="padding: 4px;margin: 3px;" id="smsShortCodeData"></div>';
 		return $str;
 	}
 	
-//	function renderJavascript($value) {
-//		global $SETTINGS;
-//		$n = $this->form->name."_".$this->name;
-//		
-//		$str = '// Facebook javascript API initialization, pulled from facebook documentation
-//				window.fbAsyncInit = function() {
-//					FB.init({appId: "'. $SETTINGS['facebook']['appid']. '", status: true, cookie: false, xfbml: true});
-//					
-//					// load the initial list of pages if possible
-//					updateFbPagesRo("'.$n.'", "'.$n.'fbpages");
-//				};
-//				(function() {
-//					var e = document.createElement("script");
-//					e.type = "text/javascript";
-//					e.async = true;
-//					e.src = document.location.protocol + "//connect.facebook.net/en_US/all.js";
-//					document.getElementById("fb-root").appendChild(e);
-//				}());
-//				';
-//		return $str;
-//	}
+	function renderJavascript () {
+		
+		$jsData = json_encode($this->args['values']['js']);
+		
+		$str = "
+			// create a namespace; give a hoot.
+			var smsFunctions = {
+				data: $jsData
+			};
+			
+			smsFunctions.showData = function() {
+				var sf = smsFunctions;
+				
+				// get currently selected option
+				var selected = jQuery( '#newcustomer_shortcodegroup' ).val();
+				
+				// find the matching codeGroup data via selected value
+				var htmlData =  '<div>';
+				
+				// create header line
+				htmlData += '<div>' + 
+								'<strong>vendor</strong> - <strong>shortcode</strong>' + 
+							'</div>';
+				
+				jQuery.each( sf.data, function( key, data ) {
+					if( selected === data.shortcodeid ) {
+						htmlData += '<div>' + 
+										data.smsaggregatorname + 
+										' - ' + 
+										data.shortcode + 
+									'</div>';
+					}
+				});
+				htmlData += '</div>'
 
+				jQuery( '#smsShortCodeData' ).html( htmlData );
+			};
+			
+			smsFunctions.showData();
+		";		
+		
+		return $str;
+	}
 }
-
 ?>
