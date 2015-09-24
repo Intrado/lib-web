54ec6f02998eb3d108da661eaeacce7ee66001a2
Sean Kelly <skelly@schoolmessenger.com>
2015-09-24 12:37:55 -0700

    Moves API URL setting to external configuration file UI-138 UI-139

---
+++ quicktip/quicktipsettings.ini.example.php
@@ -0,0 +1,8 @@
+;<?/* this prevents browsers from seeing anything
+
+; API(s)
+[api]
+; The %s is a customer URL component which must be substituted in
+csCustomer="https://HOSTNAME/%s/api/2/"
+
+;*/?>
