f9989cf28abcea3546086703f9bf4b467c72223c
chanhannigan <chanhannigan@gmail.com>
2015-10-01 16:37:08 -0700

    adds error checking to shortcode refresh cURL requests UI-42 UI-167

--- manager/obj/SMSAggregatorData.php
+++ manager/obj/SMSAggregatorData.php
@@ -8,11 +8,12 @@ class SMSAggregatorData {
 	public $shortcodeData;
 	public $shortcodeGroups;
 	
-
 	public function __construct($customerId) {
+		
 		$this->currentShortcodeGroupId = $this->getCurrentShortcodeGroupId($customerId);
 		$this->shortcodeData = $this->fetchRequiredData();
 		$this->shortcodeGroups = $this->getAllShortCodeGroups(); 
+		
 	}
 
 	// gets the current short code group id of the customer
@@ -51,6 +52,85 @@ class SMSAggregatorData {
 		$query = "update customer set shortcodegroupid=$selection where id = $customerId";
 		
 		QuickQuery($query);
+	}
+	
+	public function jmxUpdateShortcodeGroups () {
+		global $SETTINGS;
+		
+		// init curl with the url we hit with a GET request
+		$username = $SETTINGS['jmx']['username'];
+		$password = $SETTINGS['jmx']['password'];
+		$domains = $SETTINGS['jmx']['domain'];
+		
+		// one result per cURL request, one cURL request per domain.
+		$errors = array();
+		
+		foreach($domains as $domain) {
+			
+			$curlResult = $this->executeCurlRequest($username, $password, $domain);
+			
+			// if the result is not false there must be an error
+			if($curlResult) {
+				$errors[] = $curlResult;
+			}
+		}
+		
+		return $errors;
+	}
+	
+	private function executeCurlRequest($username, $password, $domain) {
+		$ch = curl_init(
+			"http://$username:$password@$domain/jolokia/exec/commsuite:type=service,name=smsServiceManager/refreshShortcodeGroups()/"
+		);
+
+		// returned data will be a string
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
+
+		// run it, get result. 
+		$result = curl_exec($ch);
+
+		// if cURL could not execute
+		if (! result) {
+			$errorString = "cURL request  <b style='color:darkred'>FAILED</b> attempting to update shortcode groups for: (<b>$domain)</b>) --stack trace: " . curl_error($ch);
+			
+			error_log($errorString);
+			
+			return $errorString;
+		} 
+		
+		$resultArray = $this->parseJMSShortcodeResponse($result);
+		
+		if(isset($resultArray['error'])) {
+			 $errorString =  "Shortcode groups <b style='color:darkred'>FAILED</b> to refresh for: (<b>$domain</b>) -- stack trace: {$resultArray['error']}";
+			 
+			 error_log($errorString);
+			 
+			 return $errorString;
+		}
+		
+		// no problems!
+		return false;
+		
+	}
+	
+	// parse response JSON string into an array
+	// @param: String $response
+	// @returns: Array ('status' => '...', 'error' => '...')
+	private function parseJMSShortcodeResponse ($response) {
+		
+		$jsonResponse = json_decode ($response);
+		
+		// status should always exist in response
+		$returnArray = array(
+			'status' => $jsonResponse->status
+		);
+		
+		// and there might be an error if not status 200
+		if(isset($jsonResponse->error)) {
+			$returnArray['error'] = $jsonResponse->error;
+		}
+		
+		return $returnArray;
 	}
 	
 	// gets all the available short code group id and description columns
