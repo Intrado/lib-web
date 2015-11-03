08ad23610a343dac8671b77c2311977a29f1f8d3
Eugenio Lattanzio <eugenio63@gmail.com>
2015-11-03 16:23:46 -0300

    TS-61 Notification response dates are not properly formatted

--- notificationReport.php
+++ notificationReport.php
@@ -26,85 +26,85 @@ require_once("obj/JobType.obj.php");
 require_once("obj/Person.obj.php");
 require_once("inc/rulesutils.inc.php");
 
-function fmt_dst_src($row, $index){
-	if($row[$index] != null){
-		$type = $row[5];
-		$maxtypes = fetch_max_types();
-		$actualsequence = isset($maxtypes[$type]) ? ($row[$index] % $maxtypes[$type]) : $row[$index];
-		return escapehtml(destination_label($row[5], $actualsequence));
-	}
-	else
-		return "";
+function fmt_dst_src($row, $index)
+{
+    if ($row[$index] != null) {
+        $type = $row[5];
+        $maxtypes = fetch_max_types();
+        $actualsequence = isset($maxtypes[$type]) ? ($row[$index] % $maxtypes[$type]) : $row[$index];
+        return escapehtml(destination_label($row[5], $actualsequence));
+    } else
+        return "";
 }
 
 if (!isset($_REQUEST["api"])) {
-	header("HTTP/1.1 404 Not Found");
-	exit(json_encode(Array("code" => "resourceNotFound")));
+    header("HTTP/1.1 404 Not Found");
+    exit(json_encode(Array("code" => "resourceNotFound")));
 }
 
 if (!$USER->authorize('createreport') && !$USER->authorize('viewsystemreports')) {
-	header("HTTP/1.1 403 Forbidden");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "accessDenied")));
+    header("HTTP/1.1 403 Forbidden");
+    header('Content-Type: application/json');
+    exit(json_encode(Array("code" => "accessDenied")));
 }
 
 // viewsystemreports
 if (!$USER->authorize('viewsystemreports')) {
-	header("HTTP/1.1 403 Forbidden");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "accessDenied")));
+    header("HTTP/1.1 403 Forbidden");
+    header('Content-Type: application/json');
+    exit(json_encode(Array("code" => "accessDenied")));
 }
 
 if (!isset($_GET['jobid'])) {
-	header("HTTP/1.1 404 Not Found");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "resourceNotFound")));
+    header("HTTP/1.1 404 Not Found");
+    header('Content-Type: application/json');
+    exit(json_encode(Array("code" => "resourceNotFound")));
 }
 
 $jobid = $_GET['jobid'];
 
 if (!isset($_GET['reporttype'])) {
-	header("HTTP/1.1 404 Not Found");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "resourceNotFound")));
+    header("HTTP/1.1 404 Not Found");
+    header('Content-Type: application/json');
+    exit(json_encode(Array("code" => "resourceNotFound")));
 }
 
 $reporttype = $_GET['reporttype'];
 
 switch ($reporttype) {
-case "email":
-	$reportspec = "emaildetail";
-	break;
-case "phone":
-	$reportspec = "phonedetail";
-	break;
-case "sms":
-	$reportspec = "smsdetail";
-	break;
-case "device":
-	$reportspec = "devicedetail";
-	break;
-default:
-	header("HTTP/1.1 404 Not Found");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "resourceNotFound")));
+    case "email":
+        $reportspec = "emaildetail";
+        break;
+    case "phone":
+        $reportspec = "phonedetail";
+        break;
+    case "sms":
+        $reportspec = "smsdetail";
+        break;
+    case "device":
+        $reportspec = "devicedetail";
+        break;
+    default:
+        header("HTTP/1.1 404 Not Found");
+        header('Content-Type: application/json');
+        exit(json_encode(Array("code" => "resourceNotFound")));
 }
 
 // Check userowns
 if (!userOwns("job", $jobid)) {
-	header("HTTP/1.1 404 Not Found");
-	header('Content-Type: application/json');
-	exit(json_encode(Array("code" => "notificationNotFound")));
+    header("HTTP/1.1 404 Not Found");
+    header('Content-Type: application/json');
+    exit(json_encode(Array("code" => "notificationNotFound")));
 }
 
 $job = new Job($jobid);
 
 $options = Array(
-	"jobid" => $jobid,
-	"reporttype" => $reportspec,
-	"order1" => 'rp.pkey',
-	"pagestart" => 0,
-	"activefields" => null
+    "jobid" => $jobid,
+    "reporttype" => $reportspec,
+    "order1" => 'rp.pkey',
+    "pagestart" => 0,
+    "activefields" => null
 );
 
 $instance = new ReportInstance();
@@ -116,6 +116,12 @@ $reportgenerator->generateQuery();
 $report = $reportgenerator->getData();
 
 header('Content-Type: application/json');
+
+if (isset($_REQUEST["api"])) {
+    foreach ($report as &$row) {
+        $row[8] = $row[8] ? date(DATE_ISO8601, strtotime($row[8])) : null;
+    }
+}
+
 exit(json_encode($report));
 
-?>
