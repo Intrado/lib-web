cfa01faccb840dfdc6658e4cb31ad12d107a571e
Bill Karwin <bill@karwin.com>
2016-01-07 17:10:57 -0800

    Build Kona. CS-8207 CS-8933

--- build.sh
+++ build.sh
@@ -1,11 +1,26 @@
 #!/bin/bash
+# build.sh - every project has a thin bash script that can run a build,
+# and produce a zip archive to be deployed.
+# The first argument is the directory into which to deliver the zip.
 
 dist_dir=${1:-/tmp/upgrade}
-date=`date +%F`
+date=${2:-`date +%F_%T`}
 
 mkdir -p $dist_dir
 
-[ -L /usr/commsuite/www ] && exit 0
+if [ -L /usr/commsuite/www ]
+then
+	exit 0
+fi
 
-ant dist || exit $?
-mv dist/kona-$date.zip $dist_dir/ || exit $?
+ant dist
+if [ $? -ne 0 ]; then
+       echo "ERROR: Build Failed!"
+       exit 1
+fi
+
+mv dist/kona-*.zip $dist_dir/kona-$date.zip
+if [ $? -ne 0 ]; then
+       echo "ERROR: Moving the ZIP failed!"
+       exit 1
+fi
