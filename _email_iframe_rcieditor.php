3e4601612fe5062155691c511d34b8608a20de46
Justin Burns <jburns@schoolmessenger.com>
2016-01-12 09:30:20 -0800

    adds new iframe page for use with email translation items (editing with inline rcieditor) : UI-665

---
+++ _email_iframe_rcieditor.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<style type="text/css">
+		.cke_contents {height: 280px !important;}
+	    #translations_container iframe {height: 550px;}
+	    #email_ckeditor_override_iframe {border: 0 !important;}
+	</style>
+	<script type="text/javascript" src="script/jquery-1.8.3.min.js"></script>
+	<script type="text/javascript" src="script/ckeditor/ckeditor.js"></script>
+	<script type="text/javascript" src="script/rcieditor.js"></script>
+</head>
+<body>
+	<div id="email_ckeditor_override-htmleditor" class="email_ckeditor_override_htmleditor-htmleditor"></div>
+    <textarea id="email_ckeditor_override" name="email_ckeditor_override"></textarea>
+    <div id="email_ckeditor_msg" class="validationLabel alert alert-error span4 er pull-left margL15" style="display:none;" data-field-error="body"></div>
+</body>
+</html>
