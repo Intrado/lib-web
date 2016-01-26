368704851ee37fb56e5c11d1c655d333cb8c736d
sshyh <shawn.shyh@gmail.com>
2016-01-26 11:47:26 -0800

    Add schema changes for GDR.

--- build/dbupgrade_globaldestinationregistry/db_1-0_pre.sql
+++ build/dbupgrade_globaldestinationregistry/db_1-0_pre.sql
@@ -1,7 +1,6 @@
 -- $rev 1
 
-CREATE TABLE endpoint
-(
+CREATE TABLE endpoint (
     id            INT(11) PRIMARY KEY AUTO_INCREMENT,
     destination   VARCHAR(128) NOT NULL UNIQUE,
     type          ENUM('PHONE', 'EMAIL', 'PUSH') NOT NULL,
@@ -13,8 +12,7 @@ CREATE TABLE endpoint
 ) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
 
-CREATE TABLE consent
-(
+CREATE TABLE consent (
     endpoint_id INT(11) PRIMARY KEY,
     sms         ENUM('PENDING', 'YES', 'NO') NOT NULL DEFAULT 'PENDING',
     `call`      ENUM('PENDING', 'YES', 'NO') NOT NULL DEFAULT 'PENDING',
@@ -23,8 +21,7 @@ CREATE TABLE consent
 ) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
 
-CREATE TABLE block
-(
+CREATE TABLE block (
    endpoint_id INT(11) PRIMARY KEY,
    sms         BOOLEAN NOT NULL DEFAULT FALSE,
    `call`      BOOLEAN NOT NULL DEFAULT FALSE,
