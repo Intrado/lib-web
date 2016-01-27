a6324eb2bbc7246df400965e7246b26b86a30a42
sshyh <shawn.shyh@gmail.com>
2016-01-27 10:59:39 -0800

    Add schema changes for GDR.

--- build/dbupgrade_globaldestinationregistry/db_1-0_pre.sql
+++ build/dbupgrade_globaldestinationregistry/db_1-0_pre.sql
@@ -8,34 +8,36 @@ CREATE TABLE endpoint (
     created_date  DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     modified_date DATETIME NOT NULL ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     deleted       BOOLEAN NOT NULL DEFAULT FALSE,
-    UNIQUE INDEX destination_UK (destination)
+    UNIQUE INDEX destination_UK (destination),
+    INDEX created_IK (created_date)
 ) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
 
 CREATE TABLE consent (
     endpoint_id INT(11) PRIMARY KEY,
     sms         ENUM('PENDING', 'YES', 'NO') NOT NULL DEFAULT 'PENDING',
-    phone_call      ENUM('PENDING', 'YES', 'NO') NOT NULL DEFAULT 'PENDING',
+    phone_call  ENUM('PENDING', 'YES', 'NO') NOT NULL DEFAULT 'PENDING',
     FOREIGN KEY endpoint_FK (endpoint_id)
     REFERENCES endpoint(id)
 ) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
 
 CREATE TABLE block (
-   endpoint_id INT(11) PRIMARY KEY,
-   sms         BOOLEAN NOT NULL DEFAULT FALSE,
-   phone_call      BOOLEAN NOT NULL DEFAULT FALSE,
-   FOREIGN KEY endpoint_FK (endpoint_id)
-   REFERENCES endpoint(id)
+    endpoint_id INT(11) PRIMARY KEY,
+    sms         BOOLEAN NOT NULL DEFAULT FALSE,
+    phone_call  BOOLEAN NOT NULL DEFAULT FALSE,
+    FOREIGN KEY endpoint_FK (endpoint_id)
+    REFERENCES endpoint(id)
 ) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
 
-CREATE TABLE endpoint_event
-(
-  id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
-  destination VARCHAR(50) NOT NULL,
-  action ENUM ('CREATE', 'DELETE', 'BLOCK', 'UNBLOCK', 'OPT_IN', 'OPT_OUT') NOT NULL,
-  details  VARCHAR(255) NOT NULL,
-  created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-)ENGINE = INNODB DEFAULT CHARSET=utf8
+CREATE TABLE endpoint_event (
+    id           INT(11) PRIMARY KEY AUTO_INCREMENT,
+    destination  VARCHAR(128) NOT NULL,
+    action       ENUM ('CREATE', 'DELETE', 'BLOCK', 'UNBLOCK', 'OPT_IN', 'OPT_OUT') NOT NULL,
+    details      VARCHAR(8192),
+    created_date DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    INDEX destination_IX (destination),
+    INDEX created_IK (created_date)
+) ENGINE = INNODB DEFAULT CHARSET=utf8
 $$$
