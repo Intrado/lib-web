5817a8564f981c1cd325127e1c73960915e94a21
Walter Rhodes <rrhodes@schoolmessenger.com>
2016-04-11 16:01:10 -0500

    CS-8965  CS-9706  SparkPost - added emailevent

--- build/dbupgrade_aspshard/db_11-8.php
+++ build/dbupgrade_aspshard/db_11-8.php
@@ -5,8 +5,11 @@ function aspshard_upgrade_11_8($rev, $db) {
 	    case "0":
 		//no op
 	    case "1":
-		echo "|";
-		apply_sql_db("dbupgrade_aspshard/db_11-8_pre.sql", $db, 2);	   
+			echo "|";
+			apply_sql_db("dbupgrade_aspshard/db_11-8_pre.sql", $db, 2);
+		case "2":
+			echo "|";
+			apply_sql_db("dbupgrade_aspshard/db_11-8_pre.sql", $db, 3);
 	}
 
 	return true;
--- build/dbupgrade_aspshard/db_11-8_pre.sql
+++ build/dbupgrade_aspshard/db_11-8_pre.sql
@@ -8,6 +8,35 @@ CREATE TABLE `cloudContentSynchronization` (
     `lastCheckedContentId` bigint NOT NULL,
     `scanInitiatedTimeMs` bigint NOT NULL,
     PRIMARY KEY (`cloudStorageProviderName`,`customerId`)
-  ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  ) ENGINE=InnoDB DEFAULT CHARSET=utf8
 $$$
 
+-- $rev 3
+CREATE TABLE emailevent (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `timestampMs` bigint(20) NOT NULL,
+  `type` enum('message','track','generation','unsubscribe','relay') not null,
+  `subType` enum('bounce','delivery','injection','sms_status','spam_complaint','out_of_band','policy_rejection','delay','click','open','failure','rejection','link','list','tempFail','permFail') not null,
+  `jobId` int(11) DEFAULT NULL,
+  `personId` int(11) DEFAULT NULL,
+  `userId` int(11) DEFAULT NULL,
+  `sequence` smallint(6) DEFAULT NULL,
+  `source` enum('customer_job','contact_manager','password_reset','report_subscription','subscriber_expiration','cm_password_reset','job_monitor','internal_monitoring') DEFAULT NULL,
+  `messageId` varchar(64) DEFAULT NULL,
+  `vendorEventId` varchar(32) DEFAULT NULL,
+  `fromName` varchar(100) NOT NULL,
+  `fromDomain` varchar(100) NOT NULL,
+  `toName` varchar(100) NOT NULL,
+  `toDomain` varchar(100) NOT NULL,
+  `statusCode` smallint(4) unsigned NOT NULL,
+  `responseText` text NOT NULL,
+  `rawResponseText` text NOT NULL,
+  `numRetries` int DEFAULT NULL,
+  `queueTime` int DEFAULT NULL,
+  `vendorBounceCode` varchar(64) DEFAULT NULL,
+  `feedBackType` enum('abuse','fraud','other','virus') DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `timestamp_statusCode` (`timeStampMs`,`statusCode`),
+  KEY `statusCode_timestamp` (`statusCode`,`timeStampMs`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8
+$$$
