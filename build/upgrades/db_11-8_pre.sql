9ab5e204f2a964529510db48e82322ba22365de9
Bill Karwin <bill@karwin.com>
2016-04-14 14:28:57 -0700

    Create global smslanguage table and customer smslanguage view. UI-1587 UI-1612

--- build/upgrades/db_11-8_pre.sql
+++ build/upgrades/db_11-8_pre.sql
@@ -1,2 +1,9 @@
 -- $rev 1
 -- no-op
+
+-- $rev 2
+
+-- NOTE: the base table in aspshard must be created before this view can reference it
+CREATE VIEW smslanguage AS
+  SELECT l.* FROM language AS l INNER JOIN aspshard.smslanguage AS sl USING (code)
+$$$
